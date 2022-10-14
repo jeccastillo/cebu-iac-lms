fa8f4557ff3de7e064ddd85aae4a59acd7dbfd2b
System Administrator <root@agdeasis-M1.local>
2022-10-15 00:10:14 +0800

    added control for addmissions v2

---
+++ application/modules/admissionsv2/views/studentApplication.php
@@ -0,0 +1,87 @@
+<div class="container py-4" style="margin-top:100px;">
+    <h4 class="font-bold"><strong>Student Information Sheet</strong></h4>
+    <p>Hello future Game Changers! Kindly fill out your information sheet. If you have any questions, feel free to email
+        us at <strong><u>admissions@iacademy.edu.ph</u></strong> </p>
+    <hr>
+
+    <form class="row">
+        <div class="col-md-6">
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" required>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputEmail2">Re-type Email</label>
+                    <input type="email" class="form-control" id="exampleInputEmail2" required>
+                    <small>( You won't receive our Admissions Letter if this is incorrect )</small>
+                </div>
+                <div class="form-group">
+                    <label>First Name</label>
+                    <input type="text" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label>Middle Name</label>
+                    <input type="text" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label>Last Name</label>
+                    <input type="text" class="form-control" required>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="col-md-6">
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Mobile Number</label>
+                    <input type="number" class="form-control" id="" required>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Telephone Number</label>
+                    <input type="number" class="form-control" id="">
+                    <span>&nbsp</span>
+                </div>
+                <div class="form-group">
+                    <label>Address</label>
+                    <input type="text" class="form-control" id="">
+                </div>
+                <div class="form-group">
+                    <label>Birthday</label>
+                    <input type="date" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label>Previous School</label>
+                    <input type="text" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label>Applying Position</label>
+                    <div class="checkbox">
+                        <label>
+                            <input type="radio" name="course"> UG- Freshman
+                        </label>
+                    </div>
+                    <div class="checkbox">
+                        <label>
+                            <input type="radio" name="course"> UG- Transferee
+                        </label>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="col-sm-12">
+            <hr />
+            <div class=" text-right">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+
+    </form>
+</div>
+
+</div>
+</div>
