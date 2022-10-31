071007d99c4adc530fdda65db75b098bca9840b4
System Administrator <root@agdeasis-M1.local>
2022-11-01 01:19:16 +0800

    update student applicant details

--- application/modules/admissionsv1/views/admin/view_lead.php
+++ application/modules/admissionsv1/views/admin/view_lead.php
@@ -10,119 +10,317 @@
         </ol>
     </section>
     <div class="content  container">
-        <form class="box box-primary">
-            <div class="box-header with-border font-weight-bold" style="text-align:left; font-weight:bold">
-                <h3 class="box-title text-left">Juan Dela Cruz - Details </h3>
+        <form action="">
+            <div class="box ">
+                <div class="box-header with-border font-weight-bold" style="text-align:left; font-weight:bold">
+                    <h3 class="box-title text-left">Juan Dela Cruz - Details </h3>
+                </div>
+
+                <div class="box-body" style="padding:2rem">
+                    <div>
+                        <strong><i class="fa fa-sitemap margin-r-5"></i> Current Status</strong>
+                        <p>
+                            <span class="label label-danger">New</span>
+                            <span class="label label-primary">For Interview</span>
+                            <span class="label label-warning">Scheduled</span>
+                            <span class="label label-info">For Reservation</span>
+                            <span class="label label-success">Reserved</span>
+                            <span class="label label-danger">Rejected</span>
+                        </p>
+                        <hr>
+                    </div>
+
+                    <div>
+                        <strong><i class="fa fa-book margin-r-5"></i> Name</strong>
+                        <p class="text-muted">
+                            Juan G. Dela Cruz
+                        </p>
+                        <hr>
+                    </div>
+
+                    <div>
+                        <strong><i class="fa fa-envelope margin-r-5"></i> Email</strong>
+                        <p class="text-muted">
+                            juandelacruz@gmail.com
+                        </p>
+                        <hr>
+                    </div>
+
+                    <div>
+                        <strong><i class="fa fa-phone margin-r-5"></i> Mobile Number</strong>
+                        <p class="text-muted">
+                            091278212545
+                        </p>
+                        <hr>
+                    </div>
+
+                    <div>
+                        <strong><i class="fa fa-phone-square margin-r-5"></i> Telephone Number</strong>
+                        <p class="text-muted">
+                            +18 091278212545
+                        </p>
+                        <hr>
+                    </div>
+
+                    <div>
+                        <strong><i class="fa fa-location-arrow margin-r-5"></i> Address</strong>
+                        <p class="text-muted">
+                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                        </p>
+                        <hr>
+                    </div>
+
+                    <div>
+                        <strong><i class="fa fa-calendar margin-r-5"></i> Birthday</strong>
+                        <p class="text-muted">
+                            May 01, 1998
+                        </p>
+                        <hr>
+                    </div>
+
+                    <div>
+                        <strong><i class="fa fa-home margin-r-5"></i> Previous School</strong>
+                        <p class="text-muted">
+                            Lorem Ipsum State University
+                        </p>
+                        <hr>
+                    </div>
+
+                    <div>
+                        <strong><i class="fa fa-user margin-r-5"></i>Student Type</strong>
+                        <p class="text-muted">
+                            Under Graduate - Transferee
+                        </p>
+                        <hr>
+                    </div>
+
+                    <div>
+                        <strong><i class="fa fa-bookmark margin-r-5"></i>Desired Program</strong>
+                        <p class="text-muted">
+                            MMA - Animation
+                        </p>
+                        <hr>
+                    </div>
+
+                    <div>
+                        <strong><i class="fa fa-sitemap margin-r-5"></i>Update Status</strong>
+                        <div class="row">
+                            <div class="text-muted mt-1 col-sm-5">
+                                <select name="" class="form-control" required id="">
+                                    <option value="" disabled selected>--select--</option>
+                                    <option value="new">New</option>
+                                    <option value="for_interview">For Interview</option>
+                                    <option value="scheduled">Scheduled</option>
+                                    <option value="For Reservation">For Reservation</option>
+                                    <option value="rejected">Rejected</option>
+                                </select>
+                            </div>
+                        </div>
+                        <hr>
+                    </div>
+
+                    <div class="text-right">
+                        <button type="submit" class="btn btn-primary">Update</button>
+                    </div>
+
+                </div>
+
             </div>
 
-            <div class="box-body" style="padding:2rem">
-                <div>
-                    <strong><i class="fa fa-sitemap margin-r-5"></i> Current Status</strong>
-                    <p>
-                        <span class="label label-danger">New</span>
-                        <span class="label label-success">Reserved</span>
-                        <span class="label label-info">For Reservation</span>
-                        <span class="label label-warning">Scheduled</span>
-                        <span class="label label-danger">Rejected</span>
-                    </p>
-                    <hr>
-                </div>
 
-                <div>
-                    <strong><i class="fa fa-book margin-r-5"></i> Name</strong>
-                    <p class="text-muted">
-                        Juan G. Dela Cruz
-                    </p>
-                    <hr>
-                </div>
+            <!-- for status Update -->
+            <div class="row">
 
-                <div>
-                    <strong><i class="fa fa-envelope margin-r-5"></i> Email</strong>
-                    <p class="text-muted">
-                        juandelacruz@gmail.com
-                    </p>
-                    <hr>
-                </div>
+                <!-- for interview -->
+                <div class="col-lg-4">
+                    <div class="box box-primary">
+                        <div class="box-header with-border  font-weight-bold" style="text-align:left; font-weight:bold">
+                            <h3 class="box-title text-left text-primary">For Interview</h3>
+                        </div>
 
-                <div>
-                    <strong><i class="fa fa-phone margin-r-5"></i> Mobile Number</strong>
-                    <p class="text-muted">
-                        091278212545
-                    </p>
-                    <hr>
-                </div>
+                        <div class="box-body" style="padding:2rem">
+                            <div>
+                                <strong><i class="fa fa-calendar margin-r-5"></i>Schedule</strong>
+                                <p class="text-muted">
+                                    December 10, 2022 10:-00 AM
+                                </p>
+                                <hr>
+                            </div>
 
-                <div>
-                    <strong><i class="fa fa-phone-square margin-r-5"></i> Telephone Number</strong>
-                    <p class="text-muted">
-                        +18 091278212545
-                    </p>
-                    <hr>
-                </div>
+                            <div>
+                                <strong><i class="fa fa-user margin-r-5"></i>AO Officer</strong>
+                                <p class="text-muted">
+                                    Pedro Biglang Awa
+                                </p>
+                                <hr>
+                            </div>
 
-                <div>
-                    <strong><i class="fa fa-location-arrow margin-r-5"></i> Address</strong>
-                    <p class="text-muted">
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                    </p>
-                    <hr>
-                </div>
+                            <div>
+                                <strong><i class="fa  fa-file margin-r-5"></i>Remarks</strong>
+                                <p class="text-muted">
+                                    Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed
+                                    quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+                                </p>
+                                <hr>
+                            </div>
 
-                <div>
-                    <strong><i class="fa fa-calendar margin-r-5"></i> Birthday</strong>
-                    <p class="text-muted">
-                        May 01, 1998
-                    </p>
-                    <hr>
-                </div>
-
-                <div>
-                    <strong><i class="fa fa-home margin-r-5"></i> Previous School</strong>
-                    <p class="text-muted">
-                        Lorem Ipsum State University
-                    </p>
-                    <hr>
-                </div>
-
-                <div>
-                    <strong><i class="fa fa-user margin-r-5"></i>Student Type</strong>
-                    <p class="text-muted">
-                        Under Graduate - Transferee
-                    </p>
-                    <hr>
-                </div>
-
-                <div>
-                    <strong><i class="fa fa-bookmark margin-r-5"></i>Desired Program</strong>
-                    <p class="text-muted">
-                        MMA - Animation
-                    </p>
-                    <hr>
-                </div>
-
-                <div>
-                    <strong><i class="fa fa-sitemap margin-r-5"></i>Update Status</strong>
-                    <div class="row">
-                        <div class="text-muted mt-1 col-sm-5">
-                            <select name="" class="form-control" required id="">
-                                <option value="" disabled selected>--select--</option>
-                                <option value="new">New</option>
-                                <option value="for_interview">For Interview</option>
-                                <option value="scheduled">Scheduled</option>
-                                <option value="For Reservation">For Reservation</option>
-                                <option value="rejected">Rejected</option>
-                            </select>
                         </div>
                     </div>
-                    <hr>
                 </div>
+                <!-- end -->
 
-                <div class="text-right">
-                    <button type="submit" class="btn btn-primary">Update</button>
+                <!-- for scheduled -->
+                <div class="col-lg-4">
+                    <div class="box box-warning">
+                        <div class="box-header with-border font-weight-bold" style="text-align:left; font-weight:bold">
+                            <h3 class="box-title text-left text-warning">Scheduled</h3>
+                        </div>
+
+                        <div class="box-body" style="padding:2rem">
+                            <div>
+                                <strong><i class="fa fa-calendar margin-r-5"></i>Date</strong>
+                                <p class="text-muted">
+                                    December 10, 2022 10:-00 AM
+                                </p>
+                                <hr>
+                            </div>
+
+                            <div>
+                                <strong><i class="fa fa-user margin-r-5"></i>AO Officer</strong>
+                                <p class="text-muted">
+                                    Pedro Biglang Awa
+                                </p>
+                                <hr>
+                            </div>
+
+                            <div>
+                                <strong><i class="fa  fa-file margin-r-5"></i>Remarks</strong>
+                                <p class="text-muted">
+                                    Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed
+                                    quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+                                </p>
+                                <hr>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
+                <!-- end -->
+
+                <!-- for reservation -->
+                <div class="col-lg-4">
+                    <div class="box box-info">
+                        <div class="box-header with-border font-weight-bold" style="text-align:left; font-weight:bold">
+                            <h3 class="box-title text-left text-info">For Reservation</h3>
+                        </div>
+
+                        <div class="box-body" style="padding:2rem">
+                            <div>
+                                <strong><i class="fa fa-money margin-r-5"></i>Reservation Fee</strong>
+                                <p class="text-muted">
+                                    ₱700
+                                </p>
+                                <hr>
+                            </div>
+
+                            <div>
+                                <strong><i class="fa fa-user margin-r-5"></i>AO Officer</strong>
+                                <p class="text-muted">
+                                    Pedro Biglang Awa
+                                </p>
+                                <hr>
+                            </div>
+
+                            <div>
+                                <strong><i class="fa  fa-file margin-r-5"></i>Remarks</strong>
+                                <p class="text-muted">
+                                    Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed
+                                    quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+                                </p>
+                                <hr>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <!-- end -->
+
+                <!-- reserved -->
+                <div class="col-lg-4">
+                    <div class="box box-success">
+                        <div class="box-header with-border font-weight-bold" style="text-align:left; font-weight:bold">
+                            <h3 class="box-title text-left text-success">Reserved</h3>
+                        </div>
+
+                        <div class="box-body" style="padding:2rem">
+                            <div>
+                                <strong><i class="fa fa-calendar margin-r-5"></i>Date Reserved</strong>
+                                <p class="text-muted">
+                                    December 15, 2022 10:-00 AM
+                                </p>
+                                <hr>
+                            </div>
+
+                            <div>
+                                <strong><i class="fa fa-user margin-r-5"></i>AO Officer</strong>
+                                <p class="text-muted">
+                                    Pedro Biglang Awa
+                                </p>
+                                <hr>
+                            </div>
+
+                            <div>
+                                <strong><i class="fa  fa-file margin-r-5"></i>Remarks</strong>
+                                <p class="text-muted">
+                                    Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed
+                                    quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+                                </p>
+                                <hr>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end -->
+
+                <!-- rejected -->
+                <div class="col-lg-4">
+                    <div class="box box-danger">
+                        <div class="box-header with-border font-weight-bold" style="text-align:left; font-weight:bold">
+                            <h3 class="box-title text-left text-danger">Rejected</h3>
+                        </div>
+
+                        <div class="box-body" style="padding:2rem">
+                            <div>
+                                <strong><i class="fa fa-calendar margin-r-5"></i>Date Reserved</strong>
+                                <p class="text-muted">
+                                    December 15, 2022 10:-00 AM
+                                </p>
+                                <hr>
+                            </div>
+
+                            <div>
+                                <strong><i class="fa fa-user margin-r-5"></i>AO Officer</strong>
+                                <p class="text-muted">
+                                    Pedro Biglang Awa
+                                </p>
+                                <hr>
+                            </div>
+
+                            <div>
+                                <strong><i class="fa  fa-file margin-r-5"></i>Remarks</strong>
+                                <p class="text-muted">
+                                    Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed
+                                    quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+                                </p>
+                                <hr>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end -->
 
             </div>
-
+            <!-- end -->
         </form>
     </div>
 </div>
