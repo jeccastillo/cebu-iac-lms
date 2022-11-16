bed1063756ddef1577c6e786b2291662591f63e4
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2022-11-16 16:44:14 +0800

    Update AdmissionsV1.php

--- application/modules/admissionsV1/controllers/AdmissionsV1.php
+++ application/modules/admissionsV1/controllers/AdmissionsV1.php
@@ -75,7 +75,7 @@ class AdmissionsV1 extends CI_Controller {
             $data['success'] = true;
             $post = $this->input->post();
             $data['data'] = $post;  
-            print_r($post);          
+            echo $post['strFirstname'];          
             //$this->data_poster->post_data('tb_mas_users',$post);
 
         }
