11fe3262b7ca2ff5ab11b4b89c5a06ad9b2110fd
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2022-11-16 15:53:29 +0800

    Update student_application.php

--- application/modules/site/views/student_application.php
+++ application/modules/site/views/student_application.php
@@ -449,7 +449,7 @@ new Vue({
                             text: data.data.message,
                             icon: "success"
                         }).then(function() {
-                            location.reload();
+                            //location.reload();
                         });
 
                     } else {
