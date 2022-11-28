5da682a28257ae047fcae37b0ce9c2e559c05d95
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2022-11-28 09:13:16 +0800

    Update Tuitionyear.php

--- application/modules/tuitionyear/controllers/Tuitionyear.php
+++ application/modules/tuitionyear/controllers/Tuitionyear.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Admissions extends CI_Controller {
+class Tuitionyear extends CI_Controller {
 
     public $validation_config = [];
     
