61a38770cadb68fff8976bce4ff762b1ceaf30a5
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2022-11-28 22:19:47 +0800

    Update Data_fetcher.php

--- application/models/Data_fetcher.php
+++ application/models/Data_fetcher.php
@@ -1949,7 +1949,7 @@ class Data_fetcher extends CI_Model {
         ->first_row('array');
 
         if($res)
-            return $res
+            return $res;
         else
             return "C".$year.'-'.$term.'-000';
     }
@@ -1965,7 +1965,7 @@ class Data_fetcher extends CI_Model {
         ->first_row('array');
 
         if($res)
-            return $res
+            return $res;
         else
             return "T".$year.'-'.$term.'-000';
     }
