ca0bbddfd5802203a4a462ef1fc3e9398e681517
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2022-12-06 13:22:56 +0800

    Update tuitionyear.php

--- application/modules/tuitionyear/views/tuitionyear.php
+++ application/modules/tuitionyear/views/tuitionyear.php
@@ -58,7 +58,7 @@
                     </div>
                     </form>
                     <hr />
-                    <table class="table-bordered table-striped">
+                    <table class="table table-bordered table-striped">
                         <thead>
                             <tr>
                                 <th>Name</th>
