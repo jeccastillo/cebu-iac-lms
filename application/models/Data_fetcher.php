70626803df57a0d9df903c977f182c945b3f22da
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2022-12-07 12:59:11 +0800

    Update Data_fetcher.php

--- application/models/Data_fetcher.php
+++ application/models/Data_fetcher.php
@@ -1999,7 +1999,7 @@ class Data_fetcher extends CI_Model {
             
             $nsf = getExtraFee($nsf_data, $sem, 'misc');
 
-            $new_student_data = $this->db->where(array('tuitionYearID'=>$tuition_year['intID'], 'type' => 'nsf'))
+            $new_student_data = $this->db->where(array('tuitionYearID'=>$tuition_year['intID'], 'type' => 'new_student'))
                          ->get('tb_mas_tuition_year_misc')->result_array();
 
             foreach($new_student_data as $nsd){
