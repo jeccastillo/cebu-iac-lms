85815e14b41feda0547df61534e2b309011983c8
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2022-12-14 11:18:01 +0800

    Update print_view_student_reg2.php

--- application/modules/pdf/views/print_view_student_reg2.php
+++ application/modules/pdf/views/print_view_student_reg2.php
@@ -30,7 +30,7 @@
     // Set some content to print
 $html = '<table border="0" cellpadding="0" style="color:#333; font-size:10;">
         <tr>
-            <td width="231" align="center" style="text-align:center;"><img src= "https://i.ibb.co/XW1DRVT/iacademy-logo.png"  width="231" height="68"/></td>
+            <td width="231" align="center" style="text-align:center;"><img src= src= "'.$img_dir .'iacademy-logo.png"  width="231" height="68"/></td>
         </tr>
         <tr>            
             <td width="600" style="text-align: center; line-height:100%">             
