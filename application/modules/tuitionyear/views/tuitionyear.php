640b9d85b2c5951242ace14550c9b0600d1116b5
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2022-12-14 14:04:47 +0800

    Update tuitionyear.php

--- application/modules/tuitionyear/views/tuitionyear.php
+++ application/modules/tuitionyear/views/tuitionyear.php
@@ -40,7 +40,7 @@
                                 <label for="year">Price Per Unit Hybrid</label>
                                 <input type="number" name="pricePerUnitHybrid" required class="form-control" id="pricePerUnitHybrid" placeholder="Enter Price per unit" v-model='request.pricePerUnitHybrid'>
                             </div> 
-                            <div class="form-group col-sm-3">
+                            <div class="form-group col-sm-6">
                                 <label for="year">Percent Increase for installment</label>
                                 <input step="any" type="number" required class="form-control" placeholder="Enter Fee Amount" v-model='request.installmentIncrease'>
                             </div>                            
