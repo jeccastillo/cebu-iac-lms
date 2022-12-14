3b910d16f9e425028cfcbfdbade8b31c74c349d4
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2022-12-14 14:50:57 +0800

    Update print_view_student_reg2.php

--- application/modules/pdf/views/print_view_student_reg2.php
+++ application/modules/pdf/views/print_view_student_reg2.php
@@ -236,20 +236,20 @@ $html.= '<table border="0" cellpadding="0" style="color:#333; font-size:10;" wid
                             </tr>
                         </table>
                     </td>
-                    <td style="border:1px solid #333;" width="264px">                                
+                    <td width="200px">                                
                         <table style="color:#333; font-size:10; ">';
                     if($tuition['misc'] != 0){
                         foreach($tuition['misc_list'] as $key=>$val){
             
                             $html .='<tr>
-                                        <td width="120px">'.$key.'</td>
-                                        <td width="120px">'.number_format($val, 2, '.' ,',').'</td>
+                                        <td width="132px">'.$key.'</td>
+                                        <td width="132px">'.number_format($val, 2, '.' ,',').'</td>
                                     </tr';                
                         }
                     $html.=' 
                         <tr>
-                            <td width="120px">Total</td>
-                            <td width="120px" style="border-bottom: 1px solid #555;">'.$tuition['misc'].'</td>                
+                            <td width="132px">Total</td>
+                            <td width="132px" style="border-bottom: 1px solid #555;">'.$tuition['misc'].'</td>                
                         </tr>';
                     }
                     $html.='                        
