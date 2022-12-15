fcf1e229b8fd3207e2264bcdc8cb7cc87b1299e6
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2022-12-16 06:52:47 +0800

    Update register_old_student.php

--- application/modules/registrar/views/admin/register_old_student.php
+++ application/modules/registrar/views/admin/register_old_student.php
@@ -189,7 +189,7 @@ new Vue({
                     this.scholarships = data.data.data.scholarships;
                     this.term_type = data.data.data.term_type;
                     this.school_years = data.data.data.sy;
-                    this.strAcademicYear = this.school_years[0].intID;
+                    this.request.strAcademicYear = this.active_sem.intID;
                     //this.loader_spinner = false;
                 })
                 .catch((error) => {
