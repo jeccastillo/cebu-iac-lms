ad9ee0aeaa4fc4b1b5a673493d2487b4d1439acd
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2022-12-16 07:35:56 +0800

    Update register_old_student.php

--- application/modules/registrar/views/admin/register_old_student.php
+++ application/modules/registrar/views/admin/register_old_student.php
@@ -1,122 +1,119 @@
 <aside class="right-side" id="registration-container">
-<section class="content-header">
-                    <h1>
-                        Registrar
-                        <small></small>
-                    </h1>
-                    <ol class="breadcrumb">
-                        <li><a href="#"><i class="fa fa-dashboard"></i>Register</a></li>
-                        <li class="active">Register Student</li>
-                    </ol>
-                </section>
-<div class="content">
-    <div class="span10 box box-primary">
-        <div class="box-header">
-                <h3 class="box-title">Register Student</h3>
-                
-                
-        </div>
-       
-        <div class="box box-solid">
-            <div v-if="reg_status == 'For Registration'" class="box-body">                                                
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>Student Number</th>
-                            <th>Lastname</th>
-                            <th>Firstname</th>
-                            <th>Middlename</th>
-                            <th>Course</th>
-                            <th>Registration Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td><?php echo $student['strStudentNumber']; ?></td>
-                            <td><?php echo $student['strLastname']; ?></td>
-                            <td><?php echo $student['strFirstname']; ?></td>
-                            <td><?php echo $student['strMiddlename']; ?></td>
-                            <td><?php echo $student['strProgramCode']; ?></td>
-                            <td><?php echo $reg_status; ?></td>
-                        </tr>
-                    </tbody>
-                </table>
-                <hr />
-                <form id="validate-student" action="<?php echo base_url(); ?>registrar/submit_registration_old2" method="post" role="form" enctype="multipart/form-data">
-                
-                
-                <div style="border:1px solid #d2d2d2;">
-                <div class="col-sm-8" style="padding:1rem;background:#f2f2f2;">
-                <h3>Registration Details</h3>
-                
-                Set Academic Year to Register
-                <select class="form-control" v-model="request.strAcademicYear">
-                    <option v-for="sy in school_years" :value="sy.intID">{{sy.enumSem + ' ' + term_type + ' ' + sy.strYearStart + '-' + sy.strYearEnd}}</option>                            
-                </select>
-                    <hr />
-                <label for="enumRegistrationStatus">Academic Status</label>
-                    <select class="form-control" v-model="request.enumRegistrationStatus">                        
-                        <option value="regular">Regular</option>
-                        <option value="irregular">Irregular</option>
-                    </select>
-                        <br />
-                    <label for="enumScholarship">Scholarship Grant</label>
-                    <select class="form-control" v-model="request.enumScholarship">                        
-                        <option value="0">None</option>                                                
-                        <option v-for="scholarship in scholarships" :value="scholarship.intID">{{scholarship.name}}</option>
-                    </select>
-                        <br />
-                    <label for="enumStudentType">Student Type</label>
-                    <select class="form-control" v-model="request.enumStudentType">                        
-                         <option value="new">NEW</option>
-                         <option value="old">RETURNING</option>
-                         <option value="transferee">TRANSFEREE</option>
-                        <option value="cross">CROSS REGISTRANT</option>
-                    </select>
-                    <br />                    
+    <section class="content-header">
+        <h1>
+            Registrar
+            <small></small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i>Register</a></li>
+            <li class="active">Register Student</li>
+        </ol>
+    </section>
+    <div class="content">
+        <div class="span10 box box-primary">
+            <div class="box-header">
+                    <h3 class="box-title">Register Student</h3>
                     
-                    <hr />
                     
-                <div id="regular-option" class="row">
-                        <div class="col-sm-12" style="margin-bottom:1rem">
-                        <a href="#" @click.prevent.stop='loadSubjects' class="btn btn-default  btn-flat">Check Classlists Enlisted and Assess Fees <i class="fa fa-arrow-circle-down"></i></a>
-                            </div>
-                    </div>
+            </div>
+        
+            <div class="box box-solid">
+                <div v-if="reg_status == 'For Registration'" class="box-body">                                                
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Student Number</th>
+                                <th>Lastname</th>
+                                <th>Firstname</th>
+                                <th>Middlename</th>
+                                <th>Course</th>
+                                <th>Registration Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><?php echo $student['strStudentNumber']; ?></td>
+                                <td><?php echo $student['strLastname']; ?></td>
+                                <td><?php echo $student['strFirstname']; ?></td>
+                                <td><?php echo $student['strMiddlename']; ?></td>
+                                <td><?php echo $student['strProgramCode']; ?></td>
+                                <td><?php echo $reg_status; ?></td>
+                            </tr>
+                        </tbody>
+                    </table>
                     <hr />
-                    <div v-html="subjectList" id="subject-list">                        
-                    </div>
-                    </div>
-                    <div class="col-sm-4" style="padding:1rem;">
-                    
-                        <h3>Accounting</h3>
-                    <?php /*
-                    <div id="accounting">
-                        <div><p><strong>Tuition: </strong><span id="tuition-fee">0</span></p></div>
-                        <div><p><strong>Misc: </strong>
-                        <span id="misc-fee">
-                            <?php echo ($student['enumScholarship'] == "paying")?$misc_fee:'0'; ?>
-                        </span>
-                        </p></div>
-                        <hr />
-                        <div><p><strong>Total: </strong><span id="total-fee"></span></p></div>
-                    </div>
-                    <hr />
-                    */ ?>
-                    <div id="tuitionContainer">
-                    
-                    </div>
-                    <input type="submit" :disabled="reg_status != For Registration" value="Register" class="btn btn-default  btn-flat btn-block">
+                    <form id="validate-student" action="<?php echo base_url(); ?>registrar/submit_registration_old2" method="post" role="form" enctype="multipart/form-data">                
+                        <div style="border:1px solid #d2d2d2;">
+                            <div class="col-sm-8" style="padding:1rem;background:#f2f2f2;">
+                            <h3>Registration Details</h3>
+                            
+                            Set Academic Year to Register
+                            <select class="form-control" v-model="request.strAcademicYear">
+                                <option v-for="sy in school_years" :value="sy.intID">{{sy.enumSem + ' ' + term_type + ' ' + sy.strYearStart + '-' + sy.strYearEnd}}</option>                            
+                            </select>
+                                <hr />
+                            <label for="enumRegistrationStatus">Academic Status</label>
+                                <select class="form-control" v-model="request.enumRegistrationStatus">                        
+                                    <option value="regular">Regular</option>
+                                    <option value="irregular">Irregular</option>
+                                </select>
+                                    <br />
+                                <label for="enumScholarship">Scholarship Grant</label>
+                                <select class="form-control" v-model="request.enumScholarship">                        
+                                    <option value="0">None</option>                                                
+                                    <option v-for="scholarship in scholarships" :value="scholarship.intID">{{scholarship.name}}</option>
+                                </select>
+                                    <br />
+                                <label for="enumStudentType">Student Type</label>
+                                <select class="form-control" v-model="request.enumStudentType">                        
+                                    <option value="new">NEW</option>
+                                    <option value="old">RETURNING</option>
+                                    <option value="transferee">TRANSFEREE</option>
+                                    <option value="cross">CROSS REGISTRANT</option>
+                                </select>
+                                <br />                    
+                                
+                                <hr />
+                                
+                            <div id="regular-option" class="row">
+                                    <div class="col-sm-12" style="margin-bottom:1rem">
+                                    <a href="#" @click.prevent.stop='loadSubjects' class="btn btn-default  btn-flat">Check Classlists Enlisted and Assess Fees <i class="fa fa-arrow-circle-down"></i></a>
+                                        </div>
+                                </div>
+                                <hr />
+                                <div v-html="subjectList" id="subject-list">                        
+                                </div>
+                                </div>
+                                <div class="col-sm-4" style="padding:1rem;">
+                                
+                                    <h3>Accounting</h3>
+                                <?php /*
+                                <div id="accounting">
+                                    <div><p><strong>Tuition: </strong><span id="tuition-fee">0</span></p></div>
+                                    <div><p><strong>Misc: </strong>
+                                    <span id="misc-fee">
+                                        <?php echo ($student['enumScholarship'] == "paying")?$misc_fee:'0'; ?>
+                                    </span>
+                                    </p></div>
+                                    <hr />
+                                    <div><p><strong>Total: </strong><span id="total-fee"></span></p></div>
+                                </div>
+                                <hr />
+                                */ ?>
+                                <div id="tuitionContainer">
+                                
+                                </div>
+                                <input type="submit" :disabled="reg_status != For Registration" value="Register" class="btn btn-default  btn-flat btn-block">
+                            </div>                             
+                        </div>
+                    </form>                           
                 </div>
                 <div v-else>
                     <h1>Not For Registration</h1>
-                </div>                
-            </div>
-            </form>        
                 </div>
             </div>
-        </div>
-       
-        </div>
+        </div>    
+    </div>
 </aside>
 
 <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
