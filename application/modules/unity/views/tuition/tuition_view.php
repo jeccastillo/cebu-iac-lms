3dbe2c1ad66d8def87dc27052f67004e869a4efb
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2022-12-16 19:07:59 +0800

    Updated

---
+++ application/modules/unity/views/tuition/tuition_view.php
@@ -0,0 +1,113 @@
+<div class="box box-solid">
+    <div class="box-header">
+        <h4 class="box-title">ASSESSMENT OF FEES</h4>
+    </div>
+    <div class="box-body">
+        <div class="row">
+            <div class="col-sm-6">Tuition:</div>
+            <div class="col-sm-6 text-green"><?php echo $tuition['tuition']; ?></div>
+        </div>
+        <hr />                
+        <div class="row">
+            <div class="col-sm-6">Miscellaneous:</div>
+            <div class="col-sm-6 text-green"></div>
+        </div>
+    
+        <?php foreach($tuition['misc_list'] as $key=>$val): ?>        
+        <div class="row">
+            <div class="col-sm-6" style="text-align:right;"><?php echo $key; ?></div>
+            <div class="col-sm-6"><?php echo $val; ?></div>
+        </div>
+        <?php endforeach; ?>
+
+        <div class="row">
+            <div class="col-sm-6" style="text-align:right;">Total:</div>
+            <div class="col-sm-6 text-green"><?php echo $tuition['misc']; ?></div>
+        </div>
+
+        <?php if($tuition['nsf']!= 0): ?>                             
+            <div class="row">
+                <div class="col-sm-6">MISC - NEW STUDENT: </div>
+                <div class="col-sm-6 text-green"><?php echo $tuition['nsf']; ?></div>
+            </div>
+            <hr />                    
+        <?php endif; ?>                  
+
+        <div class="row">
+            <div class="col-sm-6">Laboratory Fee:</div>
+            <div class="col-sm-6 text-green"></div>
+        </div>
+        <hr />                
+        <?php foreach($tuition['lab_list'] as $key=>$val): ?>
+            <div class="row">
+                <div class="col-sm-6" style="text-align:right;"><?php echo $key; ?></div>
+                <div class="col-sm-6"><?php echo $val; ?></div>
+            </div>
+        <?php endforeach; ?>
+
+        
+        <div class="row">
+            <div class="col-sm-6" style="text-align:right;">Total:</div>
+            <div class="col-sm-6 text-green"><?php echo $tuition['lab']; ?></div>
+        </div>
+        <hr />
+        <?php if($tuition['thesis_fee']!= 0): ?>                    
+            <div class="row">
+                <div class="col-sm-6">THESIS FEE: </div>
+                <div class="col-sm-6 text-green"><?php echo $tuition['thesis_fee']; ?></div>
+            </div>
+            <hr />                    
+        <?php endif; ?>
+        <?php if($tuition['internship_fee']!= 0): ?>
+            
+            <div class="row">
+                <div class="col-sm-6">Internship Fees:</div>
+                <div class="col-sm-6 text-green"></div>
+            </div>
+            <hr />
+            
+            <?php foreach($tuition['internship_fee_list'] as $key=>$val): ?>
+                <div class="row">
+                    <div class="col-sm-6" style="text-align:right;"><?php echo $key; ?></div>
+                    <div class="col-sm-6"><?php echo $val; ?></div>
+                </div>
+            <?php endforeach; ?>
+
+            
+            <div class="row">
+                <div class="col-sm-6" style="text-align:right;">Total:</div>
+                <div class="col-sm-6 text-green"><?php echo $tuition['internship_fee']; ?></div>
+            </div>
+            <hr />
+        <?php endif; ?>
+        <?php if($tuition['new_student']!= 0): ?>
+            <div class="row">
+                <div class="col-sm-6">New Student Fees:</div>
+                <div class="col-sm-6 text-green"></div>
+            </div>
+            <hr />
+            
+            
+            
+            <?php foreach($tuition['new_student_list'] as $key=>$val): ?>                
+                <div class="row">
+                    <div class="col-sm-6" style="text-align:right;"><?php echo $key; ?></div>
+                    <div class="col-sm-6"><?php echo $val; ?></div>
+                </div>
+            <?php endforeach; ?>
+
+            
+            <div class="row">
+                <div class="col-sm-6" style="text-align:right;">Total:</div>
+                <div class="col-sm-6 text-green"><?php echo $tuition['new_student']; ?></div>
+            </div>
+            <hr />';
+        <?php endif; ?>
+
+                    
+        <div class="row">
+            <div class="col-sm-6">Total:</div>
+            <div class="col-sm-6 text-green"><?php echo $tuition['total'] ?></div>
+        </div>
+    </div>
+</div>
