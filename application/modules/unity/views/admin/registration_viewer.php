7d4912268c56f5a785527407ac45ce53aa58e997
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2022-12-17 11:09:28 +0800

    Update registration_viewer.php

--- application/modules/unity/views/admin/registration_viewer.php
+++ application/modules/unity/views/admin/registration_viewer.php
@@ -3,13 +3,11 @@
         <section class="content-header">
             <h1>
                 <small>
-                    <a class="btn btn-app" href="<?php echo base_url() ?>student/view_all_students" ><i class="ion ion-arrow-left-a"></i>All Students</a> 
-                                    <a class="btn btn-app trash-student-record2" rel="<?php echo $student['intID']; ?>" href="#"><i class="ion ion-android-close"></i> Delete</a>   
-                                    <a class="btn btn-app" :href="base_url + 'student/edit_student/' + student.intID"><i class="ion ion-edit"></i> Edit</a> 
-                                    <a class="btn btn-app" href="<?php echo base_url()."pdf/student_viewer_registration_print/".$student['intID'] ."/". $active_sem['intID']; ?>">
-                                        <i class="ion ion-printer"></i>Reg Form Print Preview</a> 
-                                    
-                                    
+                    <a class="btn btn-app" :href="base_url + 'student/view_all_student'"><i class="ion ion-arrow-left-a"></i>All Students</a>                     
+                    <a class="btn btn-app" :href="base_url + 'student/edit_student/' + student.intID"><i class="ion ion-edit"></i> Edit</a> 
+                    <a class="btn btn-app" :href="base_url + 'pdf/student_viewer_registration_print/' + student.intID">
+                        <i class="ion ion-printer"></i>Reg Form Print Preview
+                    </a>            
                 </small>
             </h1>
             <div v-if="registration" class="pull-right">
