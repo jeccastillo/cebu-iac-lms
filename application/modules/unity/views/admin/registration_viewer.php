45a0cd73f5a3e1d75a4e512da09a7aad40abae0e
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2022-12-17 12:25:35 +0800

    Update registration_viewer.php

--- application/modules/unity/views/admin/registration_viewer.php
+++ application/modules/unity/views/admin/registration_viewer.php
@@ -118,61 +118,75 @@
 new Vue({
     el: '#registration-container',
     data: {
-                             
+        base_url: '<?php echo base_url(); ?>',
+        id: '<?php echo $id; ?>',    
+        sem: '<?php echo $selected_ay; ?>',        
+        student:{},    
+        advanced_privilages: false,
+        request: {
+            enumScholarship: 0,
+            enumStudentType: 'new',
+            enumRegistrationStatus: 'regular',
+            strAcademicYear: undefined,
+        },
+        registration: {},
+        registration_status: 0,
+        reg_status: undefined,
+        
+        loader_spinner: true,                        
     },
-
     mounted() {
 
-        // let url_string = window.location.href;        
-        // if(this.id != 0){            
-        //     //this.loader_spinner = true;
-        //     axios.get(this.base_url + 'unity/registration_viewer_data/' + this.id + '/' + this.sem)
-        //         .then((data) => {  
-        //             if(data.data.success){                                                                                           
-        //                 this.registration = data.data.registration;            
-        //                 this.registration_status = data.data.registration.intROG;
-        //                 this.reg_status = data.data.reg_status;
-        //                 this.student = data.data.student;         
-        //                 this.advanced_privilages = data.data.advanced_privilages;       
-        //             }
-        //             else{
-        //                 document.location = this.base_url + 'users/login';
-        //             }
+        let url_string = window.location.href;        
+        if(this.id != 0){            
+            //this.loader_spinner = true;
+            axios.get(this.base_url + 'unity/registration_viewer_data/' + this.id + '/' + this.sem)
+                .then((data) => {  
+                    if(data.data.success){                                                                                           
+                        this.registration = data.data.registration;            
+                        this.registration_status = data.data.registration.intROG;
+                        this.reg_status = data.data.reg_status;
+                        this.student = data.data.student;         
+                        this.advanced_privilages = data.data.advanced_privilages;       
+                    }
+                    else{
+                        document.location = this.base_url + 'users/login';
+                    }
 
-        //             this.loader_spinner = false;                    
-        //         })
-        //         .catch((error) => {
-        //             console.log(error);
-        //         })
-        // }
+                    this.loader_spinner = false;                    
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+        }
 
     },
 
     methods: {        
-        // changeRegStatus: function(){
-        //     let url = this.base_url + 'unity/update_rog_status';
-        //     var formdata= new FormData();
-        //     formdata.append("intRegistrationID",this.registration.intRegistrationID);
-        //     formdata.append("intROG",this.registration_status);
-        //     this.loader_spinner = true;
-        //     axios.post(url, formdata, {
-        //         headers: {
-        //             Authorization: `Bearer ${window.token}`
-        //         }
-        //     })
-        //     .then(data => {
-        //         this.loader_spinner = false;
-        //         Swal.fire({
-        //             title: "Success",
-        //             text: data.data.message,
-        //             icon: "success"
-        //         }).then(function() {
+        changeRegStatus: function(){
+            let url = this.base_url + 'unity/update_rog_status';
+            var formdata= new FormData();
+            formdata.append("intRegistrationID",this.registration.intRegistrationID);
+            formdata.append("intROG",this.registration_status);
+            this.loader_spinner = true;
+            axios.post(url, formdata, {
+                headers: {
+                    Authorization: `Bearer ${window.token}`
+                }
+            })
+            .then(data => {
+                this.loader_spinner = false;
+                Swal.fire({
+                    title: "Success",
+                    text: data.data.message,
+                    icon: "success"
+                }).then(function() {
                     
-        //         });
-        //     });
+                });
+            });
             
             
-        // }
+        }
     }
 
 })
