0efee2c31ff951b4c954831e588ca563a6abb100
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2022-12-17 12:22:42 +0800

    Update registration_viewer.php

--- application/modules/unity/views/admin/registration_viewer.php
+++ application/modules/unity/views/admin/registration_viewer.php
@@ -118,22 +118,7 @@
 new Vue({
     el: '#registration-container',
     data: {
-        // base_url: '<?php echo base_url(); ?>',
-        // id: '<?php echo $id; ?>',    
-        // sem: '<?php echo $selected_ay; ?>',        
-        // student:{},    
-        // advanced_privilages: false,
-        // request: {
-        //     enumScholarship: 0,
-        //     enumStudentType: 'new',
-        //     enumRegistrationStatus: 'regular',
-        //     strAcademicYear: undefined,
-        // },
-        // registration: {},
-        // registration_status: 0,
-        // reg_status: undefined,
-        
-        // loader_spinner: true,                        
+                             
     },
 
     mounted() {
