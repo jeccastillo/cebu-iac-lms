f8fae5918f51d1716a229c9d21b6ee8bf003ad4a
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2022-12-17 12:28:22 +0800

    Update registration_viewer.php

--- application/modules/unity/views/admin/registration_viewer.php
+++ application/modules/unity/views/admin/registration_viewer.php
@@ -108,10 +108,7 @@
 
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/themes/default/js/script.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/vue@2.6.12"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js"
-    integrity="sha512-WFN04846sdKMIP5LKNphMaWzU7YpMyCU245etK3g/2ARYbPK9Ub18eG+ljU96qKRCWh+quCY7yefSmlkQw1ANQ=="
-    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.min.js"></script>
+
 
 <script>
 new Vue({
