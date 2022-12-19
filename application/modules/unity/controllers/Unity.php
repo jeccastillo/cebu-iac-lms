7c0daeb413b577644d26f50ebcd95cc72c92c646
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2022-12-19 20:09:22 +0800

    Update Unity.php

--- application/modules/unity/controllers/Unity.php
+++ application/modules/unity/controllers/Unity.php
@@ -933,7 +933,7 @@ class Unity extends CI_Controller {
                         value="'.date('gia',strtotime($sched['dteStart'])).'" 
                         href="'.$hdiff.'" 
                         rel="'.$record['strCode'].' '.$sched['strRoomCode'].'" data-section="'.$record['strSection'].'">';                
-                    print_r($sc_ret);                        
+                    echo $sc_ret;                        
                 }
             }        
         }
