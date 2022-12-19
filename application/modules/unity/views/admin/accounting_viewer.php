36db33eb4d8bd7f380858783808d64416e531b8f
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2022-12-20 06:08:57 +0800

    Update accounting_viewer.php

--- application/modules/unity/views/admin/accounting_viewer.php
+++ application/modules/unity/views/admin/accounting_viewer.php
@@ -164,7 +164,7 @@ new Vue({
                 axios.get(api_url + 'finance/reservation/' + this.slug)
                 .then((data) => {
                     this.reservation_payment = data.data.data;    
-                    if(this.reservation_payment.status == "Paid" && data.data.student.syid == this.tuition.selected_ay)
+                    if(this.reservation_payment.status == "Paid" && data.data.student_sy == this.tuition.selected_ay)
                             this.remaining_amount = this.remaining_amount - this.reservation_payment.subtotal_order;            
                     this.loader_spinner = false;
                 })
