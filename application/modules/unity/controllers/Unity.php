574d5af78d44376781cf1624db803fb03dc751c3
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2022-12-19 20:06:04 +0800

    Update Unity.php

--- application/modules/unity/controllers/Unity.php
+++ application/modules/unity/controllers/Unity.php
@@ -926,13 +926,14 @@ class Unity extends CI_Controller {
         {
             $schedule = $this->data_fetcher->getScheduleByCode($record['classlistID']);
             foreach($schedule as $sched){
+
                 if(isset($sched['dteStart'])){
-                   $sc_ret .='
-                    <input type="hidden" class="'.$sched['strDay'].'"
-                        value="'.date('gia',strtotime($sched['dteStart'])).'"
-                        href="'.$sched['hourdiff']*2.'"
-                        rel="'.$record['strCode'].' '.$sched['strRoomCode'].'"
-                        data-section="'.$record['strSection'].'">';                
+                    $hdiff = $sched['hourdiff']*2;
+                    $sc_ret .='<input type="hidden" 
+                    class="'.$sched['strDay'].'" 
+                    value="'.date('gia',strtotime($sched['dteStart'])).'" 
+                    href="'.$hdiff.'" 
+                    rel="'.$record['strCode'].' '.$sched['strRoomCode'].'" data-section="'.$record['strSection'].'">';                
                 }
             }        
         }
