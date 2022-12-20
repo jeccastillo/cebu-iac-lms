4a9bece3eb532e94dbc302b1952f661416dca03d
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2022-12-20 09:46:57 +0800

    Update registration_viewer.php

--- application/modules/unity/views/admin/registration_viewer.php
+++ application/modules/unity/views/admin/registration_viewer.php
@@ -109,6 +109,10 @@
                                                     <label>Enter type if oher is selected:</label>
                                                     <input type="text" :disabled="description != 'Other'" required class="form-control" v-model="description_other" />
                                                 </div>
+                                                <div class="form-group">
+                                                    <label>Contact Number:</label>
+                                                    <input type="text" required class="form-control" v-model="request.contact_number" />
+                                                </div>
                                                 <button class="btn btn-primary btn-lg" type="submit">Test Manual Pay</button>
                                             </form>
                                         </div>                                 
@@ -178,7 +182,8 @@ new Vue({
                         this.student = data.data.student;         
                         this.request.first_name = this.student.strFirstname;
                         this.request.middle_name = this.student.strMiddlename;
-                        this.request.last_name = this.student.strLastname;                        
+                        this.request.last_name = this.student.strLastname;    
+                        this.request.contact_number = this.student.strMobileNumber;                    
                         this.advanced_privilages = data.data.advanced_privilages;       
                         this.tuition = data.data.tuition;
                     }
