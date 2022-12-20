30533e1abb845b7ffcc05e5b13969126fbee0c0a
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2022-12-21 06:35:39 +0800

    Update registration_viewer.php

--- application/modules/unity/views/admin/registration_viewer.php
+++ application/modules/unity/views/admin/registration_viewer.php
@@ -125,6 +125,7 @@
                                             <td>{{ payment.status }}</td>                                            
                                             <td>{{ payment.updated_at }}</td>
                                             <td><button v-if="payment.status == 'Pending' && payment.mode.name == 'MANUAL'"  class="btn btn-primary" @click="setToPaid(payment.id)">Set to paid</button></td>
+                                            <td><button v-if="payment.status == 'Pending' && payment.mode.name == 'MANUAL'"  class="btn btn-danger" @click="deletePayment(payment.id)">Delete</button></td>
                                         </tr>    
                                         <tr>
                                             <th colspan="8">
@@ -140,6 +141,7 @@
                                             <td>{{ payment.status }}</td>                                            
                                             <td>{{ payment.updated_at }}</td>
                                             <td><button v-if="payment.status == 'Pending' && payment.mode.name == 'MANUAL'" class="btn btn-primary" @click="setToPaid(payment.id)">Set to paid</button></td>
+                                            <td><button v-if="payment.status == 'Pending' && payment.mode.name == 'MANUAL'"  class="btn btn-danger" @click="deletePayment(payment.id)">Delete</button></td>
                                         </tr>                                                                           
                                         <tr>
                                             <td class="text-green" colspan="8">
