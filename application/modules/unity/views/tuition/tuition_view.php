1dd20950f08531b9eff2ee0e31c3b4ee5b9808aa
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2022-12-20 10:10:26 +0800

    Update tuition_view.php

--- application/modules/unity/views/tuition/tuition_view.php
+++ application/modules/unity/views/tuition/tuition_view.php
@@ -114,7 +114,7 @@
             <div class="col-sm-6">Down Payment</div>
             <div class="col-sm-6"><?php echo number_format($tuition['down_payment'], 2, '.' ,','); ?></div>
         </div>
-        <hr />
+        <br /><br />
         <?php for($i=0;$i<5;$i++): ?>
         <div class="row">        
             <div class="col-sm-6">
@@ -125,6 +125,7 @@
             </div>                    
         </div>
         <?php endfor; ?>
+        <hr />
         <div class="row">
             <div class="col-sm-6"></div>
             <div class="col-sm-6 text-green"><?php echo number_format($tuition['total_installment'], 2, '.' ,','); ?></div>
