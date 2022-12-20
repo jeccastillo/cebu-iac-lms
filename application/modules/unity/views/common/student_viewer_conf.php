8af026c747b2aee7e20dbfaf8adb442d93b9cd13
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2022-12-20 15:01:39 +0800

    Update student_viewer_conf.php

--- application/modules/unity/views/common/student_viewer_conf.php
+++ application/modules/unity/views/common/student_viewer_conf.php
@@ -60,7 +60,7 @@
             $("#"+st+" :nth-child(4)").addClass("bg-teal");
             $("#"+st+" :nth-child(4)").css({'border-top':'1px solid #999','border-left':'1px solid #999','border-right':'1px solid #999'});
             $("#"+st+" :nth-child(4)").html("<div style='text-align:center;'>"+text+"</div>");
-            $("#"+st+" :nth-child(3)").attr('rowspan',hourspan);
+            //$("#"+st+" :nth-child(3)").attr('rowspan',hourspan);
             nxt = $("#"+st);
             nxt.next().children(":nth-child(4)").html("<div style='text-align:center;'>"+section+"</div>");
             for(i=1;i<hourspan;i++){
@@ -72,6 +72,7 @@
                 
                 nxt = nxt.next();
             }
+            console.log($("#sched-table-container").html());
             $("#sched-table").val($("#sched-table-container").html());
             
         });
