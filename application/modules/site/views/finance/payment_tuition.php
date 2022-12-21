5791aaa3222913dc45548ddaa823ad69d8d06b3a
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2022-12-21 22:20:39 +0800

    Update payment_tuition.php

--- application/modules/site/views/finance/payment_tuition.php
+++ application/modules/site/views/finance/payment_tuition.php
@@ -38,6 +38,32 @@
     </div>
     <div class="custom-container">
         <div v-html="tuition_view"></div>
+        <table class="table-auto">
+  <thead>
+    <tr>
+      <th>Title</th>
+      <th>Author</th>
+      <th>Views</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Intro to CSS</td>
+      <td>Adam</td>
+      <td>858</td>
+    </tr>
+    <tr class="bg-emerald-200">
+      <td>A Long and Winding Tour of the History of UI Frameworks and Tools and the Impact on Design</td>
+      <td>Adam</td>
+      <td>112</td>
+    </tr>
+    <tr>
+      <td>Intro to JavaScript</td>
+      <td>Chris</td>
+      <td>1,280</td>
+    </tr>
+  </tbody>
+</table>
     </div>
     <div class="custom-container">
         <form @submit.prevent="submitPayment">                        
