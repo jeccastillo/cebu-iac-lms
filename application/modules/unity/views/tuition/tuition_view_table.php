b90778174c5bbded837c5fec77cb11db2af32316
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2022-12-21 22:01:36 +0800

    Update tuition_view_table.php

--- application/modules/unity/views/tuition/tuition_view_table.php
+++ application/modules/unity/views/tuition/tuition_view_table.php
@@ -1,134 +1,113 @@
-<div class="box box-solid">
-    <div class="box-header">
-        <h4 class="box-title">ASSESSMENT OF FEES</h4>
-    </div>
-    <div class="box-body">
-        <div class="grid grid-rows-2 grid-flow-col gap-4">
-            <div class="col-sm-6">Tuition:</div>
-            <div class="col-sm-6 text-green"><?php echo $tuition['tuition']; ?></div>
-        </div>
-        <hr />                
-        <div class="grid grid-rows-2 grid-flow-col gap-4">
-            <div class="col-sm-6">Miscellaneous:</div>
-            <div class="col-sm-6 text-green"></div>
-        </div>
-    
-        <?php foreach($tuition['misc_list'] as $key=>$val): ?>        
-        <div class="grid grid-rows-2 grid-flow-col gap-4">
-            <div class="col-sm-6" style="text-align:right;"><?php echo $key; ?></div>
-            <div class="col-sm-6"><?php echo $val; ?></div>
-        </div>
-        <?php endforeach; ?>
-
-        <div class="grid grid-rows-2 grid-flow-col gap-4">
-            <div class="col-sm-6" style="text-align:right;">Total:</div>
-            <div class="col-sm-6 text-green"><?php echo $tuition['misc']; ?></div>
-        </div>
-
-        <?php if($tuition['nsf']!= 0): ?>                             
-            <div class="grid grid-rows-2 grid-flow-col gap-4">
-                <div class="col-sm-6">MISC - NEW STUDENT: </div>
-                <div class="col-sm-6 text-green"><?php echo $tuition['nsf']; ?></div>
-            </div>
-            <hr />                    
-        <?php endif; ?>                  
-
-        <div class="grid grid-rows-2 grid-flow-col gap-4">
-            <div class="col-sm-6">Laboratory Fee:</div>
-            <div class="col-sm-6 text-green"></div>
-        </div>
+    <table class="table-auto">
+    <thead>
+        <tr>
+        <th class="text-right">Tuition:</th>
+        <td><?php echo $tuition['tuition']; ?></td>      
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th class="text-right">Miscellaneous:</th>
+            <td></td>      
+        </tr>
+    <?php foreach($tuition['misc_list'] as $key=>$val): ?>
+        <tr>
+            <th class="text-right"><?php echo $key; ?></th>
+            <td><?php echo $val; ?></td>      
+        </tr>
+    <?php endforeach; ?>
+        <tr>
+            <th class="text-right">Total:</th>
+            <td><?php echo $tuition['misc']; ?></td>      
+        </tr>    
+        <tr>
+            <th class="text-right">Laboratory Fee:</th>
+            <td class="text-green"></td>
+        </tr>
         <hr />                
         <?php foreach($tuition['lab_list'] as $key=>$val): ?>
-            <div class="grid grid-rows-2 grid-flow-col gap-4">
-                <div class="col-sm-6" style="text-align:right;"><?php echo $key; ?></div>
-                <div class="col-sm-6"><?php echo $val; ?></div>
-            </div>
-        <?php endforeach; ?>
-
-        
-        <div class="grid grid-rows-2 grid-flow-col gap-4">
-            <div class="col-sm-6" style="text-align:right;">Total:</div>
-            <div class="col-sm-6 text-green"><?php echo $tuition['lab']; ?></div>
-        </div>
+            <tr>
+                <th class="text-right"><?php echo $key; ?></th>
+                <td><?php echo $val; ?></td>
+            </tr>
+        <?php endforeach; ?>        
+        <tr>
+            <th class="text-right">Total:</th>
+            <td class="text-green"><?php echo $tuition['lab']; ?></td>
+        </tr>
         <hr />
         <?php if($tuition['thesis_fee']!= 0): ?>                    
-            <div class="grid grid-rows-2 grid-flow-col gap-4">
-                <div class="col-sm-6">THESIS FEE: </div>
-                <div class="col-sm-6 text-green"><?php echo $tuition['thesis_fee']; ?></div>
-            </div>
+            <tr>
+                <th class="text-right">THESIS FEE: </th>
+                <td class="text-green"><?php echo $tuition['thesis_fee']; ?></td>
+            </tr>
             <hr />                    
         <?php endif; ?>
         <?php if($tuition['internship_fee']!= 0): ?>
             
-            <div class="grid grid-rows-2 grid-flow-col gap-4">
-                <div class="col-sm-6">Internship Fees:</div>
-                <div class="col-sm-6 text-green"></div>
-            </div>
+            <tr>
+                <th class="text-right">Internship Fees:</th>
+                <td class="text-green"></td>
+            </tr>
             <hr />
             
             <?php foreach($tuition['internship_fee_list'] as $key=>$val): ?>
-                <div class="grid grid-rows-2 grid-flow-col gap-4">
-                    <div class="col-sm-6" style="text-align:right;"><?php echo $key; ?></div>
-                    <div class="col-sm-6"><?php echo $val; ?></div>
-                </div>
+                <tr>
+                    <th class="text-right"><?php echo $key; ?></th>
+                    <td><?php echo $val; ?></td>
+                </tr>
             <?php endforeach; ?>
 
             
-            <div class="grid grid-rows-2 grid-flow-col gap-4">
-                <div class="col-sm-6" style="text-align:right;">Total:</div>
-                <div class="col-sm-6 text-green"><?php echo $tuition['internship_fee']; ?></div>
-            </div>
+            <tr>
+                <th class="text-right">Total:</th>
+                <td class="text-green"><?php echo $tuition['internship_fee']; ?></td>
+            </tr>
             <hr />
         <?php endif; ?>
         <?php if($tuition['new_student']!= 0): ?>
-            <div class="grid grid-rows-2 grid-flow-col gap-4">
-                <div class="col-sm-6">New Student Fees:</div>
-                <div class="col-sm-6 text-green"></div>
-            </div>
+            <tr>
+                <th class="text-right">New Student Fees:</th>
+                <td class="text-green"></td>
+            </tr>
             <hr />
             
-            
-            
             <?php foreach($tuition['new_student_list'] as $key=>$val): ?>                
-                <div class="grid grid-rows-2 grid-flow-col gap-4">
-                    <div class="col-sm-6" style="text-align:right;"><?php echo $key; ?></div>
-                    <div class="col-sm-6"><?php echo $val; ?></div>
-                </div>
+                <tr>
+                    <th class="text-right"><?php echo $key; ?></th>
+                    <td><?php echo $val; ?></td>
+                </tr>
             <?php endforeach; ?>
 
             
-            <div class="grid grid-rows-2 grid-flow-col gap-4">
-                <div class="col-sm-6" style="text-align:right;">Total:</div>
-                <div class="col-sm-6 text-green"><?php echo $tuition['new_student']; ?></div>
-            </div>
+            <tr>
+                <th class="text-right">Total:</th>
+                <td class="text-green"><?php echo $tuition['new_student']; ?></td>
+            </tr>
             <hr />
         <?php endif; ?>
             
-        <div class="grid grid-rows-2 grid-flow-col gap-4">
-            <div class="col-sm-6">Total:</div>
-            <div class="col-sm-6 text-green"><?php echo $tuition['total'] ?></div>
-        </div>
+        <tr>
+            <th class="text-right">Total:</th>
+            <td class="text-green"><?php echo $tuition['total'] ?></td>
+        </tr>
         <hr />
         <h4 class="box-title">FOR INSTALLMENT</h4>
-        <div class="grid grid-rows-2 grid-flow-col gap-4">
-            <div class="col-sm-6">Down Payment</div>
-            <div class="col-sm-6"><?php echo number_format($tuition['down_payment'], 2, '.' ,','); ?></div>
-        </div>
+        <tr>
+            <th class="text-right">Down Payment</th>
+            <td><?php echo number_format($tuition['down_payment'], 2, '.' ,','); ?></td>
+        </tr>
         <br />
         <?php for($i=0;$i<5;$i++): ?>
-        <div class="grid grid-rows-2 grid-flow-col gap-4">        
-            <div class="col-sm-6">
-                <td width="140px"><?php echo switch_num($i + 1) ?> INSTALLMENT</td>
-            </div>
-            <div class="col-sm-6">
-                <td width="80px" style="text-align:right;"><?php echo number_format($tuition['installment_fee'], 2, '.' ,','); ?></td>
-            </div>                    
-        </div>
+        <tr>        
+            <th class="text-right"><?php echo switch_num($i + 1) ?> INSTALLMENT</th>
+            <td><?php echo number_format($tuition['installment_fee'], 2, '.' ,','); ?></td>            
+        </tr>
         <?php endfor; ?>
         <hr />
-        <div class="grid grid-rows-2 grid-flow-col gap-4">
-            <div class="col-sm-6">Total for installment</div>
-            <div class="col-sm-6 text-green"><?php echo number_format($tuition['total_installment'], 2, '.' ,','); ?></div>
-        </div>                                                                    
-    </div>
-</div>
+        <tr>
+            <th class="text-right">Total for installment</th>
+            <td class="text-green"><?php echo number_format($tuition['total_installment'], 2, '.' ,','); ?></td>
+        </tr>   
+    </tbody>
+    </table>
