1850d1b2343b906d80bfa4b9f82699b31d456c6a
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-01-01 18:13:50 +0800

    load schedule

--- application/modules/blocksection/views/block_section_viewer.php
+++ application/modules/blocksection/views/block_section_viewer.php
@@ -43,29 +43,7 @@ new Vue({
         .then((data) => {                                   
             this.section = data.data.section;
             var sched = data.data.schedule;                        
-            for(i in sched){
-                
-                let day = sched[i].strDay;
-                let text = sched[i].strCode;
-                let hourspan = sched[i].hourdiff * 2;
-                let st = sched[i].st;
-                
-                $("#"+st+" :nth-child("+day+")").addClass("bg-teal");
-                $("#"+st+" :nth-child("+day+")").css({'border-top':'1px solid #999','border-left':'1px solid #999','border-right':'1px solid #999'});
-                $("#"+st+" :nth-child("+day+")").html("<div style='text-align:center;'>"+text+"</div>");
-                nxt = $("#"+st);
-                nxt.next().children(":nth-child("+day+")").html("<div style='text-align:center;'></div>");
-                for(i=1;i<hourspan;i++){                    
-                    nxt.next().children(":nth-child("+day+")").addClass("bg-teal");
-                    if(i==hourspan-1)
-                    nxt.next().children(":nth-child("+day+")").css({'border-top':'none','border-bottom':'1px solid #999','border-left':'1px solid #999','border-right':'1px solid #999'});
-                    else
-                        nxt.next().children(":nth-child("+day+")").css({'border-top':'none','border-left':'1px solid #999','border-right':'1px solid #999'});
-                    
-                    nxt = nxt.next();
-                }
-                $("#sched-table").val($("#sched-table-container").html());                                                        
-            }
+            load_schedule(sched);
         })
         .catch((error) => {
             console.log(error);
