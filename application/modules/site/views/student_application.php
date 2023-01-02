475f05a15b5af3e9ca83f2e7a912fdee0524bd12
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-01-02 13:04:07 +0800

    Update student_application.php

--- application/modules/site/views/student_application.php
+++ application/modules/site/views/student_application.php
@@ -125,7 +125,7 @@
                            </label>
                            <the-mask
                            class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
-                           :mask="['(##) ###-####', '(##) ###-###-####']" value="12TgB" type="number" v-model="request.mobile_number" masked="true" placeholder="test your mask here"></the-mask>
+                           :mask="['(+63) ###-####', '(+63) ###-###-####']" value="12TgB" type="number" v-model="request.mobile_number" masked="true" placeholder="test your mask here"></the-mask>
                            <!-- <input
                                class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                                type="number" required v-model="request.mobile_number"> -->
