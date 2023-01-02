6fcd444d8378f8ba166d8368799612b9c1f30bf8
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-01-02 12:39:46 +0800

    Update print_view_student_rog.php

--- application/modules/pdf/views/print_view_student_rog.php
+++ application/modules/pdf/views/print_view_student_rog.php
@@ -3,9 +3,8 @@
     tcpdf();
     // create new PDF document
     //$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
-    //$pdf = new TCPDF("P", PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
-    //$pdf = new TCPDF("P", PDF_UNIT, array(8.5, 11), false, 'ISO-8859-1', false);
-    $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, array(8.5,11), true, 'UTF-8', false, true);
+    //$pdf = new TCPDF("P", PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);    
+    $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
     // set document information
     $pdf->SetCreator(PDF_CREATOR);
     $pdf->SetTitle($student['strLastname'] . ", " . $student['strFirstname'] . ', ' . substr($student['strMiddlename'], 0,1). ".-". $student['strProgramCode']);
@@ -30,29 +29,27 @@
 
     
     // Set some content to print
-$html = '<table border="0" cellpadding="0" style="color:#014fb3; font-size:10;">
-        <tr>
-            <td width="64" align="right"><img src= "'.$img_dir .'tagaytayseal.png"  width="50" height="50"/></td>
-            <td width="400" style="text-align: center; line-height:100%">
-             
-             <font style="font-family:Calibri Light; font-size: 10;font-weight: bold;">Information & Communications Technology Academy</font><br />
-			 <font style="font-family:Calibri Light; font-size: 10;">Filinvest Cebu Cyberzone Tower 2 Salinas Drive corner W. Geonzon St., Brgy. Apas, Lahug, Cebu City</font><br />
-             <font style="font-family:Calibri Light; font-size: 10;">Telephone No: (046) 483-0470 / (046) 483-0672</font><br />
-            </td>
-            <td width="64" align="left" valign="middle"><img src= "'.$img_dir .'cctlogo.png"  width="50" height="50"/></td>
-        </tr>
-        <tr>
-            <td colspan = "3" style="font-weight: bold;text-align:center; font-size:10; letter-spacing: 10px;">REPORT OF GRADES</td>
-        </tr>
-        <tr>
-            <td colspan = "3" style="text-align:center; color:black; font-size: 10;"> A.Y. ' .$active_sem['strYearStart']."-".$active_sem['strYearEnd'] . ", " . $active_sem['enumSem'].' Semester' . '</td>
-        </tr>
-        <tr>
-        <td colspan="3" style="font-size:10;">
+ // Set some content to print
+ $html = '<table border="0" cellspacing="0" cellpadding="0" style="color:#333; font-size:9;">
+    <tr>
+        <td width="100%" align="center" style="text-align:center;vertical-align: middle;"><img src= "https://i.ibb.co/XW1DRVT/iacademy-logo.png"  width="100" height="29"/></td>
+    </tr>
+    <tr>            
+        <td colspan = "3" width="100%" style="text-align: center;">             
+            
         </td>
-        </tr>
+    </tr>        
+    <tr>            
+        <td colspan = "3" width="100%" style="text-align: center;line-height:1">             
+            <font style="font-family:Calibri Light; font-size: 14;font-weight: bold;">Information & Communications Technology Academy </font><br /><br />
+            <font style="font-family:Calibri Light; font-size: 10;">Filinvest Cebu Cyberzone Tower 2 Salinas Drive corner W. Geonzon St., Brgy. Apas, Lahug, Cebu City</font><br />             
+        </td>           
+    </tr>
+    <tr>
+        <td colspan = "3" style="font-weight: bold;text-align:center; font-size:11; border-bottom:1px solid #333;">ASSESSMENT/REGISTRATION FORM</td>
+    </tr>    
     </table>
-     <br />
+    <br />
     <table border="0" cellpadding="0" style="color:#014fb3; font-size:9; border: 0px solid #014fb3;" width="528px">
      <tr>
             <td colspan ="3" style="text-align:center; color:black; font-size:10; font-weight:bold;">
