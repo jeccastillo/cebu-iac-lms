6e9679d17350dde2ec6549116ffbf256f60c7c13
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2023-01-03 08:20:44 +0800

    Update payment.php

--- application/modules/site/views/student_applicants/payment.php
+++ application/modules/site/views/student_applicants/payment.php
@@ -91,7 +91,7 @@
                         </label>
                         <input disabled
                             class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
-                            type="number" required v-model="student.mobile_number">
+                            type="text" required v-model="student.mobile_number">
                     </div>
                 </div>
 
