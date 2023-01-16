15886156bb639191b792ca7ffece1ae441eb3523
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-01-16 11:34:32 +0800

    Update initial_requirements.php

--- application/modules/site/views/student_applicants/initial_requirements.php
+++ application/modules/site/views/student_applicants/initial_requirements.php
@@ -76,7 +76,7 @@
                                 </div>
                             </div>
                             <div v-else>
-                                <div class="md-w-1/2">
+                                <div class="md-w-1/3">
                                     <span>Photocopy of the valid (unexpired) passport pages bearing the bio-page, 
                                         the latest admission/arrival in the Philippines with "valid authorized stay" date and the Bureau of Quarantine (BOQ) stamp.
                                         Note: Present the original passport for verification</span>
@@ -84,8 +84,8 @@
                                         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                                         type="file" required>
                                 </div>
-                                <br /><br />
-                                <div class="md-w-1/2">
+                                <!-- <br /><br /> -->
+                                <div class="md-w-1/3">
                                     <span>Copy of Alien Certificate of Registration (i-CARD) if any</span>
                                     <input ref="file_id" @change="uploadReq('icard')"
                                         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
