82963f4c32c276089aa8da996d3d076f5a03e565
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-01-16 11:58:27 +0800

    Update initial_requirements.php

--- application/modules/site/views/student_applicants/initial_requirements.php
+++ application/modules/site/views/student_applicants/initial_requirements.php
@@ -76,24 +76,27 @@
                                 </div>
                             </div>
                             <div v-else>
-                                <div class="md-w-1/2">
-                                    <span>Photocopy of the valid (unexpired) passport pages bearing the bio-page, 
+                                <div class="md-w-1/3">
+                                    <img src="<?php echo $img_dir; ?>admissions/form/id.png"
+                                        class="max-w-full h-auto mx-auto block" title='Photocopy of the valid (unexpired) passport pages bearing the bio-page, 
                                         the latest admission/arrival in the Philippines with "valid authorized stay" date and the Bureau of Quarantine (BOQ) stamp.
-                                        Note: Present the original passport for verification</span>
+                                        Note: Present the original passport for verification'>                                        
                                     <input ref="file_passport" @change="uploadReq('passport')"
                                         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                                         type="file" required>
                                 </div>
-                                <br /><br />
-                                <div class="md-w-1/2">
-                                    <span>Copy of Alien Certificate of Registration (i-CARD) if any</span>
+                                
+                                <div class="md-w-1/3">
+                                    <img src="<?php echo $img_dir; ?>admissions/form/id.png"
+                                        class="max-w-full h-auto mx-auto block" title='Copy of Alien Certificate of Registration (i-CARD) if any'>                                    
                                     <input ref="file_icard" @change="uploadReq('icard')"
                                         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                                         type="file">
                                 </div>
                                 <br /><br />
-                                <div class="md-w-1/2">
-                                    <span>Quarantine Medical Examination by the Bureau of Quarantine (BOQ)</span>
+                                <div class="md-w-1/3">
+                                    <img src="<?php echo $img_dir; ?>admissions/form/id.png"
+                                        class="max-w-full h-auto mx-auto block" title='Quarantine Medical Examination by the Bureau of Quarantine (BOQ)'>                                    
                                     <input ref="file_quarantine" @change="uploadReq('quarantine_med_exam')"
                                         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                                         type="file" required>
