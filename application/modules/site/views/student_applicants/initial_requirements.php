8e6e8fe3d26118b100f9adfdcf0c369623bee71f
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-01-16 11:38:24 +0800

    Update initial_requirements.php

--- application/modules/site/views/student_applicants/initial_requirements.php
+++ application/modules/site/views/student_applicants/initial_requirements.php
@@ -76,7 +76,7 @@
                                 </div>
                             </div>
                             <div v-else>
-                                <div class="md-w-1/3">
+                                <div class="md-w-1/2">
                                     <span>Photocopy of the valid (unexpired) passport pages bearing the bio-page, 
                                         the latest admission/arrival in the Philippines with "valid authorized stay" date and the Bureau of Quarantine (BOQ) stamp.
                                         Note: Present the original passport for verification</span>
@@ -84,12 +84,19 @@
                                         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                                         type="file" required>
                                 </div>
-                                <!-- <br /><br /> -->
-                                <div class="md-w-1/3">
+                                <br /><br />
+                                <div class="md-w-1/2">
                                     <span>Copy of Alien Certificate of Registration (i-CARD) if any</span>
                                     <input ref="file_id" @change="uploadReq('icard')"
                                         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                                         type="file">
+                                </div>
+                                <br /><br />
+                                <div class="md-w-1/2">
+                                    <span>Quarantine Medical Examination by the Bureau of Quarantine (BOQ)</span>
+                                    <input ref="file_id" @change="uploadReq('quarantine_med_exam')"
+                                        class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+                                        type="file" required>
                                 </div>
                             </div>
 
