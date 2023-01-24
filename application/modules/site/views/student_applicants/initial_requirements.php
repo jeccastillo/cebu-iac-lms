0c19dc524f630bfc108edbe923f22f4d6c211f55
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2023-01-24 08:59:27 +0800

    Update initial_requirements.php

--- application/modules/site/views/student_applicants/initial_requirements.php
+++ application/modules/site/views/student_applicants/initial_requirements.php
@@ -157,7 +157,31 @@
            </form>
        </div>
    </section>
+   <style>
+    .loader {
+        border-top-color: #3498db;
+        -webkit-animation: spinner 1.5s linear infinite;
+        animation: spinner 1.5s linear infinite;
+    }
 
+    @-webkit-keyframes spinner {
+        0% {
+            -webkit-transform: rotate(0deg);
+        }
+        100% {
+            -webkit-transform: rotate(360deg);
+        }
+    }
+
+    @keyframes spinner {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+   </style>
    <script>
 new Vue({
     el: "#adminssions-form",
