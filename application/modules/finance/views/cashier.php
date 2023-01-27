b6ae88941838ee7842f2d8a9464d3315fdd614b1
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-01-27 13:00:04 +0800

    Update cashier.php

--- application/modules/finance/views/cashier.php
+++ application/modules/finance/views/cashier.php
@@ -74,6 +74,8 @@
     integrity="sha512-WFN04846sdKMIP5LKNphMaWzU7YpMyCU245etK3g/2ARYbPK9Ub18eG+ljU96qKRCWh+quCY7yefSmlkQw1ANQ=="
     crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.min.js"></script>
+<script type="text/javascript"
+    src="<?php echo base_url(); ?>assets/lib/adminlte/js/plugins/select2/select2.full.min.js"></script>
 
 <script>
 new Vue({
