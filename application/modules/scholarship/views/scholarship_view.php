5da355f972762ebae90b2e3a3b2eede6a3c0bd7c
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2023-02-01 10:35:15 +0800

    Update scholarship_view.php

--- application/modules/scholarship/views/scholarship_view.php
+++ application/modules/scholarship/views/scholarship_view.php
@@ -44,7 +44,7 @@
                                 <div class="col-md-12 text-center"><strong>Tuition Fee</strong></div>
                                 <div class="col-md-6">
                                     <label>Rate:</label>
-                                    <input type="number"  @blur="checkVal($event)" min=0 max=100 step=1 v-model="scholarship.tuition_fee_rate" class="form-control">
+                                    <input type="number"  min=0 max=100 step=1 v-model="scholarship.tuition_fee_rate" class="form-control">
                                 </div>                        
                                 <div class="col-md-6">
                                     <label>Fixed:</label>
@@ -57,7 +57,7 @@
                                 <div class="col-md-12 text-center"><strong>Basic Fee</strong></div>
                                 <div class="col-md-6">
                                     <label>Rate:</label>
-                                    <input type="number" @blur="checkVal($event)" min=0 max=100 step=1 v-model="scholarship.basic_fee_rate" class="form-control">
+                                    <input type="number" min=0 max=100 step=1 v-model="scholarship.basic_fee_rate" class="form-control">
                                 </div>                        
                                 <div class="col-md-6">
                                     <label>Fixed:</label>
@@ -70,7 +70,7 @@
                                 <div class="col-md-12 text-center"><strong>Miscellaneous Fee</strong></div>
                                 <div class="col-md-6">
                                     <label>Rate:</label>
-                                    <input type="number" @blur="checkVal($event)" min=0 max=100 step=1 v-model="scholarship.misc_fee_rate" class="form-control">
+                                    <input type="number" min=0 max=100 step=1 v-model="scholarship.misc_fee_rate" class="form-control">
                                 </div>                        
                                 <div class="col-md-6">
                                     <label>Fixed:</label>
@@ -143,12 +143,7 @@ new Vue({
     },
 
     methods: {    
-        checkVal: function(event){
-            if(event.target.value < 0)
-                event.target.value = 0;
-            if(event.target.value > 100)
-                event.target.value = 100;
-        }    
+                 
     }
 
 })
