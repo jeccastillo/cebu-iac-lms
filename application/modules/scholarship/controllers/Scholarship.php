093bf0de2cd59fba6bbf9d081ea4244016fc934d
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2023-02-01 08:07:06 +0800

    Update

---
+++ application/modules/scholarship/controllers/Scholarship.php
@@ -0,0 +1,129 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+	
+class Scholarship extends CI_Controller {
+
+	
+    function __construct() {
+        parent::__construct();
+        
+        if(!$this->is_registrar() && !$this->is_super_admin() && !$this->is_department_head())
+		  redirect(base_url()."unity");
+        
+		$this->config->load('themes');		
+		$theme = $this->config->item('unity');
+		if($theme == "" || !isset($theme))
+			$theme = $this->config->item('global_theme');
+		
+        $settings = $this->data_fetcher->fetch_table('su-tb_sys_settings');
+		foreach($settings as $setting)
+		{
+			$this->settings[$setting['strSettingName']] = $setting['strSettingValue'];
+		}
+        
+        $this->data["user"] = $this->session->all_userdata();
+        $this->data['img_dir'] = base_url()."assets/themes/".$theme."/images/";	
+        $this->data['student_pics'] = base_url()."assets/photos/";
+        $this->data['css_dir'] = base_url()."assets/themes/".$theme."/css/";
+        $this->data['js_dir'] = base_url()."assets/themes/".$theme."/js/";
+        $this->data['title'] = "CCT Unity";
+        $this->load->library("email");	       
+        
+                
+        
+        $this->data['page'] = "finance";
+        
+        //$this->data["subjects"] = $this->data_fetcher->fetch_table('tb_mas_subjects');
+        //$this->data["students"] = $this->data_fetcher->fetch_table('tb_mas_users',array('strLastname','asc'));
+        
+        $this->data["user"] = $this->session->all_userdata();
+        $this->data['unread_messages'] = $this->data_fetcher->count_table_contents('tb_mas_message_user',null,array('intRead'=>'0','intTrash'=>0,'intFacultyID'=>$this->session->userdata('intID')));
+        
+        $this->data['all_messages'] = $this->data_fetcher->count_table_contents('tb_mas_message_user',null,array('intTrash'=>0,'intFacultyID'=>$this->session->userdata('intID')));
+        
+        $this->data['trashed_messages'] = $this->data_fetcher->count_table_contents('tb_mas_message_user',null,array('intTrash'=>1,'intFacultyID'=>$this->session->userdata('intID')));
+        
+        $this->data['sent_messages'] = $this->data_fetcher->count_sent_items($this->session->userdata('intID'));
+		
+
+    }
+
+    public function data_table(){
+        $data = $this->input->get();
+        print_r($data);
+    }
+    
+    public function scholarships(){
+        
+        $this->load->view("common/header",$this->data);
+        $this->load->view("scholarships",$this->data);
+        $this->load->view("common/footer",$this->data);
+        $this->load->view("common/scholarship_list_conf",$this->data);
+    }
+
+   
+
+    public function faculty_logged_in()
+    {
+        if($this->session->userdata('faculty_logged'))
+            return true;
+        else
+            return false;
+    }
+    
+    
+    public function is_admin()
+    {
+         $admin = $this->session->userdata('intUserLevel');
+        if($admin == 1 || $this->is_super_admin())
+            return true;
+        else
+            return false;
+    }
+    
+    public function is_super_admin()
+    {
+         $admin = $this->session->userdata('intUserLevel');
+        if($admin == 2)
+            return true;
+        else
+            return false;
+    }
+    
+    public function is_registrar()
+    {
+        $admin = $this->session->userdata('intUserLevel');
+        if($admin == 3)
+            return true;
+        else
+            return false;
+        
+    }
+    
+    public function is_department_head()
+    {
+        $admin = $this->session->userdata('intUserLevel');
+        if($admin == 4)
+            return true;
+        else
+            return false;
+        
+    }
+    
+    public function is_accounting()
+    {
+        $admin = $this->session->userdata('intUserLevel');
+        if($admin == 6)
+            return true;
+        else
+            return false;
+        
+    }
+
+
+
+   }
+
