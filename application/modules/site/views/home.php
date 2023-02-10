0c28d917e1026f7af873c5344ab94b8ada0e9c25
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2023-02-10 22:51:24 +0800

    Update home.php

--- application/modules/site/views/home.php
+++ application/modules/site/views/home.php
@@ -42,7 +42,7 @@
                                     course you can take at
                                 </p>
                                 <h1 class="text-5xl font-[900] text-center color-primary">
-                                    iACADEMY Cebu
+                                    iACADEMY Cebu 123
                                 </h1>
                                 <a href="#se-section" class="w-full">
                                     <img src="<?php echo $img_dir; ?>cebu/home/ btn-arrow copy.png"
