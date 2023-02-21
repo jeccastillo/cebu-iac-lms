b333900c5acfb0508d732ee5e3df98eded61a4a5
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-02-21 11:22:03 +0800

    Update student_application.php

--- application/modules/site/views/student_application.php
+++ application/modules/site/views/student_application.php
@@ -177,7 +177,7 @@
                         </label>
 
                         <div>
-                        <select class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700" required name="citizenship" v-model="request.citizenship">                                
+                        <select class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required name="citizenship" v-model="request.citizenship">                                
                                 <option value="Afghanistan">Afghanistan</option>
                                 <option value="Aland Islands">Åland Islands</option>
                                 <option value="Albania">Albania</option>
