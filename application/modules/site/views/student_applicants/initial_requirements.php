aef152166179abbeebe9b253806e387bcbe0a215
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2023-02-28 18:18:04 +0800

    Update initial_requirements.php

--- application/modules/site/views/student_applicants/initial_requirements.php
+++ application/modules/site/views/student_applicants/initial_requirements.php
@@ -47,7 +47,7 @@
                                         class="max-w-full h-auto mx-auto block">
 
                                     <div class="w-[200px] my-3 block mx-auto">
-                                        <input ref="file_2x2" @change="uploadReq('2x2')"
+                                        <input ref="file_2x2" @change="uploadReq('2x2',$event)"
                                             class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                                             type="file" required>
                                     </div>
@@ -57,7 +57,7 @@
                                         class="max-w-full h-auto mx-auto block">
 
                                     <div class="w-[200px] my-3 block mx-auto">
-                                        <input ref="file_nso" @change="uploadReq('psa')"
+                                        <input ref="file_nso" @change="uploadReq('psa',$event)"
                                             class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                                             type="file" required>
                                     </div>
@@ -68,7 +68,7 @@
                                         class="max-w-full h-auto mx-auto block">
 
                                     <div class="w-[200px] my-3 block mx-auto">
-                                        <input ref="file_id" @change="uploadReq('school_id')"
+                                        <input ref="file_id" @change="uploadReq('school_id',$event)"
                                             class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                                             type="file" required>
                                     </div>
@@ -82,7 +82,7 @@
                                         class="max-w-full h-auto mx-auto block" title='Photocopy of the valid (unexpired) passport pages bearing the bio-page, 
                                         the latest admission/arrival in the Philippines with "valid authorized stay" date and the Bureau of Quarantine (BOQ) stamp.
                                         Note: Present the original passport for verification'>                                        
-                                    <input ref="file_passport" @change="uploadReq('passport')"
+                                    <input ref="file_passport" @change="uploadReq('passport',$event)"
                                         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                                         type="file" required>
                                 </div>
@@ -90,21 +90,21 @@
                                 <div class="md-w-1/4">
                                     <img src="<?php echo $img_dir; ?>admissions/form/upload2.png"
                                         class="max-w-full h-auto mx-auto block" title='Copy of Alien Certificate of Registration (i-CARD) if any'>                                    
-                                    <input ref="file_icard" @change="uploadReq('icard')"
+                                    <input ref="file_icard" @change="uploadReq('icard',$event)"
                                         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                                         type="file">
                                 </div>                                                        
                                 <div class="md-w-1/4">
                                     <img src="<?php echo $img_dir; ?>admissions/form/upload3.png"
                                         class="max-w-full h-auto mx-auto block" title='Quarantine Medical Examination by the Bureau of Quarantine (BOQ)'>                                    
-                                    <input ref="file_quarantine" @change="uploadReq('quarantine_med_exam')"
+                                    <input ref="file_quarantine" @change="uploadReq('quarantine_med_exam',$event)"
                                         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                                         type="file" required>
                                 </div>
                                 <div class="md-w-1/4">
                                     <img src="<?php echo $img_dir; ?>admissions/form/upload4.png"
                                         class="max-w-full h-auto mx-auto block" title='Copy of Birth Certificate.'>
-                                    <input ref="file_birthcert" @change="uploadReq('birthcert')"
+                                    <input ref="file_birthcert" @change="uploadReq('birthcert',$event)"
                                         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                                         type="file" required>
                                 </div>                                                  
@@ -114,28 +114,28 @@
                                 <div class="md-w-1/4">
                                     <img src="<?php echo $img_dir; ?>admissions/form/upload5.png"
                                         class="max-w-full h-auto mx-auto block" title='Original copy of Scholastic Records'>                                    
-                                    <input ref="file_schrecords" @change="uploadReq('schrecords')"
+                                    <input ref="file_schrecords" @change="uploadReq('schrecords',$event)"
                                         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                                         type="file">
                                 </div>              
                                 <div class="md-w-1/4">
                                     <img src="<?php echo $img_dir; ?>admissions/form/upload6.png"
                                         class="max-w-full h-auto mx-auto block" title='Recommendation letter from the Principal/Guidance Counselor/Class Adviser'>                                    
-                                    <input ref="file_recommendation" @change="uploadReq('recommendation')"
+                                    <input ref="file_recommendation" @change="uploadReq('recommendation',$event)"
                                         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                                         type="file" required>
                                 </div>
                                 <div class="md-w-1/4">
                                     <img src="<?php echo $img_dir; ?>admissions/form/upload7.png"
                                         class="max-w-full h-auto mx-auto block" title="Proof of adequate financial support to cover expenses for the student's accommodation and subsistence, as well as school dues and other incidental expenses (to be notarized by a lawyer in the country of residence) and to be authenticated by the Philippine Embassy.">                                    
-                                    <input ref="file_financial_support" @change="uploadReq('financial_support')"
+                                    <input ref="file_financial_support" @change="uploadReq('financial_support',$event)"
                                         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                                         type="file">
                                 </div>                                
                                 <div class="md-w-1/4">
                                     <img src="<?php echo $img_dir; ?>admissions/form/upload8.png"
                                         class="max-w-full h-auto mx-auto block" title='2x2 ID picture (white background with name tag below)'>                                    
-                                    <input ref="file_2x2_foreign" @change="uploadReq('2x2_foreign')"
+                                    <input ref="file_2x2_foreign" @change="uploadReq('2x2_foreign',$event)"
                                         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                                         type="file" required>
                                 </div>
@@ -309,7 +309,7 @@ new Vue({
 
         },
 
-        uploadReq: function(type) {
+        uploadReq: function(type,event) {
 
             this.loading_spinner = true;
             let formDataUp = "";
@@ -401,6 +401,7 @@ new Vue({
                             data.data.message,
                             'error'
                         )
+                        event.target.value = null;
                         this.loading_spinner = false;
                     }
                 });
