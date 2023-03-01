3923bf92c75136ba3c178916c1d04e18ee19190c
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2023-03-01 11:10:09 +0800

    Update print_or.php

--- application/modules/pdf/views/print_or.php
+++ application/modules/pdf/views/print_or.php
@@ -61,6 +61,39 @@
                     <td style="width:50%"><?php echo !$is_cash?$check_number:""; ?></td>
                 </tr>
             </table>
+            <table>
+                <tr style="line-height:5px;">
+                    <td style=""></td>
+                </tr>
+            </table>
+            <table>
+                <tr style="line-height:12px;font-size:9px;text-align:left;color:#666;">
+                    <td style="width:50%"></td>
+                    <td style="width:50%"></td>
+                </tr>
+            </table>
+            <table>
+                <tr style="line-height:5px;">
+                    <td style=""></td>
+                </tr>
+            </table>
+            <table>
+                <tr style="line-height:12px;font-size:9px;text-align:left;color:#666;">
+                    <td style="width:50%"></td>
+                    <td style="width:50%"></td>
+                </tr>
+            </table>
+            <table>
+                <tr style="line-height:5px;">
+                    <td style=""></td>
+                </tr>
+            </table>
+            <table>
+                <tr style="line-height:12px;font-size:9px;text-align:left;color:#666;">
+                    <td style="width:50%"></td>
+                    <td style="width:50%">P<?php echo number_format($total_amount_due,2,'.',','); ?></td>
+                </tr>
+            </table>
         </td>
         <td style="width:70%">
             <table>
