8d7f628f5f815258489b456b1111d7258ae0e9e7
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2023-03-01 09:37:51 +0800

    Update registration_viewer.php

--- application/modules/unity/views/admin/registration_viewer.php
+++ application/modules/unity/views/admin/registration_viewer.php
@@ -555,7 +555,7 @@ new Vue({
             this.or_print.description = payment.description;
             this.or_print.total_amount_due = payment.total_amount_due;
             this.or_print.transaction_date = payment.updated_at;
-            this.or_print.student_name =  this.request.first_name+" "+this.request.last_name;    
+            this.or_print.student_name =  this.request.last_name+", "+this.request.first_name+", "+this.request.middle_name;    
             this.or_print.student_address = this.student.strAddress;
             this.or_print.student_id = this.student.strStudentNumber;
             this.or_print.is_cash = payment.is_cash;
