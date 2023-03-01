0475dbee543497eeabad72d02b21ed4d70600e83
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-03-02 07:06:56 +0800

    Update calendar_view.php

--- application/modules/admissionsV1/views/admin/calendar_view.php
+++ application/modules/admissionsV1/views/admin/calendar_view.php
@@ -34,7 +34,9 @@ window.moment || document.write(
     <div id="modal" class="hidden relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
         <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
         <div class="fixed inset-0 z-10 overflow-y-auto">
-            <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">                
+            <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"> 
+                <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">               
+                </div>
                 <div class=" bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">                       
                     <button type="button"
                         class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
