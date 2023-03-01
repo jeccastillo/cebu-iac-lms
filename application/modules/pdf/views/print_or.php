4d5ef05b5cfeebf64918ae5ca08f232464fd28f9
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2023-03-01 09:59:28 +0800

    Update print_or.php

--- application/modules/pdf/views/print_or.php
+++ application/modules/pdf/views/print_or.php
@@ -45,7 +45,7 @@
 <table >
     <tr style="line-height:15px;">
         <td width="50%" style="font-size:9px;color:#666;"><?php echo $description; ?></td>
-        <td width="20%" style=""></td>
+        <td width="20%" style="">TEST</td>
         <td width="30%" style="font-size:9px;color:#666;text-align:center;">Php<?php echo number_format($total_amount_due,2,'.',','); ?></td>
     </tr>
 </table>
