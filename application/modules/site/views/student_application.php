cb8ceded4f9ea3f3cc67706ab88d9977d3e55b7a
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-03-03 10:09:25 +0800

    Update student_application.php

--- application/modules/site/views/student_application.php
+++ application/modules/site/views/student_application.php
@@ -544,7 +544,7 @@
                             <input
                                 type="text"
                                 required
-                                class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+                                class="bg-gray-200 appearance-none border-2 border-gray-200 rounded md:w-4/5 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                                 v-model="request.hospitalized_reason"
                             />
                         </div>
