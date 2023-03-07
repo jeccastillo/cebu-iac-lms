4d5f33475981a4d325b8ed6ef319af472bb3371e
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-03-08 05:47:37 +0800

    Update Pdf.php

--- application/modules/pdf/controllers/Pdf.php
+++ application/modules/pdf/controllers/Pdf.php
@@ -406,7 +406,8 @@ class Pdf extends CI_Controller {
         $this->data['active_sem'] = $this->data_fetcher->get_sem_by_id($this->data['selected_ay']);
         $this->data['student'] = $this->data_fetcher->getStudent($id);
         $this->data['registration'] = $this->data_fetcher->getRegistrationInfo($id,$this->data['selected_ay']);
-        
+        print_r($this->data['registration']);
+        die();
         $this->data['academic_standing'] = $this->data_fetcher->getAcademicStanding($this->data['student']['intID'],$this->data['student']['intCurriculumID']);
 
         $this->data['transactions'] = $this->data_fetcher->getTransactions($this->data['registration']['intRegistrationID'],$this->data['selected_ay']);
