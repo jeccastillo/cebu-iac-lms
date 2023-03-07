d6c83f592ad60cf47c72e017487726e0efef7c5d
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-03-08 06:58:53 +0800

    Update print_view_student_reg2.php

--- application/modules/pdf/views/print_view_student_reg2.php
+++ application/modules/pdf/views/print_view_student_reg2.php
@@ -274,7 +274,7 @@ $html.= '<table border="0" cellpadding="0" cellspacing="0" style="color:#333; fo
                         <table  width="258px"  style="color:#333; font-size:8;">
                             <tr>
                                 <td>&nbsp;</td>
-                                <td text-align:right;"></td>                
+                                <td style="text-align:right;"></td>                
                             </tr>
                             <tr>
                                 <td width="159px" style= "font-size:9; font-weight:bold;">OTHER FEES DETAIL</td>
