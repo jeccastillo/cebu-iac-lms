3c532c991a9bb78e5f734ebd54106721dd8006e3
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-03-09 21:55:33 +0800

    Update Excel.php

--- application/modules/excel/controllers/Excel.php
+++ application/modules/excel/controllers/Excel.php
@@ -1935,7 +1935,7 @@ class Excel extends CI_Controller {
         
         foreach($data as $d){                        
             // Add some datat
-            $cashier = $this->data_fetcher->fetch_single_entry('tb_mas_faculty',$d['cashier']);
+            $cashier = $this->data_fetcher->fetch_single_entry('tb_mas_faculty',$d->cashier);
             if($cashier)
                 $cashier_name = $cashier['strLastname']." ".$cashier['strFirstname'].", ".$cashier['strMiddlename'];
             else
