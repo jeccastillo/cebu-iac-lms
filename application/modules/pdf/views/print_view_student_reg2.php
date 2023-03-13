2ddb56e9cf20a3b9d59f305a9101d00c3e239a6d
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-03-14 06:04:59 +0800

    Update print_view_student_reg2.php

--- application/modules/pdf/views/print_view_student_reg2.php
+++ application/modules/pdf/views/print_view_student_reg2.php
@@ -24,7 +24,7 @@
     $pdf->setPrintFooter(false);
     // Add a page
     // This method has several options, check the source code documentation for more information.
-    $pdf->AddPage();    
+    $pdf->AddPage('P', 'A6');    
     $payment_division = $tuition['total'] / 4;    
 
     
