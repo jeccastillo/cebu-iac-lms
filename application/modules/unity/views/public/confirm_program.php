7b92c2ad6f1c1bd0efda6ec6e759bef4bf7acdf9
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-03-13 08:00:53 +0800

    Update confirm_program.php

--- application/modules/unity/views/public/confirm_program.php
+++ application/modules/unity/views/public/confirm_program.php
@@ -378,7 +378,7 @@ new Vue({
                         })
                         .then(data => {
                             Swal.hideLoading();
-                            document.location = this.base_url+'site/awesome/confirm';
+                            //document.location = this.base_url+'site/awesome/confirm';
                         });               
                         
                     });
