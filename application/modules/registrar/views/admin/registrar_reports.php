00c9158096ae66e4d6ddeee53b7fb487a54f5615
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2023-03-21 11:50:07 +0800

    Update registrar_reports.php

--- application/modules/registrar/views/admin/registrar_reports.php
+++ application/modules/registrar/views/admin/registrar_reports.php
@@ -38,10 +38,10 @@
                          <i class="fa fa-list"></i>
                      </div>
                      <a href="<?php echo base_url(); ?>pdf/reports_enlisted" class="small-box-footer">
-                         View Report PDF<i class="fa fa-arrow-circle-right"></i>
+                         View Report PDF <i class="fa fa-arrow-circle-right"></i>
                      </a>
                      <a href="<?php echo base_url(); ?>excel/reports_enlisted" class="small-box-footer">
-                         View Report Excel<i class="fa fa-arrow-circle-right"></i>
+                         View Report Excel <i class="fa fa-arrow-circle-right"></i>
                      </a>
                  </div>
 
