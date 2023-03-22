9268fc7770203c81e78dbec12bdbcb55e0c02d29
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2023-03-22 08:37:32 +0800

    Update payments_report.php

--- application/modules/finance/views/payments_report.php
+++ application/modules/finance/views/payments_report.php
@@ -26,7 +26,7 @@
             <div class="box-header">
                 <h3 class="box-title"><?php echo date('M j, Y',strtotime($date)); ?></h3>                
             </div><!-- /.box-header -->
-            <div class="box-body table-responsive" style="overflow-x:auto;padding-right:20px;">
+            <div class="box-body table-responsive" style="overflow-x:auto;margin-right:60px;">
                 <table id="subjects-table" class="table table-hover table-bordered">
                     <thead>
                         <tr>     
