0b15714e40dc54b48a6b7e18b988a2077825feb5
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-03-23 10:36:33 +0800

    Update enlisted_students.php

--- application/modules/pdf/views/enlisted_students.php
+++ application/modules/pdf/views/enlisted_students.php
@@ -45,13 +45,13 @@
     ?>
     
     <tr style="line-height:10px;text-align:center;">
-        <td style="font-size:9px;" width="3%"><?php echo $i; ?></td>
-        <td style="font-size:9px;" width="20%"><?php echo $st['strStudentNumber']; ?></td>
-        <td style="font-size:9px;text-align:left;" width="25%"> <?php echo strtoupper($name); ?></td>        
-        <td style="font-size:9px;" width="10%"><?php echo $st['strProgramCode']; ?></td>
-        <td style="font-size:9px;" width="12%"><?php echo $st['reg_info']['type_of_class']."-".$st['reg_info']['enumStudentType']; ?></td>
-        <td style="font-size:9px;" width="10%"><?php echo date("M j, Y h:ia",strtotime($st['date_added']));?></td>
-        <td style="font-size:9px;" width="15%"><?php echo $st['fusername'] ?></td>           
+        <td style="font-size:9px;" ><?php echo $i; ?></td>
+        <td style="font-size:9px;" ><?php echo $st['strStudentNumber']; ?></td>
+        <td style="font-size:9px;text-align:left;" width="32%"> <?php echo strtoupper($name); ?></td>        
+        <td style="font-size:9px;" ><?php echo $st['strProgramCode']; ?></td>
+        <td style="font-size:9px;" ><?php echo $st['reg_info']['type_of_class']."-".$st['reg_info']['enumStudentType']; ?></td>
+        <td style="font-size:9px;" ><?php echo date("M j, Y h:ia",strtotime($st['date_added']));?></td>
+        <td style="font-size:9px;" ><?php echo $st['fusername'] ?></td>           
     </tr>
     <?php   
     $i++;
