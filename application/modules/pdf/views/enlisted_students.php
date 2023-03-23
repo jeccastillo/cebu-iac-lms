1fdc72f9e8b4250359d3c7e87bfa59b008968006
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-03-23 10:39:08 +0800

    Update enlisted_students.php

--- application/modules/pdf/views/enlisted_students.php
+++ application/modules/pdf/views/enlisted_students.php
@@ -24,18 +24,15 @@
 </table>
 <table style="font-size:9px;">
     <tr style="line-height:12px;text-align:center;">
-        <th style="font-size:10px;" width="3%">#</th>
-        <th style="font-size:10px;" width="15%">Student Number</th>
-        <th style="font-size:10px;" width="32%">Student Name</th>
-        <th style="font-size:10px;" width="10%">Course</th>
-        <th style="font-size:10px;" width="10%">Enrollment Status</th>
-        <th style="font-size:10px;" width="15%">Date Enlisted</th>
-        <th style="font-size:10px;" width="15%">Enlisted By</th>
+        <th style="font-size:9px;" width="3%"><b>#</b></th>
+        <th style="font-size:9px;" width="15%"><b>Student Number</b></th>
+        <th style="font-size:9px;" width="25%"><b>Student Name</b></th>
+        <th style="font-size:9px;" width="10%"><b>Course</b></th>
+        <th style="font-size:9px;" width="10%"><b>Enrollment Status</b></th>
+        <th style="font-size:9px;" width="20%"><b>Date Enlisted</b></th>
+        <th style="font-size:9px;" width="15%"><b>Enlisted By</b></th>
     
-    </tr>
-    <tr style="line-height:10px;">
-        <td colspan="7"></td>        
-    </tr>  
+    </tr>    
     <?php 
     $cs = count($students);
     $i = $snum;
@@ -47,7 +44,7 @@
     <tr style="line-height:10px;text-align:center;">
         <td style="font-size:9px;" ><?php echo $i; ?></td>
         <td style="font-size:9px;" ><?php echo $st['strStudentNumber']; ?></td>
-        <td style="font-size:9px;text-align:left;" width="32%"> <?php echo strtoupper($name); ?></td>        
+        <td style="font-size:9px;"> <?php echo strtoupper($name); ?></td>        
         <td style="font-size:9px;" ><?php echo $st['strProgramCode']; ?></td>
         <td style="font-size:9px;" ><?php echo $st['reg_info']['type_of_class']."-".$st['reg_info']['enumStudentType']; ?></td>
         <td style="font-size:9px;" ><?php echo $st['date_added'];?></td>
