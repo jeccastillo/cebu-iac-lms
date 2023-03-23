00d4b6ea9b0ba738b78ca1df93b5e6e3777c482c
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-03-23 10:23:26 +0800

    Update Data_fetcher.php

--- application/models/Data_fetcher.php
+++ application/models/Data_fetcher.php
@@ -2374,12 +2374,14 @@ class Data_fetcher extends CI_Model {
                      ->result_array();
         else        
             return  $this->db
-                 ->select("intID, strFirstname,strMiddlename,strLastname,strStudentNumber, strGSuiteEmail, strProgramCode")
-                 ->from("tb_mas_registration")
+                 ->select("tb_mas_classlist_student.intCSID,intID, strFirstname,strMiddlename,strLastname,strStudentNumber, strGSuiteEmail, tb_mas_classlist_student.floatFinalGrade,floatPrelimGrade,floatMidtermGrade,floatFinalsGrade,enumStatus,strRemarks, strUnits,strProgramCode,date_added")
+                 ->from("tb_mas_classlist_student")
                  //->group_by("intSubjectID")             
                  ->where(array("tb_mas_registration.intAYID"=>$sem))    
-                 ->join('tb_mas_users', 'tb_mas_users.intID = tb_mas_registration.intStudentID')                 
-                 ->join('tb_mas_programs','tb_mas_programs.intProgramID = tb_mas_users.intProgramID')                                                               
+                 ->join('tb_mas_users', 'tb_mas_users.intID = tb_mas_classlist_student.intStudentID')
+                 ->join('tb_mas_registration','tb_mas_registration.intStudentID = tb_mas_users.intID')
+                 ->join('tb_mas_programs','tb_mas_programs.intProgramID = tb_mas_users.intProgramID')        
+                 ->group_by('tb_mas_users.intID')                                                       
                  ->order_by('strLastName asc, strFirstname asc')
                  ->get()
                  ->result_array();
