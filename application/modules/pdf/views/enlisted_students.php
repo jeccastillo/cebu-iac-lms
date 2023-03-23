a980bca952c9bab2ef8a727c870ee5dbdd9fbae1
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-03-23 10:45:35 +0800

    Update enlisted_students.php

--- application/modules/pdf/views/enlisted_students.php
+++ application/modules/pdf/views/enlisted_students.php
@@ -29,8 +29,8 @@
         <th style="font-size:9px;" width="26%"><b>Student Name</b></th>
         <th style="font-size:9px;" width="10%"><b>Course</b></th>
         <th style="font-size:9px;" width="15%"><b>Enrollment Status</b></th>
-        <th style="font-size:9px;" width="15%"><b>Date Enlisted</b></th>
-        <th style="font-size:9px;" width="15%"><b>Enlisted By</b></th>
+        <th style="font-size:9px;" width="20%"><b>Date Enlisted</b></th>
+        <th style="font-size:9px;" width="10%"><b>Enlisted By</b></th>
     
     </tr>    
     <?php 
