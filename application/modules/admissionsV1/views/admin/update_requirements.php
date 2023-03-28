ca442af9d9e12d22bddee18cc46772451392ccaf
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2023-03-28 14:51:33 +0800

    Update update_requirements.php

--- application/modules/admissionsV1/views/admin/update_requirements.php
+++ application/modules/admissionsV1/views/admin/update_requirements.php
@@ -42,7 +42,7 @@
                            v-if="request.email && request.citizenship == 'Philippines'">                            
                                 <div class="md-w-1/3">
                                     <img src="<?php echo $img_dir; ?>admissions/form/2x2.png"
-                                        class="max-w-full h-auto mx-auto block">
+                                        class="img-responsive">
 
                                     <div class="w-[200px] my-3 block mx-auto">
                                         <input ref="file_2x2" @change="uploadReq('2x2',$event)"
@@ -52,7 +52,7 @@
                                 </div>
                                 <div class="md-w-1/3">
                                     <img src="<?php echo $img_dir; ?>admissions/form/nso.png"
-                                        class="max-w-full h-auto mx-auto block">
+                                        class="img-responsive">
 
                                     <div class="w-[200px] my-3 block mx-auto">
                                         <input ref="file_nso" @change="uploadReq('psa',$event)"
@@ -63,7 +63,7 @@
 
                                 <div class="md-w-1/3">
                                     <img src="<?php echo $img_dir; ?>admissions/form/id.png"
-                                        class="max-w-full h-auto mx-auto block">
+                                        class="img-responsive">
 
                                     <div class="w-[200px] my-3 block mx-auto">
                                         <input ref="file_id" @change="uploadReq('school_id',$event)"
@@ -77,7 +77,7 @@
                            v-if="request.email && request.citizenship != 'Philippines'">
                                 <div class="col-md-3">
                                     <img src="<?php echo $img_dir; ?>admissions/form/upload1.png"
-                                        class="max-w-full h-auto mx-auto block" title='Photocopy of the valid (unexpired) passport pages bearing the bio-page, 
+                                        class="img-responsive" title='Photocopy of the valid (unexpired) passport pages bearing the bio-page, 
                                         the latest admission/arrival in the Philippines with "valid authorized stay" date and the Bureau of Quarantine (BOQ) stamp.
                                         Note: Present the original passport for verification'>                                        
                                     <input ref="file_passport" @change="uploadReq('passport',$event)"
@@ -87,21 +87,21 @@
                                 
                                 <div class="col-md-3">
                                     <img src="<?php echo $img_dir; ?>admissions/form/upload2.png"
-                                        class="max-w-full h-auto mx-auto block" title='Copy of Alien Certificate of Registration (i-CARD) if any'>                                    
+                                        class="img-responsive" title='Copy of Alien Certificate of Registration (i-CARD) if any'>                                    
                                     <input ref="file_icard" @change="uploadReq('icard',$event)"
                                         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                                         type="file">
                                 </div>                                                        
                                 <div class="col-md-3">
                                     <img src="<?php echo $img_dir; ?>admissions/form/upload3.png"
-                                        class="max-w-full h-auto mx-auto block" title='Quarantine Medical Examination by the Bureau of Quarantine (BOQ)'>                                    
+                                        class="img-responsive" title='Quarantine Medical Examination by the Bureau of Quarantine (BOQ)'>                                    
                                     <input ref="file_quarantine" @change="uploadReq('quarantine_med_exam',$event)"
                                         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                                         type="file" required>
                                 </div>
                                 <div class="col-md-3">
                                     <img src="<?php echo $img_dir; ?>admissions/form/upload4.png"
-                                        class="max-w-full h-auto mx-auto block" title='Copy of Birth Certificate.'>
+                                        class="img-responsive" title='Copy of Birth Certificate.'>
                                     <input ref="file_birthcert" @change="uploadReq('birthcert',$event)"
                                         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                                         type="file" required>
@@ -111,28 +111,28 @@
                            v-if="request.email && request.citizenship != 'Philippines'">                                
                                 <div class="col-md-3">
                                     <img src="<?php echo $img_dir; ?>admissions/form/upload5.png"
-                                        class="max-w-full h-auto mx-auto block" title='Original copy of Scholastic Records'>                                    
+                                        class="img-responsive" title='Original copy of Scholastic Records'>                                    
                                     <input ref="file_schrecords" @change="uploadReq('schrecords',$event)"
                                         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                                         type="file">
                                 </div>              
                                 <div class="col-md-3">
                                     <img src="<?php echo $img_dir; ?>admissions/form/upload6.png"
-                                        class="max-w-full h-auto mx-auto block" title='Recommendation letter from the Principal/Guidance Counselor/Class Adviser'>                                    
+                                        class="img-responsive" title='Recommendation letter from the Principal/Guidance Counselor/Class Adviser'>                                    
                                     <input ref="file_recommendation" @change="uploadReq('recommendation',$event)"
                                         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                                         type="file" required>
                                 </div>
                                 <div class="col-md-3">
                                     <img src="<?php echo $img_dir; ?>admissions/form/upload7.png"
-                                        class="max-w-full h-auto mx-auto block" title="Proof of adequate financial support to cover expenses for the student's accommodation and subsistence, as well as school dues and other incidental expenses (to be notarized by a lawyer in the country of residence) and to be authenticated by the Philippine Embassy.">                                    
+                                        class="img-responsive" title="Proof of adequate financial support to cover expenses for the student's accommodation and subsistence, as well as school dues and other incidental expenses (to be notarized by a lawyer in the country of residence) and to be authenticated by the Philippine Embassy.">                                    
                                     <input ref="file_financial_support" @change="uploadReq('financial_support',$event)"
                                         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                                         type="file">
                                 </div>                                
                                 <div class="col-md-3">
                                     <img src="<?php echo $img_dir; ?>admissions/form/upload8.png"
-                                        class="max-w-full h-auto mx-auto block" title='2x2 ID picture (white background with name tag below)'>                                    
+                                        class="img-responsive" title='2x2 ID picture (white background with name tag below)'>                                    
                                     <input ref="file_2x2_foreign" @change="uploadReq('2x2_foreign',$event)"
                                         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                                         type="file" required>
