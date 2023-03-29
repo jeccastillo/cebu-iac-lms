889247f26f2b59034b2a63734b45e2b279368a83
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2023-03-29 14:00:22 +0800

    Update initial_requirements.php

--- application/modules/site/views/student_applicants/initial_requirements.php
+++ application/modules/site/views/student_applicants/initial_requirements.php
@@ -99,18 +99,15 @@
                                     <input ref="file_2x2_foreign" @change="uploadReq('2x2_foreign',$event)"
                                         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
                                         type="file" required>
-                                </div>     
-                                <div class="md-w-1/3">
+                                </div>  
+                                <div class="md-w-1/4">
                                     <img src="<?php echo $img_dir; ?>admissions/form/id.png"
-                                        class="max-w-full h-auto mx-auto block">
-
-                                    <div class="w-[200px] my-3 block mx-auto">
-                                        <input ref="file_id" @change="uploadReq('school_id',$event)"
-                                            class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
-                                            type="file" required>
-                                    </div>
-
-                                </div>                                    
+                                        class="max-w-full h-auto mx-auto block" title='school id'>                                    
+                                    <input ref="file_id" @change="uploadReq('school_id',$event)"
+                                        class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+                                        type="file" required>
+                                </div>     
+                                                                
                         </div>
                         
                    </div>
