a8f24f3709646c8a91396457efc8e13fc3df57f1
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-03-31 05:58:30 +0800

    Update initial_requirements.php

--- application/modules/site/views/student_applicants/initial_requirements.php
+++ application/modules/site/views/student_applicants/initial_requirements.php
@@ -1,4 +1,4 @@
-   <section id="adminssions-form" class="section section_port relative">
+   <section id="adminssions-form" class="section section_port relative pb-[100px]" style="border-bottom: 1px solid #f2f2f2;">
         <div v-if="loading_spinner" wire:loading class="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-gray-700 opacity-75 flex flex-col items-center justify-center">
             <div class="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
             <h2 class="text-center text-white text-xl font-semibold">Loading...</h2>
