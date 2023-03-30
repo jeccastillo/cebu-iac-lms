29de3c67ee533fdf5afb656a60a51b8784da80d1
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-03-30 08:01:50 +0800

    Update initial_requirements.php

--- application/modules/site/views/student_applicants/initial_requirements.php
+++ application/modules/site/views/student_applicants/initial_requirements.php
@@ -52,12 +52,11 @@
                                                 </h2>
                                             </div>
                                         </div>                                        
-                                    </div>     
-                                    <div class="w-[200px] my-3 block mx-auto">
-                                        <input ref="file_2x2" @change="uploadReq('2x2',$event)"
-                                            class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
-                                            type="file" required>
-                                    </div>
+                                    </div>                                     
+                                    <input ref="file_2x2" @change="uploadReq('2x2',$event)"
+                                        class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+                                        type="file" required>
+                                    
                                 </div>
                                 <div class="md-w-1/3">
                                     <img src="<?php echo $img_dir; ?>admissions/form/nso.png"
