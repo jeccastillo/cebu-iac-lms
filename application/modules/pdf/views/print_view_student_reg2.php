df285494943db1bb13bd2620c3ddfb7836a982ae
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-04-17 10:54:20 +0800

    Update print_view_student_reg2.php

--- application/modules/pdf/views/print_view_student_reg2.php
+++ application/modules/pdf/views/print_view_student_reg2.php
@@ -50,14 +50,14 @@
                 <td width="20%" align="center" style="text-align:center;vertical-align: middle;><img src= "https://i.ibb.co/1spYkNx/seal.png"  width="154" height="146"/></td>
                 <td colspan = "3" width="80%" style="text-align: center;line-height:1;">             
                     <font style="font-family:Calibri Light; font-size: 14;font-weight: bold;">Information & Communications Technology Academy </font><br /><br />
-                    <font style="font-family:Calibri Light; font-size: 10;">Filinvest Cebu Cyberzone Tower 2 Salinas Drive corner W. Geonzon St., Brgy. Apas, Lahug, Cebu City</font><br /><br />                               
+                    <font style="font-family:Calibri Light; font-size: 10;">Filinvest Cebu Cyberzone Tower 2 Salinas Drive corner W. Geonzon St., Brgy. Apas, Lahug, Cebu City</font><br />                              
                 </td>  
             </tr>
             <tr>
-            <td colspan = "3" style="font-weight: bold;text-align:center; font-size:11; border-bottom:1px solid #333;">ASSESSMENT/REGISTRATION FORM</td>
+                <td colspan = "2" style="font-weight: bold;text-align:center; font-size:11; border-bottom:1px solid #333;">ASSESSMENT/REGISTRATION FORM</td>
             </tr>
             <tr>            
-                <td colspan = "3" width="100%" style="text-align: center;">             
+                <td colspan = "2" width="100%" style="text-align: center;">             
                     
                 </td>
             </tr>        
