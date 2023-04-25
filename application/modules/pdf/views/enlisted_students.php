200daadd83952840a5c8856860913ec77c2f8f60
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2023-04-25 15:02:04 +0800

    Update enlisted_students.php

--- application/modules/pdf/views/enlisted_students.php
+++ application/modules/pdf/views/enlisted_students.php
@@ -25,8 +25,8 @@
 <table style="font-size:9px;">
     <tr style="line-height:12px;text-align:center;">
         <th style="font-size:8px;" width="3%"><b>#</b></th>
-        <th style="font-size:8px;" width="12%"><b>Student No.</b></th>
-        <th style="font-size:8px;" width="34%"><b>Student Name</b></th>
+        <th style="font-size:8px;" width="11%"><b>Student No.</b></th>
+        <th style="font-size:8px;" width="35%"><b>Student Name</b></th>
         <th style="font-size:8px;" width="9%"><b>Course</b></th>
         <th style="font-size:8px;" width="14%"><b>Enrollment Status</b></th>
         <th style="font-size:8px;" width="14%"><b>Date Enlisted</b></th>
