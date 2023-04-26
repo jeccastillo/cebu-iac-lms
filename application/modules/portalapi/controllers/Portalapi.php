d41c9d1fc7d1f7999490f9cc81c38f2e42fcb2f9
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2023-04-26 09:03:24 +0800

    Update Portalapi.php

--- application/modules/portalapi/controllers/Portalapi.php
+++ application/modules/portalapi/controllers/Portalapi.php
@@ -1,4 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+Header('Access-Control-Allow-Origin: *'); //for allow any domain, insecure
+Header('Access-Control-Allow-Headers: *'); //for allow any headers, insecure
+Header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE'); //method allowed
 
 class PortalApi extends CI_Controller {
 
