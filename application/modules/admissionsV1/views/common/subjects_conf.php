017ff85add140a2c9869cd6018e64f5f1b2e9671
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-05-04 15:36:51 +0800

    Update subjects_conf.php

--- application/modules/admissionsV1/views/common/subjects_conf.php
+++ application/modules/admissionsV1/views/common/subjects_conf.php
@@ -5,24 +5,24 @@
 $(document).ready(function() {
 
     $('#daterange-btn-users').daterangepicker(
-            {
-                ranges: {
-                    'Today': [moment(), moment()],
-                    'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
-                    'Last 7 Days': [moment().subtract('days', 6), moment()],
-                    'This Month': [moment().startOf('month'), moment().endOf('month')],
-                    'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month',1).endOf('month')]
-                },
-                startDate: moment().subtract('days', 29),
-                endDate: moment()
-            },
-            function(start, end) {
-               //var daterange = start.format('YYYY-MM-D') + '/' + end.format('YYYY-MM-D');
-               //document.location="<?php echo base_url(); ?>unity/transactions/"+daterange;
-            }
-            );                   
+    {
+        ranges: {
+            'Today': [moment(), moment()],
+            'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
+            'Last 7 Days': [moment().subtract('days', 6), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+            'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month',1).endOf('month')]
+        },
+        startDate: moment().subtract('days', 29),
+        endDate: moment()
+    },
+    function(start, end) {
+        //var daterange = start.format('YYYY-MM-D') + '/' + end.format('YYYY-MM-D');
+        //document.location="<?php echo base_url(); ?>unity/transactions/"+daterange;
+    }
+    );                   
         
-    });
+    
 
     var filter_status = $("#status_filter").val();
     
