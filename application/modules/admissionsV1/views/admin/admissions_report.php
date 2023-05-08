2ae51a95a9b95c81a65de41b1dbbc09456f9e898
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-05-09 05:48:50 +0800

    Create admissions_report.php

---
+++ application/modules/admissionsV1/views/admin/admissions_report.php
@@ -0,0 +1,79 @@
+<aside class="right-side" id="registration-container">    
+    <section class="content-header">
+        <h1>
+            Admissions Report
+            <small>
+                <a class="btn btn-app" href="<?php echo base_url(); ?>registrar/registrar_reports" >
+                    <i class="ion ion-arrow-left-a"></i>
+                    All Reports
+                </a> 
+            </small>
+        </h1>     
+    </section>
+        <hr />
+    <div class="content">        
+        <h4>Reserved</h4>
+        
+       
+        <!-- <h4>Enrolled</h4>
+        <div v-for="prog in reserved" class="row">
+            <div class="col-md-6">
+                {{ prog.program }}
+            </div>
+            <div class="col-md-6">
+                {{ prog.reserved_count }}
+            </div>
+        </div> -->
+    </div>
+  
+</aside>
+
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/themes/default/js/script.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/vue@2.6.12"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js"
+    integrity="sha512-WFN04846sdKMIP5LKNphMaWzU7YpMyCU245etK3g/2ARYbPK9Ub18eG+ljU96qKRCWh+quCY7yefSmlkQw1ANQ=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.min.js"></script>
+
+<script>
+new Vue({
+    el: '#registration-container',
+    data: {                    
+        base_url: '<?php echo base_url(); ?>',
+        current_sem: '<?php echo $active_sem['intID']; ?>',
+        reserved: undefined,
+        enrolled: undefined,        
+        programs: undefined,
+        all_reserved: 0,
+        all_enrolled: 0,
+                      
+    },
+
+    mounted() {
+
+        let url_string = window.location.href;        
+        if(this.id != 0){            
+            //this.loader_spinner = true;
+            
+            axios.get(api_url + 'admissions/applications/stats/all?current_sem='+this.current_sem)
+            .then((data) => {  
+                console.log(data);
+                //this.reserved = data.data;                                                         
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+                
+        }
+
+    },
+
+    methods: {      
+       
+                                       
+    }
+
+})
+</script>
+
