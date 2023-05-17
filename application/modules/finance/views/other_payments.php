47fed0a2f31e3955994b38eec78cf61bc64fa2b4
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2023-05-17 12:47:07 +0800

    Update other_payments.php

--- application/modules/finance/views/other_payments.php
+++ application/modules/finance/views/other_payments.php
@@ -2,6 +2,9 @@
 <aside class="right-side">
     <div id="vue-container">
         <section class="content-header">
+            <small>
+                <a class="btn btn-app" :href="base_url + 'finance/payments/0/1'" ><i class="ion ion-arrow-left-a"></i>All Students Applicants</a>                                                                                                                     
+            </small>            
             <h1>                                
                 Other Payments
             </h1>
