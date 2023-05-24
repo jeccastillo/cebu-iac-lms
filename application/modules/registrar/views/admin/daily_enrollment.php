67824a18ecd86d94120899d0ebb5a4848caa7adf
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-05-24 12:14:54 +0800

    Update daily_enrollment.php

--- application/modules/registrar/views/admin/daily_enrollment.php
+++ application/modules/registrar/views/admin/daily_enrollment.php
@@ -64,8 +64,8 @@ new Vue({
             .then((data) => {       
                 var formdata= new FormData();
                 formdata.append('applicant_data', JSON.stringify(data.data.data)); 
-                form.append('start','<?php echo ($start!=0)?$start:date("Y-m-d"); ?>');                            
-                form.append('end','<?php echo ($end!=0)?$end:date("Y-m-d"); ?>');
+                formdata.append('start','<?php echo ($start!=0)?$start:date("Y-m-d"); ?>');                            
+                formdata.append('end','<?php echo ($end!=0)?$end:date("Y-m-d"); ?>');
                 axios.post(this.base_url + 'registrar/daily_enrollment_report_data/',formdata, {
                     headers: {
                         Authorization: `Bearer ${window.token}`
