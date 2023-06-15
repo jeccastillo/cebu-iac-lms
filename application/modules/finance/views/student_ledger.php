96ee9993ac86586855fb1278024bd5ff3c4bd76f
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-06-15 11:20:55 +0800

    Update student_ledger.php

--- application/modules/finance/views/student_ledger.php
+++ application/modules/finance/views/student_ledger.php
@@ -96,13 +96,31 @@ new Vue({
         running_balance: 0,
         sy: undefined,        
         request:{
-            date: new Date().toISOString().slice(0,10),
+            date: undefined,
             name: undefined,
             syid: 0,
             amount: undefined,            
         }
     },
     mounted() {        
+        var now = new Date();
+        var year = now.getFullYear();
+        var month = now.getMonth() + 1;
+        var day = now.getDate();
+        var hour = now.getHours();
+        var minute = now.getMinutes();
+        var localDatetime =
+        year +
+        '-' +
+        (month < 10 ? '0' + month.toString() : month) +
+        '-' +
+        (day < 10 ? '0' + day.toString() : day) +
+        'T' +
+        (hour < 10 ? '0' + hour.toString() : hour) +
+        ':' +
+        (minute < 10 ? '0' + minute.toString() : minute);
+        this.request.date = localDatetime;
+        
         axios
             .get(base_url + 'finance/student_ledger_data/' + this.id, {
                 headers: {
