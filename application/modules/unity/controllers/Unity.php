d7839ff649ae1ab4d5e4232b9308af19f2d2ba37
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-06-29 09:12:01 +0800

    Update Unity.php

--- application/modules/unity/controllers/Unity.php
+++ application/modules/unity/controllers/Unity.php
@@ -1633,9 +1633,9 @@ class Unity extends CI_Controller {
     {
         if($this->faculty_logged_in())
         {
-            $active_sem = $this->data_fetcher->get_active_sem();
-            
+            $active_sem = $this->data_fetcher->get_active_sem();            
             $this->data['classlist'] = $this->data_fetcher->fetch_classlist_by_faculty($this->session->userdata('intID'),$active_sem['intID']);
+            print_r($this->data['classlist']);
             $this->data['page'] = "view_classlist";
             $this->load->view("common/header",$this->data);
             $this->load->view("faculty/classlist",$this->data);
