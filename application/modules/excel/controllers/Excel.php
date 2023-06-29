478c3e9545c23687cff102f1133aaf7f1edcd61f
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-06-29 12:27:59 +0800

    Update Excel.php

--- application/modules/excel/controllers/Excel.php
+++ application/modules/excel/controllers/Excel.php
@@ -213,6 +213,10 @@ class Excel extends CI_Controller {
         $students = $this->data_fetcher->getClassListStudents($id);
         $subject = $this->data_fetcher->getSubjectNoCurr($classlist['intSubjectID']);
         
+      
+
+
+
         error_reporting(E_ALL);
         ini_set('display_errors', TRUE);
         ini_set('display_startup_errors', TRUE);
@@ -226,12 +230,16 @@ class Excel extends CI_Controller {
 
         // Set document properties
         $objPHPExcel->getProperties()->setCreator("Jec Castillo")
-                                     ->setLastModifiedBy("Jec Castillo")
-                                     ->setTitle($subject['strCode']." ".$classlist['strSection'])
-                                     ->setSubject("Classlist Download")
-                                     ->setDescription("Classlist Download.")
-                                     ->setKeywords("office 2007 openxml php")
-                                     ->setCategory("Classlist");
+                                        ->setLastModifiedBy("Jec Castillo")
+                                        ->setTitle("Export Classlist")
+                                        ->setSubject("Classlist Download")
+                                        ->setDescription("Export Leads Download.")
+                                        ->setKeywords("office 2007 openxml php")
+                                        ->setCategory("Classlist");
+    
+            
+          
+            
 
 
         $objPHPExcel->setActiveSheetIndex(0)
@@ -323,24 +331,24 @@ class Excel extends CI_Controller {
         $objPHPExcel->setActiveSheetIndex(0);
 
 
-        // Redirect output to a client’s web browser (Excel2007)
-        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        header('Content-Disposition: attachment;filename="'.$subject['strCode']."-".$classlist['strSection'].'-classlist.xlsx"');
-        header('Cache-Control: max-age=0');
-        // If you're serving to IE 9, then the following may be needed
-        header('Cache-Control: max-age=1');
-
-        // If you're serving to IE over SSL, then the following may be needed
-        header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
-        header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
-        header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
-        header ('Pragma: public'); // HTTP/1.0
-
-        $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
-        $objWriter->save('php://output');
-        //$objWriter->save('assets/excel/'.$subject['strCode']."-".$classlist['strSection'].'-classlist.xlsx');
-        //unlink('assets/excel/'.$subject['strCode']."-".$classlist['strSection'].'-classlist.xlsx');
-        exit;
+         $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+ 
+         // Redirect output to a client’s web browser (Excel2007)
+         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');      
+         header('Content-Disposition: attachment;filename="export_leads'.$date.'.xls"');
+         header('Cache-Control: max-age=0');
+         // If you're serving to IE 9, then the following may be needed
+         header('Cache-Control: max-age=1');
+ 
+         // If you're serving to IE over SSL, then the following may be needed
+         header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+         header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
+         header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+         header ('Pragma: public'); // HTTP/1.0
+ 
+         
+         $objWriter->save('php://output');
+         exit;
     }
     
     public function download_schedules($id)
