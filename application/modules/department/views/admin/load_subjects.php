03201f546240515a158dd57caef26dfb41266d30
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-06-29 08:49:00 +0800

    Update load_subjects.php

--- application/modules/department/views/admin/load_subjects.php
+++ application/modules/department/views/admin/load_subjects.php
@@ -79,11 +79,12 @@
                                         <?php foreach($class['schedule'] as $sched):
                                                 if(isset($sched['dteStart'])):                                
                                                     $hourdiff = round((strtotime($sched['dteEnd']) - strtotime($sched['dteStart']))/3600, 1);
+                                                    $section = $class['strClassName'].$class['year'].$class['strSection']." ".$class['sub_section'];
                                         ?>
                                                 <input type="hidden" class="<?php echo $sched['strDay']; ?>"
                                                     value="<?php echo date('gia',strtotime($sched['dteStart'])); ?>"
                                                     href="<?php echo $hourdiff*2; ?>"
-                                                    rel="<?php echo $class['strCode']; ?> <?php echo $sched['strRoomCode']; ?>"
+                                                    rel="<?php echo $section; ?> <?php echo $sched['strRoomCode']; ?>"
                                                     data-section="<?php echo $class['strSection']; ?>">
                                         
                                         <?php 
