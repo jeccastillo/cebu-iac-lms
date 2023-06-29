2bf684a6e9589258b87e8d48ca773edd26a41e82
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-06-29 08:46:54 +0800

    Update load_subjects.php

--- application/modules/department/views/admin/load_subjects.php
+++ application/modules/department/views/admin/load_subjects.php
@@ -44,6 +44,77 @@
         
     
         <div class="col-sm-12">
+            <div class="box box-solid box-primary">
+                <div class="box-header">
+                    <h4 class="box-title">Schedule View of Courses Handled - 
+                        <?php echo "A.Y." . " " .$active_sem['strYearStart']."-".$active_sem['strYearEnd'] . " " . $active_sem['enumSem']." ".$term_type ; ?>
+                    </h4>
+                </div><!-- /.box-header -->
+                <div class="box-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Class Section</th>
+                                <th>Course Code</th>
+                                <th>Course Description</th>
+                                <th>Units</th>
+                                <th>Schedule</th>
+                                <th>Remarks</th>
+        <!--                        <th>Action</th>-->
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                                <?php if(!empty($classlist)):
+                                        foreach($classlist as $class): ?>
+                                <tr>
+                                    <td><?php echo $class['strSection']; ?></td>
+                                    <td><?php echo $class['strClassName']; ?></td>
+                                    <td><?php echo $class['strDescription']; ?></td>
+                                    <td><?php echo $class['strUnits']; ?></td>
+                                    <?php if(!empty($class['schedule'])): ?>
+
+                                    <td>
+                                        <?php echo $class['schedule']['schedString']; ?>
+                                        <?php foreach($class['schedule'] as $sched):
+                                                if(isset($sched['dteStart'])):                                
+                                                    $hourdiff = round((strtotime($sched['dteEnd']) - strtotime($sched['dteStart']))/3600, 1);
+                                        ?>
+                                                <input type="hidden" class="<?php echo $sched['strDay']; ?>"
+                                                    value="<?php echo date('gia',strtotime($sched['dteStart'])); ?>"
+                                                    href="<?php echo $hourdiff*2; ?>"
+                                                    rel="<?php echo $class['strCode']; ?> <?php echo $sched['strRoomCode']; ?>"
+                                                    data-section="<?php echo $class['strSection']; ?>">
+                                        
+                                        <?php 
+                                                endif;
+                                            endforeach; 
+                                        ?>
+                                    </td>
+                                    <?php else: ?>
+                                        <td></td>
+
+                                    <?php endif; ?>
+                                    <td>
+                                        <?php if($class['intFinalized'] == 1): ?>
+                                            Submitted
+                                        <?php else: ?>
+                                            Not Yet Submitted
+                                        <?php endif;  ?>
+                                    </td>                           
+                                </tr>
+                                    <?php endforeach; ?>
+                                    <?php else: ?>
+                                <tr>
+                                    <th>No Classlists for this term</th>
+                                </tr>
+                                <?php endif; ?>
+
+                            </tbody>
+
+                    </table>
+                </div>
+            </div>
             <div class="box box-primary">
                 <div class="box-body">
                     <form method="post" action="<?php echo base_url() ?>pdf/print_sched">   
@@ -298,77 +369,76 @@
                     </form>     
                 </div>
             </div>
-                <div class="box box-primary">
-                    <div class="box-header">
-                        <h3>Load Classlists</h3>
-                        
-                    </div>
-                                   
-                    <div class="box-body">
-                        <div class="row">
-                            <div class="col-md-5">
-                                <h4>Days Available</h4>
-                                <select style="height:200px" class="form-control" id="day-selector" multiple>
-                                    <option value="1">Monday</option>
-                                    <option value="2">Tuesday</option>
-                                    <option value="3">Wednesday</option>
-                                    <option value="4">Thursday</option>
-                                    <option value="5">Friday</option>
-                                    <option value="6">Saturday</option>
-                                </select>
-                            </div>
-                            <div class="col-md-2">
-                                <br /><br />
-                                <!--a href="#" class="btn btn-default  btn-flat btn-block" id="autoload-advised">Autoload <br /> Subjects </a-->
-                                <a href="#" id="load-days" class="btn btn-default  btn-flat btn-block">Load <i class="ion ion-arrow-right-c"></i> </a>
-                                <a href="#" id="unload-days" class="btn btn-default  btn-flat btn-block"><i class="ion ion-arrow-left-c"></i> Remove</a>
-                                
-                            </div>
-                            <div class="col-md-5">
-                                <h4>Days</h4>
-                                <select style="height:200px" class="form-control" id="day-selected" multiple>
-                                    
-                                </select>
-                            </div>
+            <div class="box box-primary">
+                <div class="box-header">
+                    <h3>Load Classlists</h3>
+                    
+                </div>
+                            
+                <div class="box-body">
+                    <div class="row">
+                        <div class="col-md-5">
+                            <h4>Days Available</h4>
+                            <select style="height:200px" class="form-control" id="day-selector" multiple>
+                                <option value="1">Monday</option>
+                                <option value="2">Tuesday</option>
+                                <option value="3">Wednesday</option>
+                                <option value="4">Thursday</option>
+                                <option value="5">Friday</option>
+                                <option value="6">Saturday</option>
+                            </select>
                         </div>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <h4>Classlists</h4>
-                                <select style="height:300px" class="select2" id="classlist-selector" multiple>
-                                    <?php foreach($all_classlist as $sn):                                         
-                                        $section = $sn['strClassName'].$sn['year'].$sn['strSection']." ".$sn['sub_section'];
-                                        ?>
-                                        
-                                        <option value="<?php echo $sn['intID']; ?>"><?php echo $sn['strCode']." Section: ".$section." Desc: ".$sn['strDescription']." Schedule:".$sn['schedule']['schedString']; ?></option>
-                                    <?php endforeach; ?>
-                                </select>
-                            </div>
-                            <div class="col-md-12" style="text-center">
-                               <br />
-                                <a href="#" id="load-classlist" class="btn btn-default  btn-flat">Load <i class="ion ion-arrow-down-c"></i> </a>
-                                <a href="#" id="unload-classlist" class="btn btn-default  btn-flat"><i class="ion ion-arrow-up-c"></i> Remove</a>
-                               
-                            </div>
-                            <div class="col-md-12">
-                                <h4>Loaded Classlists</h4>
-                                <select style="height:300px" class="form-control" id="loaded-classlist" multiple>
-                                    <?php foreach($faculty_classlist as $sn): ?>
-                                        <option value="<?php echo $sn['intID']; ?>"><?php echo $sn['strCode']." ".$sn['strSection']." Desc: ".$sn['strDescription']; ?></option>
-                                    <?php endforeach; ?>
-                                </select>
-                            </div>
-                            <div class="col-md-12" style="text-center">
-                                <br />
-                           
-                                <a href="#" id="save-classlist" class="btn btn-default  btn-flat">Save</a>
+                        <div class="col-md-2">
+                            <br /><br />
+                            <!--a href="#" class="btn btn-default  btn-flat btn-block" id="autoload-advised">Autoload <br /> Subjects </a-->
+                            <a href="#" id="load-days" class="btn btn-default  btn-flat btn-block">Load <i class="ion ion-arrow-right-c"></i> </a>
+                            <a href="#" id="unload-days" class="btn btn-default  btn-flat btn-block"><i class="ion ion-arrow-left-c"></i> Remove</a>
+                            
+                        </div>
+                        <div class="col-md-5">
+                            <h4>Days</h4>
+                            <select style="height:200px" class="form-control" id="day-selected" multiple>
                                 
-                            </div>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h4>Classlists</h4>
+                            <select style="height:300px" class="select2" id="classlist-selector" multiple>
+                                <?php foreach($all_classlist as $sn):                                         
+                                    $section = $sn['strClassName'].$sn['year'].$sn['strSection']." ".$sn['sub_section'];
+                                    ?>
+                                    
+                                    <option value="<?php echo $sn['intID']; ?>"><?php echo $sn['strCode']." Section: ".$section." Desc: ".$sn['strDescription']." Schedule:".$sn['schedule']['schedString']; ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
+                        <div class="col-md-12" style="text-center">
+                        <br />
+                            <a href="#" id="load-classlist" class="btn btn-default  btn-flat">Load <i class="ion ion-arrow-down-c"></i> </a>
+                            <a href="#" id="unload-classlist" class="btn btn-default  btn-flat"><i class="ion ion-arrow-up-c"></i> Remove</a>
+                        
+                        </div>
+                        <div class="col-md-12">
+                            <h4>Loaded Classlists</h4>
+                            <select style="height:300px" class="form-control" id="loaded-classlist" multiple>
+                                <?php foreach($faculty_classlist as $sn): ?>
+                                    <option value="<?php echo $sn['intID']; ?>"><?php echo $sn['strCode']." ".$sn['strSection']." Desc: ".$sn['strDescription']; ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
+                        <div class="col-md-12" style="text-center">
+                            <br />
+                    
+                            <a href="#" id="save-classlist" class="btn btn-default  btn-flat">Save</a>
+                            
                         </div>
                     </div>
                 </div>
             </div>
-              
-            </div>
+        </div>              
+    </div>
             <!-- /.tab-content -->
           </div>
         </div>
