eec21f5a59d5cdb494d0fdceb63ecb95484ada48
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-07-06 11:03:26 +0800

    Update Data_fetcher.php

--- application/models/Data_fetcher.php
+++ application/models/Data_fetcher.php
@@ -2548,7 +2548,7 @@ class Data_fetcher extends CI_Model {
             $where['tb_mas_registration.intAYID'] = $sem;
         // if($year != 0)
         //     $where['tb_mas_registration.intYearLevel'] = $year;
-        
+        print_r($where);
         return  $this->db
                 ->select("tb_mas_classlist_student.intCSID,tb_mas_users.intID, tb_mas_users.strFirstname,tb_mas_users.strMiddlename,tb_mas_users.strLastname,strStudentNumber, strGSuiteEmail, tb_mas_classlist_student.floatFinalGrade,floatPrelimGrade,floatMidtermGrade,floatFinalsGrade,enumStatus,strRemarks, strUnits,strProgramCode,date_added,tb_mas_faculty.strUsername as fusername")
                 ->from("tb_mas_classlist_student")
