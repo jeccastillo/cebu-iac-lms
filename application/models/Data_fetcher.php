6e7ab68f926b55ea0b1da79215c21cc5822ac7a4
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-07-06 11:02:54 +0800

    Update Data_fetcher.php

--- application/models/Data_fetcher.php
+++ application/models/Data_fetcher.php
@@ -2543,41 +2543,27 @@ class Data_fetcher extends CI_Model {
             else
                 $gender = "female";
             $where['tb_mas_users.enumGender'] = $gender;
-        }
-
-        if($sem == 0){
-            return  $this->db
-                     ->select("tb_mas_classlist_student.intCSID,intID, strFirstname,strMiddlename,strLastname,strStudentNumber, strGSuiteEmail, tb_mas_classlist_student.floatFinalGrade,floatPrelimGrade,floatMidtermGrade,floatFinalsGrade,enumStatus,strRemarks, strUnits,strProgramCode,date_added")
-                     ->from("tb_mas_classlist_student")
-                     //->group_by("intSubjectID")
-                     ->where($where)
-                     ->join('tb_mas_users', 'tb_mas_users.intID = tb_mas_classlist_student.intStudentID')
-                     ->join('tb_mas_programs','tb_mas_programs.intProgramID = tb_mas_users.intProgramID')                                                               
-                     ->order_by('strLastName asc, strFirstname asc')
-                     ->get()
-                     ->result_array();
-        }
-        else{        
-            if($sem != 0)
-                $where['tb_mas_registration.intAYID'] = $sem;
-            // if($year != 0)
-            //     $where['tb_mas_registration.intYearLevel'] = $year;
-            
-            return  $this->db
-                 ->select("tb_mas_classlist_student.intCSID,tb_mas_users.intID, tb_mas_users.strFirstname,tb_mas_users.strMiddlename,tb_mas_users.strLastname,strStudentNumber, strGSuiteEmail, tb_mas_classlist_student.floatFinalGrade,floatPrelimGrade,floatMidtermGrade,floatFinalsGrade,enumStatus,strRemarks, strUnits,strProgramCode,date_added,tb_mas_faculty.strUsername as fusername")
-                 ->from("tb_mas_classlist_student")
-                 //->group_by("intSubjectID")             
-                 ->where($where)    
-                 ->where(array("tb_mas_registration.intROG"=>0))
-                 ->join('tb_mas_users', 'tb_mas_users.intID = tb_mas_classlist_student.intStudentID')
-                 ->join('tb_mas_registration','tb_mas_registration.intStudentID = tb_mas_users.intID')
-                 ->join('tb_mas_programs','tb_mas_programs.intProgramID = tb_mas_users.intProgramID')
-                 ->join('tb_mas_faculty','tb_mas_faculty.intID = tb_mas_registration.enlisted_by','left')        
-                 ->group_by('tb_mas_users.intID')                                                       
-                 ->order_by('strLastName asc, strFirstname asc')
-                 ->get()
-                 ->result_array();
-        }
+        }        
+        if($sem != 0)
+            $where['tb_mas_registration.intAYID'] = $sem;
+        // if($year != 0)
+        //     $where['tb_mas_registration.intYearLevel'] = $year;
+        
+        return  $this->db
+                ->select("tb_mas_classlist_student.intCSID,tb_mas_users.intID, tb_mas_users.strFirstname,tb_mas_users.strMiddlename,tb_mas_users.strLastname,strStudentNumber, strGSuiteEmail, tb_mas_classlist_student.floatFinalGrade,floatPrelimGrade,floatMidtermGrade,floatFinalsGrade,enumStatus,strRemarks, strUnits,strProgramCode,date_added,tb_mas_faculty.strUsername as fusername")
+                ->from("tb_mas_classlist_student")
+                //->group_by("intSubjectID")             
+                ->where($where)    
+                ->where(array("tb_mas_registration.intROG"=>0))
+                ->join('tb_mas_users', 'tb_mas_users.intID = tb_mas_classlist_student.intStudentID')
+                ->join('tb_mas_registration','tb_mas_registration.intStudentID = tb_mas_users.intID')
+                ->join('tb_mas_programs','tb_mas_programs.intProgramID = tb_mas_users.intProgramID')
+                ->join('tb_mas_faculty','tb_mas_faculty.intID = tb_mas_registration.enlisted_by','left')        
+                ->group_by('tb_mas_users.intID')                                                       
+                ->order_by('strLastName asc, strFirstname asc')
+                ->get()
+                ->result_array();
+        
     }
     
     function checkStudentSubject($sem,$subjectId,$studentId)
