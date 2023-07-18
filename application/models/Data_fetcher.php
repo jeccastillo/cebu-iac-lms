49f6cc0eb9a2f6a30400bbd34bfcfc059c231613
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-07-18 14:13:01 +0800

    Update Data_fetcher.php

--- application/models/Data_fetcher.php
+++ application/models/Data_fetcher.php
@@ -2632,7 +2632,7 @@ class Data_fetcher extends CI_Model {
                                           ->get('tb_mas_classlist_student_adjustment_log')
                                           ->first_row('array');
 
-                $schedule = $this->getScheduleByCode($id);        
+                $schedule = $this->getScheduleByCode($classlist);        
                 $sched_day = '';
                 $sched_time = '';
                 $sched_room = '';                
