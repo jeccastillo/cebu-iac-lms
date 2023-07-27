223467dd368c32d49e3d982fff7cb7b056aba2a9
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-07-27 13:37:44 +0800

    Update classlist_view_admin.php

--- application/modules/unity/views/admin/classlist_view_admin.php
+++ application/modules/unity/views/admin/classlist_view_admin.php
@@ -9,10 +9,10 @@
                                 <a class="btn btn-app" href="<?php echo base_url()."unity/view_classlist_archive_admin/".$selected_ay."/".$program."/0" ?>"><i class="fa fa-list"></i> Show Non Dissolved</a>
                             <?php endif; ?>
                             <?php if($has_faculty == 0): ?>
-                                <a class="btn btn-app" href="<?php echo base_url()."unity/view_classlist_archive_admin/".$selected_ay."/".$program."/".$dissolved."/1" ?>" ><i class="fa fa-list"></i> Faculty Loading Summary</a>                                
+                                <a class="btn btn-app" href="<?php echo base_url()."unity/view_classlist_archive_admin/".$selected_ay."/".$program."/".$dissolved."/1" ?>" ><i class="fa fa-user"></i> Faculty Loading Summary</a>                                
                             <?php else: ?>
                                 <a class="btn btn-app" href="<?php echo base_url() ?>excel/download_classlists/<?php echo $selected_ay.'/'.$program.'/'.$dissolved.'/1'; ?>"><i class="fa fa-download"></i> Download Report</a>                                
-                                <a class="btn btn-app" href="<?php echo base_url()."unity/view_classlist_archive_admin/".$selected_ay."/".$program."/".$dissolved."/0"?>"><i class="fa fa-list"></i> Show All</a>
+                                <a class="btn btn-app" href="<?php echo base_url()."unity/view_classlist_archive_admin/".$selected_ay."/".$program."/".$dissolved."/0"?>"><i class="fa fa-file"></i> Show All</a>
                             <?php endif; ?>
 
                         </small>
