435e9df6c24cfe502d86af7c71f5f2a5bd7e964d
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-08-01 13:08:45 +0800

    Update cashier.php

--- application/modules/finance/views/cashier.php
+++ application/modules/finance/views/cashier.php
@@ -141,7 +141,7 @@ new Vue({
             document.getElementById(id).focus();
         },
         changeValue: function(id){
-            console.log(this.$refs['or_start'+id].value);
+            console.log(this.$refs.['or_start'+id].value);
             // var formdata = new FormData();
             // formdata.append('intID',id);
             // formdata.append(event.target.value); 
