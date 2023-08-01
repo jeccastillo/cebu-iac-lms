ac0c58b186ee1d3194cedaece44f1077fd3334f6
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-08-01 10:34:49 +0800

    Update edit_grading_conf.php

--- application/modules/grading/views/common/edit_grading_conf.php
+++ application/modules/grading/views/common/edit_grading_conf.php
@@ -1,6 +1,7 @@
 <script type="text/javascript">
     $(document).ready(function(){
         $("#add-grade-line").click(function(e){
+            e.preventDefault();
             $("#item-container").append('<div class="row"><div class="col-sm-4"><input type="text" name="item[]" class="form-control" placeholder="Enter Value" /></div></div>');
         })
     });
