8fbea03901b93c65b320c3ad051af57d577c29c1
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-08-01 11:41:50 +0800

    Update Unity.php

--- application/modules/unity/controllers/Unity.php
+++ application/modules/unity/controllers/Unity.php
@@ -1802,7 +1802,8 @@ class Unity extends CI_Controller {
             $this->data['classlist'] = $this->data_fetcher->fetch_classlist_by_id(null,$id);
             if(!$this->data['classlist']['grading_system'])
                 $this->data['classlist']['grading_system'] = 1;
-            $this->data['grading_items'] = $this->db->get_where('tb_mas_grading_item',array("grading_id"=>$this->data['classlist']['grading_system']))
+            $this->data['grading_items'] = $this->db->get('tb_mas_grading_item')
+                                                    ->where(array("grading_id"=>$this->data['classlist']['grading_system']))
                                                     ->order_by('value','ASC')
                                                     ->result_array();
             
