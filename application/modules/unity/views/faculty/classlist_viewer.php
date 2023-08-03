8f43b081d55135b40df3333de4b7955711c0338d
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-08-03 12:53:00 +0800

    Update classlist_viewer.php

--- application/modules/unity/views/faculty/classlist_viewer.php
+++ application/modules/unity/views/faculty/classlist_viewer.php
@@ -85,7 +85,7 @@
                                     <?php if($classlist['is_numerical']): ?>
                                         <?php if(!empty($student['registered'])): ?>                                       
                                         <td>
-                                            <select <?php echo (($classlist['intFinalized'] >= 1 || $active_midterm_grading['enumMGradingPeriod'] != 'active')  && !$is_super_admin)?'disabled':''; ?> id="inputMidtermID-<?php echo $student['intCSID']; ?>"class="midtermInput grade-input form-control" rel="<?php echo $student['intCSID'] ?>" value="<?php echo $student['floatMidtermGrade']; ?>">                              
+                                            <select <?php echo (($classlist['intFinalized'] >= 1 || ($active_midterm_grading['midterm_start'] <= date("Y-m-d") && $active_midterm_grading['midterm_end'] >= date("Y-m-d")) != 'active')  && !$is_super_admin)?'disabled':''; ?> id="inputMidtermID-<?php echo $student['intCSID']; ?>"class="midtermInput grade-input form-control" rel="<?php echo $student['intCSID'] ?>" value="<?php echo $student['floatMidtermGrade']; ?>">                              
                                                 <?php foreach($grading_items as $grading_item): ?>
                                                     <option <?php echo $student['floatMidtermGrade'] == $grading_item['value']?'selected':''; ?> value="<?php echo $grading_item['value']; ?>"><?php echo $grading_item['value']; ?></option>
                                                 <?php endforeach; ?>                                                                                                
