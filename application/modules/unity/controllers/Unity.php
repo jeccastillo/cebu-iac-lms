d3de025aa91deb0935765a6b231724ee903fa867
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-08-08 13:08:04 +0800

    Update Unity.php

--- application/modules/unity/controllers/Unity.php
+++ application/modules/unity/controllers/Unity.php
@@ -1830,7 +1830,7 @@ class Unity extends CI_Controller {
                     $this->data['classlist']['midterm_end']  = $ext['date'];
             }
 
-            echo $this->data['classlist']['midterm_end'];
+            print_r($mx);
                 
             if($fx){
                 $ext = $this->db->get_where('tb_mas_sy_grading_extension_faculty',array('faculty_id'=>$clist['intFacultyID'],'grading_extension_id'=>$fx['id']))
