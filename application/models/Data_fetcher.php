28b7dfbd76fbbb646656a0ef0f8eaaa6630f952e
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-08-10 14:26:43 +0800

    Update Data_fetcher.php

--- application/models/Data_fetcher.php
+++ application/models/Data_fetcher.php
@@ -421,8 +421,43 @@ class Data_fetcher extends CI_Model {
         
         //echo $this->db->last_query();
         //print_r($subjects);
-       
-        return $subjects;
+       //PREREQUISITES CODE----------------------------------------------------------------------------
+        
+       foreach($subjects as $subj)
+       {
+           $add = true;
+           
+           $r = $this->db
+                     ->get_where('tb_mas_prerequisites',array('intSubjectID'=>$subj['intID']))
+                     ->result_array();
+           
+           if(!empty($r))
+           {
+               foreach($r as $res){
+                   $s = $this->db
+                     ->select('tb_mas_classlist_student.intCSID')
+                     ->from('tb_mas_classlist_student')
+                     ->join('tb_mas_classlist','tb_mas_classlist.intID = tb_mas_classlist_student.intClassListID')
+                     ->where(array('intSubjectID'=>$res['intPrerequisiteID'],'strRemarks'=>'Passed','intStudentID'=>$studentID))
+                     ->get()
+                     ->result_array();
+                   
+                   if(empty($s))
+                   {
+                       $add = false;
+                       break;
+                   }
+                   
+               }
+               
+               
+           }
+           
+           if($add)
+               $ret[] = $subj;
+                   
+       }
+       return $ret;
 
     }
 
