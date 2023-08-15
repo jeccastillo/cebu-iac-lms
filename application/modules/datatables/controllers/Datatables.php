d129b5e7ad8d679c7359b9b9b0e5abcd5cc20964
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2023-08-15 13:47:05 +0800

    Update Datatables.php

--- application/modules/datatables/controllers/Datatables.php
+++ application/modules/datatables/controllers/Datatables.php
@@ -1604,7 +1604,7 @@ class Datatables extends CI_Controller {
             {
                 switch($level){
                     case 1:
-                        $sWhere .= "AND ".$table.".ba = 'shs' ";
+                        $sWhere .= "AND ".$table.".level = 'shs' ";
                     break;
                     case 2:
                         $sWhere .= "AND ".$table.".level = 'college' ";
@@ -1621,7 +1621,7 @@ class Datatables extends CI_Controller {
             {
                 switch($level){
                     case 1:
-                        $sWhere .= "WHERE ".$table.".ba = 'shs' ";
+                        $sWhere .= "WHERE ".$table.".level = 'shs' ";
                     break;
                     case 2:
                         $sWhere .= "WHERE ".$table.".level = 'college' ";
