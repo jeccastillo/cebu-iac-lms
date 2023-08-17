46531bfcbc4c9bee5b4e20d7d339435fe9a11749
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-08-17 15:54:08 +0800

    Update registration_viewer.php

--- application/modules/unity/views/admin/registration_viewer.php
+++ application/modules/unity/views/admin/registration_viewer.php
@@ -573,7 +573,7 @@ new Vue({
                                     if(this.tuition_data.installment_fee > temp){
                                         this.installments.push(this.tuition_data.installment_fee - temp);
                                         temp = 0;
-                                        console.log(this.tuition_data.installment_fee+" "+this.remaining_amount_formatted);
+                                        console.log(this.tuition_data.installment_fee+" "+(this.tuition_data.installment_fee * 5));
                                     }
                                     else{
                                         this.installments.push(0);
