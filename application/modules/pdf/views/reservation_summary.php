c6a6c616a4dbc9b94211c8ebc0f25c8ea103835d
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-08-18 11:32:46 +0800

    updated

--- application/modules/pdf/views/reservation_summary.php
+++ application/modules/pdf/views/reservation_summary.php
@@ -1,138 +1,111 @@
-<aside class="right-side" id="registration-container">    
+<?php
+
+    tcpdf();
+    // create new PDF document
+    //$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+    //$pdf = new TCPDF("P", PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+    $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+    // set document information
+    $pdf->SetCreator(PDF_CREATOR);
+    $pdf->SetTitle("Reservation Summary");
     
-    <table ref="ret" v-if="reserved" class="table table-bordered table-striped">
-        <tr>
-            <th>Program</th>
-            <th>Freshman</th>
-            <th>Transferee</th>
-            <th>Foreign</th>
-            <th>Second Degree</th>
-            <th>Total</th>
-        </tr>
-        <tr v-for="prog in reserved">
-            <td>{{ prog[0].program }}</td>
-            <td v-for="type in prog" v-if="type.student_type == 'freshman'">
-                {{ type.reserved_count }}
-            </td>
-            <td v-if="r_fresh[prog[0].type_id] == false">
-                0
-            </td>
-            <td v-for="type in prog" v-if="type.student_type == 'transferee'">
-                {{ type.reserved_count }}
-            </td>
-            <td v-if="r_trans[prog[0].type_id] == false">
-                0
-            </td>
-            <td v-for="type in prog" v-if="type.student_type == 'foreign'">
-                {{ type.reserved_count }}
-            </td>
-            <td v-if="r_foreign[prog[0].type_id] == false">
-                0
-            </td>
-            <td v-for="type in prog" v-if="type.student_type == 'second degree'">
-                {{ type.reserved_count }}
-            </td>
-            <td v-if="r_sd[prog[0].type_id] == false">
-                0
-            </td>
-            <td>
-                {{ totals[prog[0].type_id] }}
-            </td>
-        </tr>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td><strong>{{ all_reserved }}</strong></td>
-        </tr>
-    </table>            
+    // set margins
+    $pdf->SetMargins(10, 20 , 10);
+    $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+    $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+    $pdf->SetFont('helvetica','',10);
+    //$pdf->SetAutoPageBreak(TRUE, 6);
+    
+   //font setting
+    //$pdf->SetFont('calibril_0', '', 10, '', 'false');
+    
+    $pdf->setPrintHeader(false);
+    $pdf->setPrintFooter(false);
+    // Add a page
+    // This method has several options, check the source code documentation for more information.
+    $pdf->AddPage('P', 'LEGAL');            
 
-</aside>
+    
+    
+    // Set some content to print
+    $html = '<table border="0" cellspacing="0" cellpadding="1" style="color:#333; font-size:9;">
+            <tr>                            
+                <td width="100%" style="text-align: center; border-bottom:1px solid #333">             
+                    <font style="font-family:Calibri Light; font-size: 11;font-weight: bold;">Reservation Summary for '.$sem['enumSem'].' Term SY'.$sem['strYearStart'].'-'.$sem['strYearEnd'].'</font>
+                </td>
+            </tr>        
+            </table>
+           ';
+    
 
-<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/themes/default/js/script.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/vue@2.6.12"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js"
-    integrity="sha512-WFN04846sdKMIP5LKNphMaWzU7YpMyCU245etK3g/2ARYbPK9Ub18eG+ljU96qKRCWh+quCY7yefSmlkQw1ANQ=="
-    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.min.js"></script>
 
-<script>
-new Vue({
-    el: '#registration-container',
-    data: {                    
-        base_url: '<?php echo base_url(); ?>',
-        current_sem: '<?php echo $sem['intID']; ?>',
-        reserved: undefined,
-        enrolled: undefined,
-        totals: [],
-        r_fresh: [],
-        r_trans: [],
-        r_foreign: [],
-        r_sd: [],
-        programs: undefined,
-        all_reserved: 0,
-        all_enrolled: 0,
-                      
-    },
 
-    mounted() {
-
-        let url_string = window.location.href;        
-        if(this.id != 0){            
-            //this.loader_spinner = true;
-            axios.get(this.base_url + 'registrar/enrollment_summary_data/')
-                .then((data) => {  
-                   this.enrolled = data.data.data;
-                   for(i in this.enrolled){
-                        this.all_enrolled +=  this.enrolled[i].enrolled_freshman + this.enrolled[i].enrolled_foreign + this.enrolled[i].enrolled_second + this.enrolled[i].enrolled_transferee;
-                   }                   
-                   this.programs = data.data.programs;
-                   axios.get(api_url + 'admissions/applications/stats?current_sem='+this.current_sem)
-                    .then((data) => {  
-                        this.reserved = data.data;                         
-                        for(i in this.reserved){       
-                            this.r_fresh[i] = false;
-                            this.r_trans[i] = false;
-                            this.r_foreign[i] = false;
-                            this.r_sd[i] = false;
-                            this.totals[this.reserved[i][0].type_id] = 0;                                         
-                            for(j in this.reserved[i]){     
-                                if(this.reserved[i][j].student_type == "freshman")
-                                    this.r_fresh[i] = true;
-                                if(this.reserved[i][j].student_type == "transferee")
-                                    this.r_trans[i] = true;
-                                if(this.reserved[i][j].student_type == "foreign")
-                                    this.r_foreign[i] = true;
-                                if(this.reserved[i][j].student_type == "second degree")
-                                    this.r_sd[i] = true;
-
-                                this.totals[this.reserved[i][j].type_id] += parseInt(this.reserved[i][j].reserved_count);
-                                this.all_reserved += parseInt(this.reserved[i][j].reserved_count);
-                            }
-                        }
-
-                        console.log(this.totals);                   
-                    })
-                    .catch((error) => {
-                        console.log(error);
-                    });
-                })
-            .catch((error) => {
-                console.log(error);
-                
-            });
-        }
-
-    },
-
-    methods: {      
-       
-                                       
+$html .= '       
+     <br />
+     <table v-if="enrolled" class="table table-bordered table-striped">
+     <tr>
+         <th style="width:50%;font-size:9px;">Program</th>
+         <th style="width:10%;font-size:9px;">Freshman</th>
+         <th style="width:10%;font-size:9px;">Transferee</th>
+         <th style="width:10%;font-size:9px;">Foreign</th>
+         <th style="width:10%;font-size:9px;">SD</th>
+         <th style="width:10%;font-size:9px;">Total</th>
+     </tr>
+     <tr style="line-height:10px;">
+        <th colspan="6"></th>
+     </tr>
+     ';
+     
+     $all_enrolled = 0;
+    foreach($enrollment as $item){
+        $major = ($item['strMajor'] != "None" && $item['strMajor'] != "")?'Major in '.$item['strMajor']:''; 
+        $all_enrolled +=  $item['enrolled_freshman'] + $item['enrolled_transferee'] + $item['enrolled_foreign'] + $item['enrolled_second'];
+        $html .= '            
+            <tr>
+                <td style="font-size:8px;">'.trim($item['strProgramDescription']).' '.$major.'</td>
+                <td style="font-size:8px;">
+                    '.$item['enrolled_freshman'].'
+                </td>
+                <td style="font-size:8px;">
+                    '.$item['enrolled_transferee'].'
+                </td>
+                <td style="font-size:8px;">
+                    '.$item['enrolled_foreign'].'
+                </td>
+                <td style="font-size:8px;">
+                    '.$item['enrolled_second'].'
+                </td>
+                <td style="font-size:8px;">
+                    '.($item['enrolled_freshman'] + $item['enrolled_transferee'] + $item['enrolled_foreign'] + $item['enrolled_second']).'
+                </td>
+            </tr>
+            <tr style="line-height:5px;">
+                <th colspan="6"></th>
+            </tr>
+            ';
     }
+$html .= ' 
+     <tr>
+         <td></td>
+         <td></td>
+         <td></td>
+         <td></td>
+         <td></td>
+         <td><strong>&nbsp;'.$all_enrolled.'</strong></td>
+     </tr>
+ </table>'; 
+  
+            
+$pdf->writeHTML($html, true, false, true, false, '');
 
-})
-</script>
+//$pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
 
+// ---------------------------------------------------------
+
+// Close and output PDF document
+// This method has several options, check the source code documentation for more information.
+$pdf->Output("reservationSummary".date("Ymdhis").".pdf", 'I');
+
+
+?>
