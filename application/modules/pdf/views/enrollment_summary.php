98f8670c0bffddb4c48a1b1173feec84332b8b97
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-08-18 10:45:16 +0800

    Update enrollment_summary.php

--- application/modules/pdf/views/enrollment_summary.php
+++ application/modules/pdf/views/enrollment_summary.php
@@ -11,7 +11,7 @@
     $pdf->SetTitle("Enrollment Summary");
     
     // set margins
-    $pdf->SetMargins(10, $mt , 10);
+    $pdf->SetMargins(10, 15 , 10);
     $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
     $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
     $pdf->SetFont('helvetica','',10);
