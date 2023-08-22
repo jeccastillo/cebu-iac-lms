1aa3238832a30ee8af3612d57804e4de751cfdba
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-08-22 18:03:01 +0800

    Update faculty_load_form.php

--- application/modules/pdf/views/faculty_load_form.php
+++ application/modules/pdf/views/faculty_load_form.php
@@ -39,15 +39,15 @@
 <?php 
 foreach($classlists as $classlist): ?>
 
-    <tr style="line-height:12px;">        
-        <td cellpadding="2px" style="font-size:8px;"><?php echo $classlist['subjectDescription']; ?></td>
-        <td cellpadding="2px" style="font-size:8px;"> <?php echo $classlist['strCode']; ?></td>        
-        <td cellpadding="2px" style="font-size:8px;text-align:center;"><?php echo $classlist['strClassName'].$classlist['year'].$classlist['strSection']." ".$classlist['sub_section']; ?></td>
-        <td cellpadding="2px" style="font-size:8px;text-align:center;"><?php echo $classlist['sched_day']; ?></td>
-        <td cellpadding="2px" style="font-size:8px;text-align:center;"><?php echo $classlist['sched_time']; ?></td>
-        <td cellpadding="2px" style="font-size:8px;text-align:center;"><?php echo $classlist['sched_room']; ?></td>        
-        <td cellpadding="2px" style="font-size:8px;text-align:center;"><?php echo $classlist['strUnits']; ?></td>            
-        <td cellpadding="2px" style="font-size:8px;text-align:center;"><?php echo $classlist['slots_taken_enrolled']; ?></td>
+    <tr style="line-height:12px;" cellpadding="2px">        
+        <td  style="font-size:8px;"><?php echo $classlist['subjectDescription']; ?></td>
+        <td  style="font-size:8px;"> <?php echo $classlist['strCode']; ?></td>        
+        <td  style="font-size:8px;text-align:center;"><?php echo $classlist['strClassName'].$classlist['year'].$classlist['strSection']." ".$classlist['sub_section']; ?></td>
+        <td  style="font-size:8px;text-align:center;"><?php echo $classlist['sched_day']; ?></td>
+        <td  style="font-size:8px;text-align:center;"><?php echo $classlist['sched_time']; ?></td>
+        <td  style="font-size:8px;text-align:center;"><?php echo $classlist['sched_room']; ?></td>        
+        <td  style="font-size:8px;text-align:center;"><?php echo $classlist['strUnits']; ?></td>            
+        <td  style="font-size:8px;text-align:center;"><?php echo $classlist['slots_taken_enrolled']; ?></td>
     </tr>
    <?php        
     endforeach; ?>
