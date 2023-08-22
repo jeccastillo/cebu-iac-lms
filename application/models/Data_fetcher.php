b994dffa37dcd4b424741886e1fa7d21f9b994d8
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-08-22 10:57:06 +0800

    Update Data_fetcher.php

--- application/models/Data_fetcher.php
+++ application/models/Data_fetcher.php
@@ -3620,7 +3620,7 @@ class Data_fetcher extends CI_Model {
         $results = [];
         if(!empty($classlist_sched)){
             foreach($classlist_sched as $sched){
-                $query ="SELECT intRoomSchedID,strCode,strSection
+                $query ="SELECT intRoomSchedID,strCode,strSection,tb_mas_classlist.intID
                         FROM tb_mas_room_schedule
                         JOIN tb_mas_classlist ON tb_mas_classlist.intID = tb_mas_room_schedule.strScheduleCode                
                         JOIN tb_mas_subjects ON tb_mas_classlist.intSubjectID = tb_mas_subjects.intID                
