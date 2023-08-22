75beaed451433b4b4525c1ca08598dfe8d7d720f
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-08-22 18:40:23 +0800

    updated

--- application/modules/pdf/controllers/Pdf.php
+++ application/modules/pdf/controllers/Pdf.php
@@ -418,6 +418,7 @@ class Pdf extends CI_Controller {
         $sem = $this->data_fetcher->get_active_sem();
         $this->data['classlists'] = $this->data_fetcher->getClasslistsByFaculty($sem['intID'],$id);
         $this->data['faculty'] = $this->db->get_where('tb_mas_faculty',array('intID'=>$id))->first_row('array');
+        $this->data['user'] =  $this->session->all_userdata();
         $this->data['sem'] = $sem;
         
         tcpdf();
