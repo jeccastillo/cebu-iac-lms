e23dd276d04e2f60e45b9460363b76386e3fa1ea
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-08-22 17:54:11 +0800

    Update Pdf.php

--- application/modules/pdf/controllers/Pdf.php
+++ application/modules/pdf/controllers/Pdf.php
@@ -451,8 +451,8 @@ class Pdf extends CI_Controller {
         $pdf->setPrintFooter(false);         
          
         $html = $this->load->view("faculty_load_form",$this->data); 
-        //$pdf->writeHTML($html, true, false, true, false, '');            
-        //$pdf->Output("faculty_load_form.pdf", 'I');
+        $pdf->writeHTML($html, true, false, true, false, '');            
+        $pdf->Output("faculty_load_form.pdf", 'I');
         
     }
     
