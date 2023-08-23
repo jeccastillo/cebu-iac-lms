ab5af891c1a38dad4c86e1c64b63e1413e3e5a71
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-08-24 05:04:21 +0800

    Update list_conf.php

--- application/modules/finance/views/common/list_conf.php
+++ application/modules/finance/views/common/list_conf.php
@@ -45,7 +45,7 @@ $(document).ready(function() {
                         + row.slug
                         +'">Finance Viewer</a></li>'
                         +'<li><a href="<?php echo base_url(); ?>finance/remove_or_print/'
-                        + row.or_number +'/'+row.sy_reference
+                        + row.or_number
                         +'">Finance Viewer</a></li>'
                         +'</ul></div>';
                 }
