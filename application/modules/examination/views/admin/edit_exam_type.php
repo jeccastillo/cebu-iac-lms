9f97f87238692557bcb666c4a62efc31b08b3ac9
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-08-23 13:40:13 +0800

    Update edit_exam_type.php

--- application/modules/examination/views/admin/edit_exam_type.php
+++ application/modules/examination/views/admin/edit_exam_type.php
@@ -116,7 +116,7 @@
                                 <td><?php echo $q['strSection']; ?></td>
                                 <td>
                                     <a href="<?php echo base_url(); ?>examination/edit_question/<?php echo $q['intID']; ?>" class="btn btn-primary">Edit</a>
-                                    <a class="delete-question btn btn-danger" rel="<?php echo $q['intID']; ?>" href="#" class="btn btn-primary">Delete</a>
+                                    <a class="delete-question btn btn-danger" rel="<?php echo $q['intID']; ?>" href="#">Delete</a>
                                     
                                 </td>
                             </tr>
