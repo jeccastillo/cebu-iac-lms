ab8bbfbf5560c23f2b83e8c44217aba7d95be0ff
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-08-23 14:27:17 +0800

    Update Examination.php

--- application/modules/examination/controllers/Examination.php
+++ application/modules/examination/controllers/Examination.php
@@ -102,7 +102,7 @@ class Examination extends CI_Controller {
     public function get_questions_per_section($id){
                 
         $questions = $this->db->get_where('tb_mas_questions',array('exam_id'=>$id))->first_row('array');
-        
+        print_r($questions);
         $question_array = [];        
         foreach($questions as $question){            
             $choices = $this->db->get_where('tb_mas_choices',array('question_id'=>$question['intID']))->result_array();
