2aabfda1ca9f89f8c2638e6dae8073df65672b17
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-08-23 15:57:46 +0800

    Update view_lead.php

--- application/modules/admissionsV1/views/admin/view_lead.php
+++ application/modules/admissionsV1/views/admin/view_lead.php
@@ -1169,7 +1169,7 @@ new Vue({
             
             /* Select the text field */
             copyText.select();
-            copyText.setSelectionRange(0, 99999); // For mobile devices
+            //copyText.setSelectionRange(0, 99999); // For mobile devices
             
             /* Copy the text inside the text field */
             navigator.clipboard.writeText(copyText.value);
