e806a93f34865302e075c4e6f1952ddead0ac7ce
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-08-29 08:38:00 +0800

    Update admissions_report.php

--- application/modules/admissionsV1/views/admin/admissions_report.php
+++ application/modules/admissionsV1/views/admin/admissions_report.php
@@ -122,9 +122,9 @@
 
 <script>
 <?php if($start!=0): ?>
-    var query_str = 'admissions/applications/adstats?current_sem=<?php echo $active_sem['intID']; ?>&campus=<?php echo $campus; ?>&start=<?php echo $start; ?>&end=<?php echo $end; ?>';
+    var query_str = 'admissions/applications/adstats?current_sem=<?php echo $active_sem['intID']; ?>&campus=<?php echo $campus; ?>&term=<?php echo $current_sem; ?>&start=<?php echo $start; ?>&end=<?php echo $end; ?>';
 <?php else: ?>
-    var query_str = 'admissions/applications/adstats?current_sem=<?php echo $active_sem['intID']; ?>&campus=<?php echo $campus; ?>';
+    var query_str = 'admissions/applications/adstats?current_sem=<?php echo $active_sem['intID']; ?>&campus=<?php echo $campus; ?>&term=<?php echo $current_sem; ?>';
 <?php endif; ?>
 new Vue({
     el: '#registration-container',
