525024f07c6c8281717e09b84d0fa3d944bfcc6f
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2023-08-29 14:51:55 +0800

    Update student_application.php

--- application/modules/site/views/student_application.php
+++ application/modules/site/views/student_application.php
@@ -50,6 +50,20 @@
                 <div class="mb-6">
                     <div class="md:w-5/5">
                         <label class="block t color-primary font-bold  mb-3  pr-4" for="inline-full-name">
+                            Select Term <span class="text-red-500">*</span>
+                        </label>
+                        <select class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+                            type="text" required v-model="request.syid">
+                            <?php foreach($sy as $s): ?>
+                                <option value="<?php $s['intID'] ?>"><?php $s['enumSem']." Term SY ".$s['strYearStart']."-".$s['strYearEnd']; ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="mb-6">
+                    <div class="md:w-5/5">
+                        <label class="block t color-primary font-bold  mb-3  pr-4" for="inline-full-name">
                             First Name <span class="text-red-500">*</span>
                         </label>
                         <input
