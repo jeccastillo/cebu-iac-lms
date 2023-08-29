c5df9db0b6ec7d668a1bce181d5bf7c08d5d4e84
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-08-29 08:26:25 +0800

    Update student_application.php

--- application/modules/site/views/student_application.php
+++ application/modules/site/views/student_application.php
@@ -17,10 +17,11 @@
 <div class="custom-container" id="adminssions-form" style="margin-top:10px;">
     <div class="color-primary text-center">
         <h4 class="font-medium text-2xl mb-5">
-            Student Information Sheet <strong>(Cebu Campus)</strong></h4>
-        <h4>
+            Student Information Sheet 
+            <strong>(Cebu Campus)</strong><br />
             <?php echo $term['enumSem']." ".$term_type." ".$term['strYearStart']."-".$term['strYearEnd']; ?>
         </h4>
+        
         <p>Hello future Game Changers! Kindly fill out your information sheet. If you have any questions, feel free
             to email us at <strong><u>admissionscebu@iacademy.edu.ph</u></strong> </p>
 
