dee5e97b86ff54f533bbaec6dafcb3def2bb23b8
Jp <root@LTP-151.local>
2023-08-31 10:46:42 +0800

    Generate Exam links per program type

--- application/modules/examination/controllers/Examination.php
+++ application/modules/examination/controllers/Examination.php
@@ -547,25 +547,17 @@ class Examination extends CI_Controller {
 
             foreach($applicants as $post){
                 $program = $this->db->get_where('tb_mas_programs',array('strProgramDescription'=>$post['program']))->first_row('array');
-                // if($post['id'] == '87'){
                 if($program){
-                    // print('EXAM ID :' .$examID);
-                    // print('programType :' .$programType);
-
-                    // print_r($program);
-                    // print_r('111');
                     $examType = $this->db->get_where('tb_mas_exam',array('programType'=>$programType, 'intID'=> $examID))->first_row('array');
-                    // print_r($examType);
-                    // print_r('222');
                     if($examType){
-                        print_r($program);
-                        print_r('333');
-                        if($program['school'] == $examType['programType']){
-                            print_r($post);
-                            print_r('444');
+                        // if($program['school'] == $examType['programType']){
+                        //temporary checking for tb_mas_programs
+                        if((($program['school'] == 'Computing' || $program['school'] == 'computing') && $examType['programType'] == 'computing') ||
+                        (($program['school'] == 'Business' || $program['school'] == 'business') && $examType['programType'] == 'business') ||
+                        (($program['school'] == 'Design' || $program['school'] == 'design') && $examType['programType'] == 'design') ||
+                        (($program['school'] == 'shs' || $program['school'] == 'iacademy') && $examType['programType'] == 'shs')    
+                        ){
                             $isGenerated = $this->db->get_where('tb_mas_student_exam',array('student_id'=>$post['slug']))->first_row('array');
-                            print_r($isGenerated);
-                            print_r('555');
                             if(!$isGenerated){
                                 $sem = $this->data_fetcher->get_active_sem();
                                 $applicant = array(
@@ -584,8 +576,6 @@ class Examination extends CI_Controller {
                         }
                     }
                 }
-                // die();
-                // }
             }
         }
     }
