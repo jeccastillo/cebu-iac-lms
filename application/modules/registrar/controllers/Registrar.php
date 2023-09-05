1bb86a7cdcaf23540c719f12ae9fdb7d173bd8ba
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-09-05 14:02:07 +0800

    updated

--- application/modules/registrar/controllers/Registrar.php
+++ application/modules/registrar/controllers/Registrar.php
@@ -1725,6 +1725,9 @@ class Registrar extends CI_Controller {
         $post['midterm_end'] = date("Y-m-d",strtotime($post['midterm_end']));
         $post['final_start'] = date("Y-m-d",strtotime($post['final_start']));
         $post['final_end'] = date("Y-m-d",strtotime($post['final_end']));
+        $post['start_of_classes'] = date("Y-m-d",strtotime($post['start_of_classes']));
+        $post['final_exam_start'] = date("Y-m-d",strtotime($post['final_exam_start']));
+        $post['final_exam_end'] = date("Y-m-d",strtotime($post['final_exam_end']));        
        // $this->data_poster->set
         $this->data_poster->post_data('tb_mas_sy',$post,$post['intID']);
         $this->data_poster->log_action('School Year','Updated SY Info: '.$post['enumSem']." ".$post['strYearStart']." - ".$post['strYearEnd'],'aqua');
--- application/modules/registrar/views/admin/edit_ay.php
+++ application/modules/registrar/views/admin/edit_ay.php
@@ -17,101 +17,115 @@
         <form id="validate-subject" action="<?php echo base_url(); ?>registrar/edit_submit_ay" method="post" role="form">
             <input type="hidden" name="intID" value="<?php echo $item['intID'] ?>" />
                 <div class="box-body">
-                    <div class="form-group col-xs-12 col-lg-4">
-                    <label for="enumSem"><?php echo $term_type; ?></label>
-                    <select name="enumSem" class="form-control">
-                        <?php foreach($terms as $term): ?>
-                            <option <?php echo $item['enumSem']==$term?'selected':''; ?> value="<?php echo $term ?>"><?php echo $term ?></option>
-                        <?php endforeach; ?>
-                        </select>
-                </div>
-                <div class="form-group col-xs-12 col-lg-4">
-                    <label>Label</label>
-                    <input required type="text" name="term_label" value="<?php echo $item['term_label']; ?>" class="form-control" />                         
-                </div>
-                <div class="form-group col-xs-12 col-lg-4">
-                    <label for="classType">Classes Type</label>
-                    <select name="classType" class="form-control">
-                            <option <?php echo $item['classType']=='regular'?'selected':''; ?> value="regular">Regular</option>
-                            <option <?php echo $item['classType']=='online'?'selected':''; ?> value="online">Online</option>
-                            <option <?php echo $item['classType']=='hyflex'?'selected':''; ?> value="hyflex">Hyflex</option>
-                            <option <?php echo $item['classType']=='hybrid'?'selected':''; ?> value="hybrid">Hybrid</option>
-                        </select>
-                </div>
-                    <div class="form-group col-xs-12 col-lg-4">
-                    <label for="strYearStart">Year</label>
-                        <select id="year-start" name="strYearStart" class="form-control">
-                        <?php for($y=2001;$y<2060;$y++): ?>
-                            <option <?php echo $item['strYearStart']==$y?'selected':''; ?> value="<?php echo $y; ?>"><?php echo $y."-".($y+1); ?></option>
-                        <?php endfor; ?>
-                    </select>
-                </div>
-                    <div class="form-group col-xs-12 col-lg-4">
-                    <label for="enumStatus">Status</label>
-                    <select name="enumStatus" class="form-control">
-                        <?php if($item['enumStatus'] != "active"): ?> 
-                            <option <?php echo $item['enumStatus']=='inactive'?'selected':''; ?>  value="inactive">Inactive</option>
-                        <?php endif; ?>
-                        <option <?php echo $item['enumStatus']=='active'?'selected':''; ?> value="active">Active</option>
-                        </select>
-                </div>                                                  
-                <input type="hidden" name="enumGradingPeriod" value="inactive" class="form-control">                                                                    
-                <input type="hidden" name="enumMGradingPeriod" value="inactive" class="form-control">
-                <input type="hidden" name="enumFGradingPeriod" value="inactive" class="form-control">
-                <div class="form-group col-xs-12 col-lg-4">
-                    <label for="term_student_type">Student Type for Term</label>
-                    <select name="term_student_type" class="form-control">
-                        <option <?php echo $item['term_student_type']=='college'?'selected':''; ?>  value="college">College</option>
-                        <option <?php echo $item['term_student_type']=='shs'?'selected':''; ?> value="shs">SHS</option>
-                        </select>
-                </div>
-                <div class="form-group col-xs-12 col-lg-4">
-                    <label for="pay_student_visa">Pay Student Visa this term?</label>
-                    <select name="pay_student_visa" class="form-control">
-                        <option <?php echo $item['pay_student_visa']=='0'?'selected':''; ?>  value="0">No</option>
-                        <option <?php echo $item['pay_student_visa']=='1'?'selected':''; ?> value="1">Yes</option>
-                        </select>
-                </div>     
-                <div class="form-group col-xs-12 col-lg-4">
-                    <label>Start of Classes</label>
-                    <input required type="date" name="start_of_classes" value="<?php echo $item['start_of_classes']; ?>" class="form-control" />                         
-                </div>
-                <div class="form-group col-xs-12 col-lg-4">
-                    <label>Start of Final Exams</label>
-                    <input required type="date" name="final_exam_start" value="<?php echo $item['final_exam_start']; ?>" class="form-control" />                         
-                </div>
-                <div class="form-group col-xs-12 col-lg-4">
-                    <label>End of Final Exams</label>
-                    <input required type="date" name="final_exam_end" value="<?php echo $item['final_exam_end']; ?>" class="form-control" />                         
-                </div>
-                <div class="form-group col-xs-12 col-lg-4">
-                    <label for="midterm_start">End of Application</label>
-                    <input type="datetime-local" name="endOfApplicationPeriod" value="<?php echo $item['endOfApplicationPeriod']; ?>" class="form-control" />                         
-                </div>
-                <div class="form-group col-xs-12 col-lg-4">
-                    <label for="midterm_start">Start of Midterm Grading</label>
-                    <input type="date" name="midterm_start" value="<?php echo $item['midterm_start']; ?>" class="form-control" />                         
-                </div>
-                <div class="form-group col-xs-12 col-lg-4">
-                    <label for="midterm_end">End of Midterm Grading</label>
-                    <input type="date" name="midterm_end" value="<?php echo $item['midterm_end']; ?>" class="form-control" />                         
-                </div>
-                <div class="form-group col-xs-12 col-lg-4">
-                    <label for="final_start">Start of Final Grading</label>
-                    <input type="date" name="final_start" value="<?php echo $item['final_start']; ?>" class="form-control" />                         
-                </div>
-                <div class="form-group col-xs-12 col-lg-4">
-                    <label for="final_end">End of Final Grading</label>
-                    <input type="date" name="final_end" value="<?php echo $item['final_end']; ?>" class="form-control" />                         
-                </div>          
-                                              
-                <div class="form-group col-xs-12 col-lg-4">
-                    <label for="enumFinalized">Finalized</label>
-                    <select name="enumFinalized" class="form-control">
-                        <option <?php echo $item['enumFinalized']=='no'?'selected':''; ?>  value="no">No</option>
-                        <option <?php echo $item['enumFinalized']=='yes'?'selected':''; ?> value="yes">Yes</option>
-                        </select>
-                </div>              
+                    <h3>Details</h3>
+                    <div class="row">
+                        <div class="form-group col-xs-12 col-lg-4">
+                            <label for="enumSem"><?php echo $term_type; ?></label>
+                            <select name="enumSem" class="form-control">
+                                <?php foreach($terms as $term): ?>
+                                    <option <?php echo $item['enumSem']==$term?'selected':''; ?> value="<?php echo $term ?>"><?php echo $term ?></option>
+                                <?php endforeach; ?>
+                                </select>
+                        </div>
+                        <div class="form-group col-xs-12 col-lg-4">
+                            <label>Label</label>
+                            <input required type="text" name="term_label" value="<?php echo $item['term_label']; ?>" class="form-control" />                         
+                        </div>
+                        <div class="form-group col-xs-12 col-lg-4">
+                            <label for="classType">Classes Type</label>
+                            <select name="classType" class="form-control">
+                                    <option <?php echo $item['classType']=='regular'?'selected':''; ?> value="regular">Regular</option>
+                                    <option <?php echo $item['classType']=='online'?'selected':''; ?> value="online">Online</option>
+                                    <option <?php echo $item['classType']=='hyflex'?'selected':''; ?> value="hyflex">Hyflex</option>
+                                    <option <?php echo $item['classType']=='hybrid'?'selected':''; ?> value="hybrid">Hybrid</option>
+                                </select>
+                        </div>
+                            <div class="form-group col-xs-12 col-lg-4">
+                            <label for="strYearStart">Year</label>
+                                <select id="year-start" name="strYearStart" class="form-control">
+                                <?php for($y=2001;$y<2060;$y++): ?>
+                                    <option <?php echo $item['strYearStart']==$y?'selected':''; ?> value="<?php echo $y; ?>"><?php echo $y."-".($y+1); ?></option>
+                                <?php endfor; ?>
+                            </select>
+                        </div>
+                            <div class="form-group col-xs-12 col-lg-4">
+                            <label for="enumStatus">Status</label>
+                            <select name="enumStatus" class="form-control">
+                                <?php if($item['enumStatus'] != "active"): ?> 
+                                    <option <?php echo $item['enumStatus']=='inactive'?'selected':''; ?>  value="inactive">Inactive</option>
+                                <?php endif; ?>
+                                <option <?php echo $item['enumStatus']=='active'?'selected':''; ?> value="active">Active</option>
+                                </select>
+                        </div>                                                  
+                        <input type="hidden" name="enumGradingPeriod" value="inactive" class="form-control">                                                                    
+                        <input type="hidden" name="enumMGradingPeriod" value="inactive" class="form-control">
+                        <input type="hidden" name="enumFGradingPeriod" value="inactive" class="form-control">
+                        <div class="form-group col-xs-12 col-lg-4">
+                            <label for="term_student_type">Student Type for Term</label>
+                            <select name="term_student_type" class="form-control">
+                                <option <?php echo $item['term_student_type']=='college'?'selected':''; ?>  value="college">College</option>
+                                <option <?php echo $item['term_student_type']=='shs'?'selected':''; ?> value="shs">SHS</option>
+                                </select>
+                        </div>
+                        <div class="form-group col-xs-12 col-lg-4">
+                            <label for="pay_student_visa">Pay Student Visa this term?</label>
+                            <select name="pay_student_visa" class="form-control">
+                                <option <?php echo $item['pay_student_visa']=='0'?'selected':''; ?>  value="0">No</option>
+                                <option <?php echo $item['pay_student_visa']=='1'?'selected':''; ?> value="1">Yes</option>
+                            </select>
+                        </div>                     
+                    </div>
+                    <hr />
+                    <h3>Dates Setup</h3>
+                    <div class="row">
+                        <div class="form-group col-xs-12 col-lg-4">
+                            <label>Start of Classes</label>
+                            <input type="date" name="start_of_classes" value="<?php echo $item['start_of_classes']; ?>" class="form-control" />                         
+                        </div>
+                        <div class="form-group col-xs-12 col-lg-4">
+                            <label>Start of Final Exams</label>
+                            <input type="date" name="final_exam_start" value="<?php echo $item['final_exam_start']; ?>" class="form-control" />                         
+                        </div>
+                        <div class="form-group col-xs-12 col-lg-4">
+                            <label>End of Final Exams</label>
+                            <input type="date" name="final_exam_end" value="<?php echo $item['final_exam_end']; ?>" class="form-control" />                         
+                        </div>
+                    </div>
+                    <hr />
+                    <h3>Grading Period Setup</h3>
+                    <div class="row">
+                        <div class="form-group col-xs-12 col-lg-4">
+                            <label for="midterm_start">End of Application</label>
+                            <input type="datetime-local" name="endOfApplicationPeriod" value="<?php echo $item['endOfApplicationPeriod']; ?>" class="form-control" />                         
+                        </div>
+                        <div class="form-group col-xs-12 col-lg-4">
+                            <label for="midterm_start">Start of Midterm Grading</label>
+                            <input type="date" name="midterm_start" value="<?php echo $item['midterm_start']; ?>" class="form-control" />                         
+                        </div>
+                        <div class="form-group col-xs-12 col-lg-4">
+                            <label for="midterm_end">End of Midterm Grading</label>
+                            <input type="date" name="midterm_end" value="<?php echo $item['midterm_end']; ?>" class="form-control" />                         
+                        </div>
+                        <div class="form-group col-xs-12 col-lg-4">
+                            <label for="final_start">Start of Final Grading</label>
+                            <input type="date" name="final_start" value="<?php echo $item['final_start']; ?>" class="form-control" />                         
+                        </div>
+                        <div class="form-group col-xs-12 col-lg-4">
+                            <label for="final_end">End of Final Grading</label>
+                            <input type="date" name="final_end" value="<?php echo $item['final_end']; ?>" class="form-control" />                         
+                        </div>
+                    </div>
+                    <hr />          
+                    <h3>Term Finalization</h3>
+                    <div class="row">
+                        <div class="form-group col-xs-12 col-lg-4">
+                            <label for="enumFinalized">Finalized</label>
+                            <select name="enumFinalized" class="form-control">
+                                <option <?php echo $item['enumFinalized']=='no'?'selected':''; ?>  value="no">No</option>
+                                <option <?php echo $item['enumFinalized']=='yes'?'selected':''; ?> value="yes">Yes</option>
+                                </select>
+                        </div>           
+                    </div>                        
                     <div class="form-group col-xs-12">
                         <input type="submit" value="update" class="btn btn-default  btn-flat">
                     </div>
