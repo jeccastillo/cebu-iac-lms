a3d24637ce1b4ce250b577308d391b9b0941177c
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2023-09-05 12:34:21 +0800

    Update payments_report.php

--- application/modules/finance/views/payments_report.php
+++ application/modules/finance/views/payments_report.php
@@ -32,11 +32,13 @@
                 <h3 class="box-title">Collection Report <?php echo ($other)?'(NON-STUDENT)':''; ?></h3>                
             </div><!-- /.box-header -->
             <div class="box-body table-responsive" style="overflow-x:auto;margin-right:60px;">
-                <div class="col-sm-6">
-                    <label>Search Field<label>
-                    <select id="search_field" class="form-control">
-                        <option value="is_cash">Payment Mode</option>
-                    </select>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <label>Search Field<label>
+                        <select id="search_field" class="form-control">
+                            <option value="is_cash">Payment Mode</option>
+                        </select>
+                    </div>
                 </div>
                 <hr />
                 <table id="subjects-table" class="table table-hover table-bordered">
