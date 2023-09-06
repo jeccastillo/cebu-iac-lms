e55d89ea330b0957e883ac555f727b440f3ff9d9
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-09-06 14:09:20 +0800

    updated

--- application/modules/finance/views/add_payee.php
+++ application/modules/finance/views/add_payee.php
@@ -4,7 +4,8 @@
         <section class="content-header">
             <h1>
                 <small>
-                    <!-- <a class="btn btn-app" :href="base_url + 'unity/logs/0/0/Cashier'" ><i class="fa fa-file" aria-hidden="true"></i>View Cashier Logs</a>                                                                                                                               -->
+                    <a class="btn btn-app" :href="base_url + 'finance/payee'" ><i class="fa fa-user" aria-hidden="true"></i>Add Payee</a> 
+                    <a class="btn btn-app" :href="base_url + 'finance/view_payees'" ><i class="fa fa-users" aria-hidden="true"></i>View All Payees</a> 
                 </small>
             </h1>
         </section>
--- application/modules/finance/views/view_payees.php
+++ application/modules/finance/views/view_payees.php
@@ -2,35 +2,23 @@
 <aside class="right-side">
     <section class="content-header">
         <h1>
-            Student
-            <small></small>
-        </h1> 
-        <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Student</a></li>
-            <li class="active">View All Student</li>
-        </ol>
+            Payee
+            <small>
+               <a class="btn btn-app" :href="base_url + 'finance/payee'" ><i class="fa fa-user" aria-hidden="true"></i>Add Payee</a> 
+               <a class="btn btn-app" :href="base_url + 'finance/view_payees'" ><i class="fa fa-users" aria-hidden="true"></i>View All Payees</a> 
+            </small>
+        </h1>         
     </section>
-    <div class="content">
-        <div class="alert alert-danger" style="display:none;">
-            <i class="fa fa-ban"></i>
-            <b>Alert!</b> Only admins can delete student records.
-        </div>
+    <div class="content">       
 
         <div class="box box-solid box-default">
             <div class="box-header">                  
                 <div>
-
                     <div style="width:50%;float:right; text-align:right;">
-<!--
-                        <form method="post" action="<?php echo base_url(). student/view_all_students/20 ?>">
-                            <h5>Search: <input type="text" name="search_string"/>
-                            </h5>
-                        </form>
--->
                     </div>
                 </div>
 
-                <h3 class="box-title">List of Students</h3>
+                <h3 class="box-title">List of Payees</h3>
                 <div class="box-tools">
 
                 </div>
