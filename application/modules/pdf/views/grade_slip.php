999a63e5e124deaba31e6feadb356a9036344cc3
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-09-13 05:34:21 +0800

    Update grade_slip.php

--- application/modules/pdf/views/grade_slip.php
+++ application/modules/pdf/views/grade_slip.php
@@ -11,7 +11,7 @@
     $pdf->SetTitle("Enrollment Summary");
     
     // set margins
-    $pdf->SetMargins(10, 5 , 10);
+    $pdf->SetMargins(10, 10 , 10);
     $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
     $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
     $pdf->SetFont('helvetica','',10);
