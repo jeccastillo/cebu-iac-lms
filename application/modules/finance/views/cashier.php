c3caa652db5b6e17b0db2ee91e688bb564ed1a15
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-09-12 10:20:06 +0800

    Update cashier.php

--- application/modules/finance/views/cashier.php
+++ application/modules/finance/views/cashier.php
@@ -41,7 +41,7 @@
                                         {{ cashier.or_current }}
                                     </td>  
                                     <td>
-                                        <input @click="updateTemporaryAdmin(cashier.intID,$event)" type="checkbox" :checked="cashier.temporary_admin?true:false" />
+                                        <input @click="updateTemporaryAdmin(cashier.intID,$event)" type="checkbox" :checked="cashier.temporary_admin?checked:''" />
                                     </td>                                                                       
                                 </tr>                                 
                             </table>
