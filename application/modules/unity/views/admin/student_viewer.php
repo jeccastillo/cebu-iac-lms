acc0807ae845dea51e9ad6bec23df91bdfd8a21b
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-09-15 05:07:47 +0800

    Update student_viewer.php

--- application/modules/unity/views/admin/student_viewer.php
+++ application/modules/unity/views/admin/student_viewer.php
@@ -21,10 +21,7 @@
                     </a>
                     <a v-if="reg_status == 'For Registration' && (user_level == 2 || user_level == 3)"  class="btn btn-app" :href="base_url + 'unity/edit_sections/' + student.intID + '/' + active_sem.intID">
                         <i class="fa fa-book"></i> Update Sections
-                    </a>    
-                    <a v-if="reg_status == 'For Registration' && (user_level == 2 || user_level == 3)"  class="btn btn-app" :href="base_url + 'unity/edit_sections/' + student.intID + '/' + active_sem.intID">
-                        <i class="fa fa-book"></i> Update Sections
-                    </a>                       
+                    </a>                        
                     <a v-if="reg_status =='For Registration' && (user_level == 2 || user_level == 3)" class="btn btn-app" :href="base_url + 'registrar/register_old_student2/' + student.intID">
                         <i class="fa fa-book"></i>Student Fee Asssessment
                     </a>
