0e2308aacaeeb8a4a402ceb2fc3f9d44208c74e4
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-09-15 06:51:08 +0800

    Update classlist_viewer_v.php

--- application/modules/unity/views/faculty/classlist_viewer_v.php
+++ application/modules/unity/views/faculty/classlist_viewer_v.php
@@ -39,7 +39,7 @@
                                 <td v-if="is_super_admin"><input type="checkbox" class="student-select minimal" :value="student.intID" /></td>                                                                                    
                                 <td>{{ index + 1 }}</td>
                                 <td><a :href="base_url + 'unity/student_viewer/' + student.intID">{{ student.strLastname +' '+student.strFirstname+' '+student.strMiddlename }}</a></td>
-                                <td>{{ student.strProgramCode }}</td>
+                                <td>{{ student.strProgramCode }} {{ classlist.midterm_start +'<='+ cdate }}</td>
                                 <td v-if="student.registered">        
                                     <span v-if="student.floatMidtermGrade == 'OW' || student.floatFinalGrade == 'OW' || classlist.intFinalized >= 1 || (classlist.midterm_start <= cdate && classlist.midterm_end >= cdate) && !is_super_admin">
                                         {{ student.floatMidtermGrade?student.floatMidtermGrade:"NGS" }}
