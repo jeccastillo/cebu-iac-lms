5c88519660967a023d27f83bbb0a9c23ab10691b
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-09-15 16:10:00 +0800

    Update Data_fetcher.php

--- application/models/Data_fetcher.php
+++ application/models/Data_fetcher.php
@@ -2220,20 +2220,21 @@ class Data_fetcher extends CI_Model {
         $student = $this->db->where('intID',$id)->get('tb_mas_users')->first_row('array'); 
 
         $tuition_year = $this->db->where('intID',$student['intTuitionYear'])->get('tb_mas_tuition_year')->first_row('array');
-        $unit_fee = getUnitPrice($tuition_year,$class_type);
-        echo $scholarship;
-        if($scholarship == 0)
+        $unit_fee = getUnitPrice($tuition_year,$class_type);        
+        if($scholarship == 0){
             $scholarships = $this->db->select('tb_mas_student_discount.*,tb_mas_scholarships.*')
                 ->where(array('syid'=>$syid,'student_id'=>$student['intID'],'deduction_type'=>'scholarship','tb_mas_student_discount.status'=>'applied'))
                 ->join('tb_mas_scholarships','tb_mas_scholarships.intID = tb_mas_student_discount.discount_id')
                 ->get('tb_mas_student_discount')
                 ->result();
-        else
+        }
+        else{
             $scholarships = $this->db->select('tb_mas_student_discount.*,tb_mas_scholarships.*')
                 ->where(array('intID'=>$scholarship,'syid'=>$syid,'student_id'=>$student['intID']))
                 ->join('tb_mas_scholarships','tb_mas_scholarships.intID = tb_mas_student_discount.discount_id')
                 ->get('tb_mas_student_discount')
                 ->result();
+        }
         
         if($discount == 0)
             $discounts = $this->db->select('tb_mas_student_discount.*,tb_mas_scholarships.*')
