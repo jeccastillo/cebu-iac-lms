2a8d314eb0d712ba3b356005861e49f1a5da046c
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-09-19 07:23:00 +0800

    Update register_old_student_conf.php

--- application/modules/registrar/views/common/register_old_student_conf.php
+++ application/modules/registrar/views/common/register_old_student_conf.php
@@ -5,42 +5,40 @@
        var data_sub = {};
        data_sub['subjects-loaded'] = [];
 
-      
-        $(this).attr('disabled','disabled');
-        
-        index = 0;
-        $("#validate-student :input").each(function(){
-            
-            if($(this).attr('name') == "subjects-loaded[]"){
-                data_sub['subjects-loaded'][index] = $(this).val();
-                index++;
-            }
-            else if($(this).attr('name') != undefined){                    
-                data_sub[$(this).attr('name')] = $(this).val();
-            }
-            
-
-        }).promise().done( function(){ 
-
-            console.log(data_sub);
-            
-            $.ajax({
-                'url':'<?php echo base_url(); ?>registrar/submit_registration_old',
-                'method':'post',
-                'data':data_sub,
-                'dataType':'json',
-                'success':function(ret){
-                    btn.removeAttr('disabled');                        
-                    document.location = '<?php echo base_url(); ?>registrar/advising_done';
-                }
-            
-            });
-                
-            
-            
-
+       $("#submit-button").click(function(e){
+            e.preventDefault();            
+            var btn = $(this);
+            $(this).attr('disabled','disabled');
            
+            index = 0;
+            $("#validate-student :input").each(function(){
+                
+                if($(this).attr('name') == "subjects-loaded[]"){
+                    data_sub['subjects-loaded'][index] = $(this).val();
+                    index++;
+                }
+                else if($(this).attr('name') != undefined){                    
+                    data_sub[$(this).attr('name')] = $(this).val();
+                }
+                
 
+            }).promise().done( function(){ 
+
+                console.log(data_sub);
+                
+                $.ajax({
+                    'url':'<?php echo base_url(); ?>registrar/submit_registration_old',
+                    'method':'post',
+                    'data':data_sub,
+                    'dataType':'json',
+                    'success':function(ret){
+                        btn.removeAttr('disabled');                        
+                        document.location = '<?php echo base_url(); ?>registrar/advising_done';
+                    }
+                
+                });
+                
+            });                       
            
         
        });
