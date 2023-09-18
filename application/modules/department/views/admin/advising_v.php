5def7e3b3158376f292b755932c5167f19cf12ff
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2023-09-18 16:23:50 +0800

    Update advising_v.php

--- application/modules/department/views/admin/advising_v.php
+++ application/modules/department/views/admin/advising_v.php
@@ -205,7 +205,7 @@ new Vue({
                     text: "Update Success",
                     icon: "success"
                 }).then(function() {
-                    document.location = base_url + 'registrar/register_old_student/' + this.student.intID;
+                    document.location = base_url + 'registrar/register_old_student/' + this.id;
                 });                         
                 
             })
