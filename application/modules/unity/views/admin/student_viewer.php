345a809be41f8ba6378d88dd220fc9726fb06929
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-09-19 11:59:47 +0800

    Update student_viewer.php

--- application/modules/unity/views/admin/student_viewer.php
+++ application/modules/unity/views/admin/student_viewer.php
@@ -7,14 +7,11 @@
                     <a class="btn btn-app" :href="base_url + 'student/view_all_students'" ><i class="ion ion-arrow-left-a"></i>All Students</a>                     
                     <a v-if="user_level == 2 || user_level == 3" class="btn btn-app" :href="base_url + 'student/edit_student/' + student.intID"><i class="ion ion-edit"></i> Edit</a>                     
                     <a v-if="user_level == 2 || user_level == 3" class="btn btn-app" :href="base_url + 'unity/student_records/' + '/' + student.intID"><i class="fa fa-user"></i>Assessment</a> 
-                    <!-- <a v-if="user_level == 2 || user_level == 3" target="_blank" v-if="registration" class="btn btn-app" :href="base_url + 'pdf/student_viewer_registration_print/' + student.intID +'/'+ applicant_data.id +'/'+ active_sem.intID">
+                    <a v-if="user_level == 2 || user_level == 3" target="_blank" v-if="registration" class="btn btn-app" :href="base_url + 'pdf/student_viewer_registration_print/' + student.intID +'/'+ applicant_data.id +'/'+ active_sem.intID">
                         <i class="ion ion-printer"></i>RF Print
-                    </a>                      -->
-                    <a  target="_blank" v-if="registration && (user_level == 2 || user_level == 3) && deficiencies.length == 0" class="btn btn-app" :href="base_url + 'pdf/student_viewer_registration_print/' + student.intID +'/'+ applicant_data.id +'/'+ active_sem.intID + '/35'">
-                        <i class="ion ion-printer"></i>RF No Header
                     </a>                     
-                    <a  target="_blank" class="btn btn-app" :href="base_url + 'deficiencies/student_deficiencies/' + student.intID'">
-                        <i class="ion ion-user"></i>Deficiencies
+                    <a  target="_blank" v-if="registration && (user_level == 2 || user_level == 3)" class="btn btn-app" :href="base_url + 'pdf/student_viewer_registration_print/' + student.intID +'/'+ applicant_data.id +'/'+ active_sem.intID + '/35'">
+                        <i class="ion ion-printer"></i>RF No Header
                     </a>                     
                     <a v-if="reg_status != 'For Subject Enlistment' && reg_status != 'For Sectioning' && (user_level == 2 || user_level == 3)" target="_blank" class="btn btn-app" :href="base_url + 'pdf/student_viewer_advising_print/' + student.intID + '/' + active_sem.intID">
                         <i class="ion ion-printer"></i>Print Subjects
@@ -307,7 +304,6 @@ new Vue({
         records: [],
         other_data: undefined,
         reg_status: '',
-        deficiencies:[],
         sy: undefined,
         term_type: undefined,
         sem_student: undefined,
@@ -341,7 +337,8 @@ new Vue({
         if(this.id != 0){            
             //this.loader_spinner = true;
             axios.get(this.base_url + 'unity/student_viewer_data/' + this.id + '/' + this.sem )
-                .then((data) => {                      
+                .then((data) => {  
+                    console.log(data);
                     if(data.data.success){                                                                                                                   
                         this.student = data.data.student;
                         if(data.data.scholarship.length > 0){
