4d17847c42870dd5d21714c3e9b06cb4979b0678
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-09-21 06:51:26 +0800

    Update print_or.php

--- application/modules/pdf/views/print_or.php
+++ application/modules/pdf/views/print_or.php
@@ -111,7 +111,7 @@
                 <tr style="line-height:15px;">                    
                     <td style="font-size:10px;text-align:left;color:#666;">
                         <span style="color:#fff;">RECEIVED fr</span>
-                        <?php if($student_id != '0'): ?>
+                        <?php if($student_id != 'undefined' && $student_id != '' && $student_id != 0): ?>
                         <?php echo preg_replace("/[^a-zA-Z0-9]+/", "", $student_id); ?> <?php echo $student_name; ?>                        
                         <?php else: ?>
                         <?php echo $student_name; ?>                        
