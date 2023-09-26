7af535ce765eb469739ff084dc2f0223b85ed59d
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-09-26 21:18:05 +0800

    Update Finance.php

--- application/modules/finance/controllers/Finance.php
+++ application/modules/finance/controllers/Finance.php
@@ -602,7 +602,7 @@ class Finance extends CI_Controller {
     }
 
     public function ns_transactions_data($payee,$sem){   
-        $data['sy'] = $this->db->get('tb_mas_sy');            
+        $data['sy'] = $this->db->get('tb_mas_sy')->result_array();            
         $data['cashier'] = $this->db->get_where('tb_mas_cashier',array('user_id'=>$this->data['user']['intID']))->first_row();
         $data['user'] = $this->data['user'];
         $sem = $this->data_fetcher->get_sem_by_id($sem);        
