c88a5f596a9e3e00dc49a639da26a08a0745a77e
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-09-26 14:35:45 +0800

    Update shifting.php

--- application/modules/registrar/views/admin/shifting.php
+++ application/modules/registrar/views/admin/shifting.php
@@ -12,8 +12,30 @@
         </section>
         <hr />
         <div class="content">
-            <div>
-                
+            <div v-if="student" class="box box-widget widget-user-2">
+                <!-- Add the bg color to the header using any of the bg-* classes -->
+                <div class="widget-user-header bg-red">
+                    <!-- /.widget-user-image -->
+                    <h3 class="widget-user-username" style="text-transform:capitalize;margin-left:0;font-size:1.3em;">{{ student.strLastname.toUpperCase() }}, {{ student.strFirstname.toUpperCase() }} {{ student.strMiddlename?student.strMiddlename.toUpperCase():'' }}</h3>
+                    <h5 class="widget-user-desc" style="margin-left:0;">{{ student.strProgramDescription }} {{ (student.strMajor != 'None')?'Major in '+student.strMajor:'' }}</h5>
+                </div>
+                <div class="box-footer no-padding">
+                    <ul class="nav nav-stacked">
+                    <li><a href="#" style="font-size:13px;">Student Number <span class="pull-right text-blue">{{ student.strStudentNumber.replace(/-/g, '') }}</span></a></li>
+                    <li><a href="#" style="font-size:13px;">Curriculum <span class="pull-right text-blue">{{ student.strName }}</span></a></li>                            
+                    <li><a style="font-size:13px;" href="#">Registration Status <span class="pull-right">{{ reg_status }}</span></a></li>
+                    <li><a @click.prevent="resetStatus()" href="#"><i class="ion ion-android-close"></i> Reset Status</a> </li>
+                    <li>
+                        <a style="font-size:13px;" href="#">Date Registered <span class="pull-right">
+                            <span style="color:#009000" v-if="registration" >{{ registration.date_enlisted }}</span>
+                            <span style="color:#900000;" v-else>N/A</span>                                
+                        </a>
+                    </li>                                                
+                    <li v-if="registration"><a style="font-size:13px;" href="#">Scholarship <span class="pull-right">{{ scholarship.name }}</span></a></li>
+                    <li v-if="registration"><a style="font-size:13px;" href="#">Discount <span class="pull-right">{{ discount.name }}</span></a></li>
+                        
+                    </ul>
+                </div>
             </div>
         </div><!---content container--->
     </div><!---vue container--->
