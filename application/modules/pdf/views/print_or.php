fd178f96ea3d100d23fb831e916197a0be9aefbe
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-09-28 08:42:23 +0800

    Update print_or.php

--- application/modules/pdf/views/print_or.php
+++ application/modules/pdf/views/print_or.php
@@ -18,7 +18,7 @@
             </table>
             <table>
                 <tr style="line-height:12px;font-size:10px;text-align:left;">
-                    <td style="width:50%;font-size:8px;height:150px;"><?php echo $description; ?> <?php echo $description == "Reservation Payment" ? "<br />NON REFUNDABLE AND NON <br />TRANSFERABLE":""; ?></td>
+                    <td style="width:50%;font-size:8px;height:110px;"><?php echo $description; ?> <?php echo $description == "Reservation Payment" ? "<br />NON REFUNDABLE AND NON <br />TRANSFERABLE":""; ?></td>
                     <td style="width:50%"><?php echo number_format($total_amount_due,2,'.',','); ?></td>
                 </tr>
             </table>
@@ -34,7 +34,7 @@
             </table>     
             <table>
                 <tr style="line-height:12px;font-size:10px;text-align:left;">
-                    <td style="">TEST</td>                    
+                    <td style="height:40px;">TEST</td>                    
                 </tr>
             </table>            
             
