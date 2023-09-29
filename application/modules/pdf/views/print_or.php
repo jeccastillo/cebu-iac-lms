34b0beb9ffc8753db280e7fdee63429bea43c061
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-09-30 06:54:32 +0800

    Update print_or.php

--- application/modules/pdf/views/print_or.php
+++ application/modules/pdf/views/print_or.php
@@ -40,13 +40,13 @@
             <table>
                 <tr style="line-height:12px;font-size:10px;text-align:left;">
                     <td style="width:30%;font-size:8px;"><?php $description == "Reservation Payment" ? 'Name:' : ''; ?></td>
-                    <td style="width:70%;<?php $description == "border-bottom:1px solid #000;" ? 'Name:' : ''; ?>"></td>                  
+                    <td style="width:70%;<?php $description == "Reservation Payment" ? 'border-bottom:1px solid #000;' : ''; ?>"></td>                  
                 </tr>
             </table>
             <table>
                 <tr style="line-height:12px;font-size:10px;text-align:left;">
-                    <td style="width:30%;font-size:8px;"><?php $description == "Reservation Payment" ? 'Signature:' : ''; ?> </td>
-                    <td style="width:70%;<?php $description == "border-bottom:1px solid #000;" ? 'Name:' : ''; ?>"></td>                  
+                    <td style="width:30%;font-size:8px;"><?php $description == "Reservation Payment" ? 'Signature:' : ''; ?></td>
+                    <td style="width:70%;<?php $description == "Reservation Payment" ? 'border-bottom:1px solid #000;' : ''; ?>"></td>                  
                 </tr>
             </table>                  
             <table>
