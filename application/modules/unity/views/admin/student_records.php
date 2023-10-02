ce042c0571df60a4171c153ede9f2478cec5eb20
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-10-03 07:51:19 +0800

    Update student_records.php

--- application/modules/unity/views/admin/student_records.php
+++ application/modules/unity/views/admin/student_records.php
@@ -31,7 +31,7 @@
             <div class="nav-tabs-custom">
                 <ul class="nav nav-tabs">
                     <li class="active"><a href="#tab_1" data-toggle="tab">Grades</a></li>
-                    <li><a href="#tab_2" data-toggle="tab">Assessment</a></li>                        
+                    <li><a href="#tab_2" data-toggle="tab">Curriculum Evaluation</a></li>                        
                 </ul>
                 <div class="tab-content">
                     <div class="tab-pane active" id="tab_1">
