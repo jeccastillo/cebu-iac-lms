dbeb8d4b4182804336c131f74779f5c4e9267e3f
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-10-04 02:36:54 +0800

    Update tor.php

--- application/modules/pdf/views/tor.php
+++ application/modules/pdf/views/tor.php
@@ -56,151 +56,124 @@
                     <td width="70%">
                         <table border="0" cellspacing="0" cellpadding="0" style="color:#333; font-size:9;">                                
                             <tr>                            
-                                <td width="20%">             
-                                    <font style="font-size: 8;">Name</font>
-                                </td>
-                                <td colspan="3">             
-                                    <font style="font-size: 8;">: '.$student['strLastname'].' '.$student['strFirstname'].' '.$student['strMiddlename'].'</font>
-                                </td>                    
+                                <td width="20%"><font style="font-size: 8;">Name</font></td>
+                                <td colspan="3"><font style="font-size: 8;">: '.$student['strLastname'].' '.$student['strFirstname'].' '.$student['strMiddlename'].'</font></td>                    
                                 
                             </tr>
                             <tr>                            
-                                <td>             
-                                    <font style="font-size: 8;">Program Persued</font>
-                                </td>
-                                <td colspan="3">             
-                                    <font style="font-size: 8;">: '.trim($student['strProgramDescription']).'</font>
-                                </td>                      
+                                <td><font style="font-size: 8;">Program Persued</font></td>
+                                <td colspan="3"><font style="font-size: 8;">: '.trim($student['strProgramDescription']).'</font></td>                      
                             </tr>
                             <tr>                            
-                                <td>             
-                                    <font style="font-size: 8;">Date of Birth</font>
-                                </td>
-                                <td width="40%">             
-                                    <font style="font-size: 8;">: '.$student['dteBirthDate'].'</font>
-                                </td>    
+                                <td><font style="font-size: 8;">Date of Birth</font></td>
+                                <td width="40%"><font style="font-size: 8;">: '.$student['dteBirthDate'].'</font></td>    
                                 <td width="20%"><font style="font-size: 8;">Place of Birth</font></td>                
-                                <td width="20%">
-                                    <font style="font-size: 8;">: '.$student['place_of_birth'].'</font>
-                                </td>                
+                                <td width="20%"><font style="font-size: 8;">: '.$student['place_of_birth'].'</font></td>                
                             </tr>
                             <tr>                            
-                                <td>             
-                                    <font style="font-size: 8;">Citizenship</font>
-                                </td>
-                                <td>             
-                                    <font style="font-size: 8;">: '.$student['strCitizenship'].'</font>
-                                </td>    
+                                <td><font style="font-size: 8;">Citizenship</font></td>
+                                <td><font style="font-size: 8;">: '.$student['strCitizenship'].'</font></td>    
                                 <td><font style="font-size: 8;">Gender</font></td>                
-                                <td>
-                                    <font style="font-size: 8;">: '.$student['enumGender'].'</font>
-                                </td>                
+                                <td><font style="font-size: 8;">: '.$student['enumGender'].'</font></td>                
                             </tr>
                             <tr>                            
-                                <td>             
-                                    <font style="font-size: 8;">Secondary School</font>
-                                </td>
-                                <td>             
-                                    <font style="font-size: 8;">: '.$student['high_school'].'</font>
-                                </td>    
+                                <td><font style="font-size: 8;">Secondary School</font></td>
+                                <td><font style="font-size: 8;">: '.$student['high_school'].'</font></td>    
                                 <td><font style="font-size: 8;">ID No.</font></td>                
-                                <td>
-                                    <font style="font-size: 8;">: '.preg_replace("/[^a-zA-Z0-9]+/", "", $student['strStudentNumber']).'</font>
-                                </td>                
+                                <td><font style="font-size: 8;">: '.preg_replace("/[^a-zA-Z0-9]+/", "", $student['strStudentNumber']).'</font></td>                
                             </tr>
                             <tr>                            
-                                <td>             
-                                    <font style="font-size: 8;">Tertiary School</font>
-                                </td>
-                                <td>             
-                                    <font style="font-size: 8;">: '.$student['college'].'</font>
-                                </td>                     
+                                <td><font style="font-size: 8;">Tertiary School</font></td>
+                                <td><font style="font-size: 8;">: '.$student['college'].'</font></td>                     
                                 <td><font style="font-size: 8;">Date of Admission</font></td>                
-                                <td>
-                                    <font style="font-size: 8;">: '.$other_details['admission_date'].'</font>
-                                </td>                
+                                <td><font style="font-size: 8;">: '.$other_details['admission_date'].'</font></td>                
                             </tr>
                             <tr>                            
-                                <td colspan="2">             
-                                    <font style="font-size: 8;"></font>
-                                </td>
+                                <td colspan="2"><font style="font-size: 8;"></font></td>
                                 <td><font style="font-size: 8;">Date of Graduation</font></td>                
-                                <td>
-                                    <font style="font-size: 8;">: '.$student['date_of_graduation'].'</font>
-                                </td>                
+                                <td><font style="font-size: 8;">: '.$student['date_of_graduation'].'</font></td>                
                             </tr>
                             <tr>                            
-                                <td colspan="2">             
-                                    <font style="font-size: 8;"></font>
-                                </td>
+                                <td colspan="2"><font style="font-size: 8;"></font></td>
                                 <td><font style="font-size: 8;">NSTP Serial No.</font></td>
-                                <td>
-                                    <font style="font-size: 8;">: '.$student['nstp_serial'].'</font>
-                                </td>                
+                                <td><font style="font-size: 8;">: '.$student['nstp_serial'].'</font></td>                
                             </tr>
                         </table>
                     </td>
-                    <td width="30%">                    
-                        <img src="'.$other_details['picture'].'" width="200px" />                    
-                    </td>
+                    <td width="30%"><img src="'.$other_details['picture'].'" width="200px" /></td>
                 </tr>     
            </table>
           ';
 
-$footer ='
-        <tr>
-            <td style="line-height:15px;" colspan=6></td>         
-        </tr>
-        <tr>
-            <td style="font-size:9px;">Remarks</td>
-            <td colspan="5"><p style="font-size:8px;">'.$other_details['remarks'].'</p></td>                    
-        </tr> 
-        <tr>
-            <td style="line-height:5px;" colspan=6></td>         
-        </tr>                                            
-        <tr>
-            <td style="font-size:9px;">Grading System</td>
-            <td colspan="5"><p style="font-size:8px;">1.00 (98-100) Excelent; 1.25 (95-97); 1.50 (92-94) Very Good; 1.75 (89-91); 2.00 (86-88); 2.25 (83-85);2.50 (80-82) Satisfactory; 2.75 (77-79) Fair; 3.00 (75-76); 5.00 (Below 75) Failed; OD (Officially Dropped); UD (Unofficially Dropped); FA (Failure due to Absences); IP (In Progress) for internship only; P (Passed); F (Failed); OW (Officially Withdrawn); UW (Unofficially Withdrawn); NGS (No Grade Submitted)</p>      
-            </td>                    
-        </tr>
-        <tr>
-            <td style="line-height:5px;" colspan=6></td>         
-        </tr>                       
-        <tr>
-            <td style="font-size:9px;">Note</td>
-            <td colspan="5"><p style="font-size:8px;">This document is valid only when it bears the seal of the College and affixed with the original signature in ink. Any erasure or alteration made on this copy renders the whole document invalid.</p>      
-            </td>                    
-        </tr>
-        <tr>
-            <td style="line-height:20px;" colspan=6></td>         
-        </tr> 
-        <tr>
-            <td style="font-size:9px;">Prepared By</td>
-            <td colspan="2" style="font-size:9px;"><b>'.$other_details['prepared_by'].'</b></td>                     
-        </tr>
-        <tr>
-            <td style="line-height:10px;" colspan=6></td>         
-        </tr> 
-        <tr>
-            <td style="font-size:9px;">Verified By</td>
-            <td colspan="2" style="font-size:9px;"><b>'.$other_details['verified_by'].'</b></td>         
-            <td colspan="3" style="text-align:center;font-size:9px;"><b>'.$other_details['registrar'].'</b></td>                       
-        </tr>
-        <tr>
-            <td style="line-height:10px;" colspan=6></td>         
-        </tr> 
-        <tr>
-            <td style="font-size:9px;">Date Issued</td>
-            <td colspan="2" style="font-size:9px;"><b>'.$other_details['date_generated'].'</b></td>         
-            <td colspan="3" style="text-align:center;font-size:9px;">Registrar</td>                       
-        </tr>
-          ';          
-$generated_line = '<p style="font-size:8px;margin-top:10px;">GENERATED BY:'.$user['strFirstname']." ".$user['strLastname'].'<br />                 
-              RUNDATE&TIME:'.date('Y-m-d h:i a').'
-          </p>';
-$html .= $header_first_page;
+        $header_page = '<table border="0" cellspacing="0" cellpadding="0" style="color:#333; font-size:9;">
+                     <tr>                            
+                          <td width="20%"><font style="font-size: 8;">Name</font></td>
+                          <td width="30%"><font style="font-size: 8;">: '.$student['strLastname'].' '.$student['strFirstname'].' '.$student['strMiddlename'].'</font></td> 
+                          <td width="20%"><font style="font-size: 8;">ID No.</font></td>
+                          <td width="30%"><font style="font-size: 8;">: '.preg_replace("/[^a-zA-Z0-9]+/", "", $student['strStudentNumber']).'</font></td>                    
+                          
+                      </tr>
+                      <tr>                            
+                          <td><font style="font-size: 8;">Program Persued</font></td>
+                          <td colspan="3"><font style="font-size: 8;">: '.trim($student['strProgramDescription']).'</font></td>                      
+                      </tr>                                                                                 
+                    </table>
+    ';
 
-$table_header_page = '<tr>
+        $footer ='
+                <tr>
+                    <td style="line-height:15px;" colspan=6></td>         
+                </tr>
+                <tr>
+                    <td style="font-size:9px;">Remarks</td>
+                    <td colspan="5"><p style="font-size:8px;">'.$other_details['remarks'].'</p></td>                    
+                </tr> 
+                <tr>
+                    <td style="line-height:5px;" colspan=6></td>         
+                </tr>                                            
+                <tr>
+                    <td style="font-size:9px;">Grading System</td>
+                    <td colspan="5"><p style="font-size:8px;">1.00 (98-100) Excelent; 1.25 (95-97); 1.50 (92-94) Very Good; 1.75 (89-91); 2.00 (86-88); 2.25 (83-85);2.50 (80-82) Satisfactory; 2.75 (77-79) Fair; 3.00 (75-76); 5.00 (Below 75) Failed; OD (Officially Dropped); UD (Unofficially Dropped); FA (Failure due to Absences); IP (In Progress) for internship only; P (Passed); F (Failed); OW (Officially Withdrawn); UW (Unofficially Withdrawn); NGS (No Grade Submitted)</p>      
+                    </td>                    
+                </tr>
+                <tr>
+                    <td style="line-height:5px;" colspan=6></td>         
+                </tr>                       
+                <tr>
+                    <td style="font-size:9px;">Note</td>
+                    <td colspan="5"><p style="font-size:8px;">This document is valid only when it bears the seal of the College and affixed with the original signature in ink. Any erasure or alteration made on this copy renders the whole document invalid.</p>      
+                    </td>                    
+                </tr>
+                <tr>
+                    <td style="line-height:20px;" colspan=6></td>         
+                </tr> 
+                <tr>
+                    <td style="font-size:9px;">Prepared By</td>
+                    <td colspan="2" style="font-size:9px;"><b>'.$other_details['prepared_by'].'</b></td>                     
+                </tr>
+                <tr>
+                    <td style="line-height:10px;" colspan=6></td>         
+                </tr> 
+                <tr>
+                    <td style="font-size:9px;">Verified By</td>
+                    <td colspan="2" style="font-size:9px;"><b>'.$other_details['verified_by'].'</b></td>         
+                    <td colspan="3" style="text-align:center;font-size:9px;"><b>'.$other_details['registrar'].'</b></td>                       
+                </tr>
+                <tr>
+                    <td style="line-height:10px;" colspan=6></td>         
+                </tr> 
+                <tr>
+                    <td style="font-size:9px;">Date Issued</td>
+                    <td colspan="2" style="font-size:9px;"><b>'.$other_details['date_generated'].'</b></td>         
+                    <td colspan="3" style="text-align:center;font-size:9px;">Registrar</td>                       
+                </tr>
+                ';          
+            $generated_line = '<p style="font-size:8px;margin-top:10px;">GENERATED BY:'.$user['strFirstname']." ".$user['strLastname'].'<br />                 
+                        RUNDATE&TIME:'.date('Y-m-d h:i a').'
+                    </p>';
+            $html .= $header_first_page;
+
+            $table_header_page = '<tr>
                         <th style="width:15%;font-size:9px;border-bottom:1px solid #333;"><b>Course Code</b></th>
                         <th style="width:40%;font-size:9px;border-bottom:1px solid #333;"><b>Descriptive Title</b></th>
                         <th style="width:10%;font-size:9px;border-bottom:1px solid #333;text-align:center;"><b>Units</b></th>         
@@ -262,6 +235,26 @@ foreach($records as $record){
                     </tr>            
                     ';
             }
+
+            if(($page_ctr == 20 && $page == 1) || $page_ctr == 25){
+                
+                $page++;
+                $html .= $footer;
+                $html .= '</table>';
+                $pdf->writeHTML($html, true, false, true, false, '');
+                $pdf->AddPage();
+                $html = '<table border="0" cellspacing="0" cellpadding="1" style="color:#333; font-size:9;">                
+                        <tr>
+                            <td style="line-height:150px;" colspan=6></td>         
+                        </tr>        
+                    </table>
+                ';
+                $html .= $header_page;
+                $html .='<table>';
+                $html.=$table_header_page;
+
+            }
+            
         }        
         
 }
