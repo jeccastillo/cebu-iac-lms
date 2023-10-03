cc0a38609d467dd1489b0f36add3e2a3f2858349
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-10-04 02:19:47 +0800

    Update tor.php

--- application/modules/pdf/views/tor.php
+++ application/modules/pdf/views/tor.php
@@ -19,7 +19,7 @@
     // create new PDF document
     //$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
     //$pdf = new TCPDF("P", PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
-    $pdf = new TCPDF("L", "mm", array(210,146), true, 'UTF-8', false);
+    $pdf = new MYPDF("L", "mm", array(210,146), true, 'UTF-8', false);
 
     // set document information
     $pdf->SetCreator(PDF_CREATOR);
