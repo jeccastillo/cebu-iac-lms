c208a6c316e1b29464361f599a951d6f5f5220c4
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-10-04 00:22:29 +0800

    Update tor.php

--- application/modules/pdf/views/tor.php
+++ application/modules/pdf/views/tor.php
@@ -66,7 +66,7 @@
                         <font style="font-size: 8;">: '.$student['dteBirthDate'].'</font>
                     </td>    
                     <td width="15%"><font style="font-size: 8;">Place of Birth</font></td>                
-                    <td width="25%">
+                    <td width="20%">
                         <font style="font-size: 8;">: '.$student['place_of_birth'].'</font>
                     </td>                
                 </tr>
