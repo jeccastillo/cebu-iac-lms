899d3d36d174bb03ee79c46e19792e0f8ce5857a
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-10-04 00:26:04 +0800

    Update tor.php

--- application/modules/pdf/views/tor.php
+++ application/modules/pdf/views/tor.php
@@ -37,9 +37,9 @@
            ';
     $html .= '<table border="0" cellspacing="0" cellpadding="0" style="color:#333; font-size:9;">
                 <tr>
-                    <td colspan="4"></td>
-                    <td rowspan="8">
-                        <img src="'.$other_details['picture'].'" />
+                    <td colspan="3"></td>
+                    <td rowspan="8" colspan="2">
+                        <img src="'.$other_details['picture'].'" width="200px" />
                     </td>                    
                 </tr>
                 <tr>                            
