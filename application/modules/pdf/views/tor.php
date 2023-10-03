b1d3c4b159e6fd9c1f482b893956f6c08d5d80f7
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-10-04 02:00:33 +0800

    Update tor.php

--- application/modules/pdf/views/tor.php
+++ application/modules/pdf/views/tor.php
@@ -138,7 +138,10 @@ $footer ='
         <tr>
             <td style="font-size:9px;">Remarks</td>
             <td colspan="5"><p style="font-size:8px;">'.$other_details['remarks'].'</p></td>                    
-        </tr>                                             
+        </tr> 
+        <tr>
+            <td style="line-height:10px;" colspan=6></td>         
+        </tr>                                            
         <tr>
             <td style="font-size:9px;">Grading System</td>
             <td colspan="5"><p style="font-size:8px;">1.00 (98-100) Excelent; 1.25 (95-97); 1.50 (92-94) Very Good; 1.75 (89-91); 2.00 (86-88); 2.25 (83-85);2.50 (80-82) Satisfactory; 2.75 (77-79) Fair; 3.00 (75-76); 5.00 (Below 75) Failed; OD (Officially Dropped); UD (Unofficially Dropped); FA (Failure due to Absences); IP (In Progress) for internship only; P (Passed); F (Failed); OW (Officially Withdrawn); UW (Unofficially Withdrawn); NGS (No Grade Submitted)</p>      
