65f14444cc64d2193b1adebc55d38d7a5bcbab16
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-10-05 09:21:46 +0800

    Update Unity.php

--- application/modules/unity/controllers/Unity.php
+++ application/modules/unity/controllers/Unity.php
@@ -2124,9 +2124,9 @@ class Unity extends CI_Controller {
                                                     ->result_array();
 
             if($clist['intMajor'] == 0)
-                $data['legend'] = $this->load->view('faculty/grade_college',false);
+                $data['legend'] = $this->load->view('faculty/grade_college',$this->data,true);
             else
-                $data['legend'] = $this->load->view('faculty/grade_shs',false);
+                $data['legend'] = $this->load->view('faculty/grade_shs',$this->data,true);
             
             if(!$data['subject']['grading_system_id_midterm']) 
                 $data['grading_items_midterm'] = $data['grading_items'];                                                
