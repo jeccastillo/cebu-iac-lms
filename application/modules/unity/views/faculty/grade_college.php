e379d4ada8c50d27b86db3ba3937e74b8216f256
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-10-05 09:30:40 +0800

    Update grade_college.php

--- application/modules/unity/views/faculty/grade_college.php
+++ application/modules/unity/views/faculty/grade_college.php
@@ -8,37 +8,77 @@
         <tr>
             <td>1.00</td>
             <td>98-100</td>
+            <td>(Excellent)</td>
         </tr>
         <tr>
             <td>1.25</td>
+            <td>95-97</td>
+            <td></td>
         </tr>
         <tr>
             <td>1.50</td>
+            <td>92-94</td>
+            <td>(Very Good)</td>
         </tr>
         <tr>
             <td>1.75</td>
+            <td>89-91</td>
+            <td></td>
         </tr>                   
         <tr>
             <td>2.00</td>
+            <td>86-88</td>
+            <td></td>
         </tr>
         <tr>
             <td>2.25</td>
+            <td>83-85</td>
+            <td></td>
         </tr>
         <tr>
             <td>2.50</td>
+            <td>80-82</td>
+            <td>(Satisfactory)</td>
         </tr>
         <tr>
             <td>2.75</td>
+            <td>77-79</td>
+            <td>(Fair)</td>
         </tr>
         <tr>
             <td>3.00</td>
+            <td>75-76</td>
+            <td></td>
         </tr>
         <tr>
             <td>5.00</td>
+            <td>Below 75</td>
+            <td>(Failed)</td>
         </tr>                                                                                    
         <tr>
-            <td>OW</td>
-            <td>Officially Withdrawn</td>
+            <td>FA</td>
+            <td>Failiure due to Abscences</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>UD</td>
+            <td>Unofficially Dropped</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>P</td>
+            <td>Passed</td>
+            <td>(Applicable for INTERNSHIP only)</td>
+        </tr>
+        <tr>
+            <td>F</td>
+            <td>Failed</td>
+            <td>(Applicable for INTERNSHIP only)</td>
+        </tr>
+        <tr>
+            <td>IP</td>
+            <td>In Progress</td>
+            <td>(Applicable for INTERNSHIP only)</td>
         </tr>
     </tbody>
 </table>
