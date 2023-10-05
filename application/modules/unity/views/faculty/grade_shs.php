ca1b9295f9fec0481ed3c151e1bed2be273b0163
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-10-05 09:31:23 +0800

    Update grade_shs.php

--- application/modules/unity/views/faculty/grade_shs.php
+++ application/modules/unity/views/faculty/grade_shs.php
@@ -1,44 +1,84 @@
 <table class="table table-striped" style="font-size: .8em;">
     <thead>
         <tr>
-            <th colspan="2" class="text-center">Grading System for SHS</th>
+            <th colspan="3" class="text-center">Grading System for SHS</th>
         </tr>                                        
     </thead>
     <tbody>
         <tr>
             <td>1.00</td>
             <td>98-100</td>
+            <td>(Excellent)</td>
         </tr>
         <tr>
             <td>1.25</td>
+            <td>95-97</td>
+            <td></td>
         </tr>
         <tr>
             <td>1.50</td>
+            <td>92-94</td>
+            <td>(Very Good)</td>
         </tr>
         <tr>
             <td>1.75</td>
+            <td>89-91</td>
+            <td></td>
         </tr>                   
         <tr>
             <td>2.00</td>
+            <td>86-88</td>
+            <td></td>
         </tr>
         <tr>
             <td>2.25</td>
+            <td>83-85</td>
+            <td></td>
         </tr>
         <tr>
             <td>2.50</td>
+            <td>80-82</td>
+            <td>(Satisfactory)</td>
         </tr>
         <tr>
             <td>2.75</td>
+            <td>77-79</td>
+            <td>(Fair)</td>
         </tr>
         <tr>
             <td>3.00</td>
+            <td>75-76</td>
+            <td></td>
         </tr>
         <tr>
             <td>5.00</td>
+            <td>Below 75</td>
+            <td>(Failed)</td>
         </tr>                                                                                    
         <tr>
-            <td>OW</td>
-            <td>Officially Withdrawn</td>
+            <td>FA</td>
+            <td>Failiure due to Abscences</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>UD</td>
+            <td>Unofficially Dropped</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>P</td>
+            <td>Passed</td>
+            <td>(Applicable for INTERNSHIP only)</td>
+        </tr>
+        <tr>
+            <td>F</td>
+            <td>Failed</td>
+            <td>(Applicable for INTERNSHIP only)</td>
+        </tr>
+        <tr>
+            <td>IP</td>
+            <td>In Progress</td>
+            <td>(Applicable for INTERNSHIP only)</td>
         </tr>
     </tbody>
 </table>
