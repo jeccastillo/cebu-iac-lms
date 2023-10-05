1eabd20858a33efe04d5f554b7ced5dce489b2a1
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-10-06 07:42:21 +0800

    Update ay_view_conf.php

--- application/modules/registrar/views/common/ay_view_conf.php
+++ application/modules/registrar/views/common/ay_view_conf.php
@@ -23,33 +23,35 @@
                         showCloseButton: true,
                         showLoaderOnConfirm: true,
                         preConfirm: (inputValue) => {  
-                            var sem = $(this).attr('rel');
-                            var data = {'date':inputValue};
-                            $.ajax({
-                                'url':'<?php echo base_url(); ?>registrar/cut_off_registration/'+sem,
-                                'method':'post',
-                                'data':data,
-                                'dataType':'json',
-                                'success':function(ret){
-                                    if(ret.success){
-                                        Swal.fire({
-                                            title: "Success",
-                                            text: ret.message,
-                                            icon: "success"
-                                        }).then(function() {
-                                            location.reload();
-                                        });      
-                                    }
-                                    else{
-                                        Swal.fire({
-                                            title: "Failed",
-                                            text: ret.message,
-                                            icon: "error"
-                                        }).then(function() {                                            
-                                        });      
-                                    }                                    
-                                }
-                            });
+                            console.log(inputValue);
+
+                            // var sem = $(this).attr('rel');
+                            // var data = {'date':inputValue};
+                            // $.ajax({
+                            //     'url':'<?php echo base_url(); ?>registrar/cut_off_registration/'+sem,
+                            //     'method':'post',
+                            //     'data':data,
+                            //     'dataType':'json',
+                            //     'success':function(ret){
+                            //         if(ret.success){
+                            //             Swal.fire({
+                            //                 title: "Success",
+                            //                 text: ret.message,
+                            //                 icon: "success"
+                            //             }).then(function() {
+                            //                 location.reload();
+                            //             });      
+                            //         }
+                            //         else{
+                            //             Swal.fire({
+                            //                 title: "Failed",
+                            //                 text: ret.message,
+                            //                 icon: "error"
+                            //             }).then(function() {                                            
+                            //             });      
+                            //         }                                    
+                            //     }
+                            // });
                         }
                     });
                 }
