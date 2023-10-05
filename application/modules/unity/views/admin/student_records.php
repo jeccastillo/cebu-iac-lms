e3ebe2a0705597505c90b9a9d57b48cc59a1c556
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-10-05 08:29:51 +0800

    Update student_records.php

--- application/modules/unity/views/admin/student_records.php
+++ application/modules/unity/views/admin/student_records.php
@@ -133,7 +133,7 @@
                                                 <td>{{ item.strDescription }}</td>   
                                                 <td>{{ item.rec?item.rec.floatFinalGrade:'---' }}</td>
                                                 <td>{{ item.rec?item.rec.strRemarks:'---' }}</td>
-                                                <td v-if="item.rec && item.rec.include_gwa == 1">{{ (item.rec && item.rec.strRemarks == 'Passed')?item.rec.strUnits:'---' }}</td>
+                                                <td v-if="item.rec && item.rec.include_gwa == 1">{{ (item.rec && (item.rec.strRemarks == 'Passed' || item.rec.strRemarks == 'Officially Withdrawn'))?item.rec.strUnits:'---' }}</td>
                                                 <td v-else>{{ (item.rec && item.rec.strRemarks == 'Passed')?'('+item.rec.strUnits+')':'---' }}</td>                                                                                                                                                                               
                                             </tr>                                                                                    
                                         </tbody>
