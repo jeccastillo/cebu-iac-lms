dce9b5f30a2db1b8eda579c5ad36a4ba7b9933e6
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-10-06 10:51:11 +0800

    Update tor.php

--- application/modules/pdf/views/tor.php
+++ application/modules/pdf/views/tor.php
@@ -53,7 +53,7 @@
            ';
     $header_first_page = '<table border="0" cellspacing="0" cellpadding="0" style="color:#333; font-size:9;">
                 <tr>                    
-                    <td width="70%">
+                    <td width="80%">
                         <table border="0" cellspacing="0" cellpadding="0" style="color:#333; font-size:9;">                                
                             <tr>                            
                                 <td width="20%"><font style="font-size: 8;">Name</font></td>
@@ -100,7 +100,7 @@
                             </tr>
                         </table>
                     </td>
-                    <td width="30%">
+                    <td width="20%">
                         <img src="'.$other_details['picture'].'" witdh="200px" />
                     </td>
                 </tr>     
