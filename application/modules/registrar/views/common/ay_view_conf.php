cb5822f67b511f3c3541e44cb98b6d3271299d83
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-10-11 22:56:00 +0800

    Update ay_view_conf.php

--- application/modules/registrar/views/common/ay_view_conf.php
+++ application/modules/registrar/views/common/ay_view_conf.php
@@ -1,40 +1,41 @@
 <script type="text/javascript">
     $(document).ready(function(){
         $(".cut-off-registration").click(function(e){
-            var cutoff_start = $('#cutoff').val();
-            var cutoff_end = $('#cutoffend').val();
-            if(cutoff_end < cutoff_start){
-                Swal.fire({
-                    title: "Failed",
-                    text: "start can not be greater than end",
-                    icon: "error"
-                })
-            }
-            else
-                Swal.fire({
-                title: 'Cut Off Registration',
-                text: "Are you sure you want continue with cut off?",
-                showCancelButton: true,
-                confirmButtonText: "Yes",
-                imageWidth: 100,
-                icon: "question",
-                cancelButtonText: "No, cancel!",
-                showCloseButton: true,
-                showLoaderOnConfirm: true,
-                    preConfirm: (login) => {       
-                        Swal.fire({
-                            title: 'Cut Off Registration',
-                            text: "Are you absolutely sure you want continue with cut off? Enter today's date in yyyy-mm-dd format.",                            
-                            showCancelButton: true,
-                            input:"text",
-                            confirmButtonText: "Yes",
-                            imageWidth: 100,
-                            icon: "question",
-                            cancelButtonText: "No, cancel!",
-                            showCloseButton: true,
-                            showLoaderOnConfirm: true,
-                            preConfirm: (inputValue) => {  
-                                var sem = $(this).attr('rel');                            
+           
+            Swal.fire({
+            title: 'Cut Off Registration',
+            text: "Are you sure you want continue with cut off?",
+            showCancelButton: true,
+            confirmButtonText: "Yes",
+            imageWidth: 100,
+            icon: "question",
+            cancelButtonText: "No, cancel!",
+            showCloseButton: true,
+            showLoaderOnConfirm: true,
+                preConfirm: (login) => {       
+                    Swal.fire({
+                        title: 'Cut Off Registration',
+                        text: "Are you absolutely sure you want continue with cut off? Enter today's date in yyyy-mm-dd format.",                            
+                        showCancelButton: true,
+                        input:"text",
+                        confirmButtonText: "Yes",
+                        imageWidth: 100,
+                        icon: "question",
+                        cancelButtonText: "No, cancel!",
+                        showCloseButton: true,
+                        showLoaderOnConfirm: true,
+                        preConfirm: (inputValue) => {  
+                            var sem = $(this).attr('rel');     
+                            var cutoff_start = $('#cutoff').val();
+                            var cutoff_end = $('#cutoffend').val();
+                            if(cutoff_end < cutoff_start){
+                                Swal.fire({
+                                    title: "Failed",
+                                    text: "start can not be greater than end",
+                                    icon: "error"
+                                })
+                            }
+                            else{                                                
                                 var data = {'date':inputValue,'cutoff':cutoff,'cutoff_end':cutoff_end};
                                 $.ajax({
                                     'url':'<?php echo base_url(); ?>registrar/cut_off_registration/'+sem,
@@ -62,9 +63,10 @@
                                     }
                                 });
                             }
-                        });
-                    }
-                });
+                        }
+                    });
+                }
+            });
         });
         
         
