de959c3040ed973be497bfb706e81434bbf65dd9
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-10-11 09:54:43 +0800

    updated

--- application/modules/unity/controllers/Unity.php
+++ application/modules/unity/controllers/Unity.php
@@ -1006,9 +1006,7 @@ class Unity extends CI_Controller {
             $cs['equivalent'] = $this->db->get_where('tb_mas_credited',array('equivalent_subject'=>$cs['intSubjectID'],'student_id'=>$data['student']['intID']))->first_row();
             if(!isset($cs['rec'])){
                 if($cs['equivalent'])
-                    $cs['rec']['bg'] = "#009900";
-                else 
-                    $cs['rec']['bg'] = "#f2f2f2";
+                    $cs['rec']['bg'] = "#009900";               
             }
             
                      
