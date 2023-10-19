46ed7945138d552308464cf1849fedc90c826fea
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2023-10-19 08:50:01 +0800

    Update tor.php

--- application/modules/pdf/views/tor.php
+++ application/modules/pdf/views/tor.php
@@ -117,7 +117,7 @@
                                 <td style="line-height:1px;" colspan=2></td>         
                             </tr>
                             <tr>                            
-                                <td colspan="2"><font style="font-size: 8;"></font></td>                                
+                                <td colspan="3"><font style="font-size: 8;"></font></td>                                
                                 <td><font style="font-size: 8;">Date of Graduation</font></td>                
                                 <td width="1%" style="text-align:center">:</td>
                                 <td><font style="font-size: 8;">'.$student['date_of_graduation'].'</font></td>                
@@ -126,7 +126,7 @@
                                 <td style="line-height:1px;" colspan=2></td>         
                             </tr>
                             <tr>                            
-                                <td colspan="2"><font style="font-size: 8;"></font></td>
+                                <td colspan="3"><font style="font-size: 8;"></font></td>
                                 <td><font style="font-size: 8;">NSTP Serial No.</font></td>
                                 <td width="1%" style="text-align:center">:</td>
                                 <td><font style="font-size: 8;">'.$student['nstp_serial'].'</font></td>                
