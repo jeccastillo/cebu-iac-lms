c7d7fe40c842e4f4cb00986e6215971e0975a14c
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-10-24 07:29:31 +0800

    Update Unity.php

--- application/modules/unity/controllers/Unity.php
+++ application/modules/unity/controllers/Unity.php
@@ -1020,7 +1020,7 @@ class Unity extends CI_Controller {
         
         foreach($curicculum as $cs){
             $recs = 
-            $this->db->select('floatFinalGrade,strRemarks,tb_mas_subjects.strUnits,tb_mas_subjects.include_gwa')
+            $this->db->select('floatFinalGrade,strRemarks,tb_mas_subjects.strUnits,tb_mas_subjects.include_gwa,tb_mas_subjects.strCode')
                      ->join('tb_mas_classlist','tb_mas_classlist_student.intClassListID = tb_mas_classlist.intID')  
                      ->join('tb_mas_subjects','tb_mas_classlist.intSubjectID = tb_mas_subjects.intID')                                              
                      ->where(array('tb_mas_classlist.intFinalized'=>2,'tb_mas_classlist.intSubjectID'=>$cs['intSubjectID'],'tb_mas_classlist_student.intStudentID'=>$data['student']['intID'],'tb_mas_classlist_student.strRemarks !='=>'Officially Withdrawn'))                     
@@ -1028,6 +1028,14 @@ class Unity extends CI_Controller {
                      ->result_array();
             
             foreach($recs as $temp_rec){
+                $temp_rec['current'] = false;
+                foreach($data['current_records'] as $current_rec){
+                    if($temp_rec['strCode'] == $current_rec['strCode']){
+                        $temp_rec['floatFinalGrade'] = $current_rec['v3'];
+                        $temp_rec['current'] = true;
+                        break;
+                    }
+                }
                 if($temp_rec && $temp_rec['include_gwa']){                
                     switch($temp_rec['floatFinalGrade']){
                         case 'FA':
