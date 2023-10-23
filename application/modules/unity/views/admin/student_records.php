a599ba8acf1915f70bcc645ac54cf9f5ccb18f2f
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-10-24 07:13:26 +0800

    Update student_records.php

--- application/modules/unity/views/admin/student_records.php
+++ application/modules/unity/views/admin/student_records.php
@@ -44,7 +44,8 @@
                                 <div class="row">
                                     <div class="col-sm-3">School Year: <span style="font-weight:400;">{{ term.reg.strYearStart + "-" + term.reg.strYearEnd }}</span></div>
                                     <div class="col-sm-3">Term: <span style="font-weight:400;">{{term.reg.enumSem + " " + term.reg.term_label}}</span></div>
-                                    <div v-if="term.reg.intROG" class="col-sm-3">Enrollment Status: {{ term.reg.intROG }}</div>                                
+                                    <div v-if="term.reg.intROG" class="col-sm-3">Enrollment Status: {{ term.reg.intROG > 0 ? 'Enrolled' : 'Enlisted' }}</div>
+                                    <div v-else class="col-sm-3">Enrollment Status:</div>                                
                                     <div class="col-sm-3">Course: <span style="font-weight:400;">{{ term.reg.strProgramCode }}</span></div>
                                 </div>                                
                                 <h5></h5>
