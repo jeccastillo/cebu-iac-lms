653a50a017de58d22d9041e1c03c6e4286c24dee
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-10-23 13:56:11 +0800

    Update adjustments.php

--- application/modules/pdf/views/adjustments.php
+++ application/modules/pdf/views/adjustments.php
@@ -1,5 +1,5 @@
 <h4 style="text-align:center">Adjustments</h4>
-<h5><?php $student['strFirstname']." ".$student['strLastname']; ?></h5>
+<h5><?php echo $student['strFirstname']." ".$student['strLastname']; ?></h5>
 <hr />
 <table style="font-size:11px;" class="table table-condensed table-bordered">
     <thead>
