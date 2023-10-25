e91253aacecf2fa717ee7f638a007e56bb0a1c1b
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2023-10-25 12:58:48 +0800

    Update Unity.php

--- application/modules/unity/controllers/Unity.php
+++ application/modules/unity/controllers/Unity.php
@@ -2417,9 +2417,19 @@ class Unity extends CI_Controller {
             $post['date_added'] = date("Y-m-d H:i:s");
             $this->data_poster->update_classlist('tb_mas_classlist_student',$post,$post['intCSID']);
 
-            if($clist['intFinalized'] == 2){
+            if($clist['intFinalized'] == 1 && $term == 2){
                 $cg['student_id'] = $item['intStudentID'];
-                $cg['from_grade'] = $item['floatFinalGrade']?$item['floatFinalGrade']:'NGS';
+                $cg['from_grade'] = "MIDTERM: ".$item['floatMidtermGrade']?$item['floatMidtermGrade']:'NGS';
+                $cg['to_grade'] = $post['floatMidtermGrade']; 
+                $cg['changed_by'] = $this->data["user"]["strFirstname"]." ".$this->data["user"]["strLastname"];
+                $cg['date'] = date("Y-m-d H:i:s");
+                $cg['classlist_id'] = $item['intClassListID'];
+
+                $this->db->insert('tb_mas_student_grade_change',$cg);
+            }
+            if($clist['intFinalized'] == 2 && $term == 3){
+                $cg['student_id'] = $item['intStudentID'];
+                $cg['from_grade'] = "FINAL: ".$item['floatFinalGrade']?$item['floatFinalGrade']:'NGS';
                 $cg['to_grade'] = $post['floatFinalGrade']; 
                 $cg['changed_by'] = $this->data["user"]["strFirstname"]." ".$this->data["user"]["strLastname"];
                 $cg['date'] = date("Y-m-d H:i:s");
