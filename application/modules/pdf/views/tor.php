86d0ca12faef1efa2720924f18b41816d58bad2f
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-10-26 13:36:29 +0800

    Update tor.php

--- application/modules/pdf/views/tor.php
+++ application/modules/pdf/views/tor.php
@@ -26,7 +26,7 @@
     $pdf->SetTitle("Enrollment Summary");
     
     // set margins
-    $pdf->SetMargins(15, 5 , 10);
+    $pdf->SetMargins(20, 5 , 5);
     $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
     $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
     $pdf->setFooterData();
