9a4deae95d290fba09ef8faacacde28bf122c4b9
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-11-09 11:18:00 +0800

    Update ledger_table_conf.php

--- application/modules/finance/views/common/ledger_table_conf.php
+++ application/modules/finance/views/common/ledger_table_conf.php
@@ -31,7 +31,7 @@
                     "aTargets":[3],
                     "mData": null,
                     "bSortable":false,
-                    "mRender": function (data,type,row,meta) { return '<a href="<?php echo base_url(); ?>finance/student_ledger/'+row[0]+'">'+row[2]+'</a>'; }
+                    "mRender": function (data,type,row,meta) { return '<a href="<?php echo base_url(); ?>finance/student_ledger/'+row[0]+'">'+row[3]+'</a>'; }
                 },          
                 <?php endif; ?>                  
                 {
