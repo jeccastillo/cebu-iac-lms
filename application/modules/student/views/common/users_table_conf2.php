c121b9b7a961948afaaa66e526f24f05ada71d62
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-11-09 11:25:42 +0800

    sd

--- application/modules/student/views/common/users_table_conf2.php
+++ application/modules/student/views/common/users_table_conf2.php
@@ -37,7 +37,7 @@
                     "aTargets":[3],
                     "mData": null,
                     "bSortable":false,
-                    "mRender": function (data,type,row,meta) { return '<a href="<?php echo base_url(); ?>finance/student_ledger/'+row[0]+'">'+row[3]+'</a>'; }
+                    "mRender": function (data,type,row,meta) { return '<a href="<?php echo base_url(); ?>unity/student_viewer/'+row[0]+'">'+row[3]+'</a>'; }
                 },
             ],
             "aaSorting": [[2,'asc']],
