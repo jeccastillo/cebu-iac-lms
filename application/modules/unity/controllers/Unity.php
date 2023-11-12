3f729c3acaa94da77f9136d936f1cd76ef119ba1
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-11-13 01:08:26 +0800

    Update Unity.php

--- application/modules/unity/controllers/Unity.php
+++ application/modules/unity/controllers/Unity.php
@@ -645,7 +645,7 @@ class Unity extends CI_Controller {
         $student = $this->data_fetcher->getStudent($id, 'slug');            
         $data['selected_ay'] = $active_sem['intID'];
         $data['id'] = $student['intID'];
-        $data['slug'] = $id;
+        
 
         $this->load->view("public/header",$this->data);
         $this->load->view("public/payment_online_tuition",$data);
@@ -662,7 +662,7 @@ class Unity extends CI_Controller {
         $student = $this->data_fetcher->getStudent($id, 'slug');            
         $data['sem'] = $active_sem['intID'];
         $data['id'] = $student['intID'];
-
+        $data['slug'] = $id;
         $this->load->view("public/payment_online_tuition_bdo",$data);
         
     }  
