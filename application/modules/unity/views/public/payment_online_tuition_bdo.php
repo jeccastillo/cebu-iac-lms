7d2a63a0a98772157a34ee4a7c9acf5441f072ad
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-11-13 01:09:45 +0800

    Update payment_online_tuition_bdo.php

--- application/modules/unity/views/public/payment_online_tuition_bdo.php
+++ application/modules/unity/views/public/payment_online_tuition_bdo.php
@@ -59,8 +59,8 @@
             'url':'<?php echo base_url(); ?>unity/online_payment_data/<?php echo $id ?>/<?php echo $sem; ?>',
             'method':'get',            
             'dataType':'json',
-            'success':function(data){
-                console.log(data.data);
+            'success':function(ret){
+                console.log(ret.data);
             }
         });
     });
