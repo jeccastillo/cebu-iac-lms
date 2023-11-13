2e80b19487ef41556cf0b60f91adc0e41de77ff1
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2023-11-13 12:16:46 +0800

    Update Unity.php

--- application/modules/unity/controllers/Unity.php
+++ application/modules/unity/controllers/Unity.php
@@ -1751,14 +1751,12 @@ class Unity extends CI_Controller {
                          ->result_array();
                 $subject['prereq'] = [];    
                 foreach($prereq_array as $prereq){
-                    if(!isset($prereq['program']) || $prereq['program'] == 0  || $prereq['program'] == $this->data['item']['intID'])
+                    if($prereq['program'] == $this->data['item']['intID'])
                         $subject['prereq'][] =  $prereq;                
                 }       
     
                 $this->data['curriculum_subjects'][] = $subject;
-            }  
-
-            print_r($this->data['curriculum_subjects']);
+            }   
 
             
             $this->load->view("common/header",$this->data);
