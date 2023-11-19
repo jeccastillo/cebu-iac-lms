15660cbbfd34ada5e1e66f258a93acb9e5df2b25
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-11-19 18:26:45 +0800

    Update payment_tuition.php

--- application/modules/site/views/finance/payment_tuition.php
+++ application/modules/site/views/finance/payment_tuition.php
@@ -59,6 +59,14 @@
                             <img class="img-fluid d-block mx-auto" width="51px" :src="t.image_url" alt="">
                         </div>
                     </div>
+                    <h5 class="my-3">BDO PAY</h5>
+                    <div class="d-flex flex-wrap" style="display:flex; flex:wrap;">
+                        <div style="border:1px solid #000" @click="selectPayment(bdo_pay)"
+                            class="box_mode_payment d-flex align-items-center justify-content-center mr-3 my-3 p-1"
+                            style="display:flex; align-itenms:center;">
+                            <img class="img-fluid d-block mx-auto" width="51px" :src="https://portalv2.iacademy.edu.ph/images/finance_online_payment/bdo.jpg" alt="">                                                
+                        </div>
+                    </div>   
                 </div>
 
                 <hr>
@@ -196,6 +204,15 @@ new Vue({
         total_price_cart_with_charge: 0,
         payload: {},
         slug: "<?php echo $this->uri->segment('3'); ?>",
+        bdo_pay:{
+            charge: 0,
+            id: 99,
+            is_nonbank: true,
+            name: "BDO PAY",
+            pchannel: "bdo_pay",
+            pmethod: "onlinebanktransfer",
+            type: "none"
+        }
     },
     mounted() {
 
