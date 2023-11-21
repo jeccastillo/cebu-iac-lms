471f363b004cf95c7f4f77065233096abf66aa93
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-11-21 20:47:19 +0800

    Update student_viewer.php

--- application/modules/unity/views/admin/student_viewer.php
+++ application/modules/unity/views/admin/student_viewer.php
@@ -24,7 +24,7 @@
                     <a v-if="reg_status != 'For Subject Enlistment' && reg_status != 'For Sectioning' && (user_level == 2 || user_level == 3)" target="_blank" class="btn btn-app" :href="base_url + 'pdf/student_viewer_advising_print/' + student.intID + '/' + active_sem.intID">
                         <i class="ion ion-printer"></i>Print Subjects
                     </a> 
-                    <a v-else-if="user_level == 2 || user_level == 3" class="btn btn-app" :href="base_url + 'department/load_subjects/' + student.intID">
+                    <a v-else-if="user_level == 2 || user_level == 3" class="btn btn-app" :href="base_url + 'department/load_subjects/' + student.intID + '/' + active_sem.intID">
                         <i class="fa fa-book"></i>Subject Enlistment</a> 
                     </a>
                     <!-- <a v-if="reg_status == 'For Registration' && (user_level == 2 || user_level == 3)"  class="btn btn-app" :href="base_url + 'unity/edit_sections/' + student.intID + '/' + active_sem.intID">
