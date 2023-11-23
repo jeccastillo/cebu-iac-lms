e27d3db6e8b10a729c7920e29874c0a7e24fb799
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-11-24 06:23:21 +0800

    Update payment_online_tuition.php

--- application/modules/unity/views/public/payment_online_tuition.php
+++ application/modules/unity/views/public/payment_online_tuition.php
@@ -422,70 +422,71 @@ new Vue({
                             this.other_payments = data.data.other;
                                    
                                                          
-
-                            axios.get(api_url + 'finance/reservation/' + this.slug + '/' + this.sem)
-                            .then((data) => {
-                                this.reservation_payment = data.data.data;    
-                                this.application_payment = data.data.application;
-                                
-                                if(this.reservation_payment.status == "Paid" && data.data.student_sy == this.sem){
-                                        this.remaining_amount = this.remaining_amount - this.reservation_payment.subtotal_order;                                                                                            
-                                        this.amount_paid = this.amount_paid + this.reservation_payment.subtotal_order;                                        
-                                }                                
-                                this.remaining_amount = (this.remaining_amount < 0.02) ? 0 : this.remaining_amount;
-                                this.remaining_amount = Math.round(this.remaining_amount * 100) / 100;
-                                this.remaining_amount_formatted = this.remaining_amount.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-                                this.amount_paid_formatted = this.amount_paid.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');                                
-                                this.item_details.price = this.remaining_amount;
-                                this.loader_spinner = false;
-
-                                let down_payment = (this.tuition_data.down_payment <= this.amount_paid) ? 0 : ( this.tuition_data.down_payment - this.amount_paid );
-                                
-                                if(this.registration.downpayment == 1 || down_payment == 0){
-                                    this.has_down = true;                                    
-                                    //installment amounts                                                                    
-                                    var temp = (this.tuition_data.installment_fee * 5) - parseFloat(this.remaining_amount);
-                                    console.log(temp);
-                                    for(i=0; i < 5; i++){
-                                        if(this.tuition_data.installment_fee > temp){
-                                            val = this.tuition_data.installment_fee - temp;                                            
-                                            this.item_details.price = val;
-                                            break;
-                                        }     
-                                        else{
-                                            temp = temp - this.tuition_data.installment_fee;
-                                        }                                                                       
-                                    }
-                                    
-                                    
-                                }
-                                else if(this.payment_type == "partial"){
-                                    
-                                    this.item_details.price = down_payment;
-                                }                            
-                                else{
-                                    
-                                    this.item_details.price = this.remaining_amount;
-                                }      
-                                axios.get(api_url + 'admissions/student-info/' + this.slug)
+                            if(this.registration.enumStudentType == "new"){
+                                axios.get(api_url + 'finance/reservation/' + this.slug + '/' + this.sem)
                                 .then((data) => {
-                                    this.student_api_data = data.data.data;
-                                    Swal.close();
-                                    $(function() {
-                                        $(".box_mode_payment").click(function() {
-                                            $(".box_mode_payment").removeClass("active");
-                                            $(this).addClass("active");
+                                    this.reservation_payment = data.data.data;    
+                                    this.application_payment = data.data.application;
+                                    
+                                    if(this.reservation_payment.status == "Paid" && data.data.student_sy == this.sem){
+                                            this.remaining_amount = this.remaining_amount - this.reservation_payment.subtotal_order;                                                                                            
+                                            this.amount_paid = this.amount_paid + this.reservation_payment.subtotal_order;                                        
+                                    }                                
+                                    this.remaining_amount = (this.remaining_amount < 0.02) ? 0 : this.remaining_amount;
+                                    this.remaining_amount = Math.round(this.remaining_amount * 100) / 100;
+                                    this.remaining_amount_formatted = this.remaining_amount.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+                                    this.amount_paid_formatted = this.amount_paid.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');                                
+                                    this.item_details.price = this.remaining_amount;
+                                    this.loader_spinner = false;
+
+                                    let down_payment = (this.tuition_data.down_payment <= this.amount_paid) ? 0 : ( this.tuition_data.down_payment - this.amount_paid );
+                                    
+                                    if(this.registration.downpayment == 1 || down_payment == 0){
+                                        this.has_down = true;                                    
+                                        //installment amounts                                                                    
+                                        var temp = (this.tuition_data.installment_fee * 5) - parseFloat(this.remaining_amount);
+                                        console.log(temp);
+                                        for(i=0; i < 5; i++){
+                                            if(this.tuition_data.installment_fee > temp){
+                                                val = this.tuition_data.installment_fee - temp;                                            
+                                                this.item_details.price = val;
+                                                break;
+                                            }     
+                                            else{
+                                                temp = temp - this.tuition_data.installment_fee;
+                                            }                                                                       
+                                        }
+                                        
+                                        
+                                    }
+                                    else if(this.payment_type == "partial"){
+                                        
+                                        this.item_details.price = down_payment;
+                                    }                            
+                                    else{
+                                        
+                                        this.item_details.price = this.remaining_amount;
+                                    }      
+                                    axios.get(api_url + 'admissions/student-info/' + this.slug)
+                                    .then((data) => {
+                                        this.student_api_data = data.data.data;
+                                        Swal.close();
+                                        $(function() {
+                                            $(".box_mode_payment").click(function() {
+                                                $(".box_mode_payment").removeClass("active");
+                                                $(this).addClass("active");
+                                            })
                                         })
                                     })
+                                    .catch((error) => {
+                                        console.log(error);
+                                    })
+                                    
                                 })
                                 .catch((error) => {
                                     console.log(error);
                                 })
-                                
-                            })
-                            .catch((error) => {
-                                console.log(error);
-                            })
+                            }
                         })
                         .catch((error) => {
                             console.log(error);
