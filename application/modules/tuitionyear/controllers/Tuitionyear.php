60698ddaafaaa9e80cf395a8c5461e147e966348
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-11-29 10:23:47 +0800

    Update Tuitionyear.php

--- application/modules/tuitionyear/controllers/Tuitionyear.php
+++ application/modules/tuitionyear/controllers/Tuitionyear.php
@@ -121,7 +121,7 @@ class Tuitionyear extends CI_Controller {
 
     public function delete_type(){
         $post = $this->input->post();
-        if($post['type'] != "track")
+        if($post['type'] != "track" && $post['type'] != "program")
             $this->data_poster->deleteItem('tb_mas_tuition_year_'.$post['type'],$post['id'],'intID');
         else
             $this->data_poster->deleteItem('tb_mas_tuition_year_'.$post['type'],$post['id'],'id');
