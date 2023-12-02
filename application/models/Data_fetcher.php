47bc0fe309871aa7afa95d48439564d5c5a7402b
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-12-02 10:46:20 +0800

    Update Data_fetcher.php

--- application/models/Data_fetcher.php
+++ application/models/Data_fetcher.php
@@ -2488,7 +2488,7 @@ class Data_fetcher extends CI_Model {
         
 
         foreach($misc as $m){            
-            if($stype != 'new' || trim($m['name']) != 'ID Validation' ){
+            if($stype != 'new' || strtoupper(trim($m['name'])) != 'ID VALIDATION' ){
                 $misc_list[$m['name']] = getExtraFee($m, $class_type, 'misc');
                 $total_misc += $misc_list[$m['name']];
             }
