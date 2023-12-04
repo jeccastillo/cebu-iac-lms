384071722d61a5e969d7c79e45c2d4314d9ef5a9
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2023-12-04 10:16:36 +0800

    Update print_view_student_reg2_makati.php

--- application/modules/pdf/views/print_view_student_reg2_makati.php
+++ application/modules/pdf/views/print_view_student_reg2_makati.php
@@ -70,7 +70,7 @@
 $html .= '   
     </table>
      <br />
-    <table border="0" cellpadding="0" style="color:#333; font-size:8;" width="550px">     
+    <table border="0" cellpadding="0" style="color:#333; font-size:8;" width="580px">     
      <tr>
       <td width="80px" >&nbsp;</td>
       <td width="250px">&nbsp;</td>
@@ -178,13 +178,13 @@ $html.= '<table border="0" cellpadding="0" cellspacing="0" style="color:#333; fo
 
                          
         $html.='
-        <table border="0" cellpadding="0" style="color:#333; font-size:8; " width="550px">        
+        <table border="0" cellpadding="0" style="color:#333; font-size:8; " width="580px">        
             <tr>
                 <td colspan="2" style= "font-size:8; line-height:1.5; border-top:1px solid #333;"></td>                
             </tr>
         </table>
         
-        <table border="0" cellpadding="0" style="color:#333; font-size:8; " width="550px">        
+        <table border="0" cellpadding="0" style="color:#333; font-size:8; " width="580px">        
             
         </table>
         ';
@@ -389,18 +389,18 @@ $html.= '<table border="0" cellpadding="0" cellspacing="0" style="color:#333; fo
                     </td>
                 </tr>
             </table>
-            <table border="0" cellpadding="0" style="color:#333; font-size:9; " width="550px">
+            <table border="0" cellpadding="0" style="color:#333; font-size:9; " width="580px">
                 <tr>
                     <td colspan="2" style="font-size:9; line-height:2; color:#fff;">Space</td>
                 </tr>
             </table>
-            <table border="0" cellpadding="0" style="color:#333; font-size:9; " width="550px">        
+            <table border="0" cellpadding="0" style="color:#333; font-size:9; " width="580px">        
                 <tr>
                     <td width="120px" style= "font-size:9;">Official Receipt Number/date</td>
                     <td width="120px" style= "font-size:9; border-bottom:1px solid #333;"></td>            
                 </tr>
             </table>
-            <table border="0" cellpadding="0" style="color:#333; font-size:9; " width="550px">        
+            <table border="0" cellpadding="0" style="color:#333; font-size:9; " width="580px">        
                 <tr>
                     <td width="264px" style= "font-size:9;">Enrollment Confirmed by:</td>
                     <td width="264px" style= "font-size:9;"></td>            
@@ -418,30 +418,30 @@ $html.= '<table border="0" cellpadding="0" cellspacing="0" style="color:#333; fo
                     <td style= "font-size:9; text-align:center">Registrar</td>            
                 </tr>
             </table>
-            <table border="0" cellpadding="0" style="color:#333; font-size:9; " width="550px">
+            <table border="0" cellpadding="0" style="color:#333; font-size:9; " width="580px">
                 <tr>
                     <td colspan="2" style="font-size:9; line-height:1; color:#fff;">Space</td>
                 </tr>
             </table>
-            <table border="0" cellpadding="0" style="color:#333; font-size:8; " width="550px">        
+            <table border="0" cellpadding="0" style="color:#333; font-size:8; " width="580px">        
                 <tr>
                     <td width="264px" style= "font-size:8;">Note: Class schedule is subject to change</td>
                     <td width="264px" style= "font-size:8;">Generated: '.date("Y-m-d h:i A").' by '.$user['strFirstname'].' '.$user['strLastname'].'</td>            
                 </tr>
             </table>
-            <table border="0" cellpadding="0" style="color:#333; font-size:8; " width="550px">
+            <table border="0" cellpadding="0" style="color:#333; font-size:8; " width="580px">
                 <tr>
                     <td colspan="2" style="font-size:9; line-height:1; color:#fff;">Space</td>
                 </tr>
             </table>
-            <table border="0" cellpadding="0" style="color:#333; font-size:8; " width="550px">
+            <table border="0" cellpadding="0" style="color:#333; font-size:8; " width="580px">
                 <tr>
                     <td colspan="2" style="font-size:8;">I shall abide by all existing rules and regulations of the School and those that may be promulgated from time to time.
                     I understand that the school has to collect my personal data and I allow the school to process all my information and all
                     purposes related to this.</td>
                 </tr>
             </table> 
-            <table border="0" cellpadding="0" style="color:#333; font-size:8; " width="550px">
+            <table border="0" cellpadding="0" style="color:#333; font-size:8; " width="580px">
                 <tr>
                     <td colspan="2" style="font-size:9; line-height:1; color:#fff;">Space</td>
                 </tr>
@@ -449,7 +449,7 @@ $html.= '<table border="0" cellpadding="0" cellspacing="0" style="color:#333; fo
                           
         ';
 
-        $html .=' <table border="0" cellspacing="5px" cellpadding="0" style="color:#333; font-size:7; " width="550px">        
+        $html .=' <table border="0" cellspacing="5px" cellpadding="0" style="color:#333; font-size:7; " width="580px">        
         <tr>
             <td>Policy on School Charges and Refund of Fees<br />
                 Officially Enrolled Students who withdraw their enrollment before the official start of classes shall be charged a Withdrawal Fee of two thousand
@@ -466,7 +466,7 @@ $html.= '<table border="0" cellpadding="0" cellspacing="0" style="color:#333; fo
             </td>
         </tr>                                              
     </table> 
-    <table border="0" cellpadding="0" style="color:#333; font-size:8; " width="550px">        
+    <table border="0" cellpadding="0" style="color:#333; font-size:8; " width="580px">        
                 <tr>
                     <td width="230px" style= "font-size:9; text-align:center;">&nbsp;</td>
                     <td width="30px" style= "font-size:9; text-align:center;">&nbsp;</td>
