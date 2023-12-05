8347bb67e1112e8dab2098c675fb4cab69bb4edb
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2023-12-05 09:56:47 +0800

    Update print_or_makati.php

--- application/modules/pdf/views/print_or_makati.php
+++ application/modules/pdf/views/print_or_makati.php
@@ -94,7 +94,7 @@
                 </tr>
             </table>
             <table>
-                <tr style="line-height:5px;">
+                <tr style="line-height:15px;">
                     <td style=""></td>
                 </tr>
             </table>
