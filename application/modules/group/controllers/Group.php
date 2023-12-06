eb39b9219a8d75942d2b7d0410fa381e9ca6eb2d
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-12-07 06:00:19 +0800

    updated

---
+++ application/modules/group/controllers/Group.php
@@ -0,0 +1,116 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Group extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->config->load('themes');		
+		$theme = $this->config->item('unity');
+		if($theme == "" || !isset($theme))
+			$theme = $this->config->item('global_theme');
+
+        //User Level Validation
+        $userlevel = $this->session->userdata('intUserLevel');        
+        if($userlevel != 2) 
+		  redirect(base_url()."unity");
+          
+        $settings = $this->data_fetcher->fetch_table('su-tb_sys_settings');
+		foreach($settings as $setting)
+		{
+			$this->settings[$setting['strSettingName']] = $setting['strSettingValue'];
+		}
+        
+        $this->data['img_dir'] = base_url()."assets/themes/".$theme."/images/";	
+        $this->data['student_pics'] = base_url()."assets/photos/";
+        $this->data['css_dir'] = base_url()."assets/themes/".$theme."/css/";
+        $this->data['js_dir'] = base_url()."assets/themes/".$theme."/js/";
+        $this->data['title'] = "iACADEMY";
+        $this->load->library("email");	
+        $this->load->helper("cms_form");	
+		$this->load->model("user_model");
+        $this->config->load('courses');
+        $this->data['department_config'] = $this->config->item('department');
+        $this->data['terms'] = $this->config->item('terms');
+        $this->data['term_type'] = $this->config->item('term_type');
+        $this->data['unit_fee'] = $this->config->item('unit_fee');
+        $this->data['misc_fee'] = $this->config->item('misc_fee');
+        $this->data['lab_fee'] = $this->config->item('lab_fee');
+        $this->data['id_fee'] = $this->config->item('id_fee');
+        $this->data['athletic'] = $this->config->item('athletic');
+        $this->data['srf'] = $this->config->item('srf');
+        $this->data['sfdf'] = $this->config->item('sfdf');
+        $this->data['csg'] = $this->config->item('csg');
+        $this->data["subjects"] = $this->data_fetcher->fetch_table('tb_mas_subjects');
+        $this->data["students"] = $this->data_fetcher->fetch_table('tb_mas_users',array('strLastname','asc'));
+        $this->data["user"] = $this->session->all_userdata();
+        $this->data['unread_messages'] = $this->data_fetcher->count_table_contents('tb_mas_message_user',null,array('intRead'=>'0','intTrash'=>0,'intFacultyID'=>$this->session->userdata('intID')));
+        
+        $this->data['all_messages'] = $this->data_fetcher->count_table_contents('tb_mas_message_user',null,array('intTrash'=>0,'intFacultyID'=>$this->session->userdata('intID')));
+        
+        $this->data['trashed_messages'] = $this->data_fetcher->count_table_contents('tb_mas_message_user',null,array('intTrash'=>1,'intFacultyID'=>$this->session->userdata('intID')));
+        
+        $this->data['sent_messages'] = $this->data_fetcher->count_sent_items($this->session->userdata('intID'));
+        $this->data['page'] = "subjects";
+    }
+    
+   
+    public function group($id = 0){
+
+        $this->data['id'] = $id;        
+        $this->data['opentree'] = "admin";
+        $this->data['page'] = "group";              
+        $this->load->view("common/header",$this->data);
+        $this->load->view("admin/group",$this->data);
+        $this->load->view("common/footer",$this->data);
+        
+        
+
+    }
+
+    public function group_data(){
+        $post = $this->input->post();
+        $ret['group'] = $this->db->get_where('tb_mas_user_group',array('id' => $post['id']))->first_row();
+        $ret['group_access'] = $this->db->get_where('tb_mas_user_group_access',array('group_id' => $post['id']))->result_array();
+        $ret['group_users'] = $this->db->get_where('tb_mas_user_access',array('id' => $post['id']))->result_array();
+        echo json_encode($ret);
+    }
+
+    public function submit_group(){
+        $post = $this->input->post();                
+        if($post['id'] != 0)
+            if($this->db->insert('tb_mas_user_group',$post)){
+                $data['success'] = true;
+                $data['message'] = "Successfully Added Group";
+            }
+            else{
+                $data['success'] = false;
+                $data['message'] = "Failed to Add";
+            }
+        else
+            if($this->db
+                    ->where('id',$post['id'])
+                    ->update('tb_mas_user_group',$post)){
+                $data['success'] = true;
+                $data['message'] = "Successfully Updated Group";
+            }
+            else{
+                $data['success'] = false;
+                $data['message'] = "Failed to Update";
+            }
+
+        echo json_encode($data);
+    }
+    
+    public function is_super_admin()
+    {
+         $admin = $this->session->userdata('intUserLevel');
+        if($admin == 2)
+            return true;
+        else
+            return false;
+    }
+    
+
+
+}
---
+++ application/modules/group/views/admin/group.php
@@ -0,0 +1,167 @@
+<aside class="right-side" id="registration-container">    
+<section class="content-header">
+        <h1>
+            Student Deficiencies
+            <small>
+                <a class="btn btn-app" :href="base_url + 'unity/student_viewer/' + student.intID"><i class="ion ion-arrow-left-a"></i>All Details</a>                                                 
+            </small>
+        </h1>
+        <hr />
+    </section>
+        <hr />
+    <div class="content">  
+        <div class="box box-primary">
+            <div class="box-header">
+                <h3>Group</h3>
+            </div>
+            <div class="box-body">                
+                <h4>Add/Edit Group</h4>
+                <form method="post" @submit.prevent="submitGroup">
+                    <div class="row">
+                        <div class="col-sm-6 form-group">
+                            <label>Group Name</label>
+                            <textarea required class="form-control" v-model="request.group_name"></textarea>
+                        </div>
+                    </div>  
+                    <hr />
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+                <hr />
+                <!-- <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>Term</th>
+                            <th>Deficiency</th>
+                            <th>Department</th>
+                            <th>Remarks</th>
+                            <th>Date Added</th>
+                            <th>Added By</th>
+                            <th>Date Resolved</th>
+                            <th>Resolved By</th>
+                            <th>Status</th>                             
+                            <th>Actions</th>                                                       
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-if="deficiencies.length == 0">
+                            <td colspan='8'>No Deficiencies for this term</td>
+                        </tr>
+                        <tr v-else v-for="item in deficiencies">
+                            <td>{{ item.enumSem + " " + item.term_label + " " + item.strYearStart + "-" + item.strYearEnd}}</td>
+                            <td>{{ item.details }}</td>
+                            <td>{{ item.department }}</td>
+                            <td>{{ item.remarks }}</td>
+                            <td>{{ item.date_added }}</td>
+                            <td>{{ item.added_by }}</td>
+                            <td>{{ item.date_resolved }}</td>
+                            <td>{{ item.resolved_by }}</td>
+                            <td>{{ item.status  }}</td>
+                            <td v-if="item.department == request.department && item.status != 'resolved' || user.intUserLevel == 2">
+                                <a class="btn btn-primary" @click.prevent="resolveDeficiency(item.id)">Resolve</a> <a v-if="user.intUserLevel == 2 || user.special_role == 2" class="btn btn-success" href="#" data-toggle="modal" data-target="#temporaryResolve" @click="setResolveID(item.id)">Temp Resolve</a>
+                            </td>
+                            <td v-else></td>
+                        </tr>
+                    </tbody>
+                </table>                               -->
+            </div>        
+        </div>
+        
+    </div>
+    
+</aside>
+
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/themes/default/js/script.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/vue@2.6.12"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js"
+    integrity="sha512-WFN04846sdKMIP5LKNphMaWzU7YpMyCU245etK3g/2ARYbPK9Ub18eG+ljU96qKRCWh+quCY7yefSmlkQw1ANQ=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.min.js"></script>
+
+<script>
+new Vue({
+    el: '#registration-container',
+    data: {                    
+        base_url: '<?php echo base_url(); ?>',       
+        id: '<?php echo $id; ?>',
+        group: {},
+        group_access:[],
+        group_users:[],
+        request:{
+            id: '<?php echo $id; ?>',
+            group_name: undefined,            
+        }  
+        
+    },
+
+    mounted() {
+
+        let url_string = window.location.href;        
+                
+            //this.loader_spinner = true;
+            axios.get(this.base_url + 'group/group_data/'+this.id)
+                .then((data) => {                                      
+                    this.group = data.data.group;
+                    this.group_access = data.data.group_access;
+                    this.group_users = data.data.group_users;                                        
+                })
+            .catch((error) => {
+                console.log(error);
+                
+            });    
+
+    },
+
+    methods: {              
+        submitGroup: function(){            
+            Swal.fire({
+                title: 'Submit Group?',
+                text: "Continue?",
+                showCancelButton: true,
+                confirmButtonText: "Yes",
+                imageWidth: 100,
+                icon: "question",
+                cancelButtonText: "No, cancel!",
+                showCloseButton: true,
+                showLoaderOnConfirm: true,
+                preConfirm: (login) => {
+                    var formdata= new FormData();
+                    for (const [key, value] of Object.entries(this.request)) {
+                        formdata.append(key,value);
+                    }                                                              
+                    return axios
+                        .post('<?php echo base_url(); ?>group/submit_group',formdata, {
+                                headers: {
+                                    Authorization: `Bearer ${window.token}`
+                                }
+                            })
+                        .then(data => {
+                            console.log(data.data);
+                            if (data.data.success) {
+                                Swal.fire({
+                                    title: "Success",
+                                    text: data.data.message,
+                                    icon: "success"
+                                }).then(function() {
+                                    location.reload();
+                                });
+                            } else {
+                                Swal.fire(
+                                    'Failed!',
+                                    data.data.message,
+                                    'error'
+                                )
+                            }
+                        });
+                },
+                allowOutsideClick: () => !Swal.isLoading()
+            });
+        },
+        
+       
+                                       
+    }
+
+})
+</script>
+
