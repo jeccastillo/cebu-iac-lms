849c9637c0e6135a553a553a8a277beb71dc48a6
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2023-12-12 09:37:43 +0800

    Update group.php

--- application/modules/group/views/admin/group.php
+++ application/modules/group/views/admin/group.php
@@ -202,7 +202,7 @@ new Vue({
                                     text: data.data.message,
                                     icon: "success"
                                 }).then(function() {
-                                    document.location = base_url + 'group/add_group' + data.data.group_id;
+                                    document.location = base_url + 'group/add_group/' + data.data.group_id;
                                 });
                             } else {
                                 Swal.fire(
