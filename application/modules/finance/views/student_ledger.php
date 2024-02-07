22dde12c91db32be0b8e295804a32618225be533
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-02-07 14:09:07 +0800

    Update student_ledger.php

--- application/modules/finance/views/student_ledger.php
+++ application/modules/finance/views/student_ledger.php
@@ -104,7 +104,7 @@
                                 <td :class="item.muted">{{  item.remarks }}</td>
                                 <td :class="item.muted">{{ (item.amount >= 0)?numberWithCommas(item.amount):'-' }}</td>
                                 <td :class="item.muted">{{ (item.amount < 0)?numberWithCommas(item.amount * -1):'-' }}</td>
-                                <td :class="item.muted">{{ item.balance < 0 ?"(" + (item.balance * -1) + ")": item.balance }}</td>
+                                <td :class="item.muted">{{ item.balance < 0 ?"(" + numberWithCommas(item.balance * -1) + ")": numberWithCommas(item.balance) }}</td>
                                 <td :class="item.muted">{{ (item.added_by != 0) ? 'Manually Generated': 'System Generated' }}</td>   
                                 <td :class="item.muted" v-if="item.added_by == 0"><a @click="cashierDetails(item.cashier)" href="#">{{ item.cashier }}</a></td>
                                 <td :class="item.muted" v-else><a @click="cashierDetails(item.added_by)" href="#">{{ item.added_by }}</a></td>
