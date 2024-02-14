6465bc6bfb85aad450fa4dfc59e492e9d31de04d
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-02-14 13:25:58 +0800

    Update list2_conf.php

--- application/modules/finance/views/common/list2_conf.php
+++ application/modules/finance/views/common/list2_conf.php
@@ -95,7 +95,7 @@ $(document).ready(function() {
                                     const hiddenField = document.createElement('input');
                                     hiddenField.type = 'hidden';
                                     hiddenField.name = 'data';
-                                    hiddenField.value = JSON.stringify(json.data);
+                                    hiddenField.value = JSON.stringify(payee_data.data);
 
                                     form.appendChild(hiddenField);
 
