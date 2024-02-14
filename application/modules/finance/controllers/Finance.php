70922f6d60eed60666182c16c54bc07fa53b560e
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-02-14 12:48:22 +0800

    Update Finance.php

--- application/modules/finance/controllers/Finance.php
+++ application/modules/finance/controllers/Finance.php
@@ -185,7 +185,7 @@ class Finance extends CI_Controller {
 
     public function get_payee_details(){
         $post =  $this->input->post();
-        $post = json_decode($post);
+        $post = json_decode(current($post));
         print_r($post);
     }
 
