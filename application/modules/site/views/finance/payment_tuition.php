f23577e7ac7f5506aafa55996431e1b73c168ef6
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-02-16 18:06:11 +0800

    asdsd

--- application/modules/site/views/finance/payment_tuition.php
+++ application/modules/site/views/finance/payment_tuition.php
@@ -59,14 +59,14 @@
                             <img class="img-fluid d-block mx-auto" width="51px" :src="t.image_url" alt="">
                         </div>
                     </div>
-                    <h5 class="my-3">BDO PAY</h5>
+                    <!-- <h5 class="my-3">BDO PAY</h5>
                     <div class="d-flex flex-wrap" style="display:flex; flex:wrap;">
                         <div style="border:1px solid #000" @click="selectPayment(bdo_pay)"
                             class="box_mode_payment d-flex align-items-center justify-content-center mr-3 my-3 p-1"
                             style="display:flex; align-itenms:center;">
                             <img class="img-fluid d-block mx-auto" width="51px" src="https://portalv2.iacademy.edu.ph/images/finance_online_payment/bdo.jpg" alt="">
                         </div>
-                    </div> 
+                    </div>  -->
                     <div>
                     <h5 class="my-3">Maya</h5>
                         <hr />  
