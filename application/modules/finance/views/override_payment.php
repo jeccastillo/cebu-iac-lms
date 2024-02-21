85afa74bd7e36ecf06cbd2ac1016a693c75bca46
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-02-21 16:07:38 +0800

    Update override_payment.php

--- application/modules/finance/views/override_payment.php
+++ application/modules/finance/views/override_payment.php
@@ -116,6 +116,7 @@ new Vue({
                             var formdata = new FormData();
                             formdata.append('request_id',this.request_id);                                    
                             formdata.append('response_message','Transaction Successful');
+                            format.append('response_advise','Paid');
                             formdata.append('date_paid',this.date_paid);                            
                         }
                         Swal.fire({
