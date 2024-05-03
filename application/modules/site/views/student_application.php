30fe19443ac9c564a80cf0032c6d2263e3ec647f
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-05-03 11:21:00 +0800

    Update student_application.php

--- application/modules/site/views/student_application.php
+++ application/modules/site/views/student_application.php
@@ -569,8 +569,8 @@
                             How did you find out about iACADEMY? <span class="text-red-500">*</span>
                         </label>
                         <select
-                            class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
-                             required v-model="request.source">
+                            class="bg-gray-200 border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+                            required name="source" v-model="request.source">
                              <option value="cos">COS (Career Orientation Seminar)</option>
                              <option value="facebook">Facebook</option>
                              <option value="instagram">Instagram</option>
