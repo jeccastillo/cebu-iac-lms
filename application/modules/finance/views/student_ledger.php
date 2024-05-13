df6a544f167deda6cdad5d18d0f9cde5ac3c2def
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-05-13 08:35:50 +0800

    Update student_ledger.php

--- application/modules/finance/views/student_ledger.php
+++ application/modules/finance/views/student_ledger.php
@@ -27,7 +27,7 @@
                     <h3 class="widget-user-username" style="text-transform:capitalize;margin-left:0;font-size:1.3em;">{{ student.strLastname }}, {{ student.strFirstname }} {{ student.strMiddlename }}</h3>                    
                     <h4 class="widget-user-desc" style="margin-left:0;">{{ student.strStudentNumber }}</h4> 
                     <h4 class="widget-user-desc" style="margin-left:0;">{{ student.strProgramDescription }}</h4> 
-                    <h4 class="widget-user-desc" style="margin-left:0;">{{ student_type }}</h4> 
+                    <h4 class="widget-user-desc" style="margin-left:0;">{{ student_type.toUpperCase() }}</h4> 
                     
                 </div>                
             </div>                            
