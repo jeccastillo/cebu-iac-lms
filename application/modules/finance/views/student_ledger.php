0b15b59f3e737e1fc44075edb5456e5f8c75dc23
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-05-16 09:23:47 +0800

    Update student_ledger.php

--- application/modules/finance/views/student_ledger.php
+++ application/modules/finance/views/student_ledger.php
@@ -796,7 +796,7 @@ new Vue({
             this.balance_change = parseFloat(this.apply_term_balance) + parseFloat(event.target.value);
             if(this.balance_change > 0)
                 this.balance_change = 0;
-            this.apply_term_amount = this.apply_term_balance;
+            this.apply_term_amount = parseFloat(this.apply_term_balance);
         },
         applyToTerm: function(){            
             let url = this.base_url + 'finance/apply_to_term';                        
