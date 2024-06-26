ead038f6fb9b569de17227c2661fd0665d3b9726
ronngb <ronny.gabi@gmail.com>
2024-06-26 13:25:43 +0800

    update in particualars

--- application/modules/finance/views/view_particulars.php
+++ application/modules/finance/views/view_particulars.php
@@ -1,17 +1,18 @@
-<aside class="right-side" id="registration-container">    
+<aside class="right-side"
+    id="registration-container">
     <section class="content-header">
         <h1>
-            <?php echo strtoupper($type); ?>            
-        </h1>     
+            <?php echo strtoupper($type); ?>
+        </h1>
     </section>
-        <hr />
-    <div class="content">  
+    <hr />
+    <div class="content">
         <div class="box box-primary">
             <div class="box-header">
-                <h4><?php echo strtoupper($type); ?></h4>                
+                <h4><?php echo strtoupper($type); ?></h4>
             </div>
             <div class="box-body">
-                                                      
+
                 <table class="table table-bordered table-striped">
                     <thead>
                         <tr>
@@ -24,71 +25,117 @@
                         <tr v-if="items.length == 0">
                             <td colspan='3'>No Items Found</td>
                         </tr>
-                        <tr v-else v-for="(item,index) in items">
+                        <tr v-else
+                            v-for="(item,index) in items">
                             <td>{{ index+1 }}</td>
                             <td>{{ item.name }}</a></td>
-                            <td><button class="btn btn-primary">Delete Item</button></td>                          
+                            <td><button class="btn btn-primary"
+                                    @click="removeItem(index)">Delete Item</button></td>
                         </tr>
                         <tr>
                             <td colspan='3'>
-                                <button class="btn btn-primary">Add Item</button>
+                                <button class="btn btn-primary"
+                                    @click="openModal()">Add Item</button>
                             </td>
                         </tr>
                     </tbody>
-                </table>                              
-            </div>        
+                </table>
+            </div>
         </div>
-        
+        <dialog>
+            <div>
+                <form v-on:submit.prevent="submit()">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Name</label>
+                        <input type="text"
+                            class="form-control"
+                            v-model="nameObj.name"
+                            placeholder="Name">
+                    </div>
+                    <button type="submit"
+                        class="btn btn-default">Submit</button>
+                </form>
+            </div>
+        </dialog>
     </div>
-  
+
 </aside>
 
 <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/themes/default/js/script.js"></script>
+<script type="text/javascript"
+    src="<?php echo base_url(); ?>assets/themes/default/js/script.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/vue@2.6.12"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js"
     integrity="sha512-WFN04846sdKMIP5LKNphMaWzU7YpMyCU245etK3g/2ARYbPK9Ub18eG+ljU96qKRCWh+quCY7yefSmlkQw1ANQ=="
-    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    crossorigin="anonymous"
+    referrerpolicy="no-referrer"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.min.js"></script>
 
 <script>
 new Vue({
     el: '#registration-container',
-    data: {                    
+    data: {
         base_url: '<?php echo base_url(); ?>',
-        sem: '<?php echo $type; ?>',        
-        active_sem: undefined,      
-        items:[],              
-        
-        
-      
-        
+        type: '<?php echo $type; ?>',
+        active_sem: undefined,
+        items: [],
+        dialogEl: '',
+        nameObj: {
+            name: ''
+        }
+
+
     },
 
     mounted() {
+        this.dialogEl = document.querySelector("dialog");
 
-        let url_string = window.location.href;        
-        if(this.id != 0){            
+        let url_string = window.location.href;
+        if (this.id != 0) {
             //this.loader_spinner = true;
-            axios.get(this.base_url + 'finance/view_particulars_data/'+this.type)
-                .then((data) => {                                      
-                    this.terms = data.data.sy;                                        
-                    this.sem = data.data.active_sem.intID;                     
+            axios.get(this.base_url + 'finance/view_particulars_data/' + this.type)
+                .then((data) => {
+                    this.terms = data.data.sy;
+                    this.sem = data.data.active_sem.intID;
                     this.active_sem = data.data.active_sem;
-                    this.students = data.data.students;                    
+                    this.students = data.data.students;
                 })
-            .catch((error) => {
-                console.log(error);
-                
-            });
+                .catch((error) => {
+                    console.log(error);
+
+                });
         }
 
     },
 
-    methods: {      
-                                          
+    methods: {
+        openModal() {
+            this.dialogEl.show()
+        },
+        submit() {
+            console.log('adsd');
+            axios.post(this.base_url + 'finance/add_particular/' + this.type, {
+                    name: 'dasd'
+                })
+                .then((data) => {
+                    console.log(data);
+                })
+                .catch((error) => {
+                    console.log(error);
+
+                });
+        },
+        removeItem(index) {
+            axios.post(this.base_url + `finance/delete_particular/${index}`)
+                .then((data) => {
+                    console.log(data);
+                })
+                .catch((error) => {
+                    console.log(error);
+
+                });
+        }
     }
 
 })
 </script>
-
