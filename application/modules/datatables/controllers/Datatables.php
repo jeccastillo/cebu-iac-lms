12092d33337c88d61d7d55a3cf3d762f1ec1503c
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-07-09 12:57:13 +0800

    Update Datatables.php

--- application/modules/datatables/controllers/Datatables.php
+++ application/modules/datatables/controllers/Datatables.php
@@ -2077,8 +2077,10 @@ class Datatables extends CI_Controller {
                     $ct = 0;
                     $str = str_split($_GET['sSearch_'.$i]);
                     foreach($str as $letter){                                                                        
-                        if(count($str) >= 5 && $str[4] == "S" && ($ct == 7 || $ct == 9))
-                            $st .= "-";
+                        if(count($str) >= 5 && $str[4] == "S"){
+                            if($ct == 7 || $ct == 9)
+                                $st .= "-";
+                        }
                         elseif($str[0] != "T" && ($ct == 4 || $ct == 6))
                             $st .= "-";
                         elseif(($str[0] == "T" || $str[0] == "C") && ($ct == 5 || $ct == 7))
