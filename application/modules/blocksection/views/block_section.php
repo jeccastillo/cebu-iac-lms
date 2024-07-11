96bd6993ddae85cfe8a744ddeaa6cbfac6082487
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-07-11 08:28:44 +0800

    Update block_section.php

--- application/modules/blocksection/views/block_section.php
+++ application/modules/blocksection/views/block_section.php
@@ -27,12 +27,12 @@
                                         <option v-for="program in programs" :value="program.intProgramID">{{ program.strProgramCode }}</option>                                        
                                     </select>
                                 </div>
-                                <div class="form-group col-xs-6">
+                                <!-- <div class="form-group col-xs-6">
                                     <label for="intSYID">Program</label>
                                     <select class="form-control" name="intSYID" v-model="request.intSYID">                                        
                                         <option v-for="s in sy" :value="s.intID">{{ s.term_student_type + ' ' + s.enumSem + ' ' + s.term_label + ' ' s.strYearStart + ' - ' + s.strYearEnd }}</option>                                        
                                     </select>
-                                </div>                                
+                                </div>                                 -->
                                 <div class="form-group col-xs-6">
                                     <label for="intYearLevel">Year</label>
                                     <select class="form-control" name="year" v-model="request.year">                                        
