c0b36a552ce0149f95ef304e9674995238da6cac
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-07-16 10:02:18 +0800

    Update Data_fetcher.php

--- application/models/Data_fetcher.php
+++ application/models/Data_fetcher.php
@@ -3894,7 +3894,7 @@ class Data_fetcher extends CI_Model {
     }
 
     function getBlockSectionsPerProgram($program, $sem, $enhanced = 0){
-        return $this->db->get_where('tb_mas_block_sections',array('intProgramID'=> $program, 'intSYID' => $sem, 'enhanced' => $enhanced))
+        return $this->db->get_where('tb_mas_block_sections',array('intProgramID'=> $program, 'intSYID' => $sem, 'enhanced' => $enhanced, 'year' => 1))
                         ->result_array();
     }
 
