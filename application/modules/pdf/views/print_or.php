63c28ebb9be76c50936edf4638703f5366fb989a
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-07-23 16:53:52 +0800

    Update print_or.php

--- application/modules/pdf/views/print_or.php
+++ application/modules/pdf/views/print_or.php
@@ -229,7 +229,7 @@
                 </tr>
             </table>
             <table >
-                <tr style="line-height:5px;">
+                <tr style="line-height:7px;">
                     <td style=""></td>
                 </tr>
             </table>
