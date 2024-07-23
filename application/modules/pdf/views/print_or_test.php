df096de4dc57f0d9be60c8731e08fd3023d57001
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-07-23 13:30:24 +0800

    Update print_or_test.php

---
+++ application/modules/pdf/views/print_or_test.php
@@ -0,0 +1,2 @@
+<div style="margin-top:65px"></div>
+<div style="width:30%"><?php echo $description; ?> <?php echo $description == "Reservation Payment" ? "<br />NON REFUNDABLE AND NON <br />TRANSFERABLE":""; ?></div>
