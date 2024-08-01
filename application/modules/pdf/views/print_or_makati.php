fc006793864a9ee789e7036d3a5aa61a859c5718
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-08-01 16:55:38 +0800

    Update print_or_makati.php

--- application/modules/pdf/views/print_or_makati.php
+++ application/modules/pdf/views/print_or_makati.php
@@ -39,21 +39,27 @@
             </table>
             <table>
                 <tr style="line-height:12px;font-size:10px;text-align:left;">
-                    <td style="width:30%;font-size:8px;"><?php $description == "Reservation Payment" ? 'Name:' : ''; ?></td>
-                    <td style="width:70%;<?php $description == "border-bottom:1px solid #000;" ? 'Name:' : ''; ?>"></td>                  
+                    <td style="width:30%;font-size:9px;"><?php echo $description == "Reservation Payment" ? 'Name:' : ''; ?></td>
+                    <td style="width:70%;<?php echo $description == "Reservation Payment" ? 'border-bottom:1px solid #000;' : ''; ?>"></td>                  
+                </tr>
+            </table>
+            <table>
+                <tr style="font-size:10px;line-height:2px;text-align:left;">
+                    <td style="width:50%;color:#fff;">SPACE</td>                    
                 </tr>
             </table>
             <table>
                 <tr style="line-height:12px;font-size:10px;text-align:left;">
-                    <td style="width:30%;font-size:8px;"><?php $description == "Reservation Payment" ? 'Signature:' : ''; ?></td>
-                    <td style="width:70%;<?php $description == "border-bottom:1px solid #000;" ? 'Name:' : ''; ?>"></td>                  
+                    <td style="width:30%;font-size:9px;"><?php echo $description == "Reservation Payment" ? 'Signature:' : ''; ?></td>
+                    <td style="width:70%;<?php echo $description == "Reservation Payment" ? 'border-bottom:1px solid #000;' : ''; ?>"></td>                  
                 </tr>
             </table>                  
             <table>
                 <tr style="font-size:10px;text-align:left;">
-                    <td style="width:50%;height:30px;color:#fff;">SPACE</td>                    
+                    <td style="width:50%;height:28px;color:#fff;">SPACE</td>                    
                 </tr>
             </table>
+            <br />            
             <table>
                 <tr style="line-height:12px;font-size:10px;text-align:left;">
                     <td style="width:50%"></td>
@@ -94,7 +100,7 @@
                 </tr>
             </table>
             <table>
-                <tr style="line-height:20px;">
+                <tr style="line-height:5px;">
                     <td style=""></td>
                 </tr>
             </table>
@@ -104,8 +110,10 @@
                     <td style="width:50%"><?php 
                         if($remarks == "Paynamics" || $remarks == "BDO Pay")
                             echo $remarks;                                                
-                        else                        
-                            echo !$is_cash?$check_number:"";                         
+                        else
+                            echo !$is_cash?$check_number:""; 
+                        
+                    
                     ?></td>
                 </tr>
             </table>   
@@ -122,16 +130,13 @@
             </table>
         </td>
         <td style="width:70%">
+            <br />
+            <br />
             <table>
-                <tr style="line-height:20px;">
+                <tr style="line-height:17px;">
                     <td width="75%" style="text-align:right;font-weight:bold;font-size:9px;">OR No:<?php echo $or_number; ?></td>
                 </tr>
-            </table>
-            <table>
-                <tr style="line-height:5px;">
-                    <td></td>
-                </tr>
-            </table>
+            </table>           
             <table >
                 <tr style="line-height:12px;">
                     <td style="font-size:10px;text-align:right;padding-right:15px;"><?php echo "  ".date("m/d/y",strtotime($transaction_date)); ?></td>
@@ -192,20 +197,18 @@
                 </tr>
             </table> -->
             <table >
-                <tr style="line-height:5px;">                    
+                <tr style="line-height:3px;">                    
                     <td style="font-size:10px;text-align:left;"></td>
                 </tr>
             </table>
             <table >
-                <tr style="line-height:10px;">
+                <tr style="line-height:15px;">
                     <td style=""></td>
                 </tr>
             </table>
             <table >
                 <tr style="line-height:15px;">                    
                     <td style="font-size:10px;text-align:left;">
-                        <br />
-                        <br />
                         <span style="color:#fff;">the amount of pesos &nbsp;</span>
                         <?php echo convert_number($total_amount_due); ?> <?php echo $decimal?'and '.convert_number($decimal).' cents':'only'; ?>
                     </td>
@@ -225,7 +228,7 @@
                 </tr>
             </table>
             <table >
-                <tr style="line-height:5px;">
+                <tr style="line-height:9px;">
                     <td style=""></td>
                 </tr>
             </table>
@@ -234,14 +237,11 @@
                     <td style="font-size:10px;text-align:left;">
                         <span style="color:#fff;">as full/partial payment of &nbsp;</span>
                         <?php echo $description; ?>
-                        <?php if(trim($description) == "Tuition Fee") 
-                                    echo " ".$remarks;
-                        ?>
                     </td>
                 </tr>
             </table>
             <table >
-                <tr style="line-height:14px;">
+                <tr style="line-height:17px;">
                     <td style="width:60%"></td>
                     <td style="width:40%;font-size:8px;text-align:center;">                        
                     </td>
@@ -261,7 +261,7 @@
                 </tr>
             </table>
             <table >
-                <tr style="line-height:5px;">
+                <tr style="line-height:10px;">
                     <td style=""></td>
                 </tr>
             </table>
@@ -326,7 +326,7 @@
                 </tr>
             </table>
             <table>
-                <tr style="line-height:30px;">
+                <tr style="line-height:40px;">
                     <td style=""></td>
                 </tr>
             </table>
