9550a714e339f6d91099855a0bfd7956c0354b9f
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-08-02 14:47:17 +0800

    Update Pdf.php

--- application/modules/pdf/controllers/Pdf.php
+++ application/modules/pdf/controllers/Pdf.php
@@ -2222,7 +2222,7 @@ class Pdf extends CI_Controller {
     
     
         // set margins
-        $pdf->SetMargins(8, 10, 25);
+        $pdf->SetMargins(8, 10, 15);
         $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
         $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
         //$pdf->SetAutoPageBreak(TRUE, 6);
