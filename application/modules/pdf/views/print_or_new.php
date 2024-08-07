063b9cbf9e50e49fd22b208948d0957294d2ca75
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2024-08-08 05:08:34 +0800

    print_or_fof staging

---
+++ application/modules/pdf/views/print_or_new.php
@@ -0,0 +1,52 @@
+<style>
+   body { margin: 0 }
+    .sheet-outer {
+        margin: 0;
+    }
+    .sheet {
+        margin: 0;
+        overflow: hidden;
+        position: relative;
+        box-sizing: border-box;
+        page-break-after: always;
+    }
+
+    @media screen {
+        body { 
+            background: #e0e0e0 
+        }
+    
+        .sheet {
+            background: white;
+            box-shadow: 0 .5mm 2mm rgba(0,0,0,.3); 
+            margin: 5mm auto;
+        }
+    }
+
+    .sheet-outer.A4 .sheet { 
+        width: 210mm; 
+        height: 296mm 
+    }
+    .sheet.padding-5mm { padding: 5mm }
+
+    @page {
+        size: A4;
+        margin: 0
+    }
+    @media print {
+        .sheet-outer.A4, .sheet-outer.A5.landscape { 
+            width: 210mm 
+        }
+    }
+
+</style>
+<body>
+    <div class="sheet-outer A4">
+        <section class="sheet padding-5mm">
+            <article>This is an A4 document.</article>
+        </section>
+        <section class="sheet padding-5mm">
+            <article>This is an A4 document.</article> 
+        </section>
+    </div>
+</body>
