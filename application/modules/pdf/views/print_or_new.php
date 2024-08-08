040b32b616e97f7641eacf74d3fcbb312cae2565
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2024-08-08 08:57:54 +0800

    Update print_or_new.php

--- application/modules/pdf/views/print_or_new.php
+++ application/modules/pdf/views/print_or_new.php
@@ -43,7 +43,7 @@
 <body>
     <div class="sheet-outer A4">
         <section class="sheet padding-5mm">            
-            <table style="border:none;width:100%">
+            <table style="border:none;width:100%;margin-top:20mm;">
                 <tr>
                     <td style="width:30%">
                         <table>
