1f6723e19ca172b4f66f0acddf819812f8a9fa95
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2024-08-08 09:16:25 +0800

    Update print_or_new.php

--- application/modules/pdf/views/print_or_new.php
+++ application/modules/pdf/views/print_or_new.php
@@ -55,7 +55,7 @@
                                 <td colspan="2"><?php echo $description == "Reservation Payment" ? "NON REFUNDABLE AND NON TRANSFERABLE":""; ?></td>                                
                             </tr>
                         </table>
-                        <table style="min-height:98px;">
+                        <table style="height:198px;">
                             <tr style="font-size:12px;text-align:left;vertical-align:top;">
                                 <td style="vertical-align:top;">
                                     <?php echo "SY ".$term['strYearStart']."-".$term['strYearEnd']." ".$term['enumSem']." ".$term['term_label']." ".$type; ?><br />
