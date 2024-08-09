621fc030c1679fd43d116fcff491921c86681e91
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2024-08-09 11:31:35 +0800

    Update registration_viewer.php

--- application/modules/unity/views/admin/registration_viewer.php
+++ application/modules/unity/views/admin/registration_viewer.php
@@ -463,7 +463,7 @@
             </div>
         </div>
     </div>
-    <form ref="print_or" method="post" :action="base_url + 'pdf/print_or_new'" target="_blank">
+    <form ref="print_or" method="post" :action="base_url + 'pdf/print_or'" target="_blank">
         <input type="hidden" name="student_name" v-model="or_print.student_name">
         <input type="hidden" name="campus" :value="request.student_campus">
         <input type="hidden" name="cashier_id" v-model="or_print.cashier_id">
