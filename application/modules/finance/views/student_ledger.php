27ae2f0fcc0006eb260ef8293f511715189806bd
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2024-08-09 09:40:08 +0800

    Update student_ledger.php

--- application/modules/finance/views/student_ledger.php
+++ application/modules/finance/views/student_ledger.php
@@ -442,14 +442,13 @@ new Vue({
             this.term_balance_other = 0;
             this.ledger_term = [];
             this.other_term = [];
-            
             if(tuition.term.paymentType == 'partial')
                 amount = tuition.ti_before_deductions;
             else
                 amount = tuition.total_before_deductions;
 
-            
-            if(tuition)
+            this.term_balance += amount;
+
             this.ledger_term.push({                
                 'strYearStart':tuition.term.strYearStart,
                 'strYearEnd':tuition.term.strYearEnd,
