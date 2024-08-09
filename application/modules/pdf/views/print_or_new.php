a7b8ecc47d97888cd5b637a2d48529d8d124ef84
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2024-08-09 10:47:30 +0800

    Update print_or_new.php

--- application/modules/pdf/views/print_or_new.php
+++ application/modules/pdf/views/print_or_new.php
@@ -99,7 +99,7 @@
                         <table style="">
                             <tr style="font-size:12px;text-align:left;">
                                 <td style="width:50%">&nbsp;</td>
-                                <td style="width:50%;padding-top:10mm;">P<?php echo number_format($total_amount_due,2,'.',','); ?></td>
+                                <td style="width:50%;padding-top:13mm;">P<?php echo number_format($total_amount_due,2,'.',','); ?></td>
                             </tr>
                         </table>
                     </td>
