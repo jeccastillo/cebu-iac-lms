c1353cccc48dbf7f20f24092e375acf7b6e04045
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2024-08-09 09:51:40 +0800

    Update print_or_new.php

--- application/modules/pdf/views/print_or_new.php
+++ application/modules/pdf/views/print_or_new.php
@@ -136,13 +136,15 @@
                             </tr>
                         </table>           
                         <table>
-                            <tr>
-                                <td style="width:50%;padding-left:26mm;font-size:12px;text-align:left;padding-top:8mm;">
-                                    <?php if($student_id != 'undefined' && $student_id != ''): ?>
-                                        <?php echo preg_replace("/[^a-zA-Z0-9]+/", "", $student_id); ?>
-                                    <?php endif; ?>
+                            <tr style="padding-top:6mm;">
+                                <td style="width:50%;padding-left:26mm;font-size:12px;text-align:left;">
+                                    <div style="padding-top:1mm;">
+                                        <?php if($student_id != 'undefined' && $student_id != ''): ?>
+                                            <?php echo preg_replace("/[^a-zA-Z0-9]+/", "", $student_id); ?>
+                                        <?php endif; ?>
+                                    </div>
                                 </td>
-                                <td style="width:50%;font-size:12px;text-align:right;padding-right:10mm;padding-top:6mm;"><?php echo "  ".date("M j, Y",strtotime($transaction_date)); ?></td>
+                                <td style="width:50%;font-size:12px;text-align:right;padding-right:10mm;"><?php echo "  ".date("M j, Y",strtotime($transaction_date)); ?></td>
                             </tr>
                         </table>                       
                         <table >
