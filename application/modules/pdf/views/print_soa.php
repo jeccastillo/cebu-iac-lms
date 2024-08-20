d2da692d7048df9a29186840db8c8a4f16e4eb47
Jp <jpestorque0@gmail.com>
2024-08-20 09:56:35 +0800

    Update print_soa.php

--- application/modules/pdf/views/print_soa.php
+++ application/modules/pdf/views/print_soa.php
@@ -53,35 +53,42 @@
 <table>
 <?php 
     if($reg['paymentType'] == 'partial') :
+        if($request['is_paid_dp'] == '0') :
 ?>
     <tr>
+        <td style="border:1px solid #333;" width="50%"> Down Payment</td>
+        <td style="border:1px solid #333;text-align:right;padding-right:5px" width="20%"><?php echo $request['down_payment'] ?> </td>
+    </tr>
+    <?php endif; ?>
+    
+    <tr>
         <td style="border:1px solid #333;" width="50%"> 1st installment balance due <?php echo date("F d, Y", strtotime($sy->installment1)) ?></td>
-        <td style="border:1px solid #333;text-align:right;padding-right:5px" width="20%"><?php echo number_format($installment[0], 2, '.', ',') ?> </td>
+        <td style="border:1px solid #333;text-align:right;padding-right:5px" width="20%"><?php echo $installments[0] ?> </td>
     </tr>
     <tr>
         <td style="border:1px solid #333;" width="50%"> 2nd installment balance due <?php echo date("F d, Y", strtotime($sy->installment2)) ?></td>
-        <td style="border:1px solid #333;text-align:right;padding-right:5px" width="20%"><?php echo number_format($installment[1], 2, '.', ',') ?> </td>
+        <td style="border:1px solid #333;text-align:right;padding-right:5px" width="20%"><?php echo $installments[1] ?> </td>
     </tr>
     <tr>
         <td style="border:1px solid #333;" width="50%"> 3rd installment balance due <?php echo date("F d, Y", strtotime($sy->installment3)) ?></td>
-        <td style="border:1px solid #333;text-align:right;padding-right:5px" width="20%"><?php echo number_format($installment[2], 2, '.', ',') ?> </td>
+        <td style="border:1px solid #333;text-align:right;padding-right:5px" width="20%"><?php echo $installments[2] ?> </td>
     </tr>
     <tr>
         <td style="border:1px solid #333;" width="50%"> 4th installment balance due <?php echo date("F d, Y", strtotime($sy->installment4)) ?></td>
-        <td style="border:1px solid #333;text-align:right;padding-right:5px" width="20%"><?php echo number_format($installment[3], 2, '.', ',') ?> </td>
+        <td style="border:1px solid #333;text-align:right;padding-right:5px" width="20%"><?php echo $installments[3] ?> </td>
     </tr>
     <tr>
         <td style="border:1px solid #333;" width="50%"> 5th installment balance due <?php echo date("F d, Y", strtotime($sy->installment5)) ?></td>
-        <td style="border:1px solid #333;text-align:right;padding-right:5px" width="20%"><?php echo number_format($installment[4], 2, '.', ',') ?> </td>
+        <td style="border:1px solid #333;text-align:right;padding-right:5px" width="20%"><?php echo $installments[4] ?> </td>
     </tr>
     <tr>
         <td style="border:1px solid #333;font-weight:bold" width="50%">TOTAL BALANCE</td>
-        <td style="border:1px solid #333;font-weight:bold;text-align:right;padding-right:5px" width="20%"><?php echo number_format($total_installment, 2, '.', ',') ?> </td>
+        <td style="border:1px solid #333;font-weight:bold;text-align:right;padding-right:5px" width="20%"><?php echo $request['total'] ?> </td>
     </tr>
 <?php elseif($reg['paymentType'] == 'full') :?>
     <tr>
         <td style="border:1px solid #333;font-weight:bold" width="50%">TOTAL BALANCE</td>
-        <td style="border:1px solid #333;font-weight:bold;text-align:right;padding-right:5px" width="20%"><?php echo number_format($balance, 2, '.', ',') ?> </td>
+        <td style="border:1px solid #333;font-weight:bold;text-align:right;padding-right:5px" width="20%"><?php echo $request['total'] ?> </td>
     </tr>
 <?php endif; ?>
 </table>
