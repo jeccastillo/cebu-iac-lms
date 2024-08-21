551c1e9b6aaa385e20afa40b0c073afe49a9b656
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2024-08-21 10:19:26 +0800

    Update header.php

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -259,6 +259,7 @@
             <li class="<?php echo (isset($page) && $page=="view_leads")?'active':''; ?>"><a
                 href="<?php echo base_url(); ?>admissionsV1/view_all_leads"
                 style="margin-left: 10px;"><i class="fa fa-book"> </i> View Applicants</a></li>
+            <?php if(in_array($user['intUserLevel'],array(2,5,3,6)) ): ?>    
             <li class="<?php echo (isset($page) && $page=="classlist_archive")?'active':''; ?>">
               <a href="<?php echo base_url()."admissionsV1/view_classlist_archive_admin" ?>" 
               style="margin-left: 10px;"><i class="fa fa-user"></i>Slot Monitoring</a>
@@ -275,7 +276,7 @@
             <li class="<?php echo (isset($page) && $page=="view_paid")?'active':''; ?>"><a
                 href="<?php echo base_url(); ?>admissionsV1/paid_applicants"
                 style="margin-left: 10px;"><i class="fa fa-book"> </i> View Paid Applicants</a></li>
-
+            <?php endif; ?>
           </ul>
         </li>
         <?php endif; ?>
