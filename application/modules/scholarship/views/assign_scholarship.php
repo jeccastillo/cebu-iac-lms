8a5ff9a307873c89f73209d61e562c64a17ddfa4
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2024-08-21 10:07:53 +0800

    Update assign_scholarship.php

--- application/modules/scholarship/views/assign_scholarship.php
+++ application/modules/scholarship/views/assign_scholarship.php
@@ -23,9 +23,10 @@
             <div class="box-body"> 
                 <div class="row">
                     <div v-if="student" class="col-md-6">
-                        Name: {{ student.strLastname }}, {{ student.strFirstname }} {{ student.strMiddlename }}
-
-                        <a target="_blank" class="btn btn-primary text-right" :href="base_url + 'scholarship/student_ledger/'+ student.intID">View Ledger</a>
+                        Name: {{ student.strLastname }}, {{ student.strFirstname }} {{ student.strMiddlename }}                        
+                    </div>
+                    <div class="col-md-6 text-right">
+                        <a target="_blank" class="btn btn-primary" :href="base_url + 'scholarship/student_ledger/'+ student.intID">View Ledger</a>
                     </div>
                     
                 </div>
