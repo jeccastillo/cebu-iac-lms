22c8b9cf5d104c82a72ef97b632e8214d4743f26
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2024-08-21 10:17:51 +0800

    Update AdmissionsV1.php

--- application/modules/admissionsV1/controllers/AdmissionsV1.php
+++ application/modules/admissionsV1/controllers/AdmissionsV1.php
@@ -311,7 +311,7 @@ class AdmissionsV1 extends CI_Controller {
     }
 
     public function view_lead($id) {
-        if(in_array($this->session->userdata('intUserLevel'),array(2,3,5,6)))
+        if(in_array($this->session->userdata('intUserLevel'),array(2,3,5,6,7)))
         {
             $this->data['exam_type']= $this->data_fetcher->fetch_table('tb_mas_exam');
             $this->data['userlevel'] = $this->session->userdata('intUserLevel');
