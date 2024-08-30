919b617f149e27605a17282db1526f66b443a6b8
Jp <jpestorque0@gmail.com>
2024-08-30 12:56:18 +0800

    Update Excel.php

--- application/modules/excel/controllers/Excel.php
+++ application/modules/excel/controllers/Excel.php
@@ -4632,9 +4632,7 @@ class Excel extends CI_Controller {
                 array_push($notEnrolledSlugs, $studentInformation->slug);
             // array_push($enrolledSlugs, $studentInformation->slug);
         }
-        print_r($ar_students);
-        die();
-
+        
         $users = $this->db->select('tb_mas_users.*')
                     ->from('tb_mas_users')
                     ->where_not_in('slug', $notEnrolledSlugs)
