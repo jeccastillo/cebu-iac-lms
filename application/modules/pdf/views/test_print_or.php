61ed2bb146523b41cee6462953343c0744ba1adf
ronngb <ronny.gabi@gmail.com>
2024-09-12 11:31:48 +0800

    test print OR

--- application/modules/pdf/views/test_print_or.php
+++ application/modules/pdf/views/test_print_or.php
@@ -1,53 +1,73 @@
 <style>
-   body { margin: 0; font-family: Arial, Sans-Serif; }
-    .sheet-outer {
-        margin: 0;
+body {
+    margin: 0;
+    font-family: Arial, Sans-Serif;
+}
+
+.sheet-outer {
+    margin: 0;
+}
+
+.sheet {
+    margin: 0;
+    overflow: hidden;
+    position: relative;
+    box-sizing: border-box;
+    page-break-after: always;
+}
+
+section {
+    font-size: 12px
+}
+
+table {
+    width: 100%;
+}
+
+table tr td {
+    vertical-align: top;
+}
+
+@media screen {
+    body {
+        background: #e0e0e0
     }
+
     .sheet {
-        margin: 0;
-        overflow: hidden;
-        position: relative;
-        box-sizing: border-box;
-        page-break-after: always;
+        background: white;
+        box-shadow: 0 .5mm 2mm rgba(0, 0, 0, .3);
+        margin: 5mm auto;
     }
-    table{
-        width:100%;
-    }
-    table tr td{
-        vertical-align:top;
-    }
-    @media screen {
-        body { 
-            background: #e0e0e0 
-        }
-    
-        .sheet {
-            background: white;
-            box-shadow: 0 .5mm 2mm rgba(0,0,0,.3); 
-            margin: 5mm auto;
-        }
-    }
+}
 
-    .sheet-outer.A4 .sheet { 
-        width: 210mm; 
-        height: 296mm 
-    }
-    .sheet.padding-5mm { padding-top: 10mm; padding-left: 8mm; padding-right: 10mm; }
+.sheet-outer.A4 .sheet {
+    width: 210mm;
+    height: 296mm
+}
 
-    @page {
-        size: A4;
-        margin: 0
-    }
-    @media print {
-        .sheet-outer.A4, .sheet-outer.A5.landscape { 
-            width: 210mm 
-        }
-    }
+.sheet.padding-5mm {
+    padding-top: 10mm;
+    padding-left: 8mm;
+    padding-right: 10mm;
+}
 
+@page {
+    size: A4;
+    margin: 0
+}
+
+@media print {
+
+    .sheet-outer.A4,
+    .sheet-outer.A5.landscape {
+        width: 210mm
+    }
+}
 </style>
+
 <body>
     <div class="sheet-outer A4">
-        <section class="sheet padding-5mm">     
+        <section class="sheet padding-5mm">
             <div style="position:absolute; top: 160px; left: 100px; width: 100px; height: 20px;">
                 1234A
             </div>
@@ -57,6 +77,34 @@
             <div style="position:absolute; top: 200px; left: 100px; width: 200px; height: 20px;">
                 Description Box
             </div>
+            <div style="position:absolute; top: 270px; left: 200px; width: 200px; height: 20px;">
+                12,976.16
+            </div>
+            <div style="position:absolute; top: 370px; left: 100px; width: 200px; height: 20px;">
+                Remarks
+            </div>
+            <div style="position:absolute; top: 390px; left: 200px; width: 200px; height: 20px;">
+                12,976.16
+            </div>
+            <div style="position:absolute; top: 190px; right: 0; width: 200px; height: 20px;">
+                Sept 12, 2024
+            </div>
+            <div style="position:absolute; top: 222px; left: 336px; width: 200px; height: 20px;">
+                Payee No and Payee Name
+            </div>
+            <div style="position:absolute; top: 250px; left: 336px; width: 500px; height: 20px;">
+                1773 Dian St. Palanan Makati City
+            </div>
+            <div style="position:absolute; top: 275px; left: 336px; width: 500px; height: 20px;">
+                As Applicable
+            </div>
+            <div style="position:absolute; top: 295px; left: 350px; width: 500px; height: 20px;">
+                Twelve thousand, nine hundred seventy-six and sixteen hundredths.
+            </div>
+            <div style="position:absolute; top: 320px; right: -20; width: 200px; height: 20px;">
+                12,976.16
+            </div>
+            
         </section>
     </div>
 </body>
