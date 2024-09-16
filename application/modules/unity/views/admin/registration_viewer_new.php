f0820655d91fabb97935aad899a36489a58faa35
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-09-16 11:21:20 +0800

    Update registration_viewer_new.php

--- application/modules/unity/views/admin/registration_viewer_new.php
+++ application/modules/unity/views/admin/registration_viewer_new.php
@@ -1172,11 +1172,11 @@
                     <button type="button"
                         class="close"
                         data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title">Update OR Details</h4>
+                    <h4 class="modal-title">Update Details</h4>
                 </div>
                 <div class="modal-body">
                     <div class="form-group">
-                        <label>OR Date <span class="text-danger">*</span> </label>
+                        <label>Issued Date <span class="text-danger">*</span> </label>
                         <input type="date"
                             class="form-control"
                             v-model="or_details.or_date"
