fd54ead2274cea9978fe46f1e9cf1d3a32b872f0
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-09-17 16:07:53 +0800

    Update print_invoice.php

--- application/modules/pdf/views/print_invoice.php
+++ application/modules/pdf/views/print_invoice.php
@@ -68,7 +68,7 @@ table tr td {
 <body>
     <div class="sheet-outer A4">
         <section class="sheet padding-5mm">
-            <div style="position:absolute; top: 130px; right: 60px; width: 200px; height: 20px;">
+            <div style="position:absolute; top: 125px; right: 60px; width: 200px; height: 20px;">
                 Invoice No: <?php echo $invoice_number; ?>
             </div>
             <div style="position:absolute; top: 170px; right: -60px; width: 200px; height: 20px;">
@@ -76,55 +76,55 @@ table tr td {
             </div>
             <div style="position:absolute; top: 170px; left: 60px; width: 500px; height: 20px;">
                 <?php if($student_id != 'undefined' && $student_id != ''): ?>
-                    <?php echo preg_replace("/[^a-zA-Z0-9]+/", "", $student_id); ?>
+                <?php echo preg_replace("/[^a-zA-Z0-9]+/", "", $student_id); ?>
                 <?php endif; ?>
                 &nbsp; <?php echo $student_name; ?>
             </div>
             <div style="position:absolute; top: 190px; left: 60px; width: 500px; height: 20px;">
                 <?php echo $student_address; ?>
             </div>
-            <div style="position:absolute; top: 240px; left:0px; width: 500px; height: 20px;">
+            <div style="position:absolute; top: 245px; left:0px; width: 500px; height: 20px;">
                 <?php echo $type." /  ".$term['enumSem']." ".$term['term_label']." ".$term['strYearStart']."-".$term['strYearEnd']; ?>
             </div>
-            <div style="position:absolute; top: 240px; left: 500px; width: 500px; height: 20px;">
+            <div style="position:absolute; top: 245px; left: 500px; width: 500px; height: 20px;">
                 1
             </div>
-            <div style="position:absolute; top: 240px; left: 550px; width: 200px; height: 20px;">
+            <div style="position:absolute; top: 245px; left: 550px; width: 200px; height: 20px;">
                 <?php echo $full_assessment; ?>
             </div>
-            <div style="position:absolute; top:  240px; left: 650px; width: 200px; height: 20px;">
+            <div style="position:absolute; top:  245px; left: 650px; width: 200px; height: 20px;">
                 <?php echo $full_assessment; ?>
             </div>
-            <div style="position:absolute; top: 255px; left:0px; width: 500px; height: 20px;">
+            <div style="position:absolute; top: 270px; left:0px; width: 500px; height: 20px;">
                 <?php echo $reservation_description; ?>
             </div>
-            <div style="position:absolute; top: 255px; left: 500px; width: 500px; height: 20px;">
-                1
+            <div style="position:absolute; top: 270px; left: 500px; width: 500px; height: 20px;">
+               <?php echo $reservation_amount != 0 ? 1 : ""; ?>
             </div>
-            <div style="position:absolute; top: 255px; left: 550px; width: 200px; height: 20px;">
-                <?php  echo $reservation_amount; ?>
+            <div style="position:absolute; top: 270px; left: 550px; width: 200px; height: 20px;">
+                <?php echo $reservation_amount != 0 ? $reservation_amount : ""; ?>
             </div>
-            <div style="position:absolute; top:  255px; left: 650px; width: 200px; height: 20px;">
-                <?php  echo $reservation_amount; ?>
+            <div style="position:absolute; top:  270px; left: 650px; width: 200px; height: 20px;">
+                <?php echo $reservation_amount != 0 ? $reservation_amount : ""; ?>
             </div>
             <div style="position:absolute; top:  355px; left: 650px; width: 200px; height: 20px;">
                 <?php  echo $total_assessment; ?>
             </div>
-            <div style="position:absolute; top:  390px; left: 100px; width: 200px; height: 20px;">
+            <div style="position:absolute; top:  380px; left: 100px; width: 200px; height: 20px;">
                 <?php  echo $total_amount_due; ?>
             </div>
             <div style="position:absolute; top:  405px; left: 650px; width: 200px; height: 20px;">
                 <?php  echo $total_assessment; ?>
             </div>
-            <div style="position:absolute; top:  455px; left: 0px; width: 200px; height: 20px;">
+            <div style="position:absolute; top:  460px; left: 0px; width: 200px; height: 20px;">
                 <?php  echo $remarks; ?>
             </div>
             <div style="position:absolute; top:  485px; left: 300px; width: 200px; height: 20px;">
                 <?php  echo $total_assessment; ?>
-            </div>    
+            </div>
             <div style="position:absolute; top:  565px; left: 50px; width: 200px; height: 20px;">
                 <?php  echo $total_amount_due; ?>
-            </div>        
+            </div>
             <div style="position:absolute; top: 550px; right: 20px; width: 200px; height: 20px;font-size:15px">
                 <?php echo $cashier_name; ?>
             </div>
