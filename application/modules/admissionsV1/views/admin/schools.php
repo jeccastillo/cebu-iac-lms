72c7d9572bfde6d01bb59a1366cbe0f4402b63f5
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-09-18 13:26:10 +0800

    Update schools.php

--- application/modules/admissionsV1/views/admin/schools.php
+++ application/modules/admissionsV1/views/admin/schools.php
@@ -62,7 +62,7 @@ new Vue({
             axios.get(api_url + 'admissions/previous-schools')
             .then((data) => {       
                 console.log(data);           
-                this.schools = data.dat.data;                       
+                this.schools = data.data;                       
             })
             .catch((error) => {
                 console.log(error);
