605595a4b4220716ce19443fb3f1d42120db5bc6
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-09-18 13:56:22 +0800

    Update schools.php

--- application/modules/admissionsV1/views/admin/schools.php
+++ application/modules/admissionsV1/views/admin/schools.php
@@ -1,11 +1,15 @@
 <aside class="right-side" id="registration-container">    
     <section class="content-header">
         <h1>
-            Admissions Report - Awareness
+            Schools
             <small>
-                <a class="btn btn-app" href="<?php echo base_url(); ?>admissionsV1/view_all_leads" >
-                    <i class="ion ion-arrow-left-a"></i>
-                    View Schools
+                <a class="btn btn-app" 
+                    href="#"
+                    data-toggle="modal"                
+                    data-target="#addSchool"
+                >
+                    <i class="fa fa-plus"></i>
+                    Add School
                 </a> 
             </small>                       
             
@@ -15,23 +19,100 @@
     <div class="content">         
         <div class="row">
             <div class="col-md-6">
-                <h4>Awareness Stats</h4>
-                <table class="table table-bordered table-striped">
+                <h4>Schools</h4>
+                <table v-if="schools.length > 0" class="table table-bordered table-striped">
                     <thead>
                         <tr>
                             <th>Name</th>
                             <th>City</th>
+                            <th>Province</th>
+                            <th>Country</th>
+                            <th>Actions</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr v-for="item in stats" v-if="item.count > 0">
+                        <tr v-for="item in schools">
                             <td>{{ item.name }}</td>
                             <td>{{ item.city }}</td>
+                            <td>{{ item.province }}</td>
+                            <td>{{ item.country }}</td>                            
+                            <td>
+                                <div class="btn-group"><button type="button" class="btn btn-default">Actions</button>
+                                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown"><span class="caret"></span><span class="sr-only">Toggle Dropdown</span></button>
+                                    <ul class="dropdown-menu" role="menu">
+                                        <li>
+                                            <a href="#">Edit</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </td>
                         </tr>
                     </tbody>
                 </table>
+                <div v-else>
+                    <h3>No Data</h3>
+                </div>
             </div>
         </div>
+    </div>
+    <div class="modal fade"
+        id="addSchool"
+        role="dialog">
+        <form class="modal-dialog modal-lg"
+            @submit.prevent="addNewSchool">
+            <div class="modal-content">
+                <div class="modal-header">
+
+                    <button type="button"
+                        class="close"
+                        data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Add School</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label>School Name</label>
+                        <input type="text"
+                            name="school_name"
+                            class="form-control"
+                            required
+                            required
+                            placeholder="School Name">
+                    </div>
+                    <div class="form-group">
+                        <label>City</label>
+                        <input type="text"
+                            name="school_city"
+                            class="form-control"
+                            required
+                            placeholder="City">
+                    </div>
+                    <div class="form-group">
+                        <label>State/Province</label>
+                        <input type="text"
+                            name="school_province"
+                            class="form-control"
+                            required
+                            placeholder="State/Province">
+                    </div>
+                    <div class="form-group">
+                        <label>Country</label>
+                        <input type="text"
+                            name="school_country"
+                            class="form-control"
+                            required
+                            placeholder="Country">
+                    </div>
+                </div>
+                <div class=" modal-footer">
+                    <button type="submit"
+                        class="btn btn-primary">Submit</button>
+                    <button type="button"
+                        class="btn btn-default"
+                        data-dismiss="modal">Close</button>
+                </div>
+            </div>
+
+        </form>
     </div>
   
 </aside>
@@ -60,9 +141,8 @@ new Vue({
             //this.loader_spinner = true;
             
             axios.get(api_url + 'admissions/previous-schools')
-            .then((data) => {       
-                // console.log(data);           
-                this.schools = data.data;                       
+            .then((data) => {                          
+                this.schools = data.data;                                   
             })
             .catch((error) => {
                 console.log(error);
@@ -73,35 +153,44 @@ new Vue({
     },
 
     methods: {      
-       
+        async addNewSchool(e) {
+            let formData = new FormData(e.target);
+            const payload = Object.assign(Object.fromEntries(formData.entries()))
+
+            const {
+                data
+            } = await axios
+                .post(`${api_url}admissions/student-info/new-school`, payload, {
+                    headers: {
+                        Authorization: `Bearer ${window.token}`
+                    }
+                })
+            if (data.success) {
+                Swal.fire({
+                    showCancelButton: false,
+                    showCloseButton: true,
+                    allowEscapeKey: false,
+                    title: 'Successfully Added New School',
+                    text: 'Field Updated',
+                    icon: 'success',
+                });
+                e.target.reset()
+                this.getAllPrevSchool()
+                $('#addSchool').modal('hide')
+            } else {
+                Swal.fire({
+                    showCancelButton: false,
+                    showCloseButton: true,
+                    allowEscapeKey: false,
+                    title: `${data.message}`,
+                    text: 'Error',
+                    icon: 'error',
+                });
+            }
+        },
                                        
     }
 
 })
-
-$(document).ready(function(){
-    $('#daterange-btn-admissions').daterangepicker(
-    {
-        ranges: {
-            'Today': [moment(), moment()],
-            'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
-            'Last 7 Days': [moment().subtract('days', 6), moment()],
-            'This Month': [moment().startOf('month'), moment().endOf('month')],
-            'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month',1).endOf('month')]
-        },
-        startDate: moment().subtract('days', 29),
-        endDate: moment()
-    },
-    function(start, end) {
-        document.location = base_url + 'admissionsV1/admissions_report/<?php echo $current_sem; ?>/'+start.format('YYYY-MM-DD')+'/'+end.add('days', 1).format('YYYY-MM-DD');
-        
-    }
-    );
-    
-    $("#select-term-leads").on('change', function(e){
-        const term = $(this).val();
-        document.location = "<?php echo base_url()."admissionsV1/awareness_stats/"; ?>"+term;
-    });
-});
 </script>
 
