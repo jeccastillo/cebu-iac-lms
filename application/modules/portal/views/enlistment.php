ec6dee879619d3420b01fceadd8d672e8d2fb12d
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-09-24 09:24:01 +0800

    Update enlistment.php

--- application/modules/portal/views/enlistment.php
+++ application/modules/portal/views/enlistment.php
@@ -28,9 +28,7 @@
                     <hr />
                     <h4>Add Subject for Enlistment</h4>
                     <select v-model="selected_subject" class="form-control">
-                        <option v-for="subject in available_subjects" :value="subject.intID">
-                            {{ subject.strCode + " " + subject.strClassName + subject.year + subject.strSection + subject.sub_section + " " subject.sched_room + " " + subject.sched_day + " " + subject.sched_time }}
-                        </option>
+                        
                     </select>
                 </div>          
             </div>         
