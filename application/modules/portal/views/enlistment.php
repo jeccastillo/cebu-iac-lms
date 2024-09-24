8964af78015f22e165771a954a50bf37dd6bd360
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-09-24 09:34:10 +0800

    Update enlistment.php

--- application/modules/portal/views/enlistment.php
+++ application/modules/portal/views/enlistment.php
@@ -113,7 +113,7 @@ new Vue({
         addSubjectForEnlistment: function(){
             let id = this.selected_subject;
             let i = this.available_subjects.map(item => item.intID).indexOf(id) // find index of your object
-            list.splice(i, 1) // remove it from array
+            this.available_subjects.splice(i, 1) // remove it from array
         }
     }
 
