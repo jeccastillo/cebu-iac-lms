1217e81c167ece5d5b117bb4bbf21c0217c72eef
Jp <jpestorque0@gmail.com>
2024-09-24 15:10:57 +0800

    Update Excel.php

--- application/modules/excel/controllers/Excel.php
+++ application/modules/excel/controllers/Excel.php
@@ -7536,7 +7536,7 @@ class Excel extends CI_Controller {
                         'senior_high_attended' => isset($student['senior_high_attended']) ? $student['senior_high_attended'] : null,
                         'college' => isset($student['college']) ? $student['college'] : null,
                         'college_address' => isset($student['college_address']) ? $student['college_address'] : null,
-                        'college_attended' => isset($student['college_attended']) ? $student['college_attended'] : null,
+                        'college_attended_to' => isset($student['college_attended']) ? $student['college_attended'] : null,
                         
                         'strLRN' => $student['lrn'],
                     );
