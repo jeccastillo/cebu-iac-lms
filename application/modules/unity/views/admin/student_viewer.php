670e3bd77a059db85742bbc10b9a8376d245f10c
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2024-09-25 09:40:20 +0800

    Update student_viewer.php

--- application/modules/unity/views/admin/student_viewer.php
+++ application/modules/unity/views/admin/student_viewer.php
@@ -49,6 +49,11 @@
             :href="base_url + 'department/load_subjects/' + student.intID + '/' + active_sem.intID">
             <i class="fa fa-book"></i>Subject Enlistment</a>
           </a>
+          <a v-else-if="user_level == 2 || user_level == 3"
+            class="btn btn-app"
+            :href="base_url + 'academics/enlistment/' + student.intID + '/' + active_sem.intID">
+            <i class="fa fa-book"></i>Advising Form</a>
+          </a>
           <!-- <a v-if="reg_status == 'For Registration' && (user_level == 2 || user_level == 3)"  class="btn btn-app" :href="base_url + 'unity/edit_sections/' + student.intID + '/' + active_sem.intID">
                         <i class="fa fa-book"></i> Update Sections
                     </a>                         -->
@@ -901,7 +906,10 @@ new Vue({
                   .then(data => {
                       this.loader_spinner = false;                                                                                                                            
                       if(data.data.success)
-                        document.location = base_url + 'registrar/register_old_student/' + data.data.sid + '/' + this.active_sem.intID;                       
+                        if(data.data.sid)
+                          document.location = base_url + 'registrar/register_old_student/' + data.data.sid + '/' + this.active_sem.intID;                       
+                        else
+                          location.reload();
                       else
                       Swal.fire({
                         title: "Failed",
