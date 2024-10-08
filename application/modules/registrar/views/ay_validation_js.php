3d30a19596f56b46105e232851ac2b4267bf2c41
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2024-10-08 09:48:39 +0800

    Update ay_validation_js.php

--- application/modules/registrar/views/ay_validation_js.php
+++ application/modules/registrar/views/ay_validation_js.php
@@ -28,6 +28,35 @@ $(document).ready(function(){
         });
         }
         
-    });    
+    });
+    
+    $(".delete-month").click(function(e){
+        e.preventDefault();        
+        conf = confirm("Are you sure you want to delete?");        
+        if(conf)
+        {
+            $(".loading-img").show();
+            $(".overlay").show();
+            var id = $(this).attr('rel');                
+            var data = {'id':id};
+            $.ajax({
+                'url':'<?php echo base_url(); ?>index.php/registrar/delete_month',
+                'method':'post',
+                'data':data,
+                'dataType':'json',
+                'success':function(ret){
+                    if(ret.message == "failed"){
+                        alert('Error in deleting.')                        
+                    }
+                    else
+                        location.reload();
+
+                    $(".loading-img").hide();
+                    $(".overlay").hide();
+            }
+        });
+        }
+        
+    });
 });
 </script>
