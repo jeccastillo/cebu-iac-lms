ca8cf107d6e3774dc0de39f68383f024690a5980
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-10-08 18:02:59 +0800

    Update view_advising_section.php

--- application/modules/unity/views/faculty/view_advising_section.php
+++ application/modules/unity/views/faculty/view_advising_section.php
@@ -50,7 +50,24 @@
                 </div>
             </div>
             <div v-if="!loading_attendance" class="modal-body">
-
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>Month</th>
+                            <th>Days</th>
+                            <th>Abscences</th>
+                            <th>Tardies</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="ad as attendance_data">
+                            <td>{{ ad.month }}</td>
+                            <td>{{ ad.school_days }}</td>
+                            <td>{{ ad.abscences }}</td>
+                            <td>{{ ad.tardy }}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
             <div v-else class="modal-body">
                 <h3>Loading Data</h3>
@@ -79,7 +96,7 @@ new Vue({
         active_sem: undefined,
         students: [],
         loading_attendance: false,
-        attendance_data: undefined,
+        attendance_data: [],
     },
 
     mounted() {
@@ -102,10 +119,11 @@ new Vue({
 
     methods: {      
         loadAttendance: function(id){
+            this.loading = true;
             axios.get(base_url + 'unity/attendance_data/' + id + '/' + this.active_sem.intID)
             .then((data) => {
-            
-                
+                this.attendance_data = data.data.attendance;
+                this.loading = false;
             }
             )
             .catch((error) => {
