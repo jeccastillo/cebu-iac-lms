f73fa2175b3e08c1c2d87e8852574d1d0d0aee2d
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-10-08 16:51:51 +0800

    Update classlist.php

--- application/modules/unity/views/faculty/classlist.php
+++ application/modules/unity/views/faculty/classlist.php
@@ -86,6 +86,39 @@
         
             
         <?php endforeach; ?>
+        <?php foreach($advised as $class): ?>
+            <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-blue" >
+                <div class="box-tools">
+                 <div class="btn-group">
+                    <button type="button" class="btn <?php echo ($class['intFinalized']==0)?'btn-success':'btn-danger'; ?>"><i class="ion ion-android-settings"></i></button>
+                    <button type="button" class="btn <?php echo ($class['intFinalized']==0)?'btn-success':'btn-danger'; ?> dropdown-toggle" data-toggle="dropdown">
+                        <span class="caret"></span>
+                        <span class="sr-only">Toggle Dropdown</span>
+                    </button>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="<?php echo base_url() ?>unity/view_section/<?php echo $class['intID']; ?>"><i class="ion ion ion-ios7-compose"></i> View</a></li>
+                    </ul>
+                </div>               
+                 </div>
+                <div class="inner">
+                    <h3>
+                        <?php echo $class['name']; ?>
+                    </h3>                   
+                </div>
+                <div class="icon">
+                    <i class="ion ion-android-book"></i>
+                </div>
+                <a href="<?php echo base_url(); ?>unity/view_section/<?php echo $class['intID']; ?>" class="small-box-footer">
+                    View <i class="fa fa-arrow-circle-right"></i>
+                </a>                
+            </div>
+        </div>
+            
+        
+            
+        <?php endforeach; ?>
         <hr style="clear:both;" />
         <div class="col-lg-3 col-xs-6">
             <!-- small box -->
