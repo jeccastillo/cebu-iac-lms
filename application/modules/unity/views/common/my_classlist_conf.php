4a66e345c80789477990572ca59cb1720b2c5870
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-10-08 17:12:33 +0800

    Update my_classlist_conf.php

--- application/modules/unity/views/common/my_classlist_conf.php
+++ application/modules/unity/views/common/my_classlist_conf.php
@@ -1,6 +1,8 @@
 <script type="text/javascript">
     $(document).ready(function(){
-        
+        $("#select-sem-classlist").on('change',function(e){
+            document.location = "<?php echo base_url(); ?>unity/view_classlist/" + this.value;
+        });
         $(".trash-classlist").click(function(e){
                     conf = confirm("Are you sure you want to delete?");
                     if(conf)
