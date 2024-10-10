b5680f2f37ca04ba6ca87c37262288b4d4b0d01d
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-10-10 11:32:37 +0800

    Update grade_slip_shs.php

--- application/modules/pdf/views/grade_slip_shs.php
+++ application/modules/pdf/views/grade_slip_shs.php
@@ -228,15 +228,19 @@ $html .= '
                 $tardy = isset($month['attendance']['tardy'])?$month['attendance']['tardy']:0;
                 $present = $days - $abscences;
 
+                $total_attendance += $days;
+                $total_present += $present;
+                $total_tardy += $tardy;
+
                 $html .="<th style='border-right:1px solid #333;border-top:1px solid #333;' ><div class='rotate'>".strtoupper(substr($month['month'],0,3))."</div></th>";
-                $attendance_days.="<td>".$days."</td>";
-                $attendance_present.="<td>".$present."</td>";
-                $attendance_tardy.="<td>".$tardy."</td>";
+                $attendance_days.="<td style='text-align:center;'>".$days."</td>";
+                $attendance_present.="<td style='text-align:center;>".$present."</td>";
+                $attendance_tardy.="<td style='text-align:center;>".$tardy."</td>";
             }
             //FOR TOTAL
-            $attendance_days .= "<td>".$total_attendance."</td>";
-            $attendance_present .= "<td>".$total_present."</td>";
-            $attendance_tardy .= "<td>".$total_tardy."</td>";
+            $attendance_days .= "<td style='text-align:center;>".$total_attendance."</td>";
+            $attendance_present .= "<td style='text-align:center;>".$total_present."</td>";
+            $attendance_tardy .= "<td style='text-align:center;>".$total_tardy."</td>";
 
             $html .="<th style='border-right:1px solid #333;border-top:1px solid #333;'><div class='rotate'>TOTAL</div></th></tr>
                 <tr>
