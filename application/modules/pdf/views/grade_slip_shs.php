34c0c28b154357ccf44721381a005a745df83790
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-10-10 11:09:24 +0800

    Update grade_slip_shs.php

--- application/modules/pdf/views/grade_slip_shs.php
+++ application/modules/pdf/views/grade_slip_shs.php
@@ -208,20 +208,7 @@ $html .= '
                 <td style="line-height:2px;border-top:1px solid #333"></td>         
                 <td style="line-height:2px;border-top:1px solid #333"></td>         
             </tr>
-            </table>            
-            <div style="line-height:20px"></div>
-            <table>
-                <tr style="font-size:10;">
-                    <td style="width:25%;"></td>
-                    <td style="width:50%;text-align:center;border-bottom:1px solid #333;">
-                                '.$adviser_name.' 
-                    </td>
-                    <td style="width:25%;"></td>
-                </tr>
-            </table>
-            <div style="text-align:center;">
-                Class Adviser
-            </div>               
+            </table>                       
             <div style="line-height:20px"></div>         
             <div style="text-align:center;font-weight:bold;">REPORT ON LEARNING PROGRESS AND ACHIEVEMENT</div>
             ';
@@ -256,8 +243,23 @@ $html .= '
                     ".$attendance_tardy."
                 </tr>
             </table>";
+            
 
-
+            $html .= '
+            <div style="line-height:20px"></div>
+            <table>
+                <tr style="font-size:10;">
+                    <td style="width:25%;"></td>
+                    <td style="width:50%;text-align:center;border-bottom:1px solid #333;">
+                                '.$adviser_name.' 
+                    </td>
+                    <td style="width:25%;"></td>
+                </tr>
+            </table>
+            <div style="text-align:center;">
+                Class Adviser
+            </div>';
+            
             echo $html;
 ?>
         </section>
