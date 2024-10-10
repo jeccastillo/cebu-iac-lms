d2d7e1ec58589582aee4e4526e7f86c85afdbd8d
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-10-10 10:36:41 +0800

    Update grade_slip_shs.php

--- application/modules/pdf/views/grade_slip_shs.php
+++ application/modules/pdf/views/grade_slip_shs.php
@@ -198,7 +198,7 @@ $i = 0;
 foreach($term_months as $month){
     $pdf->StartTransform();
     $pdf->Rotate(90);
-    $pdf->MultiCell(0,20,$month['month'],1,1,'L',0,'');
+    $pdf->MultiCell(35,20,$month['month'],'L',1,0,'','',true);
     $pdf->StopTransform();
     $i++;
 }
