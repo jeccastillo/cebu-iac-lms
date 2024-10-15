9235144ff8769d1b4b35d0af74ad58790ca1702e
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2024-10-15 13:20:24 +0800

    Update print_invoice.php

--- application/modules/pdf/views/print_invoice.php
+++ application/modules/pdf/views/print_invoice.php
@@ -97,7 +97,7 @@ table tr td {
             </div>
             <div style="position:absolute; top: 245px; left: 500px; width: 500px; height: 20px;">
                 1
-            </div>
+            </div>            
             <div style="position:absolute; top: 245px; left: 550px; width: 200px; height: 20px;">
                 <?php echo $full_assessment; ?>
             </div>
@@ -116,25 +116,43 @@ table tr td {
             <div style="position:absolute; top:  270px; left: 650px; width: 200px; height: 20px;">
                 <?php echo $reservation_amount != 0 ? "-".$reservation_amount : ""; ?>
             </div>
+            <!---VAT ZERO RATED--->
+            <div style="position:absolute; top: 325px; left: 650px; width: 200px; height: 20px;">
+                <?php echo $vat_zero_rated_sale != 0 ? $vat_zero_rated_sale : "" ; ?>
+            </div>
+            <!---TOTAL SALE--->
             <div style="position:absolute; top:  353px; left: 650px; width: 200px; height: 20px;">
                 <?php  echo $total_assessment; ?>
             </div>
-            <div style="position:absolute; top:  380px; left: 100px; width: 200px; height: 20px;">
-                <?php  echo $total_amount_due; ?>
+            <!---- VAT--->
+            <div style="position:absolute; top: 370px; left: 650px; width: 200px; height: 20px;">
+                <?php echo $less_vat != 0 ? $less_vat : "" ; ?>
             </div>
+            <!---- Less EWT--->
+            <div style="position:absolute; top: 390px; left: 650px; width: 200px; height: 20px;">
+                <?php echo $less_ewt != 0 ? "-".$less_ewt : "" ; ?>
+            </div>            
+            <!---Payment Left Amount-->
+            <div style="position:absolute; top:  380px; left: 100px; width: 200px; height: 20px;">
+                <?php  echo $total_amount_due == 0 ? "" : $total_amount_due; ?>
+            </div>            
             <div style="position:absolute; top:  405px; left: 650px; width: 200px; height: 20px;">
                 <?php  echo $total_assessment; ?>
             </div>
             <div style="position:absolute; top:  460px; left: 10px; width: 200px; height: 20px;">
-                <?php  echo $remarks; ?>
+                <?php  echo $total_amount_due == 0 ? "" : $remarks; ?>
+            </div>
+            <!--Vat Exempt Sale-->
+            <div style="position:absolute; top:  453px; left: 305px; width: 200px; height: 20px;">
+                <?php  echo $vatable != 0 ? $vatable : $vatable; ?>
             </div>
             <!--Vat Exempt Sale-->
             <div style="position:absolute; top:  475px; left: 305px; width: 200px; height: 20px;">
-                <?php  echo $total_assessment; ?>
+                <?php  echo $vat_exempt != 0 ? $vat_exempt : $total_assessment; ?>
             </div>
             <!--Total Amount received-->
             <div style="position:absolute; top:  565px; left: 50px; width: 200px; height: 20px;">
-                <?php  echo $total_amount_due; ?>
+                <?php  echo $total_amount_due == 0 ? "" : $total_amount_due; ?>
             </div>
             <div style="position:absolute; top: 550px; right: 20px; width: 200px; height: 20px;font-size:15px">
                 <?php echo $cashier_name; ?>
