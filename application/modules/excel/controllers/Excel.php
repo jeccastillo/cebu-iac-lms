689262f2ae71d5f1da79bd14aa34b2fcc9fd5037
Jp <jpestorque0@gmail.com>
2024-10-15 19:31:41 +0800

    Update Excel.php

--- application/modules/excel/controllers/Excel.php
+++ application/modules/excel/controllers/Excel.php
@@ -4161,6 +4161,8 @@ class Excel extends CI_Controller {
         $post = $this->input->post();
         $data = json_decode($post['applicants']);
         
+        // print_r($applicants);
+        // die();
         // $data = $this->input->post();
         // $data = json_decode($data['data']);
         $date = date("Y-m-d");
