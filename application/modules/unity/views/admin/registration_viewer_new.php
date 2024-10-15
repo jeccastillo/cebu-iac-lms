aed7b15eaf0c926854f627991ce6613c5432ba79
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2024-10-15 11:23:02 +0800

    Update registration_viewer_new.php

--- application/modules/unity/views/admin/registration_viewer_new.php
+++ application/modules/unity/views/admin/registration_viewer_new.php
@@ -2017,7 +2017,7 @@ new Vue({
         computeVat: function(){
             this.net_vat = this.request.invoice_amount / 1.12;
             this.less_vat = this.net_vat * .12;             
-            this.total_sales = this.request.invoice_amount + this.request.invoice_amount_ves + this.invoice_amount_vzrs;
+            this.total_sales = parseFloat(this.request.invoice_amount) + parseFloat(this.request.invoice_amount_ves) + parseFloat(this.invoice_amount_vzrs);
             this.less_ewt = this.total_sales * this.request.withholding_tax_percentage;
             this.total_amount_computed = this.total_sales + this.less_vat - this.less_ewt;
             //Formatted
