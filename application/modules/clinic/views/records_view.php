41428cc965d81972e0b82ecd8e1a5d39aca1d9f0
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-10-16 10:18:13 +0800

    asd

---
+++ application/modules/clinic/views/records_view.php
@@ -0,0 +1,76 @@
+<aside class="right-side">
+    <section class="content-header">
+        <h1>
+            Student
+            <small></small>
+        </h1> 
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Student</a></li>
+            <li class="active">View All Student</li>
+        </ol>
+    </section>
+    <div class="content">
+        
+        <div class="box box-solid box-default">
+            <div class="box-header">                  
+                <div>
+
+                    <div style="width:50%;float:right; text-align:right;">
+<!--
+                        <form method="post" action="<?php echo base_url(). student/view_all_students/20 ?>">
+                            <h5>Search: <input type="text" name="search_string"/>
+                            </h5>
+                        </form>
+-->
+                    </div>
+                </div>
+
+                <h3 class="box-title">List of Health Records</h3>
+                <div class="box-tools">
+
+                </div>
+            </div><!-- /.box-header -->
+            <div class="box-body table-responsive">
+                <table id="users_table" class="table table-hover">
+                    <thead>                        
+                        <tr>
+                            <th>id</th>
+                            <th>Last Name</th>
+                            <th>First Name</th>
+                            <th>Classification</th>
+                            <th>Date</th>
+                            <th>Consultation Type</th>
+                            <th>Chief Complaint/Reason for the Visit</th>
+                            <th>History of Present Illness</th>
+                            <th>Actions</th>
+                        </tr>                        
+                        <tr class="search">
+                            <td>id</td>
+                            <td>Last Name</td>
+                            <td>First Name</td>
+                            <td>Classification</td>
+                            <td>Date</td>
+                            <td>Consultation Type</td>
+                            <td>Chief Complaint/Reason for the Visit</td>
+                            <td>History of Present Illness</td>
+                            <td>Actions</td>
+                        </tr>
+                    </thead>                    
+                    <tbody></tbody>
+                    <!-- <tfoot>
+                        <tr>
+                            <th>id</th>
+                            <th>slug</th>
+                            <th>Student Number</th>
+                            <th>Last Name</th>
+                            <th>Program</th>
+                            <th>Year Level</th>
+                            <th>Actions</th>
+                        </tr>
+                    </tfoot> -->
+                </table>
+            </div><!-- /.box-body -->
+        </div><!-- /.box -->
+    </div>
+    
+</aside>
