33600ebf0db8c9d5a6f443e42c649dbd6ca04a66
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-10-21 08:41:20 +0800

    Update Finance.php

--- application/modules/finance/controllers/Finance.php
+++ application/modules/finance/controllers/Finance.php
@@ -62,7 +62,7 @@ class Finance extends CI_Controller {
         $data['current_sem'] = $sem['intID'];
         $data['sy'] = $this->data_fetcher->fetch_table('tb_mas_sy');
         $data['sem_year'] = $sem['strYearStart'];
-        $ret['particulars'] = $this->db->get_where('tb_mas_particulars',array('type'=>'particular'))
+        $data['particulars'] = $this->db->get_where('tb_mas_particulars',array('type'=>'particular'))
                                         ->result_array();
         $data['payees'] = $this->db->get('tb_mas_ns_payee')->result_array();
         $data['message'] = "Success";
