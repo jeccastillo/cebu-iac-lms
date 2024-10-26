d729a09599119ad6f5aa1ba94544de7439f72025
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-10-26 11:49:16 +0800

    Update print_view_student_reg2_makati.php

--- application/modules/pdf/views/print_view_student_reg2_makati.php
+++ application/modules/pdf/views/print_view_student_reg2_makati.php
@@ -190,7 +190,7 @@ $html.= '<table border="0" cellpadding="0" cellspacing="0" style="color:#333; fo
         $html .='
             <table cellpadding="0" style="color:#333; text-align:left; font-size:8;" width="570px">                                
                 <tr>
-                    <td width="360px">
+                    <td width="380px">
                         <table cellpadding="0"  style="color:#333; font-size:7;">
                             <tr>
                                 <td colspan="3" style= "font-size:8; font-weight:bold;">ASSESSMENT SUMMARY</td>                                
