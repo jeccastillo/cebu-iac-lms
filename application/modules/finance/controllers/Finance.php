795ccbd37490f09678a606ea2ca317908aeea975
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-11-12 16:34:04 +0800

    Update Finance.php

--- application/modules/finance/controllers/Finance.php
+++ application/modules/finance/controllers/Finance.php
@@ -209,6 +209,7 @@ class Finance extends CI_Controller {
             }
             else{
                 $details = $this->db->get_where('tb_mas_ns_payee',array('lastname LIKE'=>$item->lastname,'firstname LIKE'=>$item->firstname))->first_row(); 
+                print_r($details);
                 if($details)
                     $item->student_number = $details->id_number;                
             }
