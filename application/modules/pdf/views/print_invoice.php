7895e3620cd79f168155511f0c8cd158d903fb82
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2024-11-12 14:27:15 +0800

    Update print_invoice.php

--- application/modules/pdf/views/print_invoice.php
+++ application/modules/pdf/views/print_invoice.php
@@ -86,7 +86,7 @@ table tr td {
             </div>
             <!---TIN--->
             <?php if($payee): ?>
-            <div style="position:absolute; top: 180px; right: -60px; width: 200px; height: 20px;">
+            <div style="position:absolute; top: 185px; right: -80px; width: 200px; height: 20px;">
                 <?php echo $payee['tin']; ?>
             </div>
             <?php endif; ?>
