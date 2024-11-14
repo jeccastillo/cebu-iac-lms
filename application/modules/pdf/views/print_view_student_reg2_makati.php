1788e0af9207532a9169fc84bbee2dd1bacf265b
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-11-14 17:06:15 +0800

    Update print_view_student_reg2_makati.php

--- application/modules/pdf/views/print_view_student_reg2_makati.php
+++ application/modules/pdf/views/print_view_student_reg2_makati.php
@@ -264,8 +264,9 @@ $html.= '<table border="0" cellpadding="0" cellspacing="0" style="color:#333; fo
                         $html .='   <tr>
                                         <td style="font-size:7px">'.$sch->name.'</td>
                                         <td style="text-align:right;">-'.number_format($tuition['scholarship_deductions_dc_array'][$ctr], 2, '.' ,',').'</td>
-                                        <td style="text-align:right;">-'.number_format($tuition['scholarship_deductions_installment_dc_array30'][$ctr], 2, '.' ,',').'</td>
                                         <td style="text-align:right;">-'.number_format($tuition['scholarship_deductions_installment_dc_array50'][$ctr], 2, '.' ,',').'</td>
+                                        <td style="text-align:right;">-'.number_format($tuition['scholarship_deductions_installment_dc_array30'][$ctr], 2, '.' ,',').'</td>
+                                        
                                     </tr>';
                         $ctr++;
                         endforeach;
