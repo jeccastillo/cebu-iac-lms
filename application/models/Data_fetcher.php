863e55baac052d64bb4c13b341443ebba29c2be0
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-11-15 08:24:53 +0800

    Update Data_fetcher.php

--- application/models/Data_fetcher.php
+++ application/models/Data_fetcher.php
@@ -3297,8 +3297,8 @@ class Data_fetcher extends CI_Model {
         $data['down_payment30'] = 0;
         $data['down_payment50'] = 0;
         $data['total_installment'] = $data['ti_before_deductions']  - $scholarship_installment_grand_total  - $discount_installment_grand_total;
-        $data['total_installment30'] = $data['ti_before_deductions30']  - $scholarship_installment_grand_total30  - $discount_installment_grand_total;
-        $data['total_installment50'] = $data['ti_before_deductions50']  - $scholarship_installment_grand_total50  - $discount_installment_grand_total;
+        $data['total_installment30'] = $data['ti_before_deductions30']  - $scholarship_installment_grand_total30  - $discount_installment_grand_total30;
+        $data['total_installment50'] = $data['ti_before_deductions50']  - $scholarship_installment_grand_total50  - $discount_installment_grand_total50;
         
         if($data['total'] < 0)
             $data['total'] = 0;
