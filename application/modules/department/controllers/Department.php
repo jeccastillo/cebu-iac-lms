1a3127338b49157939c0f1b1da071782e3cc3e62
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2024-11-18 16:46:04 +0800

    Update Department.php

--- application/modules/department/controllers/Department.php
+++ application/modules/department/controllers/Department.php
@@ -32,7 +32,7 @@ class Department extends CI_Controller {
 		$this->load->model("facebook_login");	
 		$this->load->model("user_model");
         $this->config->load('courses');
-        
+        $this->data['campus'] = $this->config->item('campus');
         $this->data['department_config'] = $this->config->item('department');
         $this->data['terms'] = $this->config->item('terms');
         $this->data['term_type'] = $this->config->item('term_type');
