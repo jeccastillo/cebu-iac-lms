de50693bb31e9d716908fa46e56c9d692e88769a
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2024-12-03 10:34:18 +0800

    updated

--- application/modules/pdf/views/curriculum.php
+++ application/modules/pdf/views/curriculum.php
@@ -9,7 +9,7 @@
             <td width="400" style="text-align: center; line-height:100%">
                 
              <font style="font-family:Calibri Light; font-size: 10;">Republic of the Philippines<br />City of Makati</font><br /><br />
-             <font style="font-family:Calibri Light; font-size: 10;font-weight: bold;">Information & Communications Technology Academy</font><br />
+             <font style="font-family:Calibri Light; font-size: 10;font-weight: bold;">iACADEMY Inc.</font><br />
 			 <font style="font-family:Calibri Light; font-size: 10;">Filinvest Cebu Cyberzone Tower 2 Salinas Drive corner W. Geonzon St., Brgy. Apas, Lahug, Cebu City</font><br />             
             </td>
             <td width="64" align="left" valign="middle"></td>
