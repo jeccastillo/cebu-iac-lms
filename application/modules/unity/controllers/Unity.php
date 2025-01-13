bd10efb4bfc674336fa232dcf4f46ff8270816c4
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2025-01-13 17:20:00 +0800

    Update Unity.php

--- application/modules/unity/controllers/Unity.php
+++ application/modules/unity/controllers/Unity.php
@@ -3766,8 +3766,9 @@ class Unity extends CI_Controller {
         
         if($this->is_registrar() || $this->is_super_admin()){
             $post = $this->input->post();
+            print_r($post);
             $this->data_poster->deleteItem('tb_mas_curriculum_subject',$post['id'],'intID');
-            $data['table'] = $table." ".$post['id'];
+            $data['table'] = $table;
             $data['message'] = "success";
             $this->data_poster->log_action('Curriculum','Deleted a Subject from Curriculum '.$post['code'],'red');
         }
