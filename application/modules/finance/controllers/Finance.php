bb5a90b10e4e055a5f22803f883d410b4f51cf5e
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2025-01-15 11:20:07 +0800

    Update Finance.php

--- application/modules/finance/controllers/Finance.php
+++ application/modules/finance/controllers/Finance.php
@@ -705,7 +705,7 @@ class Finance extends CI_Controller {
                     $data['invoice_current'] += 1;        
         }
 
-        unset($post['invoice_used']);
+        unset($data['invoice_used']);
 
         $this->db
             ->where('intID',$data['intID'])
