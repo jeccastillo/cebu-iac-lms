d879d41cbd9c699f917f5e9a1d1a72e074972cd1
ronngb <ronny.gabi@gmail.com>
2025-02-12 17:47:10 +0800

    finance reports

---
+++ application/modules/finance/views/common/miscellaneous_list_conf.php
@@ -0,0 +1,80 @@
+<script type="text/javascript">
+$(document).ready(function() {
+    $('#miscellaneous-table').DataTable({
+        "aLengthMenu": [10, 20, 50, 100, 250, 500, 750, 1000],
+        "bProcessing": true,
+        "serverSide": false,
+        "ordering": false,
+        "paging": true,
+        ajax: {
+            url: "<?php echo base_url(); ?>finance/miscellaneous_fee_report_data/<?php echo $current_sem; ?>/<?php echo $particular_id; ?>/<?php echo $date; ?>",
+            dataSrc: 'data'
+        },
+        columns: [{
+            data: 'index',
+            title: 'No'
+        }, {
+            data: 'student_number',
+            title: 'Student Number'
+        }, {
+            data: 'student_name',
+            title: 'Student Name'
+        }, {
+            data: 'course',
+            title: 'Course'
+        }, {
+            data: 'date_enlisted',
+            title: 'Date Enrolled'
+        }, {
+            data: 'regular',
+            title: 'Regular'
+        }, {
+            data: 'new_student',
+            title: 'NSF'
+        }, {
+            data: 'internship',
+            title: 'Internship'
+        }, {
+            data: 'nstp',
+            title: 'NSTP'
+        }, {
+            data: 'thesis',
+            title: 'Thesis'
+        }, {
+            data: 'late_enrollment',
+            title: 'LEF'
+        }]
+    });
+});
+$("#select-term-leads").on('change', function(e) {
+    let term = $(this).val();
+    let particularId = "<?php echo $particular_id; ?>";
+    let currentDate = "<?php echo $date; ?>";
+    document.location = "<?php echo base_url()."finance/miscellaneous_report/"; ?>" + term +
+        '/' + particularId + '/' + currentDate;
+});
+$("#date-picker").on('change', function(e) {
+    let term = "<?php echo $current_sem; ?>";
+    let particularId = "<?php echo $particular_id; ?>";
+    const currentDate = $(this).val();
+    document.location = "<?php echo base_url()."finance/miscellaneous_report/"; ?>" + term +
+        '/' + particularId + '/' + currentDate;
+});
+$("#select-misc-fee").on('change', function(e) {
+    let term = "<?php echo $current_sem; ?>";
+    let currentDate = "<?php echo $date; ?>";
+    let particularId = $(this).val();
+    document.location = "<?php echo base_url()."finance/miscellaneous_report/"; ?>" + term +
+        '/' + particularId + '/' + currentDate;
+});
+$(document).ready(function() {
+    $("#miscellaneous_fee_list_excel").click(function(e) {
+        var campus = "<?php echo $campus;?>";
+        var base_url = "<?php echo base_url(); ?>";
+        var url = base_url + 'excel/miscellaneous_fee_report/' + $(
+            "#select-term-leads").val() + '/' + campus + '/' + $(
+            "#select-misc-fee").val() + '/' + $("#date-picker").val();
+        window.open(url, '_blank');
+    })
+});
+</script>
