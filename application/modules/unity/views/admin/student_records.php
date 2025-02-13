fc7ead79c3ec745c14da6af50fdd74d80aa31d36
Jp <jpestorque0@gmail.com>
2025-02-13 14:58:46 +0800

    Update student_records.php

--- application/modules/unity/views/admin/student_records.php
+++ application/modules/unity/views/admin/student_records.php
@@ -87,7 +87,7 @@
                                             <td v-else style="font-weight:bold">
                                                 OW
                                             </td>
-                                            <td v-if="student.type == 'shs' && is_numeric(record.v2) && is_numeric(record.v3)">{{ (record.v2 + record.v3) / 2}}</th>
+                                            <td v-if="student.type == 'shs' && isInteger(record.v2) && isInteger(record.v3)">{{ (record.v2 + record.v3) / 2}}</th>
                                             <td v-else-if="student.type == 'shs'">T</th>
                                             <td :style="(record.strRemarks != 'Failed')?'color:#333;':'color:#990000;'">{{ record.intFinalized >=1?record.strRemarks:'---' }}</td>   
                                             <td>{{ record.strFirstname+" "+record.strLastname }}</td>
