fa0e2dc35a2f7604b1cbca191f20dc5ee872c6c9
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2025-02-20 09:11:46 +0800

    Update Data_fetcher.php

--- application/models/Data_fetcher.php
+++ application/models/Data_fetcher.php
@@ -3819,6 +3819,7 @@ class Data_fetcher extends CI_Model {
                 $sched_day = '';
                 $sched_time = '';
                 $sched_room = '';                
+                $c['elective_subject'] = $this->db->get_where('tb_mas_subjects',array('intID'=>$c['elective_classlist_id']))->first_row();
                 
                 if(isset($schedule[0]['strDay']))                                                
                     $sched_time = date('g:ia',strtotime($schedule[0]['dteStart'])).' - '.date('g:ia',strtotime($schedule[0]['dteEnd']));  
