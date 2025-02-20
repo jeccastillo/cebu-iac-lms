e035d80fdc99f803bec1a71649dfd0887c10ae19
Jp <jpestorque0@gmail.com>
2025-02-21 00:10:43 +0800

    Update Excel.php

--- application/modules/excel/controllers/Excel.php
+++ application/modules/excel/controllers/Excel.php
@@ -7885,7 +7885,6 @@ class Excel extends CI_Controller {
             $spreadsheet = PHPExcel_IOFactory::load($filePath);
             $sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
 
-
             // Now you can loop through the $sheetData array and insert into your database
             foreach ($sheetData as $index => $row) {
                 if($index >= 10){
@@ -7916,7 +7915,10 @@ class Excel extends CI_Controller {
                         
                         $faculty = $this->db->from('tb_mas_faculty')->like(array('strLastname' => $facultyLastName, 'strFirstName' => $facultyFirstName))->get()->first_row('array');
                         $subject = $this->db->get_where('tb_mas_subjects',array('strCode' => $row['E']))->first_row('array');
-                        
+                        print_r($faculty);
+                        print_r($subject);
+                        die();
+
                         if($faculty && $subject){
                             $classlistID = '';
                             //Check if classlist exists
