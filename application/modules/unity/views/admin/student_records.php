c345e4ae28635998ec623191aa45e804b22d7a39
Jp <jpestorque0@gmail.com>
2025-02-21 10:42:30 +0800

    Update student_records.php

--- application/modules/unity/views/admin/student_records.php
+++ application/modules/unity/views/admin/student_records.php
@@ -89,7 +89,7 @@
                                                 OW
                                             </td>
                                             <td v-if="student.type == 'shs' && record.v2 && record.v3">{{ (parseInt(record.v2) + parseInt(record.v3) ? Math.round((parseInt(record.v2) + parseInt(record.v3)) / 2) : 'T')}}</td>
-                                            <td v-else>---</td>
+                                            <td v-if-else="student.type == 'shs' && (!record.v2 || !(record.v3)">---</td>
                                             <td :style="(record.strRemarks != 'Failed')?'color:#333;':'color:#990000;'">{{ record.intFinalized >=1?record.strRemarks:'---' }}</td>   
                                             <td>{{ record.strFirstname+" "+record.strLastname }}</td>                                 
                                         </tr>
