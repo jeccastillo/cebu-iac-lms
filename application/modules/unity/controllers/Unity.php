9596e2ef93a5db8407f982a0de7a3b611af17a1c
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2025-03-07 00:08:46 +0800

    Update Unity.php

--- application/modules/unity/controllers/Unity.php
+++ application/modules/unity/controllers/Unity.php
@@ -1191,8 +1191,7 @@ class Unity extends CI_Controller {
     function get_tuition_ajax(){
         
         $post = $this->input->post();              
-        $post['subjects_loaded'] = json_decode($post['subjects_loaded']);   
-        $post['subjects_loaded'] =  (array) $post['subjects_loaded'];
+        $post['subjects_loaded'] = json_decode(json_encode($post['subjects_loaded']));           
         
         if(!isset($post['tuition_year']))
             $ty = $student['intTuitionYear'];
