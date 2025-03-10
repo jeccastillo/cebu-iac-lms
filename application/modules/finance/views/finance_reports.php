9312cc9c7b5b26dde055af8c94f51681cbad83ba
ronngb <ronny.gabi@gmail.com>
2025-03-10 15:31:52 +0800

    update menu

--- application/modules/finance/views/finance_reports.php
+++ application/modules/finance/views/finance_reports.php
@@ -102,22 +102,6 @@
                 </div>
                 <!-- small box -->
             </div>
-            <div class="col-md-4 col-sm-8 col-xs-12">
-                <!-- small box -->
-                <div class="small-box bg-blue">
-                    <div class="inner">
-                        <h3>Invoice Report</h3>
-                        <p>Invoice Report List</p>
-                    </div>
-                    <div class="icon">
-                        <i class="fa fa-list"></i>
-                    </div>
-                    <a href="<?php echo base_url(); ?>finance/invoice_report"
-                        class="small-box-footer"> View <i class="fa fa-arrow-circle-right"></i>
-                    </a>
-                </div>
-                <!-- small box -->
-            </div>
         </div>
     </div>
 </aside>
