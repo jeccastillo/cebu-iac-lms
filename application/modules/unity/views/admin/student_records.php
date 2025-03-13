342116cc494b1f9c576681692c729b840f113f1b
Jp <jpestorque0@gmail.com>
2025-03-13 11:27:03 +0800

    Update student_records.php

--- application/modules/unity/views/admin/student_records.php
+++ application/modules/unity/views/admin/student_records.php
@@ -77,6 +77,10 @@
                                                 OW
                                             </td>
                                             <td v-if="record.v3 != 'OW'" :style="(record.intFinalized == 2)?'font-weight:bold;':''">
+
+                                                <span v-if="record.intFinalized >=2 && student.type =='shs'" :style="(record.strRemarks != 'Failed')?'color:#333;':'color:#990000;'">
+                                                    {{ record.shsFinalsGrade }}
+                                                </span>
                                                 <span v-if="record.intFinalized >=2" :style="(record.strRemarks != 'Failed')?'color:#333;':'color:#990000;'">
                                                     {{ record.v3 }}
                                                 </span>
@@ -87,8 +91,9 @@
                                             <td v-else style="font-weight:bold">
                                                 OW
                                             </td>
-                                            <td v-if="student.type == 'shs' && record.v2 && record.v3">{{ (parseInt(record.v2) + parseInt(record.v3) ? Math.round((parseInt(record.v2) + parseInt(record.v3)) / 2) : 'T')}}</td>
-                                            <td v-else-if="student.type == 'shs' && (!record.v2 || !record.v3)">---</td>
+                                            <!-- <td v-if="student.type == 'shs' && record.v2 && record.v3">{{ (parseInt(record.v2) + parseInt(record.v3) ? Math.round((parseInt(record.v2) + parseInt(record.v3)) / 2) : 'T')}}</td> -->
+                                            <td v-else-if="student.type == 'shs' && record.v3">{{ recrod.v3 }}</td>
+                                            <td v-else-if="student.type == 'shs' && !record.v3">---</td>
                                             <td :style="(record.strRemarks != 'Failed')?'color:#333;':'color:#990000;'">{{ record.intFinalized >=1?record.strRemarks:'---' }}</td>   
                                             <td>{{ record.strFirstname+" "+record.strLastname }}</td>                                 
                                         </tr>
