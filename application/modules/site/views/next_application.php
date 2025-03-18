4c919cd4f93677643c12f6c6b66ec6ba3b2f8451
Joseph Edmund Castillo <39857324+jeccastillo@users.noreply.github.com>
2025-03-18 12:25:55 +0800

    Update next_application.php

--- application/modules/site/views/next_application.php
+++ application/modules/site/views/next_application.php
@@ -537,8 +537,8 @@
         </label>
         <div v-if="true" class=" mb-6 mt-10">
             <div class="border-[1px] border-neutral-100  rounded-lg mt-5 py-2.5 pl-2.5 pr-2.5">                
-                <div class="mb-4 ">
-                    <div>
+                <div class="mb-4">
+                    <div class="mb-4">
                         <label class="block color-primary font-bold mb-3 pr-4"> Goverment ID
                         </label>
                         <input @change="attachFile" type="file" id="government_id" required />
@@ -555,7 +555,7 @@
                         <label class="block color-primary font-bold mb-3 pr-4"> Proof </label>
                         <input @change="attachFile" type="file" id="proof" required />
                     </div> -->
-                    <div>
+                    <div class="mb-4">
                         <label class="block color-primary font-bold mb-3 pr-4"> Link to social media handles
                         </label>
                         <input
@@ -563,7 +563,7 @@
                             type="text" v-model="request.social_media_handles">
                     </div>
                    
-                    <div>
+                    <div class="mb-4">
                         <label class="block color-primary font-bold mb-3 pr-4"> Video Introduction 
                         </label>
                         <label class="block color-primary italic text-sm mb-5"> (Submit a creative
