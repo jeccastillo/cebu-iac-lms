256303da2e3dd668c08eb57dc07a0124d2c229a4
Jp <jpestorque0@gmail.com>
2025-03-19 13:23:33 +0800

    Update import_previous_balance.php

--- application/modules/finance/views/import_previous_balance.php
+++ application/modules/finance/views/import_previous_balance.php
@@ -87,8 +87,7 @@ new Vue({
         },
         downloadFormat()
         {
-            // var url = base_url + 'excel/download_previous_balance_format';
-            var url = base_url + 'pdf/certificate_of_unit_earned/35';
+            var url = base_url + 'excel/download_previous_balance_format';
             window.open(url, '_blank');
         }
     }
