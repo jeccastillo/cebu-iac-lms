e4a29e2e045bfcbbbc97395f13000a747ed6bd47
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2025-04-02 14:39:56 +0800

    Create records_view.php

---
+++ application/modules/guidance/views/records_view.php
@@ -0,0 +1,69 @@
+<aside class="right-side">
+    <section class="content-header">
+        <h1>
+            Health Records
+
+            <small>
+                <button class="btn btn-app" id="health_record_excel" target="_blank" href="#"><i
+                        class="fa fa-book"></i>Download Excel</button>
+            </small>
+        </h1> 
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Clinic</a></li>
+            <li class="active">Health Records</li>
+        </ol>
+    </section>
+    <div class="content">
+        
+        <div class="box box-solid box-default">
+            <div class="box-header">                                  
+                <h3 class="box-title">List of Health Records</h3>
+                <div class="box-tools">
+                </div>
+            </div><!-- /.box-header -->
+            <div class="box-body table-responsive">
+                <table id="users_table" class="table table-hover">
+                    <thead>                        
+                        <tr>
+                            <th>id</th>
+                            <th>patient id</th>
+                            <th>Last Name</th>
+                            <th>First Name</th>
+                            <th>Classification</th>
+                            <th>Date</th>
+                            <th>Consultation Type</th>
+                            <th>Chief Complaint/Reason for the Visit</th>
+                            <th>History of Present Illness</th>
+                            <th>Actions</th>
+                        </tr>                        
+                        <tr class="search">
+                            <td>id</td>
+                            <td>patient id</td>
+                            <td>Last Name</td>
+                            <td>First Name</td>
+                            <td>Classification</td>
+                            <td>Date</td>
+                            <td>Consultation Type</td>
+                            <td>Chief Complaint/Reason for the Visit</td>
+                            <td>History of Present Illness</td>
+                            <td>Actions</td>
+                        </tr>
+                    </thead>                    
+                    <tbody></tbody>
+                    <!-- <tfoot>
+                        <tr>
+                            <th>id</th>
+                            <th>slug</th>
+                            <th>Student Number</th>
+                            <th>Last Name</th>
+                            <th>Program</th>
+                            <th>Year Level</th>
+                            <th>Actions</th>
+                        </tr>
+                    </tfoot> -->
+                </table>
+            </div><!-- /.box-body -->
+        </div><!-- /.box -->
+    </div>
+    
+</aside>
