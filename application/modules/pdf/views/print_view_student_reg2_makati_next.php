685b042a05b6afcd7a98f35c176865d3aa5593c9
Jp <jpestorque0@gmail.com>
2025-04-02 14:17:07 +0800

    Update print_view_student_reg2_makati_next.php

--- application/modules/pdf/views/print_view_student_reg2_makati_next.php
+++ application/modules/pdf/views/print_view_student_reg2_makati_next.php
@@ -128,10 +128,7 @@ $html.= '<table border="0" cellpadding="0" cellspacing="0" style="color:#333; fo
                                         $desc = $record['strDescription'];
 
                                     $html.='<tr style="color: #333;">
-                                            <td width="60px" style="font-size:7"> ' . $record['strClassName'].' '.$record['year'].$record['strSection'];
-                                    $html.= $record['sub_section'] ? $record['sub_section'] : '';
-                                            
-                                    $html.='</td>                                            
+                                            <td width="60px" style="font-size:7"> ' . $record['strClassName'].' '.$record['year'].$record['strSection'].$record['sub_section'].'</td>                                            
                                             <td width="240px" align ="left"> '.$desc. '</td>
                                             <td width="40px" align = "left"> '. $record['intLab'] . '</td> 
                                             <td width="40px" align = "left"> '. $units . '</td> ';
