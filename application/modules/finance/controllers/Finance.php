b34c5a4195cb000ecc65bf8081ab36db9dc55b66
ronngb <ronny.gabi@gmail.com>
2025-04-10 14:28:20 +0800

    update cancelled or invoice

--- application/modules/finance/controllers/Finance.php
+++ application/modules/finance/controllers/Finance.php
@@ -1863,23 +1863,24 @@ class Finance extends CI_Controller {
         }
     }
 
-    public function cancelled_or_invoice($term = 0, $report_type = 'or', $date = 0)    
+    public function cancelled_or_invoice($date_start = 0,$date_end = 0)    
     {
         if($this->faculty_logged_in())
         {
-            if($term == 0)
-                $term = $this->data_fetcher->get_processing_sem();        
-            else
-                $term = $this->data_fetcher->get_sem_by_id($term); 
+           
 
-            if (empty($date)) {
-                $date = date('Y-m-d');
+            if (empty($date_start)) {
+                $date_start = date('Y-m-d');
             }
+            if (empty($date_end)) {
+                $date_end = date('Y-m-d');
+            }
+                     
                  
             $this->data['sy'] = $this->data_fetcher->fetch_table('tb_mas_sy');
-            $this->data['current_sem'] = $term['intID'];            
-            $this->data['date'] = $date;
-            $this->data['report_type'] = $report_type;
+            // $this->data['current_sem'] = $term['intID'];            
+            $this->data['date_start'] = $date_start;
+            $this->data['date_end'] = $date_end;
 
             $this->load->view("common/header",$this->data);
             $this->load->view("cancelled_or_invoice_list",$this->data);
--- application/modules/finance/views/common/cancelled_or_invoice_list_conf.php
+++ application/modules/finance/views/common/cancelled_or_invoice_list_conf.php
@@ -7,7 +7,7 @@ $(document).ready(function() {
         "ordering": false,
         "paging": true,
         ajax: {
-            url: "<?php echo base_url(); ?>finance/finance_cancelled_or_invoice_data/<?php echo $current_sem; ?>/<?php echo $report_type; ?>/<?php echo $date; ?>",
+            url: "<?php echo base_url(); ?>finance/finance_cancelled_or_invoice_data",
             dataSrc: 'data'
         },
         columns: [{
@@ -43,54 +43,50 @@ $(document).ready(function() {
         }]
     });
 });
-$("#select-term-leads").on('change', function(e) {
-    let term = $(this).val();
-    let reportType = "<?php echo $report_type; ?>";
-    let currentDate = "<?php echo $date; ?>";
-    document.location = "<?php echo base_url()."finance/cancelled_or_invoice/"; ?>" + term +
-        '/' + reportType + '/' + currentDate;
+$("#date-picker-from").on('change', function(e) {
+    const dateFrom = $(this).val();
+    document.location = "<?php echo base_url()."finance/cancelled_or_invoice/"; ?>" +
+        dateFrom + '/' + $("#date-picker-to").val();
 });
-$("#date-picker").on('change', function(e) {
-    let term = "<?php echo $current_sem; ?>";
-    let reportType = "<?php echo $report_type; ?>";
-    const currentDate = $(this).val();
-    document.location = "<?php echo base_url()."finance/cancelled_or_invoice/"; ?>" + term +
-        '/' + reportType + '/' + currentDate;
-});
-$("#select-report-type").on('change', function(e) {
-    let term = "<?php echo $current_sem; ?>";
-    let currentDate = "<?php echo $date; ?>";
-    let reportType = $(this).val();
-    document.location = "<?php echo base_url()."finance/cancelled_or_invoice/"; ?>" + term +
-        '/' + reportType + '/' + currentDate;
+$("#date-picker-to").on('change', function(e) {
+    const dateFrom = new Date($("#date-picker-from").val());
+    const dateTo = new Date($(this).val());
+    if (dateFrom > dateTo) {
+        alert(
+            "The start date cannot be later than the end date. Please select a valid date range.");
+        return;
+    }
+    document.location = "<?php echo base_url()."finance/cancelled_or_invoice/"; ?>" + $(
+        "#date-picker-from").val() + '/' + $("#date-picker-to").val();
 });
 $(document).ready(function() {
     $("#cancelled_or_invoice_list_excel").click(function(e) {
-
         let api_url = "<?php echo $api_url ?>";
+        var campus = "<?php echo $campus;?>"
         // axios.get(api_url + 'sms/finance/voided-payment/' + $("#select-term-leads").val() + '/' + campus + '/' + $("#date-picker-from").val(); + '/' + $("#date-picker-to").val())
-        axios.get('http://cebuapi.iacademy.edu.ph/api/v1/sms/finance/voided-payment/28/Cebu/2024-01-01/2025-04-08')
-                .then((data) => {
-                    let payments = data.data.data;
-                    var campus = "<?php echo $campus;?>";
-                    var base_url = "<?php echo base_url(); ?>";
-                    // let url = base_url + 'excel/finance_cancelled_or_invoice/' + $("#select-term-leads").val() + '/' + campus + '/' + $("#date-picker-from").val(); + '/' + $("#date-picker-to").val();
-                    let url = base_url + 'excel/finance_cancelled_or_invoice/28/Cebu/2024-01-01/2025-04-08';
-                    
-                    var f = $("<form target='_blank' method='POST' style='display:none;'></form>").attr({
-                        action: url
-                    }).appendTo(document.body);
-                        $('<input type="hidden" />').attr({
-                            name: 'cancelled_payments',
-                            value: JSON.stringify(payments)
-                        }).appendTo(f);
-                    f.submit();
-                    f.remove();
-                })
-                .catch((error) => {
-                    console.log(error);
-                })
-
+        axios.get(`http://cebuapi.iacademy.edu.ph/api/v1/sms/finance/voided-payment/28/${campus}/${$(
+        "#date-picker-from").val()}/${$("#date-picker-to").val()}`).then((data) => {
+            let payments = data.data.data;
+            console.log(payments);
+            var campus = "<?php echo $campus;?>";
+            var base_url = "<?php echo base_url(); ?>";
+            // let url = base_url + 'excel/finance_cancelled_or_invoice/' + $("#select-term-leads").val() + '/' + campus + '/' + $("#date-picker-from").val(); + '/' + $("#date-picker-to").val();
+            let url = base_url + `excel/finance_cancelled_or_invoice/28/${campus}/${$(
+        "#date-picker-from").val()}/${$("#date-picker-to").val()}`;
+            var f = $(
+                "<form target='_blank' method='POST' style='display:none;'></form>"
+                ).attr({
+                action: url
+            }).appendTo(document.body);
+            $('<input type="hidden" />').attr({
+                name: 'cancelled_payments',
+                value: JSON.stringify(payments)
+            }).appendTo(f);
+            f.submit();
+            f.remove();
+        }).catch((error) => {
+            console.log(error);
+        })
     })
 });
 </script>
