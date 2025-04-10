5fe35299beb57cfc0bddec70b1798543b8c1d378
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2025-04-10 11:49:32 +0800

    Update Unity.php

--- application/modules/unity/controllers/Unity.php
+++ application/modules/unity/controllers/Unity.php
@@ -1375,7 +1375,8 @@ class Unity extends CI_Controller {
             else
                 $curriculum_units_na += $cs['strUnits'];
 
-            $equivalent_subjects = $this->db->get_where('tb_mas_equivalents',array('intSubjectID'=>$cs['intSubjectID']))->result_array();
+            $equivalent_subjects = $this->db->get_where('tb_mas_equivalents',array('intSubjectID'=>$cs['intSubjectID']))->result_array();            
+            $elective_subjects = $this->db->get_where('tb_mas_classlist_student_elective',array('elective_classlist_id'=>$cs['intSubjectID']))->result_array();            
             $recs = 
             $this->db->select('floatFinalGrade,strRemarks,tb_mas_subjects.strUnits,tb_mas_subjects.include_gwa,tb_mas_subjects.strCode,intFinalized')
                      ->join('tb_mas_classlist','tb_mas_classlist_student.intClassListID = tb_mas_classlist.intID')  
@@ -1383,20 +1384,35 @@ class Unity extends CI_Controller {
                      ->where(array('tb_mas_classlist.intSubjectID'=>$cs['intSubjectID'],'tb_mas_classlist_student.intStudentID'=>$data['student']['intID'],'tb_mas_classlist_student.strRemarks !='=>'Officially Withdrawn'))                     
                      ->get('tb_mas_classlist_student')
                      ->result_array();            
-            if(count($recs) == 0){
+            if(count($recs) == 0){ //EQUIVALENT
                 foreach($equivalent_subjects as $es){
                     $erecs = 
                     $this->db->select('floatFinalGrade,strRemarks,tb_mas_subjects.strUnits,tb_mas_subjects.include_gwa,tb_mas_subjects.strCode,intFinalized')
                             ->join('tb_mas_classlist','tb_mas_classlist_student.intClassListID = tb_mas_classlist.intID')  
                             ->join('tb_mas_subjects','tb_mas_classlist.intSubjectID = tb_mas_subjects.intID')                                              
-                            ->where(array('tb_mas_classlist.intSubjectID'=>$es['intSubjectID'],'tb_mas_classlist_student.intStudentID'=>$data['student']['intID'],'tb_mas_classlist_student.strRemarks !='=>'Officially Withdrawn'))                     
+                            ->where(array('tb_mas_classlist.intSubjectID'=>$es['intEquivalentID'],'tb_mas_classlist_student.intStudentID'=>$data['student']['intID'],'tb_mas_classlist_student.strRemarks !='=>'Officially Withdrawn'))                     
                             ->get('tb_mas_classlist_student')
                             ->result_array();        
                     if(count($erecs) > 0){
-                        $recs = $erecs;
-                        break;
+                        $recs = $erecs;                                                
+                        break;                        
                     }
                 }             
+            }
+            if(count($recs) == 0){ //ELECTIVE
+                foreach($elective_subjects as $es){
+                    $erecs = 
+                    $this->db->select('floatFinalGrade,strRemarks,tb_mas_subjects.strUnits,tb_mas_subjects.include_gwa,tb_mas_subjects.strCode,intFinalized')
+                            ->join('tb_mas_classlist','tb_mas_classlist_student.intClassListID = tb_mas_classlist.intID')  
+                            ->join('tb_mas_subjects','tb_mas_classlist.intSubjectID = tb_mas_subjects.intID')                                              
+                            ->where(array('tb_mas_classlist.intID'=>$es['subject_classlist_id'],'tb_mas_classlist_student.intStudentID'=>$data['student']['intID'],'tb_mas_classlist_student.strRemarks !='=>'Officially Withdrawn'))                     
+                            ->get('tb_mas_classlist_student')
+                            ->result_array();        
+                    if(count($erecs) > 0){
+                        $recs = $erecs;                                                
+                        break;                        
+                    }
+                }  
             }
             
             foreach($recs as $temp_rec){
