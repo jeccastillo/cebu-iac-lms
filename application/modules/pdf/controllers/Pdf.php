083948e1a4bab959b3c3807c3c41f32c38ab7720
Jp <jpestorque0@gmail.com>
2025-04-24 17:56:18 +0800

    Update Pdf.php

--- application/modules/pdf/controllers/Pdf.php
+++ application/modules/pdf/controllers/Pdf.php
@@ -1019,7 +1019,7 @@ class Pdf extends CI_Controller {
         $this->data['active_sem'] = $this->data_fetcher->get_sem_by_id($this->data['selected_ay']);
         $this->data['student'] = $this->data_fetcher->getStudent($id);
         $this->data['registration'] = $this->data_fetcher->getRegistrationInfo($id,$this->data['selected_ay']);
-        if($this->data['registration']['enumStudentType'] == "new"){
+        if($this->data['registration']['enumStudentType'] == "new" && $this->data['student']['level'] != 'next'){
             $this->data['snum_label'] = "APP NUMBER";
             $this->data['snum'] = "A".$this->data['active_sem']['strYearStart']."-".str_pad($app_id,4,"0",STR_PAD_LEFT);
         }
