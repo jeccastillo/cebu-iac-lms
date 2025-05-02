127b63feac65f2da9efbb5d1e38357ada8455d5a
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2025-05-02 11:28:22 +0800

    Update registration_viewer_new.php

--- application/modules/unity/views/admin/registration_viewer_new.php
+++ application/modules/unity/views/admin/registration_viewer_new.php
@@ -523,7 +523,7 @@
                                                     <td>{{ record.strClassName + record.year + record.strSection + (record.sub_section?record.sub_section:'') }}</td>
                                                     <td>{{ record.strCode }} <span v-if="record.elective_classlist_id">&nbsp;( {{ record.elective_subject.strCode }} )</span></td>
                                                     <td>{{ record.strUnits }}</td>
-                                                    <td v-if="intLab != 0">yes</td>
+                                                    <td v-if="intLab != '0'">yes</td>
                                                     <td v-else>no</td>
                                                 </tr>
                                             </tbody>
