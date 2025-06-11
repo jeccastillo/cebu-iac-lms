706ca96e6de4d28234bd984c28133b74c7d570bc
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2025-06-11 10:15:40 +0800

    Update registration_viewer.php

--- application/modules/unity/views/admin/registration_viewer.php
+++ application/modules/unity/views/admin/registration_viewer.php
@@ -1027,8 +1027,7 @@ new Vue({
                             }         
                             for(i in this.ledger_items){                                
                                 this.remaining_amount += parseFloat(this.ledger_items[i].amount);                                
-                                this.amount_paid += parseFloat(this.ledger_items[i].amount);
-                                console.log("payment "+i,parseFloat(this.ledger_items[i].amount));
+                                this.amount_paid += (this.ledger_items[i].amount *-1);                                
                                 if(this.ledger_items[i].amount < 0){
                                     this.ledger_items[i].type = "payment";
                                     this.ledger_items[i].amount = this.ledger_items[i].amount * -1;
