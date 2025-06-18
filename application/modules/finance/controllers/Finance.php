864bb10bb4b65a96da8063e981bf97a6c823e493
Jp <jpestorque0@gmail.com>
2025-06-18 13:50:20 +0800

    Update Finance.php

--- application/modules/finance/controllers/Finance.php
+++ application/modules/finance/controllers/Finance.php
@@ -1440,7 +1440,7 @@ class Finance extends CI_Controller {
 
         $results = $this->db
                     ->from('payment_details')
-                    ->where(array('status !=' => 'expired','status !=' => 'Transaction Failed','status !=' => 'cancel','status !=' => 'declined','status !=' => 'error', 'updated_at >=' => $report_date_start, 'updated_at <=' => $report_date_end, 'invoice_number !=' => null, 'deleted_at !=' => null))
+                    ->where(array('status !=' => 'expired','status !=' => 'Transaction Failed','status !=' => 'cancel','status !=' => 'declined','status !=' => 'error', 'DATE_FORMAT(or_date, "%Y-%m-%d 00:00:00") >=' => $report_date_start, 'DATE_FORMAT(or_date, "%Y-%m-%d 00:00:00") <=' => $report_date_end, 'invoice_number !=' => null, 'deleted_at !=' => null))
                     ->order_by('invoice_number', 'ASC')
                     ->get()
                     ->result_array();
