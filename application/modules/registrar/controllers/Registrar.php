83a3cd63bdb1b2d41102dfb662bded303faec615
Jp <jpestorque0@gmail.com>
2025-07-29 16:46:30 +0800

    Update Registrar.php

--- application/modules/registrar/controllers/Registrar.php
+++ application/modules/registrar/controllers/Registrar.php
@@ -699,7 +699,7 @@ class Registrar extends CI_Controller {
         $enrolled = $second_degree_iac = [];
         
         $second_degree_iac_applicants = json_decode($post['second_degree_iac']);
-        print_r(json_decode($second_degree_iac_applicants));
+        print_r($second_degree_iac_applicants);
         die();
         if($second_degree_iac_applicants){
             foreach($second_degree_iac_applicants as $applicant){
