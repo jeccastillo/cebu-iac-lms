46ae05496edd6457e8bb5b8416322f73ae70d0af
Jp <jpestorque0@gmail.com>
2025-07-31 10:55:36 +0800

    Update footer.php

--- application/views/common/footer.php
+++ application/views/common/footer.php
@@ -440,7 +440,7 @@ $(document).ready(function() {
                         done = false;
 
                         let checkboxHTML = '';
-                        if (ret.subjects[i].isTickable == 0) {
+                        if (ret.subjects[i].isTickable == 1) {
                             checkboxHTML = "<div class='col-xs-1'><div class='checkbox' style='margin:0;'>" +
                                         "<label><input type='checkbox' name='additional_elective[]' value='" + ret.subjects[i].intID + "' class='tickable-checkbox' data-index='" + i + "' >Additional Elective</label>" +
                                         "</div></div>";
