b734111e789dbf140c7bb172727bfdac48c9a665
Jp <jpestorque0@gmail.com>
2025-07-31 09:58:50 +0800

    Update Pdf.php

--- application/modules/pdf/controllers/Pdf.php
+++ application/modules/pdf/controllers/Pdf.php
@@ -469,7 +469,7 @@ class Pdf extends CI_Controller {
         $pdf->SetMargins(5, .25, 5);
         $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
         $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);        
-        $pdf->SetAutoPageBreak(TRUE, 6);
+        // $pdf->SetAutoPageBreak(TRUE, 3);
 
     //font setting
         //$pdf->SetFont('calibril_0', '', 15, '', 'false');
@@ -479,7 +479,7 @@ class Pdf extends CI_Controller {
         // print standard ASCII chars, you can use core fonts like
         // helvetica or times to reduce file size.
         
-        // $pdf->SetAutoPageBreak(true, PDF_MARGIN_FOOTER);
+        $pdf->SetAutoPageBreak(true, PDF_MARGIN_FOOTER);
         // Fit to one page wide (horizontally)
         $pdf->setPrintHeader(false);
         $pdf->setPrintFooter(false);         
