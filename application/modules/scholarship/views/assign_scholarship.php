374d28d630e5697110da4b00d950c5b7f88a787d
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2025-07-31 08:45:06 +0800

    Update assign_scholarship.php

--- application/modules/scholarship/views/assign_scholarship.php
+++ application/modules/scholarship/views/assign_scholarship.php
@@ -113,7 +113,7 @@
                                     <thead>
                                         <tr>
                                             <th>Discount</th>
-                                            <th>Referree</th>
+                                            <th>Referee</th>
                                             <th>Status</th>
                                             <th>Actions</th>
                                         </tr>
@@ -121,7 +121,7 @@
                                     <tbody>
                                         <tr v-for="item in student_discounts">
                                             <td>{{ item.name }}</td>
-                                            <td>{{ item.referrer }}</td>
+                                            <td>{{ item.referrer==undefined?'not applicable':item.referrer }}</td>
                                             <td>
                                                 <select class="form-control" @change="updateScStatus($event,item.id)">
                                                     <option :selected="item.status == 'applied'" value="applied">applied</option>
