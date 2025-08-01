3708ea352813e4eddf8c3c3e339d759d14146ce5
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2025-08-01 09:35:02 +0800

    Update assign_scholarship.php

--- application/modules/scholarship/views/assign_scholarship.php
+++ application/modules/scholarship/views/assign_scholarship.php
@@ -302,6 +302,7 @@ new Vue({
                 }
 
                 this.request_scholarship.syid = this.current_sem;
+                if(this.registration){      
                     axios.get(api_url + 'finance/transactions/' + this.student.slug + '/' + this.current_sem)
                     .then((data) => {
                         this.payments = data.data.data;
@@ -320,24 +321,65 @@ new Vue({
                                 this.remaining_amount = this.remaining_amount - this.payments[i].subtotal_order;
                                 this.amount_paid = this.amount_paid + this.payments[i].subtotal_order;
                             }                                
-                        }     
-                        if(this.registration){                   
-                            if(this.registration.enumStudentType == "new"){
-                                axios.get(api_url + 'finance/reservation/' + this.student.slug + '/' + this.current_sem)
-                                .then((data) => {
-                                    this.reservation_payments = data.data.data;    
-                                    this.application_payment = data.data.application;
-                                    
-                                    for(i in this.reservation_payments){
-                                        if(this.reservation_payments[i].status == "Paid" && data.data.student_sy == this.current_sem){
-                                                this.remaining_amount = this.remaining_amount - this.reservation_payments[i].subtotal_order;                                                                                                                                    
-                                                this.amount_paid = this.amount_paid + this.reservation_payments[i].subtotal_order;      
-                                                this.tuition_data.down_payment =  this.tuition_data.down_payment - this.reservation_payments[i].subtotal_order;
-                                        }
+                        }                                          
+                        if(this.registration.enumStudentType == "new"){
+                            axios.get(api_url + 'finance/reservation/' + this.student.slug + '/' + this.current_sem)
+                            .then((data) => {
+                                this.reservation_payments = data.data.data;    
+                                this.application_payment = data.data.application;
+                                
+                                for(i in this.reservation_payments){
+                                    if(this.reservation_payments[i].status == "Paid" && data.data.student_sy == this.current_sem){
+                                            this.remaining_amount = this.remaining_amount - this.reservation_payments[i].subtotal_order;                                                                                                                                    
+                                            this.amount_paid = this.amount_paid + this.reservation_payments[i].subtotal_order;      
+                                            this.tuition_data.down_payment =  this.tuition_data.down_payment - this.reservation_payments[i].subtotal_order;
                                     }
+                                }
+                                
+                                
+                                this.remaining_amount = (this.remaining_amount < 0.02) ? 0 : this.remaining_amount;                                
+                                this.remaining_amount_formatted = this.remaining_amount.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+                                //installment amounts                                
+                                if(this.registration.downpayment == 1){
+                                    var temp = (this.tuition_data.installment_fee * 5) - parseFloat(this.remaining_amount);
+                                    for(i=0; i < 5; i++){
+                                        if(this.tuition_data.installment_fee > temp){
+                                            val = this.tuition_data.installment_fee - temp;
+                                            val = val.toFixed(2);
+                                            this.installments.push(val);
+                                            temp = 0;
+                                        }
+                                        else{
+                                            this.installments.push(0);
+                                            temp = temp - this.tuition_data.installment_fee;
+                                        }
+                                    
+                                    }
+                                }
+                                else
+                                    for(i=0; i < 5; i++)
+                                        this.installments.push(this.tuition_data.installment_fee);                                                                                                                  
                                     
                                     
-                                    this.remaining_amount = (this.remaining_amount < 0.02) ? 0 : this.remaining_amount;                                
+                                
+                                
+                                
+                                var val = 0;                                
+                                
+
+                                this.amount_paid_formatted = this.amount_paid.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');                                                                
+                                this.loader_spinner = false;
+                                if(this.remaining_amount <= 0)
+                                    this.description = "Other";
+
+                                
+                            })
+                            .catch((error) => {
+                                console.log(error);
+                            })
+                        }
+                        else{
+                                this.remaining_amount = (this.remaining_amount < 0.02) ? 0 : this.remaining_amount;                                
                                     this.remaining_amount_formatted = this.remaining_amount.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
                                     //installment amounts                                
                                     if(this.registration.downpayment == 1){
@@ -360,9 +402,7 @@ new Vue({
                                         for(i=0; i < 5; i++)
                                             this.installments.push(this.tuition_data.installment_fee);                                                                                                                  
                                         
-                                        
-                                    
-                                    
+                                                                                                        
                                     
                                     var val = 0;                                
                                     
@@ -371,52 +411,13 @@ new Vue({
                                     this.loader_spinner = false;
                                     if(this.remaining_amount <= 0)
                                         this.description = "Other";
-
-                                    
-                                })
-                                .catch((error) => {
-                                    console.log(error);
-                                })
-                            }
-                            else{
-                                    this.remaining_amount = (this.remaining_amount < 0.02) ? 0 : this.remaining_amount;                                
-                                        this.remaining_amount_formatted = this.remaining_amount.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-                                        //installment amounts                                
-                                        if(this.registration.downpayment == 1){
-                                            var temp = (this.tuition_data.installment_fee * 5) - parseFloat(this.remaining_amount);
-                                            for(i=0; i < 5; i++){
-                                                if(this.tuition_data.installment_fee > temp){
-                                                    val = this.tuition_data.installment_fee - temp;
-                                                    val = val.toFixed(2);
-                                                    this.installments.push(val);
-                                                    temp = 0;
-                                                }
-                                                else{
-                                                    this.installments.push(0);
-                                                    temp = temp - this.tuition_data.installment_fee;
-                                                }
-                                            
-                                            }
-                                        }
-                                        else
-                                            for(i=0; i < 5; i++)
-                                                this.installments.push(this.tuition_data.installment_fee);                                                                                                                  
-                                            
-                                                                                                            
-                                        
-                                        var val = 0;                                
-                                        
-
-                                        this.amount_paid_formatted = this.amount_paid.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');                                                                
-                                        this.loader_spinner = false;
-                                        if(this.remaining_amount <= 0)
-                                            this.description = "Other";
-                                }
                         }
+                        
                     })
                     .catch((error) => {
                         console.log(error);
                     })  
+                }
             
         })
         .catch((error) => {
