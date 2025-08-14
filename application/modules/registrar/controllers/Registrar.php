97d725e429c20adf08afde930d2a78cf69135b84
Jp <jpestorque0@gmail.com>
2025-08-14 10:21:46 +0800

    Update Registrar.php

--- application/modules/registrar/controllers/Registrar.php
+++ application/modules/registrar/controllers/Registrar.php
@@ -843,7 +843,7 @@ class Registrar extends CI_Controller {
             $active_sem = $this->data_fetcher->get_sem_by_id($sem);
             $this->data['sem'] = $active_sem['intID'];
         }        
-        $this->data['pdf_link'] = base_url()."pdf/enrollment_summary_by_student_number/".$this->data['sem'];
+        $this->data['pdf_link'] = base_url()."pdf/enrollment_summary/".$this->data['sem'];
         $this->data['excel_link'] = base_url()."excel/enrollment_summary/".$this->data['sem'];
 
         
