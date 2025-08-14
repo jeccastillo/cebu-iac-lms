6164dc40ca168e669ede4471e7cd7fc055ae1058
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2025-08-14 13:49:42 +0800

    Update Excel.php

--- application/modules/excel/controllers/Excel.php
+++ application/modules/excel/controllers/Excel.php
@@ -1675,11 +1675,11 @@ class Excel extends CI_Controller {
             ->setCellValue('D'.$i, $classlist['subjectDescription'])            
             ->setCellValue('E'.$i, $classlist['strUnits'])            
             ->setCellValue('I'.$i, $classlist['slots_taken_enrolled']);
-            $ctr = 1;
+            $ctr = 0;
             foreach($classlist['schedule'] as $sched){
                 if(isset($sched['strDay'])){
                     $objPHPExcel->setActiveSheetIndex(0)                            
-                    ->setCellValue('F'.$i, $sched['strDayAbvr'])
+                    ->setCellValue('F'.$i, $sched['strDayAbvr']." ".count($classlist['schedule']))
                     ->setCellValue('G'.$i, date('g:ia',strtotime($sched['dteStart'])).' - '.date('g:ia',strtotime($sched['dteEnd'])))
                     ->setCellValue('H'.$i, $sched['strRoomCode']);                    
                     if($ctr < count($classlist['schedule']))
