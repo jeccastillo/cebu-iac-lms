b27454a644843b5f0b97b8f23d5e07780148f545
Jp <jpestorque0@gmail.com>
2025-08-14 13:37:47 +0800

    Update Excel.php

--- application/modules/excel/controllers/Excel.php
+++ application/modules/excel/controllers/Excel.php
@@ -2488,7 +2488,7 @@ class Excel extends CI_Controller {
             foreach($students as $student)
             {
                 $enrollmentStatus = $student['student_type'];
-                $yearLevel = $student['intStudentYear'];
+                $yearLevel = $student['intYearLevel'];
                 // Add some data
 
                 if($type == 'shs'){
@@ -2503,7 +2503,7 @@ class Excel extends CI_Controller {
                     else
                         $enrollmentStatus = 'New';
 
-                    if($student['intStudentYear'] == 1 || $student['intStudentYear'] == 3){
+                    if($student['intYearLevel'] == 1 || $student['intYearLevel'] == 3){
                         $yearLevel = '11';
                     }else{
                         $yearLevel = '12';
