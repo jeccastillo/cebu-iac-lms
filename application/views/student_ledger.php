b6951bb117484e1c2419af0ad98391eb2c382217
Jp <jpestorque0@gmail.com>
2025-08-26 08:59:09 +0800

    Update student_ledger.php

--- application/views/student_ledger.php
+++ application/views/student_ledger.php
@@ -500,7 +500,6 @@ new Vue({
             
             var balance = tuition.balance;            
             
-            console.log(payments);
             for(i in payments){                       
                 var paid = payments[i].subtotal_order * -1;
                 this.term_balance += paid;
