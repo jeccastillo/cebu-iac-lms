106e18c332e5310e1f200b4a08e8c1f5af231a36
Jp <jpestorque0@gmail.com>
2025-09-05 17:14:47 +0800

    Update Finance.php

--- application/modules/finance/controllers/Finance.php
+++ application/modules/finance/controllers/Finance.php
@@ -1124,6 +1124,7 @@ class Finance extends CI_Controller {
         $data['cashier'] = $this->db->get_where('tb_mas_cashier',array('user_id'=>$this->data['user']['intID']))->first_row();
         $data['user'] = $this->data['user'];
         $sem = $this->data_fetcher->get_sem_by_id($sem);        
+        $data['finance_manager_privilages'] = ($role == 2)?true:false;
         $data['payee'] = $this->db->get_where('tb_mas_ns_payee',array('id'=>$payee))->first_row('array');
         $data['current_sem'] = $sem['intID'];
         $data['sem_year'] = $sem['strYearStart'];
