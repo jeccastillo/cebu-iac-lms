9eeb8d74adab2ae422fe3ed4982fb83ac40bd4e8
Jp <jpestorque0@gmail.com>
2025-09-29 13:30:14 +0800

    Update Pdf.php

--- application/modules/pdf/controllers/Pdf.php
+++ application/modules/pdf/controllers/Pdf.php
@@ -4357,8 +4357,6 @@ class Pdf extends CI_Controller {
         $this->data['running_balance'] = $post['running_balance'];
         $this->data['running_balance_other'] = $post['running_balance_other'];
         
-        print_r($this->data);
-        die();
         // Initialize TCPDF
         tcpdf();
         
