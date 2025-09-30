9adf8f96c4b9c0327a1e4a2cb49a91095ba344b3
Jp <jpestorque0@gmail.com>
2025-09-30 16:04:09 +0800

    Update Excel.php

--- application/modules/excel/controllers/Excel.php
+++ application/modules/excel/controllers/Excel.php
@@ -8392,13 +8392,14 @@ class Excel extends CI_Controller {
                         if(isset($facultyName[1])){
                             $facultyName = explode(' ', ltrim($facultyName[1]));
                             $facultyFirstName = $facultyName[0];
-                            print_r($facultyName);
-                            print($facultyFirstName);
                         }
                         
                         $faculty = $this->db->from('tb_mas_faculty')->like(array('strLastname' => $facultyLastName, 'strFirstName' => $facultyFirstName))->get()->first_row('array');
                         $subject = $this->db->get_where('tb_mas_subjects',array('strCode' => $row['G']))->first_row('array');
                         
+                        print_r($faculty);
+                        print_r($subject);
+
                         if($faculty && $subject){
                             $classlistID = '';
 
