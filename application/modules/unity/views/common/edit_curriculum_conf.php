c09bef90ac2c38ad112bcd1a207f226c4eefff5f
ronngb <ronny.gabi@gmail.com>
2025-10-09 15:26:20 +0800

    update edit curriculum

--- application/modules/unity/views/common/edit_curriculum_conf.php
+++ application/modules/unity/views/common/edit_curriculum_conf.php
@@ -1,14 +1,17 @@
 <script type="text/javascript">
-    
-    $(document).ready(function(){
-        
-        $(function () {
-          $('[data-toggle="popover"]').popover({
-              trigger:'hover'
-              
-          })
-        });
-    
+$(document).ready(function() {
+    let equivalentSubjectID = document.getElementById('equivalent')
+    $(function() {
+        $('[data-toggle="popover"]').popover({
+            trigger: 'hover'
+
+        })
     });
-    
+
+    $("#type").on('change', function(e) {
+        let isHidden = $(this).val() != 'Equivalent'
+        equivalentSubjectID.hidden = isHidden
+    });
+
+});
 </script>
