dae18a2d7589562ec868a9dc72a0e37b4b03fcf3
Jp <jpestorque0@gmail.com>
2025-10-13 15:12:22 +0800

    Update edit_curriculum_conf.php

--- application/modules/unity/views/common/edit_curriculum_conf.php
+++ application/modules/unity/views/common/edit_curriculum_conf.php
@@ -1,6 +1,8 @@
 <script type="text/javascript">
 $(document).ready(function() {
     let equivalentSubjectID = document.getElementById('equivalent')
+    let combineSubject = document.getElementById('combineSubject')
+    
     $(function() {
         $('[data-toggle="popover"]').popover({
             trigger: 'hover'
@@ -14,7 +16,7 @@ $(document).ready(function() {
     });
 
     $("#type").on('change', function(e) {
-        let isHidden = $(this).val() != 'Combine Subjects'
+        let isHidden = $(this).val() != 'Combine'
         combineSubject.hidden = isHidden
     });
 });
