d6ba39680125c4dff998b50798dceb6b8aff4e72
Jp <jpestorque0@gmail.com>
2025-10-16 15:46:05 +0800

    Update Data_fetcher.php

--- application/models/Data_fetcher.php
+++ application/models/Data_fetcher.php
@@ -595,7 +595,7 @@ class Data_fetcher extends CI_Model {
                          ->select('tb_mas_curriculum_second.intID,subj1.strCode,subj1.strUnits,subj1.intID as intSubjectID,subj1.strDescription,subj1.intLab, subj1.intLectHours,subj1.strUnits,subj1.include_gwa,tb_mas_curriculum_second.type, subj2.strCode as strCodeEquivalent')
                          ->from('tb_mas_curriculum_second')
                          ->join('tb_mas_subjects subj1','subj1.intID = tb_mas_curriculum_second.intSubjectID')
-                         ->leftJoin('tb_mas_subjects subj2','subj2.intID = tb_mas_curriculum_second.equivalentSubjectID')
+                         ->join('tb_mas_subjects subj2','subj2.intID = tb_mas_curriculum_second.equivalentSubjectID', 'left')
                          ->where('tb_mas_curriculum_second.intCurriculumID',$id)
                          ->order_by('subj1.strCode asc')
                          ->get()
