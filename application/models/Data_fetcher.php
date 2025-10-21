2df8ba3f0e52c7f99158f5690bed468a19d84d6f
Jp <jpestorque0@gmail.com>
2025-10-21 11:16:21 +0800

    Update Data_fetcher.php

--- application/models/Data_fetcher.php
+++ application/models/Data_fetcher.php
@@ -3517,6 +3517,8 @@ class Data_fetcher extends CI_Model {
                         print($tuition_scholarship_installment_current50 . ' @ ');
                         print($tuition_discount_installment50 . ' @ ');
                         print($total_assessment_rate_discount_installment50 . ' @ ');
+                        print($scholarship_installment_grand_total50 . ' @ ');
+                        print(($tuition + ($tuition * 0.09)) . ' @ ');
                         die();
                         //check if discount is external or in-house
                         if($scholar->deduction_from == 'external'){
