f9ff37500da776ad173ef421e82472b7a2bf37f6
Jp <jpestorque0@gmail.com>
2025-10-24 14:13:27 +0800

    Update Data_fetcher.php

--- application/models/Data_fetcher.php
+++ application/models/Data_fetcher.php
@@ -3511,12 +3511,6 @@ class Data_fetcher extends CI_Model {
                 else{
                     if($scholar->tuition_fee_rate > 0){                        
                         $tuition_scholarship_current = ($tuition - $in_house_grand_total - $tuition_discount_full) * ($scholar->tuition_fee_rate/100);
-                        if($scholar->tuition_fee_rate != 10){
-                            print_r($tuition_discount_full . ' @@@ ');
-                            print($tuition_scholarship_current . ' @@@ ');
-                            print_r($scholar);
-                            die();
-                        }
                         // $tuition_scholarship_installment_current = ($tuition + ($tuition * ($tuition_year['installmentIncrease']/100)) - $scholarship_installment_grand_total) * ($scholar->tuition_fee_rate/100);
                         // $tuition_scholarship_installment_current30 = ($tuition + ($tuition * 0.15) - $scholarship_installment_grand_total30) * ($scholar->tuition_fee_rate/100);
                         // $tuition_scholarship_installment_current50 = ($tuition + ($tuition * 0.09) - $scholarship_installment_grand_total50) * ($scholar->tuition_fee_rate/100);
