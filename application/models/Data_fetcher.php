52e0dc968e3fe1bc562b9480514b967ee8676af9
Jp <jpestorque0@gmail.com>
2025-10-24 14:50:50 +0800

    Update Data_fetcher.php

--- application/models/Data_fetcher.php
+++ application/models/Data_fetcher.php
@@ -3089,7 +3089,7 @@ class Data_fetcher extends CI_Model {
                         }
 
                         //get tuition percentage discount in scholar fixed
-                        $tuition_discount_full += $total_scholarship_temp * ($tuition / $scholar->total_assessment_rate/100);
+                        $tuition_discount_full += $total_scholarship_temp * ($tuition / $scholar->total_assessment_rate);
                         $tuition_discount_installment += $total_scholarship_temp * ($tuition + ($tuition * ($tuition_year['installmentIncrease']/100)));
                         $tuition_discount_installment30 += $total_scholarship_temp * ($tuition + ($tuition * 0.15));
                         $tuition_discount_installment50 += $total_scholarship_temp * ($tuition + ($tuition * 0.09));
