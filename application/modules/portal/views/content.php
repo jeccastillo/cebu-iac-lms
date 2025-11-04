3ba4899ff8ff6cbef1625adf13d1922be9db0a99
Jp <jpestorque0@gmail.com>
2025-11-04 09:26:47 +0800

    Update content.php

--- application/modules/portal/views/content.php
+++ application/modules/portal/views/content.php
@@ -2,7 +2,7 @@
     error_reporting(0);
 ?>
 
-<aside class="right-side" id="registration-container">
+<aside class="right-side">
 <section class="content-header">
             <h1>
                         My Grades
@@ -116,33 +116,59 @@
         <?php } ?>
 
         <?php else:  ?>
-        <div class="table-responsive" id="grades-container">
+        <div class="table-responsive">
             <table class="table table-striped">
                 <thead>
                     <tr>
                         <th width="13%">Section</th>
                         <th width="10%"> Course Code</th>
                         <th >Course Title</th>
-                        <th style="text-align: center;">Units</th>
-                        <th>Midterm</th>
-                        <th>Final Grade</th>
+                        <th style="text-align: center;">Units</th>                        
+                        <th>Midterm</th>                        
+                        <th>Final Grade</th>                                                
 						<th>Remarks</th>
                         <th>Faculty</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <template v-for="item in combined_subjects">
-                        <tr style="font-size: 13px;">
-                            <td></td>
-                            <td>{{ item.combineCode }}</td>
-                            <td>{{ item.combineDesc }}</td>
-                            <td>{{ item.intSubjectID }}</td>
-                            <td>---</td>
-                            <td>---</td>
-                            <td>Combined</td>
-                            <td>---</td>
-                        </tr>
-                    </template>
+                    <?php 
+                    $totalUnits = 0;
+                    $countBridg = 0;
+                    foreach($records as $record): ?>
+                    <tr>                        
+                        <td><?php echo $record['strClassName'].$record['year'].$record['strSection']." ".$record['sub_section']; ?></td>
+                        <td><?php echo $record['strCode']; ?></td>
+						<td><?php echo $record['strDescription']; ?></td>
+                        <td style="text-align: center;"><?php echo $record['strUnits']?>                                                
+                        <?php if($record['intFinalized'] >= 1 && $sem_selected->viewing_midterm_start <= date("Y-m-d")): ?>                                
+                            <td><strong><?php echo $record['v2']; ?></strong></td>
+                        <?php else: ?>
+                            <td>Not Yet Available</td>
+                        <?php endif; ?>    
+                        <?php if($record['intFinalized'] >= 2 && $sem_selected->viewing_final_start <= date("Y-m-d") && count($deficiencies) == 0): ?>                                
+                            <td><strong><?php echo $record['v3']; ?></strong></td>
+                        <?php else: ?>
+                            <td>Not Yet Available</td>
+                        <?php endif; ?>                                                        
+                        <td>
+                            <?php if($record['intFinalized'] >= 2 && $sem_selected->viewing_final_start <= date("Y-m-d") && count($deficiencies) == 0): ?> 
+                                <?php echo $record['strRemarks']; ?>
+                            <?php else: ?>
+                                -
+                            <?php endif; ?>                            
+                        </td>
+                        <td><?php if($record['strFirstname']!="unassigned"){
+                                    $firstNameInitial = substr($record['strFirstname'], 0,1);
+                                    echo $firstNameInitial.". ".$record['strLastname'];  
+                                  }
+                                  else echo "unassigned";  ?>
+                        </td>
+                        
+                    </tr>
+                    <?php endforeach; ?>
+                       
+                        
+        
                 </tbody>
             </table>
         </div>
@@ -190,245 +216,3 @@
 
 </div>
 
-
-<script src="<?php echo base_url(); ?>assets/themes/default/js/jquery.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/themes/default/js/script.js"></script>
-<script src="<?php echo base_url(); ?>assets/themes/default/js/vue.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js"
-    integrity="sha512-WFN04846sdKMIP5LKNphMaWzU7YpMyCU245etK3g/2ARYbPK9Ub18eG+ljU96qKRCWh+quCY7yefSmlkQw1ANQ=="
-    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="<?php echo base_url(); ?>assets/themes/default/js/axios.min.js"></script>
-
-<script>
-var special = ['0th','1st', '2nd', '3rd', '4th', '5th'];
-
-function stringifyNumber(n) {
-  return special[n];
-  
-  
-}
-function inArray(needle, haystack) {
-    var length = haystack.length;
-    for(var i = 0; i < length; i++) {
-        if(haystack[i] == needle) return true;
-    }
-    return false;
-}
-new Vue({
-    el: '#registration-container',
-    data: {
-        id: '<?php echo $id; ?>',    
-        base_url: '<?php echo base_url(); ?>',
-        slug: undefined,
-        student:undefined,         
-        records: [],         
-        gwa: undefined,
-        curriculum_subjects: [],
-        combined_subjects: [],
-        deficiencies: [],
-        subjects: [],
-        units: undefined,
-        assessment_gwa: undefined,  
-        balance: 0,
-        assessment_units: undefined, 
-        applicant_data: undefined,  
-        credited_subjects: [],
-        change_grades: [],
-        generated_tor:[],
-        credited_units: 0,
-        curriculum_units: 0,
-        curriculum_units_na: 0,
-        units_left: 0,
-
-    },
-
-    mounted() {
-
-        let url_string = window.location.href;        
-        if(this.id != 0){            
-            //this.loader_spinner = true;
-            axios.get(this.base_url + 'unity/student_records_data/' + this.id + '/')
-                .then((data) => {                                          
-                    this.student = data.data.student;
-                    this.credited_units = data.data.credited_units;
-                    this.curriculum_units = data.data.curriculum_units;
-                    this.curriculum_units_na = data.data.curriculum_units_na;                    
-                    this.units_left = data.data.units_left;
-                    this.generated_tor =  data.data.generated_tor;
-                    this.change_grades = data.data.change_grades;
-                    this.credited_subjects =  data.data.credited_subjects;
-                    this.records = data.data.data;                            
-                    this.balance = data.data.balance;
-                    this.subjects = data.data.all_subjects;
-                    this.deficiencies = data.data.deficiencies;
-                    this.curriculum_subjects = data.data.curriculum_subjects; 
-                    this.combined_subjects = data.data.combined_subjects;
-                    this.gwa = data.data.gwa;
-                    this.units = data.data.total_units_earned;  
-                    this.assessment_gwa = data.data.assessment_gwa; 
-                    this.assessment_units = data.data.assessment_units;
-                    for(i in this.records){
-                        switch(this.records[i].reg.intROG){
-                            case '0': 
-                                this.records[i].reg.enrollment_status = "Enlisted";
-                                this.records[i].reg.color = "box-default";
-                            break;
-                            case '1': 
-                                this.records[i].reg.enrollment_status = "Enrolled";
-                                this.records[i].reg.color = "box-success";
-                            break;
-                            case '2': 
-                                this.records[i].reg.enrollment_status = "Cleared";
-                                this.records[i].reg.color = "box-success";
-                            break;
-                            case '3': 
-                                this.records[i].reg.enrollment_status = "Officially Withdrawn";
-                                this.records[i].reg.color = "box-warning";
-                            break;
-                            case '4': 
-                                this.records[i].reg.enrollment_status = "LOA";
-                                this.records[i].reg.color = "box-info";
-                            break;
-                            case '5': 
-                                this.records[i].reg.enrollment_status = "AWOL";
-                                this.records[i].reg.color = "box-danger";
-                            break;
-                            default:
-                                this.records[i].reg.color = "box-default";
-                                this.records[i].reg.enrollment_status = "None";
-
-                        }
-                    }
-                    axios.get(api_url + 'admissions/student-info/' + this.student.slug)
-                    .then((data) => {
-                        this.applicant_data = data.data.data;
-                        for(i in this.applicant_data.uploaded_requirements){
-                                if(this.applicant_data.uploaded_requirements[i].type == "2x2" || this.applicant_data.uploaded_requirements[i].type == "2x2_foreign")
-                                this.tor.picture = this.applicant_data.uploaded_requirements[i].path;
-                        }
-                        this.tor.admission_date = this.applicant_data.date_enrolled;
-                    })
-                    .catch((error) => {
-                        console.log(error);
-                    })
-                })
-                .catch((error) => {
-                    console.log(error);
-                })
-        }
-
-    },
-
-    methods: {
-        getRecordsWithCombined: function(term) {
-            if (!Array.isArray(this.combined_subjects)) {
-                let flattened = [];
-                for (let key in this.combined_subjects) {
-                    if (Array.isArray(this.combined_subjects[key])) {
-                        flattened = flattened.concat(this.combined_subjects[key]);
-                    }
-                }
-                this.combined_subjects = flattened;
-            }
-            if (!Array.isArray(this.combined_subjects) || this.combined_subjects.length === 0) {
-                return term.records.map(record => ({type: 'record', data: record}));
-            }
-            let displayedCombined = new Set();
-            let result = [];
-            for (let record of term.records) {
-                let combined = this.combined_subjects.find(c => c.intSubjectID == record.intSubjectID);
-                if (combined && !displayedCombined.has(combined.combineCode + '|' + combined.combineDesc)) {
-                    result.push({type: 'combined', data: combined});
-                    displayedCombined.add(combined.combineCode + '|' + combined.combineDesc);
-                }
-                result.push({type: 'record', data: record});
-            }
-            return result;
-        },
-        getCurriculumRecords: function(term) {
-            if (!Array.isArray(this.combined_subjects)) {
-                let flattened = [];
-                for (let key in this.combined_subjects) {
-                    if (Array.isArray(this.combined_subjects[key])) {
-                        flattened = flattened.concat(this.combined_subjects[key]);
-                    }
-                }
-                this.combined_subjects = flattened;
-            }
-            if (!Array.isArray(this.combined_subjects) || this.combined_subjects.length === 0) {
-                return term.records.map(item => ({type: 'item', data: item}));
-            }
-            let displayedCombined = new Set();
-            let result = [];
-            for (let item of term.records) {
-                let combined = this.combined_subjects.find(c => c.intSubjectID == item.intSubjectID);
-                if (combined && !displayedCombined.has(combined.combineCode + '|' + combined.combineDesc)) {
-                    result.push({type: 'combined', data: combined});
-                    displayedCombined.add(combined.combineCode + '|' + combined.combineDesc);
-                }
-                result.push({type: 'item', data: item});
-            }
-            return result;
-        },
-        getTotalUnits: function(term, subjectID) {
-            let total = 0;
-            for (let record of term.records) {
-                if (record.intSubjectID == subjectID) {
-                    total += parseFloat(record.strUnits) || 0;
-                }
-            }
-            return total.toFixed(1);
-        },
-        getAverageMidterm: function(term, subjectID) {
-            let grades = [];
-            for (let record of term.records) {
-                if (record.intSubjectID == subjectID && record.v2 && record.v2 != 'OW' && record.intFinalized >= 1) {
-                    grades.push(parseFloat(record.v2) || 0);
-                }
-            }
-            if (grades.length === 0) return '---';
-            let avg = grades.reduce((a, b) => a + b, 0) / grades.length;
-            return Math.round(avg);
-        },
-        getAverageFinal: function(term, subjectID) {
-            let grades = [];
-            for (let record of term.records) {
-                if (record.intSubjectID == subjectID && record.v3 && record.v3 != 'OW' && record.intFinalized >= 2) {
-                    grades.push(parseFloat(record.v3) || 0);
-                }
-            }
-            if (grades.length === 0) return '---';
-            let avg = grades.reduce((a, b) => a + b, 0) / grades.length;
-            return Math.round(avg);
-        },
-        getAverageSemFinal: function(term, subjectID) {
-            if (this.student.type != 'shs') return '---';
-            let grades = [];
-            for (let record of term.records) {
-                if (record.intSubjectID == subjectID && record.semFinalGrade) {
-                    grades.push(parseFloat(record.semFinalGrade) || 0);
-                }
-            }
-            if (grades.length === 0) return '---';
-            let avg = grades.reduce((a, b) => a + b, 0) / grades.length;
-            return Math.round(avg);
-        },
-        getAverageGradeCurriculum: function(term, subjectID) {
-            let grades = [];
-            for (let record of term.records) {
-                if (record.intSubjectID == subjectID) {
-                    if (record.equivalent && record.equivalent.grade && record.equivalent.grade != 'OW') {
-                        grades.push(parseFloat(record.equivalent.grade) || 0);
-                    } else if (record.rec && record.rec.floatFinalGrade && record.rec.floatFinalGrade != 'OW') {
-                        grades.push(parseFloat(record.rec.floatFinalGrade) || 0);
-                    }
-                }
-            }
-            if (grades.length === 0) return '---';
-            let avg = grades.reduce((a, b) => a + b, 0) / grades.length;
-            return Math.round(avg);
-        },
-    }
-
-})
-</script>
