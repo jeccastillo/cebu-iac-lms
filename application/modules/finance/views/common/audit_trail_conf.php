126c7018f58c8f913de429788b9126c0bfd38a5a
ronngb <ronny.gabi@gmail.com>
2025-11-26 14:45:02 +0800

    add audit trail page

---
+++ application/modules/finance/views/common/audit_trail_conf.php
@@ -0,0 +1,28 @@
+<script type="text/javascript">
+$(document).ready(function() {
+    $('#audit_trail_table').DataTable({
+        "aLengthMenu": [10, 20, 50, 100, 250, 500, 750, 1000],
+        "bProcessing": true,
+        "serverSide": false,
+        "ordering": false,
+        "paging": true,
+        ajax: {
+            url: "<?php echo base_url(); ?>api/v1/sms/audit-trail",
+            dataSrc: 'data'
+        },
+        columns: [{
+            data: 'id',
+            title: 'ID'
+        }, {
+            data: 'query',
+            title: 'SQL Query'
+        }, {
+            data: 'user',
+            title: 'USER'
+        }, {
+            data: 'timestamp',
+            title: 'TIMESTAMP'
+        }]
+    });
+});
+</script>
