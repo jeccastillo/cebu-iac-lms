ebcc721e3aadcfcf50e8115bc58aa95d367c3fb4
Jp <jpestorque0@gmail.com>
2025-12-02 15:53:19 +0800

    Update Excel.php

--- application/modules/excel/controllers/Excel.php
+++ application/modules/excel/controllers/Excel.php
@@ -11325,8 +11325,6 @@ class Excel extends CI_Controller {
                     ->get()
                     ->result_array();
 
-        print_r($adjustments);
-        die();
         error_reporting(E_ALL);
         ini_set('display_errors', TRUE);
         ini_set('display_startup_errors', TRUE);
