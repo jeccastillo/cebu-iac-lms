91d35811c4fae7232ba98b478f51e81954a84063
Jp <jpestorque0@gmail.com>
2025-12-03 11:13:22 +0800

    Update register_old_student.php

--- application/modules/registrar/views/admin/register_old_student.php
+++ application/modules/registrar/views/admin/register_old_student.php
@@ -67,7 +67,7 @@
                                     <option value="2nd Degree iAC">2nd Degree iAC</option>                                 
                                     <option value="returning">Returnee</option>
                                     <option value="transferee">Transferee</option>
-                                    <option value="academic Residency">Academic Residency</option>
+                                    <option value="acadRes">Academic Residency</option>
                                 </select>
                                 <br /> 
                                 <label for="enumStudentType">Internship</label>
