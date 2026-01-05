e578e73fe47f8eabf20abb31400b4aeb1645a6c8
Jp <jpestorque0@gmail.com>
2026-01-05 15:15:24 +0800

    Update Data_fetcher.php

--- application/models/Data_fetcher.php
+++ application/models/Data_fetcher.php
@@ -3582,7 +3582,10 @@ class Data_fetcher extends CI_Model {
                         $tuition_scholarship_current = ($tuition - $in_house_grand_total - $tuition_discount_full) * ($scholar->tuition_fee_rate/100);
                         $tuition_scholarship_installment_current = ($tuition + ($tuition * ($tuition_year['installmentIncrease']/100)) - $scholarship_installment_grand_total) * ($scholar->tuition_fee_rate/100);
                         $tuition_scholarship_installment_current30 = ($tuition + ($tuition * 0.15) - $scholarship_installment_grand_total30) * ($scholar->tuition_fee_rate/100);
-                        $tuition_scholarship_installment_current50 = ($tuition + ($tuition * 0.09) - ((($tuition + ($tuition * 0.09)) / $total_assessment_temp50) * $scholarship_installment_grand_total50)) * ($scholar->tuition_fee_rate/100);
+                        $tuition_scholarship_installment_current50 = ($tuition + ($tuition * 0.09) - $scholarship_installment_grand_total50) * ($scholar->tuition_fee_rate/100);
+                        
+                        // $tuition_scholarship_installment_current50 = ($tuition + ($tuition * 0.09) - ((($tuition + ($tuition * 0.09)) / $total_assessment_temp50) * $scholarship_installment_grand_total50)) * ($scholar->tuition_fee_rate/100);
+
                         // print($tuition_scholarship_installment_current50 . ' @ ');
                         // print($tuition . ' @ ');$total_assessment_temp50 
                         // print($scholarship_installment_grand_total50 . ' @ ');
