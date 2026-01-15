022dac30a6adc7cc8383dbe452f115a8c05d3000
Jp <jpestorque0@gmail.com>
2026-01-15 12:01:20 +0800

    Update content.php

--- application/modules/portal/views/content.php
+++ application/modules/portal/views/content.php
@@ -2,7 +2,7 @@
     error_reporting(0);
 ?>
 
-<aside class="right-side">
+<aside class="right-side" id="registration-container">
 <section class="content-header">
             <h1>
                         My Grades
