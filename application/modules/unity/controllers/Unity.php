f3cede6f0ff10ed2ac05cb5b9cfaefc5401d7028
Jp <jpestorque0@gmail.com>
2026-01-15 11:32:57 +0800

    Update Unity.php

--- application/modules/unity/controllers/Unity.php
+++ application/modules/unity/controllers/Unity.php
@@ -4303,7 +4303,7 @@ class Unity extends CI_Controller {
         $document['studentID'] = $post['studentID'];
         $document['dateSubmitted'] = $post['dateSubmitted'];
         $document['user'] = $post['user'];
-        $document['remarks'] = $post['remarks'];
+        $document['remarks'] = isset($post['remarks']) ? $post['remarks'] : '';
         $document['created_at'] = date("Y-m-d H:i:s");
         
         $this->data_poster->post_data('tb_mas_student_documents',$document);
