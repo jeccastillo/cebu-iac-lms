47b28ff5da1de311a7e0163daecdd7f3366055eb
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2026-01-23 13:55:16 +0800

    Update student_viewer.php

--- application/modules/unity/views/admin/student_viewer.php
+++ application/modules/unity/views/admin/student_viewer.php
@@ -739,12 +739,14 @@
     display: block !important;
 }
 </style>
+<!-- jQuery (required for Select2) -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <!-- Select2 CSS -->
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<!-- Select2 JS (after jQuery) -->
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/themes/default/js/vue.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/themes/default/js/script.js">
-</script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/themes/default/js/script.js"></script>
 <script>
 new Vue({
     el: '#student-viewer-container',
