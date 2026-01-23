4686b0cd5a68a3b6539a43ea160b50d3d1514a71
Jp <jpestorque0@gmail.com>
2026-01-23 11:35:23 +0800

    Update Data_fetcher.php

--- application/models/Data_fetcher.php
+++ application/models/Data_fetcher.php
@@ -3137,6 +3137,9 @@ class Data_fetcher extends CI_Model {
                         if($scholar->deduction_from == 'external'){
                             $tuition_scholarship_current = ($tuition - $tuition_discount_full) * ($scholar->tuition_fee_rate/100);
                         }
+
+                        print($scholarship_installment_grand_total50);
+                        die();
                         $tuition_scholarship_installment_current = ($tuition + ($tuition * ($tuition_year['installmentIncrease']/100)) - $scholarship_installment_grand_total) * ($scholar->tuition_fee_rate/100);
                         $tuition_scholarship_installment_current30 = ($tuition + ($tuition * 0.15) - $scholarship_installment_grand_total30) * ($scholar->tuition_fee_rate/100);
                         $tuition_scholarship_installment_current50 = ($tuition + ($tuition * 0.09) - $scholarship_installment_grand_total50) * ($scholar->tuition_fee_rate/100);
