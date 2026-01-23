7fafd37ab5ddc979d533897c6fe9e96639e0bd72
Jp <jpestorque0@gmail.com>
2026-01-23 14:52:38 +0800

    Update Finance.php

--- application/modules/finance/controllers/Finance.php
+++ application/modules/finance/controllers/Finance.php
@@ -424,6 +424,19 @@ class Finance extends CI_Controller {
             ->get()
             ->result_array();
 
+                        //TUITION YEAR
+                        $temp['tuition_year'] = $this->db->select('tb_mas_tuition_year.year,tb_mas_tuition_year.installmentDP')     
+                                    ->from('tb_mas_registration')              
+                                    ->join('tb_mas_tuition_year', 'tb_mas_registration.tuition_year = tb_mas_tuition_year.intID')
+                                    ->where(array('tb_mas_registration.intStudentID'=>$id))
+                                    ->get()
+                                    ->first_row('array');
+
+                                    print_r($paid_terms);
+                                    print(' @@@ ');
+                                    print_r($tuition_year);
+                                    die();
+
 
             $temp['balance'] = $this->db->get_where('tb_mas_prev_balance',array('term'=>$reg['intID'],'student_number'=> $std_num))
                                 ->result_array();
@@ -495,19 +508,6 @@ class Finance extends CI_Controller {
                             $item['date'] = date('M j, Y',strtotime($item['date']));
                             $temp['ledger'][] = $item;
                         }
-
-                        //TUITION YEAR
-                        $temp['tuition_year'] = $this->db->select('tb_mas_tuition_year.year,tb_mas_tuition_year.installmentDP')     
-                                    ->from('tb_mas_registration')              
-                                    ->join('tb_mas_tuition_year', 'tb_mas_registration.tuition_year = tb_mas_tuition_year.intID')
-                                    ->where(array('tb_mas_registration.intStudentID'=>$id))
-                                    ->get()
-                                    ->first_row('array');
-
-                                    // print_r($paid_terms);
-                                    // print(' @@@ ');
-                                    // print_r($tuition_year);
-                                    // die();
 
                         // fetch other-type ledger for this term
                         $other = $this->db->select('tb_mas_student_ledger.*, enumSem, strYearStart, strYearEnd, term_label, tb_mas_faculty.strFirstname, tb_mas_faculty.strLastname')
