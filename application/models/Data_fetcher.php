c21c8c2896527aa879357e1507046cdbb40e6501
Jp <jpestorque0@gmail.com>
2026-01-23 14:07:38 +0800

    Update Data_fetcher.php

--- application/models/Data_fetcher.php
+++ application/models/Data_fetcher.php
@@ -3372,10 +3372,10 @@ class Data_fetcher extends CI_Model {
                         $tuition_fee_installment_rate = $tuition_scholarship_installment_current * ($scholar->tuition_fee_rate/100);
 
 
-                        print($$tuition - $tuition_discount_installment . ' @ ');
+                        print($tuition . ' @ ');
+                        print($tuition - $tuition_discount_installment . ' @ ');
                         print($tuition_discount_installment . ' @ ');
                         print($tuition_discount_full . ' @ ');
-                        print($tuition_discount_installment50 . ' @ ');
                         die();
                         // print($tuition_discount_full . ' @ ');
                         // print($tuition_discount_installment . ' @ ');
