da5a01ed6ece8509cd85554b5dece8962f9e42a7
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2026-01-29 08:42:16 +0800

    Update student_viewer.php

--- application/modules/unity/views/admin/student_viewer.php
+++ application/modules/unity/views/admin/student_viewer.php
@@ -791,6 +791,8 @@
 <script src="<?php echo base_url(); ?>assets/themes/default/js/vue.min.js"></script>
 <script type="text/javascript"
   src="<?php echo base_url(); ?>assets/themes/default/js/script.js"></script>
+<!-- jQuery (required for Select2) -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <!-- Select2 JS -->
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <script>
