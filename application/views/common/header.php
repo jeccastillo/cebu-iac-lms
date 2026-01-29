4834b950c6415f5ca392cde03b7efd14b807bde2
Joseph Edmund Castillo <josephedmundcastillo@gmail.com>
2026-01-29 11:42:42 +0800

    Update header.php

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -147,7 +147,7 @@
         text-decoration: none;
         padding: 8px 12px;
         display: block;
-        font-size: 1rem;
+        font-size: 2rem;
         border-radius: 4px;
         transition: background 0.2s;
     }
